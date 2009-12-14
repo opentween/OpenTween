83eb663bda890edebfc6193940ca53a3c9a15aa5
kiri_feather <kiri_feather@users.sourceforge.jp>
2009-12-14 15:08:00 +0000

    0810リリース。コンパイル後のsgenを環境変数指定に変更
    git-svn-id: http://svn.sourceforge.jp/svnroot/tween/trunk@34 e39ad16e-3079-482e-bb30-4b4d378143b6

--- Tween/My Project/AssemblyInfo.vb
+++ Tween/My Project/AssemblyInfo.vb
@@ -55,5 +55,5 @@ Imports System.Runtime.InteropServices
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
 <Assembly: AssemblyVersion("0.8.*")> 
-<Assembly: AssemblyFileVersion("0.8.0.0")> 
+<Assembly: AssemblyFileVersion("0.8.1.0")> 
 
