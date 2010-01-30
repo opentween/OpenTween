da46586016a8e32430a49c6007a81ef4f54f289b
kiri_feather <kiri_feather@e39ad16e-3079-482e-bb30-4b4d378143b6>
2010-01-30 17:42:03 +0000

    0830リリース。 コピーライト書き換え。 フォロワー取得の洗い替えバグ修正。 公式RTの振分バグ修正 流速バグ修正 ハッシュタグ管理画面の最前面オプション変更 ハッシュタグボタンを切り替えとして使用可能に。 公式検索時にタブ名操作がされると例外発生するバグ修正 ファイル書き出しでprotect表記追加
    git-svn-id: http://svn.sourceforge.jp/svnroot/tween/trunk@76 e39ad16e-3079-482e-bb30-4b4d378143b6

--- Tween/HashtagManage.Designer.vb
+++ Tween/HashtagManage.Designer.vb
@@ -37,100 +37,62 @@ Partial Class HashtagManage
         '
         'TableLayoutPanel1
         '
-        Me.TableLayoutPanel1.AccessibleDescription = Nothing
-        Me.TableLayoutPanel1.AccessibleName = Nothing
         resources.ApplyResources(Me.TableLayoutPanel1, "TableLayoutPanel1")
-        Me.TableLayoutPanel1.BackgroundImage = Nothing
         Me.TableLayoutPanel1.Controls.Add(Me.OK_Button, 0, 0)
         Me.TableLayoutPanel1.Controls.Add(Me.Cancel_Button, 1, 0)
-        Me.TableLayoutPanel1.Font = Nothing
         Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
         '
         'OK_Button
         '
-        Me.OK_Button.AccessibleDescription = Nothing
-        Me.OK_Button.AccessibleName = Nothing
         resources.ApplyResources(Me.OK_Button, "OK_Button")
-        Me.OK_Button.BackgroundImage = Nothing
-        Me.OK_Button.Font = Nothing
         Me.OK_Button.Name = "OK_Button"
         '
         'Cancel_Button
         '
-        Me.Cancel_Button.AccessibleDescription = Nothing
-        Me.Cancel_Button.AccessibleName = Nothing
         resources.ApplyResources(Me.Cancel_Button, "Cancel_Button")
-        Me.Cancel_Button.BackgroundImage = Nothing
         Me.Cancel_Button.DialogResult = System.Windows.Forms.DialogResult.Cancel
-        Me.Cancel_Button.Font = Nothing
         Me.Cancel_Button.Name = "Cancel_Button"
         '
         'DeleteButton
         '
-        Me.DeleteButton.AccessibleDescription = Nothing
-        Me.DeleteButton.AccessibleName = Nothing
         resources.ApplyResources(Me.DeleteButton, "DeleteButton")
-        Me.DeleteButton.BackgroundImage = Nothing
-        Me.DeleteButton.Font = Nothing
         Me.DeleteButton.Name = "DeleteButton"
         Me.DeleteButton.UseVisualStyleBackColor = True
         '
         'ReplaceButton
         '
-        Me.ReplaceButton.AccessibleDescription = Nothing
-        Me.ReplaceButton.AccessibleName = Nothing
         resources.ApplyResources(Me.ReplaceButton, "ReplaceButton")
-        Me.ReplaceButton.BackgroundImage = Nothing
-        Me.ReplaceButton.Font = Nothing
         Me.ReplaceButton.Name = "ReplaceButton"
         Me.ReplaceButton.UseVisualStyleBackColor = True
         '
         'AddButton
         '
-        Me.AddButton.AccessibleDescription = Nothing
-        Me.AddButton.AccessibleName = Nothing
         resources.ApplyResources(Me.AddButton, "AddButton")
-        Me.AddButton.BackgroundImage = Nothing
-        Me.AddButton.Font = Nothing
         Me.AddButton.Name = "AddButton"
         Me.AddButton.UseVisualStyleBackColor = True
         '
         'HistoryHashList
         '
-        Me.HistoryHashList.AccessibleDescription = Nothing
-        Me.HistoryHashList.AccessibleName = Nothing
-        resources.ApplyResources(Me.HistoryHashList, "HistoryHashList")
-        Me.HistoryHashList.BackgroundImage = Nothing
-        Me.HistoryHashList.Font = Nothing
         Me.HistoryHashList.FormattingEnabled = True
+        resources.ApplyResources(Me.HistoryHashList, "HistoryHashList")
         Me.HistoryHashList.Name = "HistoryHashList"
         Me.HistoryHashList.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended
         '
         'UseHashText
         '
-        Me.UseHashText.AccessibleDescription = Nothing
-        Me.UseHashText.AccessibleName = Nothing
         resources.ApplyResources(Me.UseHashText, "UseHashText")
-        Me.UseHashText.BackgroundImage = Nothing
-        Me.UseHashText.Font = Nothing
         Me.UseHashText.Name = "UseHashText"
         '
         'Label1
         '
-        Me.Label1.AccessibleDescription = Nothing
-        Me.Label1.AccessibleName = Nothing
         resources.ApplyResources(Me.Label1, "Label1")
-        Me.Label1.Font = Nothing
         Me.Label1.Name = "Label1"
         '
         'HashtagManage
         '
         Me.AcceptButton = Me.OK_Button
-        Me.AccessibleDescription = Nothing
-        Me.AccessibleName = Nothing
         resources.ApplyResources(Me, "$this")
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.BackgroundImage = Nothing
         Me.CancelButton = Me.Cancel_Button
         Me.Controls.Add(Me.DeleteButton)
         Me.Controls.Add(Me.ReplaceButton)
@@ -139,13 +101,12 @@ Partial Class HashtagManage
         Me.Controls.Add(Me.UseHashText)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.TableLayoutPanel1)
-        Me.Font = Nothing
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
-        Me.Icon = Nothing
         Me.MaximizeBox = False
         Me.MinimizeBox = False
         Me.Name = "HashtagManage"
         Me.ShowInTaskbar = False
+        Me.TopMost = True
         Me.TableLayoutPanel1.ResumeLayout(False)
         Me.ResumeLayout(False)
         Me.PerformLayout()
