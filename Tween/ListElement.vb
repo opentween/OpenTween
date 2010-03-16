12a6944c99a626e9869f9792e91193d18b85ef53
kiri_feather <kiri_feather@users.sourceforge.jp>
2010-03-16 10:30:37 +0000

    List入れ物準備
    git-svn-id: http://svn.sourceforge.jp/svnroot/tween/trunk@135 e39ad16e-3079-482e-bb30-4b4d378143b6

---
+++ Tween/ListElement.vb
@@ -0,0 +1,11 @@
+Public Class ListElement
+    Public Id As Long = 0
+    Public Name As String = ""
+    Public Description As String = ""
+    Public IsPublic As Boolean = True
+    Public SubscriberCount As Integer = 0   'w“ÇÒ”
+    Public MemberCount As Integer = 0   'ƒŠƒXƒgƒƒ“ƒo”
+    Public UserId As Long = 0
+    Public Username As String = ""
+    Public Nickname As String = ""
+End Class
