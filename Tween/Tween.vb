bc06332338b378b06f829959d12185b2ee793b7b
kiri_feather <kiri_feather@e39ad16e-3079-482e-bb30-4b4d378143b6>
2010-05-02 14:21:37 +0000

    検索演算子OR大文字置き換え修正
    git-svn-id: http://svn.sourceforge.jp/svnroot/tween/trunk@237 e39ad16e-3079-482e-bb30-4b4d378143b6

--- Tween/Tween.vb
+++ Tween/Tween.vb
@@ -7634,26 +7634,23 @@ RETRY:
         cmb.Text = cmb.Text.Trim
         ' 検索式演算子 OR についてのみ大文字しか認識しないので強制的に大文字とする
         Dim Quote As Boolean = False
-        Dim _idx As Integer = 0
         Dim buf As New StringBuilder()
         Dim c As Char() = cmb.Text.ToCharArray()
         For cnt As Integer = 0 To cmb.Text.Length - 1
-            If _idx > cmb.Text.Length - 4 Then
-                buf.Append(cmb.Text.Substring(_idx, 3))
+            If cnt > cmb.Text.Length - 4 Then
+                buf.Append(cmb.Text.Substring(cnt))
                 Exit For
             End If
             If c(cnt) = CChar("""") Then
                 Quote = Not Quote
             Else
-                If Not Quote AndAlso cmb.Text.Substring(_idx, 4).Equals(" or ") Then
+                If Not Quote AndAlso cmb.Text.Substring(cnt, 4).Equals(" or ", StringComparison.OrdinalIgnoreCase) Then
                     buf.Append(" OR ")
-                    _idx += 3
                     cnt += 3
                     Continue For
                 End If
             End If
             buf.Append(c(cnt))
-            _idx += 1
         Next
         cmb.Text = buf.ToString()
 
