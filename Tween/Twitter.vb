6b7a48c8742ba17126e43522760cce560bb6d889
syo68k <syo68k@users.sourceforge.jp>
2010-05-09 09:55:58 +0000

    ハッシュタグ解析で、アルファベットに数字が続くハッシュタグを正しく認識できていなかったのを修正
    git-svn-id: http://svn.sourceforge.jp/svnroot/tween/trunk@275 e39ad16e-3079-482e-bb30-4b4d378143b6

--- Tween/Twitter.vb
+++ Tween/Twitter.vb
@@ -4179,7 +4179,7 @@ Public Class Twitter
                 End SyncLock
             End If
         Next
-        retStr = Regex.Replace(retStr, "(^|[^a-zA-Z0-9/&])([#＃])([0-9a-zA-Z_]*[a-zA-Z_]+[a-zA-Z_\xc0-\xd6\xd8-\xf6\xf8-\xff]*)", "$1<a href=""" & _protocol & "twitter.com/search?q=%23$3"">$2$3</a>")
+        retStr = Regex.Replace(retStr, "(^|[^a-zA-Z0-9/&])([#＃])([0-9a-zA-Z_]*[a-zA-Z_]+[a-zA-Z0-9_\xc0-\xd6\xd8-\xf6\xf8-\xff]*)", "$1<a href=""" & _protocol & "twitter.com/search?q=%23$3"">$2$3</a>")
 
         retStr = Regex.Replace(retStr, "(^|[^a-zA-Z0-9_/&#＃@＠>=])(sm|nm)([0-9]{4,10})", "$1<a href=""http://www.nicovideo.jp/watch/$2$3"">$2$3</a>")
 
