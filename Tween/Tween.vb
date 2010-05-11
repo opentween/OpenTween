02b3c8b7fc86dac50442aef8631daa8a5b39b7ce
syo68k <syo68k@users.sourceforge.jp>
2010-05-11 17:25:01 +0000

    ふぁぼったーのURL変更（再）
    git-svn-id: http://svn.sourceforge.jp/svnroot/tween/trunk@290 e39ad16e-3079-482e-bb30-4b4d378143b6

--- Tween/Tween.vb
+++ Tween/Tween.vb
@@ -4249,7 +4249,7 @@ RETRY:
     Private Sub FavorareMenuItem_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles FavorareMenuItem.Click, OpenFavotterOpMenuItem.Click
         If _curList.SelectedIndices.Count > 0 Then
             Dim post As PostClass = _statuses.Item(_curTab.Text, _curList.SelectedIndices(0))
-            OpenUriAsync("http://favotter.net/user.php?user=" + post.Name)
+            OpenUriAsync("http://favotter.net/user/" + post.Name)
         End If
     End Sub
 
