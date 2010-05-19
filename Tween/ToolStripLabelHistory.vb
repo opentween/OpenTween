c3fe8561e1419f6e8ab9f280085851e5c2e65396
kiri_feather <kiri_feather@users.sourceforge.jp>
2010-05-19 16:26:45 +0000

    通信履歴一欄で時刻も表示するように(thanks takeshik)
    git-svn-id: http://svn.sourceforge.jp/svnroot/tween/trunk@341 e39ad16e-3079-482e-bb30-4b4d378143b6

--- Tween/ToolStripLabelHistory.vb
+++ Tween/ToolStripLabelHistory.vb
@@ -21,37 +21,102 @@
 ' the Free Software Foundation, Inc., 51 Franklin Street - Fifth Floor,
 ' Boston, MA 02110-1301, USA.
 
+Imports System.Text
+
 Namespace TweenCustomControl
 
     Public NotInheritable Class ToolStripLabelHistory
         Inherits ToolStripStatusLabel
 
-        Private sList As New List(Of String)
-        Private Const MAXCNT As Integer = 10
-        Private _history As String = ""
+        Public Enum LogLevel
+            Lowest = 0
+            Debug = 16
+            Info = 32
+            Notice = 64
+            Warn = 128
+            Err = 192
+            Fatal = 255
+            Highest = 256
+        End Enum
+
+        Public Class LogEntry
+            Private ReadOnly _logLevel As LogLevel
+
+            Private ReadOnly _timestamp As DateTime
+
+            Private ReadOnly _summary As String
+
+            Private ReadOnly _detail As String
+
+            Public ReadOnly Property LogLevel() As LogLevel
+                Get
+                    Return _logLevel
+                End Get
+            End Property
+
+            Public ReadOnly Property Timestamp() As DateTime
+                Get
+                    Return _timestamp
+                End Get
+            End Property
+
+            Public ReadOnly Property Summary() As String
+                Get
+                    Return _summary
+                End Get
+            End Property
+
+            Public ReadOnly Property Detail() As String
+                Get
+                    Return _detail
+                End Get
+            End Property
+
+            Public Sub New(ByVal logLevel As LogLevel, ByVal timestamp As DateTime, ByVal summary As String, ByVal detail As String)
+                _logLevel = logLevel
+                _timestamp = timestamp
+                _summary = summary
+                _detail = detail
+            End Sub
+
+            Public Sub New(ByVal timestamp As DateTime, ByVal summary As String)
+                Me.New(LogLevel.Debug, timestamp, summary, summary)
+            End Sub
+
+            Public Overrides Function ToString() As String
+                Return Timestamp.ToString("T") + ": " + Summary
+            End Function
+        End Class
+
+        Private _logs As LinkedList(Of LogEntry)
+
+        Private Const MAXCNT As Integer = 20
 
         Public Overrides Property Text() As String
             Get
                 Return MyBase.Text
             End Get
             Set(ByVal value As String)
-                sList.Add(value)
-                Do While sList.Count > MAXCNT
-                    sList.RemoveAt(0)
+                _logs.AddLast(New LogEntry(DateTime.Now, value))
+                Do While _logs.Count > MAXCNT
+                    _logs.RemoveFirst()
                 Loop
-                _history = ""
-                For Each hstr As String In sList
-                    If _history <> "" Then _history += System.Environment.NewLine
-                    _history += hstr
-                Next
                 MyBase.Text = value
             End Set
         End Property
 
         Public ReadOnly Property TextHistory() As String
             Get
-                Return _history
+                Dim sb As StringBuilder = New StringBuilder()
+                For Each e As LogEntry In _logs
+                    sb.AppendLine(e.ToString())
+                Next
+                Return sb.ToString()
             End Get
         End Property
+
+        Public Sub New()
+            _logs = New LinkedList(Of LogEntry)()
+        End Sub
     End Class
 End Namespace
