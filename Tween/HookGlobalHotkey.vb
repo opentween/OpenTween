ce50349e844428bbddc431e9d4e0c913abe9bf05
kiri_feather <kiri_feather@users.sourceforge.jp>
2010-05-24 18:32:47 +0000

    グローバルホットキー処理クラスを追加（ToDo:設定追加してホットキー登録を可能に）
    git-svn-id: http://svn.sourceforge.jp/svnroot/tween/trunk@363 e39ad16e-3079-482e-bb30-4b4d378143b6

---
+++ Tween/HookGlobalHotkey.vb
@@ -0,0 +1,98 @@
+'フォームのコンストラクタでこのクラスのインスタンス作成すること
+'インスタンス変数はwitheventsで宣言し、HotkeyPressedイベントを受け取ること
+'グローバルホットキーはRegisterOriginalHotkeyで登録。複数種類登録できるが、重複チェックはしていないので注意。
+'再設定前にはUnregisterAllOriginalHotkeyを呼ぶこと
+
+Public Class HookGlobalHotkey
+    Inherits NativeWindow
+    Implements IDisposable
+
+    Private _targetForm As Form
+    Private _hotkeyID As Dictionary(Of Integer, KeyEventArgs)
+
+    <FlagsAttribute()> _
+    Public Enum ModKeys As Integer
+        None = 0
+        Alt = &H1
+        Ctrl = &H2
+        Shift = &H4
+        Win = &H8
+    End Enum
+
+    Public Event HotkeyPressed As KeyEventHandler
+
+    Protected Overrides Sub WndProc(ByRef m As System.Windows.Forms.Message)
+        Const WM_HOTKEY As Integer = &H312
+        If m.Msg = WM_HOTKEY Then
+            If _hotkeyID.ContainsKey(m.WParam.ToInt32) Then RaiseEvent HotkeyPressed(Me, _hotkeyID(m.WParam.ToInt32))
+            Exit Sub
+        End If
+        MyBase.WndProc(m)
+    End Sub
+
+    Public Sub New(ByVal targetForm As Form)
+        _targetForm = targetForm
+        _hotkeyID = New Dictionary(Of Integer, KeyEventArgs)
+
+        AddHandler _targetForm.HandleCreated, AddressOf Me.OnHandleCreated
+        AddHandler _targetForm.HandleDestroyed, AddressOf Me.OnHandleDestroyed
+    End Sub
+
+    Public Sub OnHandleCreated(ByVal sender As Object, ByVal e As EventArgs)
+        Me.AssignHandle(_targetForm.Handle)
+    End Sub
+
+    Public Sub OnHandleDestroyed(ByVal sender As Object, ByVal e As EventArgs)
+        Me.ReleaseHandle()
+    End Sub
+
+    Public Function RegisterOriginalHotkey(ByVal hotkey As Keys, ByVal modifiers As ModKeys) As Boolean
+        Dim key As New KeyEventArgs(DirectCast(hotkey Or modifiers, Keys))
+        Dim hotkeyId As Integer = RegisterGlobalHotKey(hotkey, modifiers, Me._targetForm)
+        If hotkeyId > 0 Then
+            Me._hotkeyID.Add(hotkeyId, key)
+            Return True
+        End If
+        Return False
+    End Function
+
+    Public Sub UnregisterAllOriginalHotkey()
+        For Each hotkeyId As Integer In Me._hotkeyID.Keys
+            UnregisterGlobalHotKey(hotkeyId, Me._targetForm)
+        Next
+        Me._hotkeyID.Clear()
+    End Sub
+
+    Private disposedValue As Boolean = False        ' 重複する呼び出しを検出するには
+
+    ' IDisposable
+    Protected Overridable Sub Dispose(ByVal disposing As Boolean)
+        If Not Me.disposedValue Then
+            If disposing Then
+                ' TODO: 明示的に呼び出されたときにマネージ リソースを解放します
+            End If
+
+            ' TODO: 共有のアンマネージ リソースを解放します
+            If Me._targetForm IsNot Nothing AndAlso Not Me._targetForm.IsDisposed Then
+                Me.UnregisterAllOriginalHotkey()
+                RemoveHandler _targetForm.HandleCreated, AddressOf Me.OnHandleCreated
+                RemoveHandler _targetForm.HandleDestroyed, AddressOf Me.OnHandleDestroyed
+            End If
+        End If
+        Me.disposedValue = True
+    End Sub
+
+#Region " IDisposable Support "
+    ' このコードは、破棄可能なパターンを正しく実装できるように Visual Basic によって追加されました。
+    Public Sub Dispose() Implements IDisposable.Dispose
+        ' このコードを変更しないでください。クリーンアップ コードを上の Dispose(ByVal disposing As Boolean) に記述します。
+        Dispose(True)
+        GC.SuppressFinalize(Me)
+    End Sub
+#End Region
+
+    Protected Overrides Sub Finalize()
+        Me.Dispose(False)
+        MyBase.Finalize()
+    End Sub
+End Class
