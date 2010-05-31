1941d6504d94690bed25c827b385464c0208db6c
syo68k <syo68k@users.sourceforge.jp>
2010-05-31 10:32:13 +0000

    ユーザー情報ダイアログに「発言を検索」ボタンを追加
    git-svn-id: http://svn.sourceforge.jp/svnroot/tween/trunk@407 e39ad16e-3079-482e-bb30-4b4d378143b6

--- Tween/ShowUserInfo.Designer.vb
+++ Tween/ShowUserInfo.Designer.vb
@@ -52,6 +52,7 @@ Partial Class ShowUserInfo
         Me.BackgroundWorkerImageLoader = New System.ComponentModel.BackgroundWorker()
         Me.LabelScreenName = New System.Windows.Forms.Label()
         Me.ToolTip1 = New System.Windows.Forms.ToolTip(Me.components)
+        Me.LinkLabel1 = New System.Windows.Forms.LinkLabel()
         Me.ContextMenuStrip1 = New System.Windows.Forms.ContextMenuStrip(Me.components)
         Me.SelectionCopyToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.SelectAllToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
@@ -59,7 +60,7 @@ Partial Class ShowUserInfo
         Me.RecentPostBrowser = New System.Windows.Forms.WebBrowser()
         Me.DescriptionBrowser = New System.Windows.Forms.WebBrowser()
         Me.LabelIsVerified = New System.Windows.Forms.Label()
-        Me.LinkLabel1 = New System.Windows.Forms.LinkLabel()
+        Me.ButtonSearchPosts = New System.Windows.Forms.Button()
         CType(Me.UserPicture, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.ContextMenuStrip1.SuspendLayout()
         Me.SuspendLayout()
@@ -224,6 +225,13 @@ Partial Class ShowUserInfo
         '
         Me.ToolTip1.ShowAlways = True
         '
+        'LinkLabel1
+        '
+        resources.ApplyResources(Me.LinkLabel1, "LinkLabel1")
+        Me.LinkLabel1.Name = "LinkLabel1"
+        Me.LinkLabel1.TabStop = True
+        Me.ToolTip1.SetToolTip(Me.LinkLabel1, resources.GetString("LinkLabel1.ToolTip"))
+        '
         'ContextMenuStrip1
         '
         Me.ContextMenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.SelectionCopyToolStripMenuItem, Me.SelectAllToolStripMenuItem})
@@ -273,18 +281,18 @@ Partial Class ShowUserInfo
         Me.LabelIsVerified.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
         Me.LabelIsVerified.Name = "LabelIsVerified"
         '
-        'LinkLabel1
+        'ButtonSearchPosts
         '
-        resources.ApplyResources(Me.LinkLabel1, "LinkLabel1")
-        Me.LinkLabel1.Name = "LinkLabel1"
-        Me.LinkLabel1.TabStop = True
-        Me.ToolTip1.SetToolTip(Me.LinkLabel1, resources.GetString("LinkLabel1.ToolTip"))
+        resources.ApplyResources(Me.ButtonSearchPosts, "ButtonSearchPosts")
+        Me.ButtonSearchPosts.Name = "ButtonSearchPosts"
+        Me.ButtonSearchPosts.UseVisualStyleBackColor = True
         '
         'ShowUserInfo
         '
         resources.ApplyResources(Me, "$this")
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.CancelButton = Me.ButtonClose
+        Me.Controls.Add(Me.ButtonSearchPosts)
         Me.Controls.Add(Me.LinkLabel1)
         Me.Controls.Add(Me.LabelIsVerified)
         Me.Controls.Add(Me.RecentPostBrowser)
@@ -364,4 +372,5 @@ Partial Class ShowUserInfo
     Friend WithEvents RecentPostBrowser As System.Windows.Forms.WebBrowser
     Friend WithEvents LabelIsVerified As System.Windows.Forms.Label
     Friend WithEvents LinkLabel1 As System.Windows.Forms.LinkLabel
+    Friend WithEvents ButtonSearchPosts As System.Windows.Forms.Button
 End Class
--- Tween/ShowUserInfo.vb
+++ Tween/ShowUserInfo.vb
@@ -380,4 +380,8 @@ Public Class ShowUserInfo
     Private Sub LinkLabel1_LinkClicked(ByVal sender As System.Object, ByVal e As System.Windows.Forms.LinkLabelLinkClickedEventArgs) Handles LinkLabel1.LinkClicked
         TweenMain.OpenUriAsync("http://twitter.com/help/verified")
     End Sub
+
+    Private Sub ButtonSearchPosts_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ButtonSearchPosts.Click
+        MyOwner.AddNewTabForSearch("from:" + _info.ScreenName)
+    End Sub
 End Class
