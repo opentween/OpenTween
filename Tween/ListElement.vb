054ef4523fb6dc9328d8988a726a635e80f12461
sin_sin <sin_sin@e39ad16e-3079-482e-bb30-4b4d378143b6>
2010-09-03 10:23:49 +0000

    ソースをISO8859-1 -> UTF-8(BOM)だけｗ
    git-svn-id: http://svn.sourceforge.jp/svnroot/tween/trunk@807 e39ad16e-3079-482e-bb30-4b4d378143b6

--- Tween/ListElement.vb
+++ Tween/ListElement.vb
@@ -1,11 +1,11 @@
-Public Class ListElement
+ï»¿Public Class ListElement
     Public Id As Long = 0
     Public Name As String = ""
     Public Description As String = ""
     Public Slug As String = ""
     Public IsPublic As Boolean = True
-    Public SubscriberCount As Integer = 0   'w“ÇÒ”
-    Public MemberCount As Integer = 0   'ƒŠƒXƒgƒƒ“ƒo”
+    Public SubscriberCount As Integer = 0   'è³¼èª­è€…æ•°
+    Public MemberCount As Integer = 0   'ãƒªã‚¹ãƒˆãƒ¡ãƒ³ãƒæ•°
     Public UserId As Long = 0
     Public Username As String = ""
     Public Nickname As String = ""
