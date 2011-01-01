c790a293ef35f54d767b509e0be53447a1bfca36
anis774 <anis774@users.sourceforge.jp>
2011-01-01 06:38:10 +0000

    r1302で追加したロジックに枝切りを追加
    git-svn-id: http://svn.sourceforge.jp/svnroot/tween/trunk@1303 e39ad16e-3079-482e-bb30-4b4d378143b6

--- Tween/Twitter.vb
+++ Tween/Twitter.vb
@@ -1545,15 +1545,15 @@ Public Class Twitter
 
             post.IsRead = read
             If post.IsMe AndAlso Not read AndAlso _readOwnPost Then post.IsRead = True
-            Dim posts = From tab_ In TabInformations.GetInstance().Tabs
-                        Where tab_.Value.UnreadManage
-                        From post_ In DirectCast(IIf(tab_.Value.IsInnerStorageTabType, tab_.Value.Posts, TabInformations.GetInstance().Posts), Dictionary(Of Long, PostClass))
-                        Where post_.Value.Id = post.Id
-                        Where post_.Value.IsRead
-                        Where tab_.Value.Contains(post_.Value.Id)
+            If Not post.IsRead Then
+                Dim posts = From tab_ In TabInformations.GetInstance().Tabs
+                            Where tab_.Value.UnreadManage
+                            From post_ In DirectCast(IIf(tab_.Value.IsInnerStorageTabType, tab_.Value.Posts, TabInformations.GetInstance().Posts), Dictionary(Of Long, PostClass))
+                            Where post_.Value.Id = post.Id
+                            Where post_.Value.IsRead
+                            Where tab_.Value.Contains(post_.Value.Id)
 
-            If posts.Any() Then
-                post.IsRead = True
+                post.IsRead = posts.Any()
             End If
 
             If tab IsNot Nothing Then post.RelTabName = tab.TabName
