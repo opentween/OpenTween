6af80ee40cbf0772833072042203c2675033c90e
syo68k <syo68k@users.sourceforge.jp>
2011-10-09 17:28:36 +0900

    Fav済みチェック条件を緩和

--- Tween/Twitter.vb
+++ Tween/Twitter.vb
@@ -1738,7 +1738,7 @@ Public Class Twitter
 
             '幻覚fav対策
             Dim tc As TabClass = TabInformations.GetInstance.GetTabByType(TabUsageType.Favorites)
-            post.IsFav = tc.Contains(post.StatusId) AndAlso post.IsMe
+            post.IsFav = tc.Contains(post.StatusId)
         End If
         'HTMLに整形
         post.Text = CreateHtmlAnchor(post.TextFromApi, post.ReplyToList, entities, post.Media)
