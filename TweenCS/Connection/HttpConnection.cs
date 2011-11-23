441ae203d05dc6e0396d0bfcc739bf978c6b9fc6
Kimura Youichi <kim.upsilon@bucyou.net>
2011-11-24 02:20:25 +0900

    Update copyright in HttpConnection.cs

--- TweenCS/Connection/HttpConnection.cs
+++ TweenCS/Connection/HttpConnection.cs
@@ -4,6 +4,7 @@
 //           (c) 2008-2011 takeshik (@takeshik) <http://www.takeshik.org/>
 //           (c) 2010-2011 anis774 (@anis774) <http://d.hatena.ne.jp/anis774/>
 //           (c) 2010-2011 fantasticswallow (@f_swallow) <http://twitter.com/f_swallow>
+//           (c) 2011      kim_upsilon (@kim_upsilon) <https://upsilo.net/~upsilon/>
 // All rights reserved.
 // 
 // This file is part of Tween.
