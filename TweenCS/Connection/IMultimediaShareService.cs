59f90538c79d7bf6240aece1433eb01e1ba44798
Egtra <yusuke.ichinohe@gmail.com>
2011-11-27 12:23:55 +0900

    Port Connection/IMultimediaShareService.vb to C#

---
+++ TweenCS/Connection/IMultimediaShareService.cs
@@ -0,0 +1,15 @@
+﻿namespace Tween
+{
+    public interface IMultimediaShareService
+    {
+        string Upload(ref string filePath,
+                        ref string message,
+                        long reply_to);
+        bool CheckValidExtension(string ext) ;
+        string GetFileOpenDialogFilter();
+        MyCommon.UploadFileType GetFileType(string ext);
+        bool IsSupportedFileType(MyCommon.UploadFileType type);
+        bool CheckValidFilesize(string ext, long fileSize);
+        bool Configuration(string key, object value);
+    }
+}
