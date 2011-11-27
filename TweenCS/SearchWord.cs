90b69c42493e73e5b3df9104a08ac27987730c35
Kimura Youichi <kim.upsilon@bucyou.net>
2011-11-27 18:48:53 +0900

    Port SearchWord.vb to C#

---
+++ TweenCS/SearchWord.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Tween
+{
+    public partial class SearchWord : Form
+    {
+        public SearchWord()
+        {
+            InitializeComponent();
+        }
+
+        private void OK_Button_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.OK;
+            this.Close();
+        }
+
+        private void Cancel_Button_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.Cancel;
+            this.Close();
+        }
+
+        public string SWord
+        {
+            get { return SWordText.Text; }
+            set { SWordText.Text = value; }
+        }
+
+        public bool CheckCaseSensitive
+        {
+            get { return CheckSearchCaseSensitive.Checked; }
+            set { CheckSearchCaseSensitive.Checked = value; }
+        }
+
+        public bool CheckRegex
+        {
+            get { return CheckSearchRegex.Checked; }
+            set { CheckSearchRegex.Checked = value; }
+        }
+
+        private void SearchWord_Shown(object sender, EventArgs e)
+        {
+            SWordText.SelectAll();
+            SWordText.Focus();
+        }
+    }
+}
