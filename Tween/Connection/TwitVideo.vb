b68395b6536444267d6422e5da5d23c18b47de6e
Kimura Youichi <kim.upsilon@bucyou.net>
2011-11-28 22:32:03 +0900

    Revert "Port Connection/TwitterPhoto.vb, Connection/TwitVideo.vb to C#"
    This reverts commit 5a8159ff60d5f240bfcaab9a263cc8ac1c4e96f7.
    
    ビルドエラーが解決されていないため差し戻し、別ブランチへ待避

---
+++ Tween/Connection/TwitVideo.vb
@@ -0,0 +1,105 @@
+﻿Imports System.IO
+Imports System.Text
+Imports System.Net
+
+Public Class TwitVideo
+    Inherits HttpConnection
+
+    Private Const ConsumerKey As String = "7c4dc004a88e821b02c87a0cde2fa85c"
+    Private multimediaExt() As String = {".avi", _
+                                         ".wmv", _
+                                         ".flv", _
+                                         ".m4v", _
+                                         ".mov", _
+                                         ".mp4", _
+                                         ".rm", _
+                                         ".mpeg", _
+                                         ".mpg", _
+                                         ".3gp", _
+                                         ".3g2"}
+    Private pictureExt() As String = {".jpg", _
+                                    ".jpeg", _
+                                    ".gif", _
+                                    ".png"}
+
+    Private Const MaxPictureFileSize As Long = 10 * 1024 * 1024
+    Private Const MaxMultiMediaFileSize As Long = 20 * 1024 * 1024
+
+    Public Function Upload(ByVal mediaFile As FileInfo, _
+                           ByVal message As String, _
+                           ByVal keyword As String, _
+                           ByVal username As String, _
+                           ByVal twitter_id As String, _
+                           ByRef content As String) As HttpStatusCode
+        'Message必須
+        If String.IsNullOrEmpty(message) Then Throw New ArgumentException("'Message' is required.")
+        'Check filetype and size
+        If Array.IndexOf(multimediaExt, mediaFile.Extension.ToLower) > -1 Then
+            If mediaFile.Length > MaxMultiMediaFileSize Then Throw New ArgumentException("File is too large.")
+        ElseIf Array.IndexOf(pictureExt, mediaFile.Extension.ToLower) > -1 Then
+            If mediaFile.Length > MaxPictureFileSize Then Throw New ArgumentException("File is too large.")
+        Else
+            Throw New ArgumentException("Service don't support this filetype.")
+        End If
+        'Endpoint(URI+Token)
+        Const URLBASE As String = "http://api.twitvideo.jp/oauth/upload/"
+        Dim data As Byte() = Encoding.ASCII.GetBytes(ConsumerKey.Substring(0, 9) + username)
+        Dim bHash As Byte() = (New System.Security.Cryptography.MD5CryptoServiceProvider()).ComputeHash(data)
+        Dim url As String = URLBASE + BitConverter.ToString(bHash).ToLower.Replace("-", "")
+        'Parameters
+        Dim param As New Dictionary(Of String, String)
+        param.Add("username", username)
+        If Not String.IsNullOrEmpty(twitter_id) Then param.Add("twitter_id", twitter_id)
+        If Not String.IsNullOrEmpty(keyword) Then param.Add("keyword", keyword)
+        param.Add("type", "xml")
+        param.Add("message", message)
+        Dim binary As New List(Of KeyValuePair(Of String, FileInfo))
+        binary.Add(New KeyValuePair(Of String, FileInfo)("media", mediaFile))
+        Me.InstanceTimeout = 60000     'タイムアウト60秒
+
+        Dim req As HttpWebRequest = CreateRequest(PostMethod, _
+                                                  New Uri(url), _
+                                                  param, _
+                                                  binary, _
+                                                  False)
+        Return Me.GetResponse(req, content, Nothing, False)
+    End Function
+
+    Public Function CheckValidExtension(ByVal ext As String) As Boolean
+        If Array.IndexOf(pictureExt, ext.ToLower) > -1 Then
+            Return True
+        End If
+        If Array.IndexOf(multimediaExt, ext.ToLower) > -1 Then
+            Return True
+        End If
+        Return False
+    End Function
+
+    Public Function GetFileType(ByVal ext As String) As UploadFileType
+        If Array.IndexOf(pictureExt, ext.ToLower) > -1 Then
+            Return UploadFileType.Picture
+        End If
+        If Array.IndexOf(multimediaExt, ext.ToLower) > -1 Then
+            Return UploadFileType.MultiMedia
+        End If
+        Return UploadFileType.Invalid
+    End Function
+
+    Public Function IsSupportedFileType(ByVal type As UploadFileType) As Boolean
+        Return type.Equals(UploadFileType.Picture) OrElse type.Equals(UploadFileType.MultiMedia)
+    End Function
+
+    Public Function GetMaxFileSize(ByVal ext As String) As Long
+        If Array.IndexOf(multimediaExt, ext.ToLower) > -1 Then
+            Return MaxMultiMediaFileSize
+        ElseIf Array.IndexOf(pictureExt, ext.ToLower) > -1 Then
+            Return MaxPictureFileSize
+        End If
+        Return -1
+    End Function
+
+    Public Function GetFileOpenDialogFilter() As String
+        Return "Image Files(*.gif;*.jpg;*.jpeg;*.png)|*.gif;*.jpg;*.jpeg;*.png|" + _
+                "Movie Files(*.avi;*.wmv;*.flv;*.m4v;*.mov;*.mp4;*.rm;*.mpeg;*.mpg;*.3gp;*.3g2)|*.avi;*.wmv;*.flv;*.m4v;*.mov;*.mp4;*.rm;*.mpeg;*.mpg;*.3gp;*.3g2"
+    End Function
+End Class
---
+++ Tween/Connection/TwitterPhoto.vb
@@ -0,0 +1,88 @@
+﻿Imports System.IO
+Imports System.Net
+Imports System.Xml
+
+Public Class TwitterPhoto
+    Implements IMultimediaShareService
+
+    Private pictureExt() As String = {".jpg", _
+                                ".jpeg", _
+                                ".gif", _
+                                ".png"}
+
+    Private Const MaxfilesizeDefault As Int64 = 3145728
+
+    ' help/configurationにより取得されコンストラクタへ渡される
+    Private _MaxFileSize As Int64 = 3145728
+
+    Private tw As Twitter
+
+    Public Function CheckValidExtension(ext As String) As Boolean Implements IMultimediaShareService.CheckValidExtension
+        If Array.IndexOf(pictureExt, ext.ToLower) > -1 Then
+            Return True
+        End If
+        Return False
+    End Function
+
+    Public Function CheckValidFilesize(ext As String, fileSize As Long) As Boolean Implements IMultimediaShareService.CheckValidFilesize
+        If Me.CheckValidExtension(ext) Then
+            Return fileSize <= _MaxFileSize
+        End If
+        Return False
+    End Function
+
+    Public Function Configuration(ByVal key As String, ByVal value As Object) As Boolean Implements IMultimediaShareService.Configuration
+        If key = "MaxUploadFilesize" Then
+            Dim val As Int64
+            Try
+                val = Convert.ToInt64(value)
+                If val > 0 Then
+                    _MaxFileSize = val
+                Else
+                    _MaxFileSize = MaxfilesizeDefault
+                End If
+            Catch ex As Exception
+                _MaxFileSize = MaxfilesizeDefault
+                Return False    'error
+            End Try
+            Return True          ' 正常に設定終了
+        End If
+        Return True              ' 設定項目がない場合はとりあえずエラー扱いにしない
+    End Function
+
+    Public Function GetFileOpenDialogFilter() As String Implements IMultimediaShareService.GetFileOpenDialogFilter
+        Return "Image Files(*.gif;*.jpg;*.jpeg;*.png)|*.gif;*.jpg;*.jpeg;*.png"
+    End Function
+
+    Public Function GetFileType(ext As String) As MyCommon.UploadFileType Implements IMultimediaShareService.GetFileType
+        If Me.CheckValidExtension(ext) Then
+            Return UploadFileType.Picture
+        End If
+        Return UploadFileType.Invalid
+    End Function
+
+    Public Function IsSupportedFileType(type As MyCommon.UploadFileType) As Boolean Implements IMultimediaShareService.IsSupportedFileType
+        Return type.Equals(UploadFileType.Picture)
+    End Function
+
+    Public Function Upload(ByRef filePath As String,
+                           ByRef message As String,
+                           ByVal reply_to As Long) As String Implements IMultimediaShareService.Upload
+        If String.IsNullOrEmpty(filePath) Then Return "Err:File isn't specified."
+        If String.IsNullOrEmpty(message) Then message = ""
+        Dim mediaFile As FileInfo
+        Try
+            mediaFile = New FileInfo(filePath)
+        Catch ex As NotSupportedException
+            Return "Err:" + ex.Message
+        End Try
+        If Not mediaFile.Exists Then Return "Err:File isn't exists."
+        If IsAnimatedGif(filePath) Then Return "Err:Don't support animatedGIF."
+
+        Return tw.PostStatusWithMedia(message, reply_to, mediaFile)
+    End Function
+
+    Public Sub New(ByVal twitter As Twitter)
+        tw = twitter
+    End Sub
+End Class
