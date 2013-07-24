3c1945c668df6f807959c0082120f824344dcc6d
spx <spx268@gmail.com>
2013-07-24 18:27:33 +0900

    検索タブで返信元ステータスを辿れなかった制限を解除 今は検索にもちゃんと in_reply_to_status_id が付いてくるので

--- OpenTween/Tween.cs
+++ OpenTween/Tween.cs
@@ -3585,9 +3585,8 @@ namespace OpenTween
             //{
             //    RefreshMoreStripMenuItem.Enabled = false;
             //}
-            if (_statuses.Tabs[ListTab.SelectedTab.Text].TabType == MyCommon.TabUsageType.PublicSearch
-                                || !this.ExistCurrentPost
-                                || _curPost.InReplyToStatusId == null)
+            if (!this.ExistCurrentPost
+                || _curPost.InReplyToStatusId == null)
             {
                 RepliedStatusOpenMenuItem.Enabled = false;
             }
@@ -11930,9 +11929,8 @@ namespace OpenTween
             {
                 this.RefreshPrevOpMenuItem.Enabled = false;
             }
-            if (_statuses.Tabs[ListTab.SelectedTab.Text].TabType == MyCommon.TabUsageType.PublicSearch
-                                || !this.ExistCurrentPost
-                                || _curPost.InReplyToStatusId == null)
+            if (!this.ExistCurrentPost
+                || _curPost.InReplyToStatusId == null)
             {
                 OpenRepSourceOpMenuItem.Enabled = false;
             }
