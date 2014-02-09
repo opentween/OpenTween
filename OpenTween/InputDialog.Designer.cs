b7b48bec8dbf7ba2213c00ba55bbec4ff419774d
Kimura Youichi <kim.upsilon@bucyou.net>
2014-02-09 19:47:45 +0900

    入力ダイアログを表示するためのフォームを追加

---
+++ OpenTween/InputDialog.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace OpenTween
+{
+    partial class InputDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(InputDialog));
+            this.buttonOK = new System.Windows.Forms.Button();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.labelMain = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonOK
+            // 
+            resources.ApplyResources(this.buttonOK, "buttonOK");
+            this.buttonOK.Name = "buttonOK";
+            this.buttonOK.UseVisualStyleBackColor = true;
+            this.buttonOK.Click += new System.EventHandler(this.buttonOK_Click);
+            // 
+            // buttonCancel
+            // 
+            resources.ApplyResources(this.buttonCancel, "buttonCancel");
+            this.buttonCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            this.buttonCancel.Click += new System.EventHandler(this.buttonCancel_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            resources.ApplyResources(this.tableLayoutPanel1, "tableLayoutPanel1");
+            this.tableLayoutPanel1.Controls.Add(this.buttonOK, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.buttonCancel, 1, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            // 
+            // textBox
+            // 
+            resources.ApplyResources(this.textBox, "textBox");
+            this.textBox.Name = "textBox";
+            // 
+            // labelMain
+            // 
+            resources.ApplyResources(this.labelMain, "labelMain");
+            this.labelMain.Name = "labelMain";
+            // 
+            // InputDialog
+            // 
+            this.AcceptButton = this.buttonOK;
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.CancelButton = this.buttonCancel;
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.labelMain);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "InputDialog";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelMain;
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button buttonOK;
+        private System.Windows.Forms.Button buttonCancel;
+    }
+}
---
+++ OpenTween/InputDialog.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace OpenTween
+{
+    public partial class InputDialog : OTBaseForm
+    {
+        protected InputDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonOK_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.OK;
+        }
+
+        private void buttonCancel_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.Cancel;
+        }
+
+        public static DialogResult Show(string text, out string inputText)
+        {
+            return Show(null, text, "", out inputText);
+        }
+
+        public static DialogResult Show(IWin32Window owner, string text, out string inputText)
+        {
+            return Show(owner, text, "", out inputText);
+        }
+
+        public static DialogResult Show(string text, string caption, out string inputText)
+        {
+            return Show(null, text, caption, out inputText);
+        }
+
+        public static DialogResult Show(IWin32Window owner, string text, string caption, out string inputText)
+        {
+            using (var dialog = new InputDialog())
+            {
+                dialog.labelMain.Text = text;
+                dialog.Text = caption;
+
+                var result = dialog.ShowDialog(owner);
+                inputText = dialog.textBox.Text;
+
+                return result;
+            }
+        }
+    }
+}
