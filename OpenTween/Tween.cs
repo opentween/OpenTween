ecbf4b9a7d8a0dda25c328188759051d1887ded7
spx <spx268@gmail.com>
2014-03-15 22:09:34 +0900

    603017d の微調整 重要な設定ではないので、遅延保存にする

--- OpenTween/Tween.cs
+++ OpenTween/Tween.cs
@@ -13486,7 +13486,7 @@ namespace OpenTween
 
             if (_curList != null) _curList.Refresh();
 
-            SaveConfigsCommon();
+            _modifySettingCommon = true;
         }
 
         private void LockListSortToolStripMenuItem_Click(object sender, EventArgs e)
@@ -13496,7 +13496,7 @@ namespace OpenTween
 
             SettingDialog.SortOrderLock = state;
 
-            SaveConfigsCommon();
+            _modifySettingCommon = true;
         }
     }
 }
