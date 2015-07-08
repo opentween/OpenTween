9b8462a5e7bca371f34747fea853c380c945c0f9
spx <spx268@gmail.com>
2015-07-09 08:16:48 +0900

    YouTubeのサムネイル情報を取得する処理を削除 Data API v2が廃止されたため

--- OpenTween/Thumbnail/Services/Youtube.cs
+++ OpenTween/Thumbnail/Services/Youtube.cs
@@ -49,145 +49,15 @@ namespace OpenTween.Thumbnail.Services
                     return null;
 
                 var videoId = match.Groups["videoid"].Value;
-                var imgUrl = "http://i.ytimg.com/vi/" + videoId + "/default.jpg";
+                var imgUrl = "http://i.ytimg.com/vi/" + videoId + "/hqdefault.jpg";
 
-                // 参考
-                // http://code.google.com/intl/ja/apis/youtube/2.0/developers_guide_protocol_video_entries.html
-                // デベロッパー ガイド: Data API プロトコル - 単独の動画情報の取得 - YouTube の API とツール - Google Code
-                // http://code.google.com/intl/ja/apis/youtube/2.0/developers_guide_protocol_understanding_video_feeds.html#Understanding_Feeds_and_Entries
-                // デベロッパー ガイド: Data API プロトコル - 動画のフィードとエントリについて - YouTube の API とツール - Google Code
-                var videourl = (new HttpVarious()).GetRedirectTo(url);
-                Match mc;
-                if (videourl.StartsWith("http://www.youtube.com/index?ytsession="))
+                return new ThumbnailInfo
                 {
-                    videourl = url;
-                    mc = match;
-                }
-                else
-                {
-                    mc = Youtube.UrlPatternRegex.Match(videourl);
-                }
-                if (mc.Success)
-                {
-                    var apiurl = "http://gdata.youtube.com/feeds/api/videos/" + mc.Groups["videoid"].Value;
-                    var src = "";
-                    if ((new HttpVarious()).GetData(apiurl, null, out src, 5000))
-                    {
-                        var sb = new StringBuilder();
-                        var xdoc = new XmlDocument();
-                        try
-                        {
-                            xdoc.LoadXml(src);
-                            var nsmgr = new XmlNamespaceManager(xdoc.NameTable);
-                            nsmgr.AddNamespace("root", "http://www.w3.org/2005/Atom");
-                            nsmgr.AddNamespace("app", "http://purl.org/atom/app#");
-                            nsmgr.AddNamespace("media", "http://search.yahoo.com/mrss/");
-
-                            var xentryNode = xdoc.DocumentElement.SelectSingleNode("/root:entry/media:group", nsmgr);
-                            var xentry = (XmlElement)xentryNode;
-                            var tmp = "";
-                            try
-                            {
-                                tmp = xentry["media:title"].InnerText;
-                                if (!string.IsNullOrEmpty(tmp))
-                                {
-                                    sb.Append(Properties.Resources.YouTubeInfoText1);
-                                    sb.Append(tmp);
-                                    sb.AppendLine();
-                                }
-                            }
-                            catch (Exception)
-                            {
-                            }
-
-                            try
-                            {
-                                var sec = 0;
-                                if (int.TryParse(xentry["yt:duration"].Attributes["seconds"].Value, out sec))
-                                {
-                                    sb.Append(Properties.Resources.YouTubeInfoText2);
-                                    sb.AppendFormat("{0:d}:{1:d2}", sec / 60, sec % 60);
-                                    sb.AppendLine();
-                                }
-                            }
-                            catch (Exception)
-                            {
-                            }
-
-                            try
-                            {
-                                var tmpdate = new DateTime();
-                                xentry = (XmlElement)xdoc.DocumentElement.SelectSingleNode("/root:entry", nsmgr);
-                                if (DateTime.TryParse(xentry["published"].InnerText, out tmpdate))
-                                {
-                                    sb.Append(Properties.Resources.YouTubeInfoText3);
-                                    sb.Append(tmpdate);
-                                    sb.AppendLine();
-                                }
-                            }
-                            catch (Exception)
-                            {
-                            }
-
-                            try
-                            {
-                                var count = 0;
-                                xentry = (XmlElement)xdoc.DocumentElement.SelectSingleNode("/root:entry", nsmgr);
-                                tmp = xentry["yt:statistics"].Attributes["viewCount"].Value;
-                                if (int.TryParse(tmp, out count))
-                                {
-                                    sb.Append(Properties.Resources.YouTubeInfoText4);
-                                    sb.Append(tmp);
-                                    sb.AppendLine();
-                                }
-                            }
-                            catch (Exception)
-                            {
-                            }
-
-                            try
-                            {
-                                xentry = (XmlElement)xdoc.DocumentElement.SelectSingleNode("/root:entry/app:control", nsmgr);
-                                if (xentry != null)
-                                {
-                                    sb.Append(xentry["yt:state"].Attributes["name"].Value);
-                                    sb.Append(":");
-                                    sb.Append(xentry["yt:state"].InnerText);
-                                    sb.AppendLine();
-                                }
-                            }
-                            catch (Exception)
-                            {
-                            }
-
-                            //mc = Regex.Match(videourl, @"^http://www\.youtube\.com/watch\?v=([\w\-]+)", RegexOptions.IgnoreCase)
-                            //if (mc.Success)
-                            //{
-                            // imgurl = mc.Result("http://i.ytimg.com/vi/${1}/default.jpg");
-                            //}
-                            //mc = Regex.Match(videourl, @"^http://youtu\.be/([\w\-]+)", RegexOptions.IgnoreCase)
-                            //if (mc.Success)
-                            //{
-                            // imgurl = mc.Result("http://i.ytimg.com/vi/${1}/default.jpg");
-                            //}
-
-                        }
-                        catch (Exception)
-                        {
-
-                        }
-
-                        return new ThumbnailInfo
-                        {
-                            ImageUrl = url,
-                            ThumbnailUrl = imgUrl,
-                            TooltipText = sb.ToString().Trim(),
-                            IsPlayable = true,
-                        };
-                    }
-
-                }
-                return null;
+                    ImageUrl = url,
+                    ThumbnailUrl = imgUrl,
+                    TooltipText = null,
+                    IsPlayable = true,
+                };
             }, token);
         }
     }
