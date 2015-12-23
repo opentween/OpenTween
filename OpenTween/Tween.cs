opentween/OpenTween

commit 6cff2aa92d5bc113a53e695958439e77e806a8f5

MyList_SelectedIndexChangedメソッドが呼ばれても選択中のアイテムのStatusIdが同一であれば無視する
RefreshTimeline から呼ばれる RestoreListViewSelection のように、SelectedIndex は変わるが
選択されている発言は同一のものである（移動しただけの）場合もあるため、これを正常に無視する

OpenTween/Tween.cs
@@ -1913,14 +1913,20 @@ namespace OpenTween
             _curItemIndex = _curList.SelectedIndices[0];
             if (_curItemIndex > _curList.VirtualListSize - 1) return;
 
+            PostClass selectedPost;
             try
             {
-                _curPost = GetCurTabPost(_curItemIndex);
+                selectedPost = GetCurTabPost(_curItemIndex);
             }
             catch (ArgumentException)
             {
                 return;
             }
+
+            if (selectedPost.StatusId == this._curPost?.StatusId)
+                return;
+
+            this._curPost = selectedPost;
 
             this.PushSelectPostChain();
 

commit 34dce41343b7c1da88f66c289ff7ee055d91b5e2

MyList_SelectedIndexChangedが呼ばれたが同一ツイートだった場合に、ツイートが未読であれば既読処理を省略しないようにする
再現性は低いがたまにツイートが未読のまま選択状態となってしまうことがあり、
その場合にスペースキー等で既読にできず次の未読に移動できなくなる現象が起きていた。

Fixes: 6cff2aa9 ("MyList_SelectedIndexChangedメソッドが呼ばれても選択中のアイテムのStatusIdが同一であれば無視する")

OpenTween/Tween.cs
@@ -1938,7 +1938,7 @@ namespace OpenTween
                 return;
             }
 
-            if (selectedPost.StatusId == this._curPost?.StatusId)
+            if (selectedPost.StatusId == this._curPost?.StatusId && selectedPost.IsRead)
                 return;
 
             this._curPost = selectedPost;