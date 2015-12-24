opentween/OpenTween

commit 1ca134054ef592d11428c65750782224fe6e2f64

PostClass.RelTabNameを廃止
従来 RelTabName を必要としていた場面では、今後 TabClass.AddPostToInnerStorage メソッドを直接使用する

OpenTween/StatusDictionary.cs
@@ -29,6 +29,7 @@ using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Net;
@@ -101,7 +102,6 @@ namespace OpenTween
         public List<MediaInfo> Media { get; set; }
         public long[] QuoteStatusIds { get; set; }
 
-        public string RelTabName { get; set; }
         public int FavoritedCount { get; set; }
 
         private States _states = States.None;
@@ -175,7 +175,6 @@ namespace OpenTween
         public PostClass()
         {
             RetweetedBy = "";
-            RelTabName = "";
             Media = new List<MediaInfo>();
             ReplyToList = new List<string>();
             QuoteStatusIds = new long[0];
@@ -413,7 +412,6 @@ namespace OpenTween
                     (this.FilterHit == other.FilterHit) &&
                     (this.RetweetedBy == other.RetweetedBy) &&
                     (this.RetweetedId == other.RetweetedId) &&
-                    (this.RelTabName == other.RelTabName) &&
                     (this.IsDeleted == other.IsDeleted) &&
                     (this.InReplyToUserId == other.InReplyToUserId);
 
@@ -1061,78 +1059,57 @@ namespace OpenTween
 
         public void AddPost(PostClass Item)
         {
+            Debug.Assert(!Item.IsDm, "DM は TabClass.AddPostToInnerStorage を使用する");
+
             lock (LockObj)
             {
                 if (this.IsMuted(Item))
                     return;
 
-                if (string.IsNullOrEmpty(Item.RelTabName))
+                PostClass status;
+                if (_statuses.TryGetValue(Item.StatusId, out status))
                 {
-                    if (!Item.IsDm)
+                    if (Item.IsFav)
                     {
-                        PostClass status;
-                        if (_statuses.TryGetValue(Item.StatusId, out status))
+                        if (Item.RetweetedId == null)
                         {
-                            if (Item.IsFav)
-                            {
-                                if (Item.RetweetedId == null)
-                                {
-                                    status.IsFav = true;
-                                }
-                                else
-                                {
-                                    Item.IsFav = false;
-                                }
-                            }
-                            else
-                            {
-                                return;        //追加済みなら何もしない
-                            }
+                            status.IsFav = true;
                         }
                         else
                         {
-                            if (Item.IsFav && Item.RetweetedId != null) Item.IsFav = false;
-                            //既に持っている公式RTは捨てる
-                            if (SettingCommon.Instance.HideDuplicatedRetweets &&
-                                !Item.IsMe &&
-                                Item.RetweetedId != null &&
-                                this._retweets.TryGetValue(Item.RetweetedId.Value, out status) &&
-                                status.RetweetedCount > 0) return;
-
-                            if (BlockIds.Contains(Item.UserId))
-                                return;
-
-                            _statuses.TryAdd(Item.StatusId, Item);
+                            Item.IsFav = false;
                         }
-                        if (Item.RetweetedId != null)
-                        {
-                            this.AddRetweet(Item);
-                        }
-                        if (Item.IsFav && _retweets.ContainsKey(Item.StatusId))
-                        {
-                            return;    //Fav済みのRetweet元発言は追加しない
-                        }
-                        if (_addedIds == null) _addedIds = new List<long>(); //タブ追加用IDコレクション準備
-                        _addedIds.Add(Item.StatusId);
                     }
                     else
                     {
-                        //DM
-                        var tb = this.GetTabByType(MyCommon.TabUsageType.DirectMessage);
-                        if (tb.Contains(Item.StatusId)) return;
-                        tb.AddPostToInnerStorage(Item);
+                        return;        //追加済みなら何もしない
                     }
                 }
                 else
                 {
-                    //公式検索、リスト、関連発言の場合
-                    TabClass tb;
-                    this.Tabs.TryGetValue(Item.RelTabName, out tb);
-                    if (tb == null) return;
-                    if (tb.Contains(Item.StatusId)) return;
-                    //tb.Add(Item.StatusId, Item.IsRead, true);
-                    tb.AddPostToInnerStorage(Item);
+                    if (Item.IsFav && Item.RetweetedId != null) Item.IsFav = false;
+                    //既に持っている公式RTは捨てる
+                    if (SettingCommon.Instance.HideDuplicatedRetweets &&
+                        !Item.IsMe &&
+                        Item.RetweetedId != null &&
+                        this._retweets.TryGetValue(Item.RetweetedId.Value, out status) &&
+                        status.RetweetedCount > 0) return;
+
+                    if (BlockIds.Contains(Item.UserId))
+                        return;
+
+                    _statuses.TryAdd(Item.StatusId, Item);
                 }
+                if (Item.RetweetedId != null)
+                {
+                    this.AddRetweet(Item);
+                }
+                if (Item.IsFav && _retweets.ContainsKey(Item.StatusId))
+                {
+                    return;    //Fav済みのRetweet元発言は追加しない
+                }
+                if (_addedIds == null) _addedIds = new List<long>(); //タブ追加用IDコレクション準備
+                _addedIds.Add(Item.StatusId);
             }
         }
 
@@ -1144,9 +1121,9 @@ namespace OpenTween
 
             // これ以降は Twitter 標準のミュート機能に準じた判定
 
-            // Recent以外のツイートと、リプライはミュート対象外
+            // リプライはミュート対象外
             // 参照: https://support.twitter.com/articles/20171399-muting-users-on-twitter
-            if (!string.IsNullOrEmpty(post.RelTabName) || post.IsReply)
+            if (post.IsReply)
                 return false;
 
             if (this.MuteUserIds.Contains(post.UserId))

OpenTween.Tests/TabInformationTest.cs
@@ -149,21 +149,6 @@ namespace OpenTween
         }
 
         [Fact]
-        public void IsMuted_NotInHomeTimelineTest()
-        {
-            this.tabinfo.MuteUserIds = new HashSet<long> { 12345L };
-
-            // Recent以外のタブ（検索など）の場合は対象外とする
-            var post = new PostClass
-            {
-                UserId = 12345L,
-                Text = "hogehoge",
-                RelTabName = "Search",
-            };
-            Assert.False(this.tabinfo.IsMuted(post));
-        }
-
-        [Fact]
         public void IsMuted_MuteTabRulesTest()
         {
             this.tabinfo.MuteUserIds = new HashSet<long> { };
@@ -198,13 +183,13 @@ namespace OpenTween
                 MoveMatches = true,
             });
 
-            // ミュートタブによるミュートは Recent 以外のタブも対象とする
+            // ミュートタブによるミュートはリプライも対象とする
             var post = new PostClass
             {
                 UserId = 12345L,
                 ScreenName = "foo",
-                Text = "hogehoge",
-                RelTabName = "Search",
+                Text = "@hoge hogehoge",
+                IsReply = true,
             };
             Assert.True(this.tabinfo.IsMuted(post));
         }
@@ -220,15 +205,15 @@ namespace OpenTween
 
             // search1 に追加するツイート (StatusId: 100, 150, 200; すべて未読)
             tab1.UnreadManage = true;
-            this.tabinfo.AddPost(new PostClass { StatusId = 100L, IsRead = false, RelTabName = "search1" });
-            this.tabinfo.AddPost(new PostClass { StatusId = 150L, IsRead = false, RelTabName = "search1" });
-            this.tabinfo.AddPost(new PostClass { StatusId = 200L, IsRead = false, RelTabName = "search1" });
+            tab1.AddPostToInnerStorage(new PostClass { StatusId = 100L, IsRead = false });
+            tab1.AddPostToInnerStorage(new PostClass { StatusId = 150L, IsRead = false });
+            tab1.AddPostToInnerStorage(new PostClass { StatusId = 200L, IsRead = false });
 
             // search2 に追加するツイート (StatusId: 150, 200, 250; すべて未読)
             tab2.UnreadManage = true;
-            this.tabinfo.AddPost(new PostClass { StatusId = 150L, IsRead = false, RelTabName = "search2" });
-            this.tabinfo.AddPost(new PostClass { StatusId = 200L, IsRead = false, RelTabName = "search2" });
-            this.tabinfo.AddPost(new PostClass { StatusId = 250L, IsRead = false, RelTabName = "search2" });
+            tab2.AddPostToInnerStorage(new PostClass { StatusId = 150L, IsRead = false });
+            tab2.AddPostToInnerStorage(new PostClass { StatusId = 200L, IsRead = false });
+            tab2.AddPostToInnerStorage(new PostClass { StatusId = 250L, IsRead = false });
 
             this.tabinfo.DistributePosts();
 
@@ -265,15 +250,15 @@ namespace OpenTween
 
             // search1 に追加するツイート (StatusId: 100, 150, 200; すべて既読)
             tab1.UnreadManage = true;
-            this.tabinfo.AddPost(new PostClass { StatusId = 100L, IsRead = true, RelTabName = "search1" });
-            this.tabinfo.AddPost(new PostClass { StatusId = 150L, IsRead = true, RelTabName = "search1" });
-            this.tabinfo.AddPost(new PostClass { StatusId = 200L, IsRead = true, RelTabName = "search1" });
+            tab1.AddPostToInnerStorage(new PostClass { StatusId = 100L, IsRead = true });
+            tab1.AddPostToInnerStorage(new PostClass { StatusId = 150L, IsRead = true });
+            tab1.AddPostToInnerStorage(new PostClass { StatusId = 200L, IsRead = true });
 
             // search2 に追加するツイート (StatusId: 150, 200, 250; すべて既読)
             tab2.UnreadManage = true;
-            this.tabinfo.AddPost(new PostClass { StatusId = 150L, IsRead = true, RelTabName = "search2" });
-            this.tabinfo.AddPost(new PostClass { StatusId = 200L, IsRead = true, RelTabName = "search2" });
-            this.tabinfo.AddPost(new PostClass { StatusId = 250L, IsRead = true, RelTabName = "search2" });
+            tab2.AddPostToInnerStorage(new PostClass { StatusId = 150L, IsRead = true });
+            tab2.AddPostToInnerStorage(new PostClass { StatusId = 200L, IsRead = true });
+            tab2.AddPostToInnerStorage(new PostClass { StatusId = 250L, IsRead = true });
 
             this.tabinfo.DistributePosts();
 
@@ -306,9 +291,9 @@ namespace OpenTween
 
             // Recent に追加するツイート (StatusId: 100, 150, 200; すべて未読)
             homeTab.UnreadManage = true;
-            this.tabinfo.AddPost(new PostClass { StatusId = 100L, IsRead = false, RelTabName = "" });
-            this.tabinfo.AddPost(new PostClass { StatusId = 150L, IsRead = false, RelTabName = "" });
-            this.tabinfo.AddPost(new PostClass { StatusId = 200L, IsRead = false, RelTabName = "" });
+            this.tabinfo.AddPost(new PostClass { StatusId = 100L, IsRead = false });
+            this.tabinfo.AddPost(new PostClass { StatusId = 150L, IsRead = false });
+            this.tabinfo.AddPost(new PostClass { StatusId = 200L, IsRead = false });
 
             this.tabinfo.DistributePosts();
 
@@ -333,9 +318,9 @@ namespace OpenTween
             // Recent に追加するツイート (StatusId: 100, 150, 200; すべて未読)
             // StatusId: 150 は未読だがリプライ属性が付いている
             homeTab.UnreadManage = true;
-            this.tabinfo.AddPost(new PostClass { StatusId = 100L, IsRead = false, RelTabName = "" });
-            this.tabinfo.AddPost(new PostClass { StatusId = 150L, IsRead = false, RelTabName = "", IsReply = true });
-            this.tabinfo.AddPost(new PostClass { StatusId = 200L, IsRead = false, RelTabName = "" });
+            this.tabinfo.AddPost(new PostClass { StatusId = 100L, IsRead = false });
+            this.tabinfo.AddPost(new PostClass { StatusId = 150L, IsRead = false, IsReply = true });
+            this.tabinfo.AddPost(new PostClass { StatusId = 200L, IsRead = false });
 
             this.tabinfo.DistributePosts();
 
@@ -362,9 +347,9 @@ namespace OpenTween
 
             // Recent に追加するツイート (StatusId: 100, 150, 200; すべて未読)
             homeTab.UnreadManage = true;
-            this.tabinfo.AddPost(new PostClass { StatusId = 100L, IsRead = false, RelTabName = "" });
-            this.tabinfo.AddPost(new PostClass { StatusId = 150L, IsRead = false, RelTabName = "" });
-            this.tabinfo.AddPost(new PostClass { StatusId = 200L, IsRead = false, RelTabName = "" });
+            this.tabinfo.AddPost(new PostClass { StatusId = 100L, IsRead = false });
+            this.tabinfo.AddPost(new PostClass { StatusId = 150L, IsRead = false });
+            this.tabinfo.AddPost(new PostClass { StatusId = 200L, IsRead = false });
 
             // StatusId: 150 だけ FilterTab の振り分けルールにヒットする (PostClass.FilterHit が true になる)
             this.tabinfo.AddTab("FilterTab", MyCommon.TabUsageType.UserDefined, null);

OpenTween/Tween.cs
@@ -6312,7 +6312,6 @@ namespace OpenTween
                 sb.AppendFormat("FilterHit      : {0}<br>", _curPost.FilterHit);
                 sb.AppendFormat("RetweetedBy    : {0}<br>", _curPost.RetweetedBy);
                 sb.AppendFormat("RetweetedId    : {0}<br>", _curPost.RetweetedId);
-                sb.AppendFormat("SearchTabName  : {0}<br>", _curPost.RelTabName);
 
                 sb.AppendFormat("Media.Count    : {0}<br>", _curPost.Media.Count);
                 if (_curPost.Media.Count > 0)

OpenTween/Twitter.cs
@@ -1521,9 +1521,11 @@ namespace OpenTween
         {
             var post = this.GetStatusApi(read, id);
 
-            if (tab != null) post.RelTabName = tab.TabName;
             //非同期アイコン取得＆StatusDictionaryに追加
-            TabInformations.GetInstance().AddPost(post);
+            if (tab != null && tab.IsInnerStorageTabType)
+                tab.AddPostToInnerStorage(post);
+            else
+                TabInformations.GetInstance().AddPost(post);
         }
 
         private PostClass CreatePostsFromStatusData(TwitterStatus status)
@@ -1723,9 +1725,11 @@ namespace OpenTween
                 post.IsRead = read;
                 if (post.IsMe && !read && _readOwnPost) post.IsRead = true;
 
-                if (tab != null) post.RelTabName = tab.TabName;
                 //非同期アイコン取得＆StatusDictionaryに追加
-                TabInformations.GetInstance().AddPost(post);
+                if (tab != null && tab.IsInnerStorageTabType)
+                    tab.AddPostToInnerStorage(post);
+                else
+                    TabInformations.GetInstance().AddPost(post);
             }
 
             return minimumId;
@@ -1789,9 +1793,11 @@ namespace OpenTween
                 post.IsRead = read;
                 if ((post.IsMe && !read) && this._readOwnPost) post.IsRead = true;
 
-                if (tab != null) post.RelTabName = tab.TabName;
                 //非同期アイコン取得＆StatusDictionaryに追加
-                TabInformations.GetInstance().AddPost(post);
+                if (tab != null && tab.IsInnerStorageTabType)
+                    tab.AddPostToInnerStorage(post);
+                else
+                    TabInformations.GetInstance().AddPost(post);
             }
 
             return minimumId;
@@ -1981,8 +1987,7 @@ namespace OpenTween
                 else
                     p.IsRead = read;
 
-                p.RelTabName = tab.TabName;
-                TabInformations.GetInstance().AddPost(p);
+                tab.AddPostToInnerStorage(p);
             });
 
             if (lastException != null)
@@ -2153,7 +2158,8 @@ namespace OpenTween
                 post.IsExcludeReply = false;
                 post.IsDm = true;
 
-                TabInformations.GetInstance().AddPost(post);
+                var dmTab = TabInformations.GetInstance().GetTabByType(MyCommon.TabUsageType.DirectMessage);
+                dmTab.AddPostToInnerStorage(post);
             }
         }
 

commit eadff20384ed0124e57c2fff32745e932b202c8a

リストおよび検索のタブでミュート設定が適用されない不具合を修正 (thx @Tan90909090!)
Fixes: 1ca13405 ("PostClass.RelTabNameを廃止")

OpenTween/StatusDictionary.cs
@@ -1065,7 +1065,7 @@ namespace OpenTween
 
             lock (LockObj)
             {
-                if (this.IsMuted(Item))
+                if (this.IsMuted(Item, isHomeTimeline: true))
                     return;
 
                 PostClass status;
@@ -1114,16 +1114,20 @@ namespace OpenTween
             }
         }
 
-        public bool IsMuted(PostClass post)
+        public bool IsMuted(PostClass post, bool isHomeTimeline)
         {
             var muteTab = this.GetTabByType(MyCommon.TabUsageType.Mute);
             if (muteTab != null && muteTab.AddFiltered(post) == MyCommon.HITRESULT.Move)
                 return true;
 
             // これ以降は Twitter 標準のミュート機能に準じた判定
+            // 参照: https://support.twitter.com/articles/20171399-muting-users-on-twitter
+
+            // ホームタイムライン以外 (検索・リストなど) は対象外
+            if (!isHomeTimeline)
+                return false;
 
             // リプライはミュート対象外
-            // 参照: https://support.twitter.com/articles/20171399-muting-users-on-twitter
             if (post.IsReply)
                 return false;
 
@@ -1158,7 +1162,7 @@ namespace OpenTween
         {
             lock (LockObj)
             {
-                if (IsMuted(item) || BlockIds.Contains(item.UserId))
+                if (IsMuted(item, isHomeTimeline: false) || BlockIds.Contains(item.UserId))
                     return false;
 
                 _quotes[item.StatusId] = item;
@@ -1752,7 +1756,12 @@ namespace OpenTween
         //検索結果の追加
         public void AddPostToInnerStorage(PostClass Post)
         {
-            if (_innerPosts.ContainsKey(Post.StatusId)) return;
+            if (_innerPosts.ContainsKey(Post.StatusId))
+                return;
+
+            if (TabInformations.GetInstance().IsMuted(Post, isHomeTimeline: false))
+                return;
+
             _innerPosts.TryAdd(Post.StatusId, Post);
             this.AddPostQueue(Post.StatusId, Post.IsRead);
         }

OpenTween.Tests/TabInformationTest.cs
@@ -89,7 +89,7 @@ namespace OpenTween
                 UserId = 12345L,
                 Text = "hogehoge",
             };
-            Assert.True(this.tabinfo.IsMuted(post));
+            Assert.True(this.tabinfo.IsMuted(post, isHomeTimeline: true));
         }
 
         [Fact]
@@ -102,7 +102,7 @@ namespace OpenTween
                 UserId = 11111L,
                 Text = "hogehoge",
             };
-            Assert.False(this.tabinfo.IsMuted(post));
+            Assert.False(this.tabinfo.IsMuted(post, isHomeTimeline: true));
         }
 
         [Fact]
@@ -116,7 +116,7 @@ namespace OpenTween
                 RetweetedByUserId = 12345L,
                 Text = "hogehoge",
             };
-            Assert.True(this.tabinfo.IsMuted(post));
+            Assert.True(this.tabinfo.IsMuted(post, isHomeTimeline: true));
         }
 
         [Fact]
@@ -130,7 +130,7 @@ namespace OpenTween
                 RetweetedByUserId = 22222L,
                 Text = "hogehoge",
             };
-            Assert.False(this.tabinfo.IsMuted(post));
+            Assert.False(this.tabinfo.IsMuted(post, isHomeTimeline: true));
         }
 
         [Fact]
@@ -145,7 +145,21 @@ namespace OpenTween
                 Text = "@foo hogehoge",
                 IsReply = true,
             };
-            Assert.False(this.tabinfo.IsMuted(post));
+            Assert.False(this.tabinfo.IsMuted(post, isHomeTimeline: true));
+        }
+
+        [Fact]
+        public void IsMuted_NotInHomeTimelineTest()
+        {
+            this.tabinfo.MuteUserIds = new HashSet<long> { 12345L };
+
+            // Recent以外のタブ（検索など）の場合は対象外とする
+            var post = new PostClass
+            {
+                UserId = 12345L,
+                Text = "hogehoge",
+            };
+            Assert.False(this.tabinfo.IsMuted(post, isHomeTimeline: false));
         }
 
         [Fact]
@@ -167,7 +181,7 @@ namespace OpenTween
                 ScreenName = "foo",
                 Text = "hogehoge",
             };
-            Assert.True(this.tabinfo.IsMuted(post));
+            Assert.True(this.tabinfo.IsMuted(post, isHomeTimeline: true));
         }
 
         [Fact]
@@ -191,7 +205,7 @@ namespace OpenTween
                 Text = "@hoge hogehoge",
                 IsReply = true,
             };
-            Assert.True(this.tabinfo.IsMuted(post));
+            Assert.True(this.tabinfo.IsMuted(post, isHomeTimeline: false));
         }
 
         [Fact]