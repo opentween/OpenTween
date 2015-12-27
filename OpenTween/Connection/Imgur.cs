opentween/OpenTween

commit 52335c36c0311a8a9cd4b3bd292a7c7fc9dd62ad

画像共有サービスに使用するインタフェースを設計し直し
 * IMultimediaShareService から IMediaUploadService に変更
 * 複数枚のメディアをアップロード可能なサービスを考慮
 * URLのために確保する文字数を取得出来るようにした

OpenTween/Connection/Imgur.cs
@@ -21,16 +21,18 @@
 
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Net;
 using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
 using System.Xml.Linq;
-using System.Xml;
+using OpenTween.Api;
 
 namespace OpenTween.Connection
 {
-    public class Imgur : HttpConnectionOAuth, IMultimediaShareService
+    public class Imgur : IMediaUploadService
     {
         private readonly static long MaxFileSize = 10L * 1024 * 1024;
         private readonly static Uri UploadEndpoint = new Uri("https://api.imgur.com/3/image.xml");
@@ -48,132 +50,123 @@ namespace OpenTween.Connection
             ".xcf",
         };
 
-        private readonly Twitter _twitter;
+        private readonly Twitter twitter;
+        private TwitterConfiguration twitterConfig;
 
-        public Imgur(Twitter tw)
+        public Imgur(Twitter tw, TwitterConfiguration twitterConfig)
         {
-            this._twitter = tw;
-
-            Initialize(ApplicationSettings.TwitterConsumerKey, ApplicationSettings.TwitterConsumerSecret,
-                       tw.AccessToken, tw.AccessTokenSecret,
-                       "", "");
+            this.twitter = tw;
+            this.twitterConfig = twitterConfig;
         }
 
-        protected override void AppendOAuthInfo(HttpWebRequest webRequest, Dictionary<string, string> query, string token, string tokenSecret)
+        public int MaxMediaCount
         {
-            webRequest.Headers[HttpRequestHeader.Authorization] =
-                string.Format("Client-ID {0}", ApplicationSettings.ImgurClientID);
+            get { return 1; }
         }
 
-        public string Upload(ref string filePath, ref string message, long? reply_to)
+        public string SupportedFormatsStrForDialog
         {
-            if (!File.Exists(filePath))
-                return "Err:File isn't exists.";
+            get
+            {
+                var formats = new StringBuilder();
 
-            var mediaFile = new FileInfo(filePath);
-            var content = "";
-            HttpStatusCode result;
+                foreach (var extension in SupportedExtensions)
+                    formats.AppendFormat("*{0};", extension);
+
+                return "Image Files(" + formats + ")|" + formats;
+            }
+        }
+
+        public bool CheckFileExtension(string fileExtension)
+        {
+            return SupportedExtensions.Contains(fileExtension, StringComparer.OrdinalIgnoreCase);
+        }
+
+        public bool CheckFileSize(string fileExtension, long fileSize)
+        {
+            var maxFileSize = this.GetMaxFileSize(fileExtension);
+            return maxFileSize == null || fileSize <= maxFileSize.Value;
+        }
+
+        public long? GetMaxFileSize(string fileExtension)
+        {
+            return MaxFileSize;
+        }
+
+        public async Task PostStatusAsync(string text, long? inReplyToStatusId, string[] filePaths)
+        {
+            if (filePaths.Length != 1)
+                throw new ArgumentOutOfRangeException("filePaths");
+
+            var file = new FileInfo(filePaths[0]);
+
+            if (!file.Exists)
+                throw new ArgumentException("File isn't exists.", "filePaths[0]");
+
+            XDocument xml;
             try
             {
-                result = this.UploadFile(mediaFile, message, ref content);
+                xml = await this.UploadFileAsync(file, text)
+                    .ConfigureAwait(false);
             }
-            catch (Exception ex)
+            catch (HttpRequestException ex)
             {
-                return "Err:" + ex.Message;
-            }
-
-            if (result != HttpStatusCode.OK)
-            {
-                return "Err:" + result;
+                throw new WebApiException("Err:" + ex.Message, ex);
             }
 
-            var imageUrl = "";
-            try
+            var imageElm = xml.Element("data");
+
+            if (imageElm.Attribute("success").Value != "1")
+                throw new WebApiException("Err:" + imageElm.Attribute("status").Value);
+
+            var imageUrl = imageElm.Element("link").Value;
+
+            var textWithImageUrl = text + " " + imageUrl.Trim();
+
+            await Task.Run(() => this.twitter.PostStatus(textWithImageUrl, inReplyToStatusId))
+                .ConfigureAwait(false);
+        }
+
+        public int GetReservedTextLength(int mediaCount)
+        {
+            return this.twitterConfig.ShortUrlLength;
+        }
+
+        public void UpdateTwitterConfiguration(TwitterConfiguration config)
+        {
+            this.twitterConfig = config;
+        }
+
+        public async Task<XDocument> UploadFileAsync(FileInfo file, string title)
+        {
+            using (var content = new MultipartFormDataContent())
+            using (var fileStream = file.OpenRead())
+            using (var fileContent = new StreamContent(fileStream))
+            using (var titleContent = new StringContent(title))
             {
-                var xdoc = XDocument.Parse(content);
-                var image = xdoc.Element("data");
-                if (image.Attribute("success").Value != "1")
+                content.Add(fileContent, "image", file.Name);
+                content.Add(titleContent, "title");
+
+                using (var http = MyCommon.CreateHttpClient())
+                using (var request = new HttpRequestMessage(HttpMethod.Post, UploadEndpoint))
                 {
-                    return "APIErr:" + image.Attribute("status").Value;
+                    http.Timeout = TimeSpan.FromMinutes(1);
+
+                    request.Headers.Authorization =
+                        new AuthenticationHeaderValue("Client-ID", ApplicationSettings.ImgurClientID);
+                    request.Content = content;
+
+                    using (var response = await http.SendAsync(request).ConfigureAwait(false))
+                    {
+                        response.EnsureSuccessStatusCode();
+
+                        using (var stream = await response.Content.ReadAsStreamAsync().ConfigureAwait(false))
+                        {
+                            return XDocument.Load(stream);
+                        }
+                    }
                 }
-                imageUrl = image.Element("link").Value;
             }
-            catch (XmlException ex)
-            {
-                return "XmlErr:" + ex.Message;
-            }
-
-            filePath = "";
-            if (message == null)
-                message = "";
-
-            // Post to twitter
-            if (message.Length + AppendSettingDialog.Instance.TwitterConfiguration.CharactersReservedPerMedia + 1 > 140)
-            {
-                message = message.Substring(0, 140 - AppendSettingDialog.Instance.TwitterConfiguration.CharactersReservedPerMedia - 1) + " " + imageUrl;
-            }
-            else
-            {
-                message += " " + imageUrl;
-            }
-            return _twitter.PostStatus(message, reply_to);
-        }
-
-        private HttpStatusCode UploadFile(FileInfo mediaFile, string message, ref string content)
-        {
-            if (!CheckValidExtension(mediaFile.Extension))
-                throw new ArgumentException("Service don't support this filetype", "mediaFile");
-            if (!CheckValidFilesize(mediaFile.Extension, mediaFile.Length))
-                throw new ArgumentException("File is too large", "mediaFile");
-
-            var param = new Dictionary<string, string>
-            {
-                {"title", message},
-            };
-            var binary = new List<KeyValuePair<string, FileInfo>>
-            {
-                new KeyValuePair<string, FileInfo>("image", mediaFile)
-            };
-            this.InstanceTimeout = 60000;
-
-            return this.GetContent(PostMethod, UploadEndpoint, param, binary, ref content, null, null);
-        }
-
-        public bool CheckValidExtension(string ext)
-        {
-            return SupportedExtensions.Contains(ext, StringComparer.OrdinalIgnoreCase);
-        }
-
-        public string GetFileOpenDialogFilter()
-        {
-            var formats = new StringBuilder();
-
-            foreach (var extension in SupportedExtensions)
-                formats.AppendFormat("*{0};", extension);
-
-            return "Image Files(" + formats + ")|" + formats;
-        }
-
-        public MyCommon.UploadFileType GetFileType(string ext)
-        {
-            return this.CheckValidExtension(ext)
-                ? MyCommon.UploadFileType.Picture
-                : MyCommon.UploadFileType.Invalid;
-        }
-
-        public bool IsSupportedFileType(MyCommon.UploadFileType type)
-        {
-            return type == MyCommon.UploadFileType.Picture;
-        }
-
-        public bool CheckValidFilesize(string ext, long fileSize)
-        {
-            return CheckValidExtension(ext) && fileSize <= MaxFileSize;
-        }
-
-        public bool Configuration(string key, object value)
-        {
-            throw new NotImplementedException();
         }
     }
 }

OpenTween/Api/TwitterConfiguration.cs
@@ -41,7 +41,7 @@ namespace OpenTween.Api
         public int CharactersReservedPerMedia { get; set; }
 
         [DataMember(Name = "photo_size_limit")]
-        public int PhotoSizeLimit { get; set; }
+        public long PhotoSizeLimit { get; set; }
 
         [DataMember(Name = "photo_sizes")]
         public TwitterMediaSizes PhotoSizes { get; set; }

OpenTween/Connection/IMediaUploadService.cs
@@ -1,0 +1,84 @@
+﻿// OpenTween - Client of Twitter
+// Copyright (c) 2014 kim_upsilon (@kim_upsilon) <https://upsilo.net/~upsilon/>
+// All rights reserved.
+//
+// This file is part of OpenTween.
+//
+// This program is free software; you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the Free
+// Software Foundation; either version 3 of the License, or (at your option)
+// any later version.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+// for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program. If not, see <http://www.gnu.org/licenses/>, or write to
+// the Free Software Foundation, Inc., 51 Franklin Street - Fifth Floor,
+// Boston, MA 02110-1301, USA.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTween.Api;
+
+namespace OpenTween.Connection
+{
+    /// <summary>
+    /// Twitterでの画像の共有に使用できるサービスを表すインタフェース
+    /// </summary>
+    public interface IMediaUploadService
+    {
+        /// <summary>
+        /// アップロード可能なメディアの最大枚数
+        /// </summary>
+        int MaxMediaCount { get; }
+
+        /// <summary>
+        /// アップロード可能なファイルの種類を表す文字列 (OpenFileDialog.Filter に使用)
+        /// </summary>
+        string SupportedFormatsStrForDialog { get; }
+
+        /// <summary>
+        /// ファイルの拡張子からアップロード可能なフォーマットであるかを判定します
+        /// </summary>
+        /// <param name="fileExtension">アップロードするファイルの拡張子 (ピリオドを含む)</param>
+        bool CheckFileExtension(string fileExtension);
+
+        /// <summary>
+        /// ファイルサイズがアップロード可能な範囲内であるかを判定します
+        /// </summary>
+        /// <param name="fileExtension">アップロードするファイルの拡張子 (ピリオドを含む)</param>
+        /// <param name="fileSize">アップロードするファイルのサイズ (バイト単位)</param>
+        bool CheckFileSize(string fileExtension, long fileSize);
+
+        /// <summary>
+        /// アップロード可能なファイルサイズの上限を返します
+        /// </summary>
+        /// <param name="fileExtension">アップロードするファイルの拡張子 (ピリオドを含む)</param>
+        /// <returns>ファイルサイズの上限 (バイト単位, nullの場合は上限なし)</returns>
+        long? GetMaxFileSize(string fileExtension);
+
+        /// <summary>
+        /// メディアのアップロードとツイートの投稿を行います
+        /// </summary>
+        /// <exception cref="WebApiException"/>
+        Task PostStatusAsync(string text, long? inReplyToStatusId, string[] filePaths);
+
+        /// <summary>
+        /// 画像URLのために確保する必要のある文字数を返します
+        /// </summary>
+        /// <param name="mediaCount">アップロードするメディアの個数</param>
+        int GetReservedTextLength(int mediaCount);
+
+        /// <summary>
+        /// IMediaUploadService で使用する /help/configuration.json の値を更新します
+        /// </summary>
+        void UpdateTwitterConfiguration(TwitterConfiguration config);
+    }
+}

OpenTween/Connection/IMultimediaShareService.cs
@@ -1,37 +1,0 @@
-﻿// OpenTween - Client of Twitter
-// Copyright (c) 2011      kiri_feather (@kiri_feather) <kiri.feather@gmail.com>
-//           (c) 2011      Egtra (@egtra) <http://dev.activebasic.com/egtra/>
-// All rights reserved.
-// 
-// This file is part of OpenTween.
-// 
-// This program is free software; you can redistribute it and/or modify it
-// under the terms of the GNU General Public License as published by the Free
-// Software Foundation; either version 3 of the License, or (at your option)
-// any later version.
-// 
-// This program is distributed in the hope that it will be useful, but
-// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
-// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
-// for more details. 
-// 
-// You should have received a copy of the GNU General Public License along
-// with this program. If not, see <http://www.gnu.org/licenses/>, or write to
-// the Free Software Foundation, Inc., 51 Franklin Street - Fifth Floor,
-// Boston, MA 02110-1301, USA.
-
-namespace OpenTween
-{
-    public interface IMultimediaShareService
-    {
-        string Upload(ref string filePath,
-                        ref string message,
-                        long? reply_to);
-        bool CheckValidExtension(string ext) ;
-        string GetFileOpenDialogFilter();
-        MyCommon.UploadFileType GetFileType(string ext);
-        bool IsSupportedFileType(MyCommon.UploadFileType type);
-        bool CheckValidFilesize(string ext, long fileSize);
-        bool Configuration(string key, object value);
-    }
-}

OpenTween/Connection/TwipplePhoto.cs
@@ -1,5 +1,6 @@
 ﻿// OpenTween - Client of Twitter
 // Copyright (c) 2013 ANIKITI (@anikiti07) <https://twitter.com/anikiti07>
+//           (c) 2014 kim_upsilon (@kim_upsilon) <https://upsilo.net/~upsilon/>
 // All rights reserved.
 //
 // This file is part of OpenTween.
@@ -24,157 +25,158 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Threading.Tasks;
+using System.Windows.Forms;
 using System.Xml;
+using System.Xml.Linq;
+using System.Xml.XPath;
+using OpenTween.Api;
 
 namespace OpenTween.Connection
 {
-    public sealed class TwipplePhoto : HttpConnectionOAuthEcho,
-                                       IMultimediaShareService
+    public sealed class TwipplePhoto : IMediaUploadService
     {
-        private const long MaxFileSize = 4 * 1024 * 1024;
-
-        private readonly Twitter _twitter;
-        private readonly Uri _twipplePhotoUploadUri = new Uri("http://p.twipple.jp/api/upload2");
-        private readonly IEnumerable<string> _supportedPictureExtensions = new[]
+        private static readonly long MaxFileSize = 4L * 1024 * 1024;
+        private static readonly IEnumerable<string> SupportedPictureExtensions = new[]
         {
             ".gif",
             ".jpg",
-            ".png"
+            ".png",
         };
+
+        private readonly Twitter twitter;
+        private readonly TwippleApi twippleApi;
+
+        private TwitterConfiguration twitterConfig;
 
         #region Constructors
 
-        public TwipplePhoto(Twitter twitter)
-            : base(new Uri("http://api.twitter.com/"), new Uri("https://api.twitter.com/1.1/account/verify_credentials.json"))
+        public TwipplePhoto(Twitter twitter, TwitterConfiguration twitterConfig)
         {
             if (twitter == null)
                 throw new ArgumentNullException("twitter");
+            if (twitterConfig == null)
+                throw new ArgumentNullException("config");
 
-            _twitter = twitter;
-            Initialize(ApplicationSettings.TwitterConsumerKey, ApplicationSettings.TwitterConsumerSecret,
-                       _twitter.AccessToken, _twitter.AccessTokenSecret,
-                       "", "");
+            this.twitter = twitter;
+            this.twitterConfig = twitterConfig;
+
+            this.twippleApi = new TwippleApi(twitter.AccessToken, twitter.AccessTokenSecret);
         }
 
         #endregion
 
-        #region IMultimediaShareService Members
-
-        #region Upload Methods
-
-        public string Upload(ref string filePath, ref string message, long? reply_to)
+        public int MaxMediaCount
         {
-            if (!File.Exists(filePath))
-                return "Err:File isn't exists.";
+            get { return 1; }
+        }
 
-            var mediaFile = new FileInfo(filePath);
-            var content = "";
-            HttpStatusCode result;
-            try
+        public string SupportedFormatsStrForDialog
+        {
+            get
             {
-                result = UploadFile(mediaFile, ref content);
-            }
-            catch (Exception ex)
-            {
-                return "Err:" + ex.Message;
-            }
-
-            var imageUrl = "";
-            if (result == HttpStatusCode.OK)
-            {
-                try
+                var filterFormatExtensions = "";
+                foreach (var pictureExtension in SupportedPictureExtensions)
                 {
-                    var xdoc = new XmlDocument();
-                    xdoc.LoadXml(content);
-                    var urlNode = xdoc.SelectSingleNode("/rsp/mediaurl");
-                    if (urlNode != null)
-                    {
-                        imageUrl = urlNode.InnerText;
-                    }
+                    filterFormatExtensions += '*';
+                    filterFormatExtensions += pictureExtension;
+                    filterFormatExtensions += ';';
                 }
-                catch (XmlException ex)
+                return "Image Files(" + filterFormatExtensions + ")|" + filterFormatExtensions;
+            }
+        }
+
+        public bool CheckFileExtension(string fileExtension)
+        {
+            return SupportedPictureExtensions.Contains(fileExtension, StringComparer.OrdinalIgnoreCase);
+        }
+
+        public bool CheckFileSize(string fileExtension, long fileSize)
+        {
+            var maxFileSize = this.GetMaxFileSize(fileExtension);
+            return maxFileSize == null || fileSize <= maxFileSize.Value;
+        }
+
+        public long? GetMaxFileSize(string fileExtension)
+        {
+            return MaxFileSize;
+        }
+
+        public async Task PostStatusAsync(string text, long? inReplyToStatusId, string[] filePaths)
+        {
+            if (filePaths.Length != 1)
+                throw new ArgumentOutOfRangeException("filePaths");
+
+            var file = new FileInfo(filePaths[0]);
+
+            if (!file.Exists)
+                throw new ArgumentException("Err:File isn't exists.", "filePaths[0]");
+
+            var xml = await this.twippleApi.UploadFileAsync(file)
+                .ConfigureAwait(false);
+
+            var imageUrlElm = xml.XPathSelectElement("/rsp/mediaurl");
+            if (imageUrlElm == null)
+                throw new WebApiException("Invalid API response", xml.ToString());
+
+            var textWithImageUrl = text + " " + imageUrlElm.Value.Trim();
+
+            await Task.Run(() => this.twitter.PostStatus(textWithImageUrl, inReplyToStatusId))
+                .ConfigureAwait(false);
+        }
+
+        public int GetReservedTextLength(int mediaCount)
+        {
+            return this.twitterConfig.ShortUrlLength;
+        }
+
+        public void UpdateTwitterConfiguration(TwitterConfiguration config)
+        {
+            this.twitterConfig = config;
+        }
+
+        public class TwippleApi : HttpConnectionOAuthEcho
+        {
+            private static readonly Uri UploadEndpoint = new Uri("http://p.twipple.jp/api/upload2");
+
+            public TwippleApi(string twitterAccessToken, string twitterAccessTokenSecret)
+                : base(new Uri("http://api.twitter.com/"), new Uri("https://api.twitter.com/1.1/account/verify_credentials.json"))
+            {
+                this.Initialize(ApplicationSettings.TwitterConsumerKey, ApplicationSettings.TwitterConsumerSecret,
+                    twitterAccessToken, twitterAccessTokenSecret, "", "");
+
+                this.InstanceTimeout = 60000;
+            }
+
+            /// <summary>
+            /// 画像のアップロードを行います
+            /// </summary>
+            /// <exception cref="WebApiException"/>
+            /// <exception cref="XmlException"/>
+            public async Task<XDocument> UploadFileAsync(FileInfo file)
+            {
+                // 参照: http://p.twipple.jp/wiki/API_Upload2/ja
+
+                var param = new Dictionary<string, string>
                 {
-                    return "XmlErr:" + ex.Message;
-                }
+                    {"upload_from", Application.ProductName},
+                };
+                var paramFiles = new List<KeyValuePair<string, FileInfo>>
+                {
+                    new KeyValuePair<string, FileInfo>("media", file),
+                };
+                var response = "";
+
+                var uploadTask = Task.Run(() => this.GetContent(HttpConnection.PostMethod,
+                    UploadEndpoint, param, paramFiles, ref response, null, null));
+
+                var ret = await uploadTask.ConfigureAwait(false);
+
+                if (ret != HttpStatusCode.OK)
+                    throw new WebApiException("Err:" + ret, response);
+
+                return XDocument.Parse(response);
             }
-            else
-            {
-                return "Err:" + result;
-            }
-
-            filePath = "";
-            if (message == null)
-                message = "";
-
-            // Post to twitter
-            if (message.Length + AppendSettingDialog.Instance.TwitterConfiguration.CharactersReservedPerMedia + 1 > 140)
-            {
-                message = message.Substring(0, 140 - AppendSettingDialog.Instance.TwitterConfiguration.CharactersReservedPerMedia - 1) + " " + imageUrl;
-            }
-            else
-            {
-                message += " " + imageUrl;
-            }
-            return _twitter.PostStatus(message, reply_to);
         }
-
-        private HttpStatusCode UploadFile(FileInfo mediaFile, ref string content)
-        {
-            if (!CheckValidExtension(mediaFile.Extension))
-                throw new ArgumentException("Service don't support this filetype", "mediaFile");
-            if (!CheckValidFilesize(mediaFile.Extension, mediaFile.Length))
-                throw new ArgumentException("File is too large", "mediaFile");
-
-            var binaly = new List<KeyValuePair<string, FileInfo>>
-            {
-                new KeyValuePair<string, FileInfo>("media", mediaFile)
-            };
-            InstanceTimeout = 60000;
-
-            return GetContent(PostMethod, _twipplePhotoUploadUri, null, binaly, ref content, null, null);
-        }
-
-        #endregion
-
-        public bool CheckValidExtension(string ext)
-        {
-            return _supportedPictureExtensions.Contains(ext, StringComparer.OrdinalIgnoreCase);
-        }
-
-        public string GetFileOpenDialogFilter()
-        {
-            string filterFormatExtensions = "";
-            foreach (var pictureExtension in _supportedPictureExtensions)
-            {
-                filterFormatExtensions += '*';
-                filterFormatExtensions += pictureExtension;
-                filterFormatExtensions += ';';
-            }
-            return "Image Files(" + filterFormatExtensions + ")|" + filterFormatExtensions;
-        }
-
-        public MyCommon.UploadFileType GetFileType(string extension)
-        {
-            return CheckValidExtension(extension)
-                       ? MyCommon.UploadFileType.Picture
-                       : MyCommon.UploadFileType.Invalid;
-        }
-
-        public bool IsSupportedFileType(MyCommon.UploadFileType type)
-        {
-            return type == MyCommon.UploadFileType.Picture;
-        }
-
-        public bool CheckValidFilesize(string extension, long fileSize)
-        {
-            return CheckValidExtension(extension) && fileSize <= MaxFileSize;
-        }
-
-        public bool Configuration(string key, object value)
-        {
-            throw new NotImplementedException();
-        }
-
-        #endregion
     }
 }

commit d2dc7ae30dff88f511a0b20d3aaf1473b0650294

Imgurへの画像アップロード時にTaskCanceledExceptionが発生する事を考慮
https://osdn.jp/ticket/browse.php?group_id=6526&tid=35869

Fixes: 52335c36 ("画像共有サービスに使用するインタフェースを設計し直し")

OpenTween/Connection/Imgur.cs
@@ -119,6 +119,10 @@ namespace OpenTween.Connection
             {
                 throw new WebApiException("Err:" + ex.Message, ex);
             }
+            catch (OperationCanceledException ex)
+            {
+                throw new WebApiException("Err:Timeout", ex);
+            }
 
             var imageElm = xml.Element("data");
 