opentween/OpenTween

commit 2fe8fa0e6a9170e7e54fb94898645112e4680fc6

「APIコマンドを回避する」の設定を廃止
「D (ユーザー名)」を除き、APIでの投稿時にSMSコマンドとして解釈されることが無くなったため

OpenTween/Tween.Designer.cs
@@ -25,7 +25,6 @@
             this.HashStripSplitButton = new System.Windows.Forms.ToolStripSplitButton();
             this.ContextMenuPostMode = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.ToolStripMenuItemUrlMultibyteSplit = new System.Windows.Forms.ToolStripMenuItem();
-            this.ToolStripMenuItemApiCommandEvasion = new System.Windows.Forms.ToolStripMenuItem();
             this.ToolStripMenuItemUrlAutoShorten = new System.Windows.Forms.ToolStripMenuItem();
             this.IdeographicSpaceToSpaceToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.MultiLineMenuItem = new System.Windows.Forms.ToolStripMenuItem();
@@ -409,7 +408,6 @@
             // 
             this.ContextMenuPostMode.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.ToolStripMenuItemUrlMultibyteSplit,
-            this.ToolStripMenuItemApiCommandEvasion,
             this.ToolStripMenuItemUrlAutoShorten,
             this.IdeographicSpaceToSpaceToolStripMenuItem,
             this.MultiLineMenuItem,
@@ -429,14 +427,6 @@
             this.ToolStripMenuItemUrlMultibyteSplit.CheckOnClick = true;
             this.ToolStripMenuItemUrlMultibyteSplit.Name = "ToolStripMenuItemUrlMultibyteSplit";
             resources.ApplyResources(this.ToolStripMenuItemUrlMultibyteSplit, "ToolStripMenuItemUrlMultibyteSplit");
-            // 
-            // ToolStripMenuItemApiCommandEvasion
-            // 
-            this.ToolStripMenuItemApiCommandEvasion.Checked = true;
-            this.ToolStripMenuItemApiCommandEvasion.CheckOnClick = true;
-            this.ToolStripMenuItemApiCommandEvasion.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.ToolStripMenuItemApiCommandEvasion.Name = "ToolStripMenuItemApiCommandEvasion";
-            resources.ApplyResources(this.ToolStripMenuItemApiCommandEvasion, "ToolStripMenuItemApiCommandEvasion");
             // 
             // ToolStripMenuItemUrlAutoShorten
             // 
@@ -2515,7 +2505,6 @@
         internal System.Windows.Forms.ToolStripSplitButton HashStripSplitButton;
         internal System.Windows.Forms.ContextMenuStrip ContextMenuPostMode;
         internal System.Windows.Forms.ToolStripMenuItem ToolStripMenuItemUrlMultibyteSplit;
-        internal System.Windows.Forms.ToolStripMenuItem ToolStripMenuItemApiCommandEvasion;
         internal System.Windows.Forms.ToolStripMenuItem ToolStripMenuItemUrlAutoShorten;
         internal System.Windows.Forms.ToolStripMenuItem IdeographicSpaceToSpaceToolStripMenuItem;
         internal System.Windows.Forms.ToolStripMenuItem MultiLineMenuItem;

OpenTween/Tween.cs
@@ -5229,14 +5229,6 @@ namespace OpenTween
 
             statusText = header + statusText + footer;
 
-            if (this.ToolStripMenuItemApiCommandEvasion.Checked)
-            {
-                // APIコマンド回避
-                if (Regex.IsMatch(statusText, @"^[+\-\[\]\s\\.,*/(){}^~|='&%$#""<>?]*(get|g|fav|follow|f|on|off|stop|quit|leave|l|whois|w|nudge|n|stats|invite|track|untrack|tracks|tracking|\*)([+\-\[\]\s\\.,*/(){}^~|='&%$#""<>?]+|$)", RegexOptions.IgnoreCase)
-                   && !statusText.EndsWith(" .", StringComparison.Ordinal))
-                    statusText += " .";
-            }
-
             return statusText;
         }
 

OpenTween/Tween.en.resx
@@ -127,12 +127,6 @@
   <data name="ToolStripMenuItemUrlMultibyteSplit.Text" xml:space="preserve">
     <value>&amp;Separation of multibyte character string from URL</value>
   </data>
-  <data name="ToolStripMenuItemApiCommandEvasion.Size" type="System.Drawing.Size, System.Drawing">
-    <value>445, 24</value>
-  </data>
-  <data name="ToolStripMenuItemApiCommandEvasion.Text" xml:space="preserve">
-    <value>The &amp;collision with API command is evaded</value>
-  </data>
   <data name="ToolStripMenuItemUrlAutoShorten.Size" type="System.Drawing.Size, System.Drawing">
     <value>445, 24</value>
   </data>

OpenTween/Tween.resx
@@ -166,12 +166,6 @@
   <data name="ToolStripMenuItemUrlMultibyteSplit.Text" xml:space="preserve">
     <value>URLからの全角文字列の切り離し</value>
   </data>
-  <data name="ToolStripMenuItemApiCommandEvasion.Size" type="System.Drawing.Size, System.Drawing">
-    <value>257, 22</value>
-  </data>
-  <data name="ToolStripMenuItemApiCommandEvasion.Text" xml:space="preserve">
-    <value>APIコマンドを回避する</value>
-  </data>
   <data name="ToolStripMenuItemUrlAutoShorten.Size" type="System.Drawing.Size, System.Drawing">
     <value>257, 22</value>
   </data>
@@ -2716,12 +2710,6 @@
     <value>ToolStripMenuItemUrlMultibyteSplit</value>
   </data>
   <data name="&gt;&gt;ToolStripMenuItemUrlMultibyteSplit.Type" xml:space="preserve">
-    <value>System.Windows.Forms.ToolStripMenuItem, System.Windows.Forms, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089</value>
-  </data>
-  <data name="&gt;&gt;ToolStripMenuItemApiCommandEvasion.Name" xml:space="preserve">
-    <value>ToolStripMenuItemApiCommandEvasion</value>
-  </data>
-  <data name="&gt;&gt;ToolStripMenuItemApiCommandEvasion.Type" xml:space="preserve">
     <value>System.Windows.Forms.ToolStripMenuItem, System.Windows.Forms, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089</value>
   </data>
   <data name="&gt;&gt;ToolStripMenuItemUrlAutoShorten.Name" xml:space="preserve">

OpenTween/Tween.zh-CHS.resx
@@ -124,12 +124,6 @@
   <data name="ToolStripMenuItemUrlMultibyteSplit.Text" xml:space="preserve">
     <value>排除URL中的全角字符</value>
   </data>
-  <data name="ToolStripMenuItemApiCommandEvasion.Size" type="System.Drawing.Size, System.Drawing">
-    <value>235, 22</value>
-  </data>
-  <data name="ToolStripMenuItemApiCommandEvasion.Text" xml:space="preserve">
-    <value>避免使用API命令</value>
-  </data>
   <data name="ToolStripMenuItemUrlAutoShorten.Size" type="System.Drawing.Size, System.Drawing">
     <value>235, 22</value>
   </data>

commit f8a48eb4b26815b43ab162d2b861114ee4efa1e8

「SMSコマンドを回避する」機能を追加
「M+フォント」から始まるツイートなどが投稿できない問題などを回避する
v1.2.9 までの「APIコマンドを回避する」と目的は同じだがゼロ幅スペース (U+200B) を使用するため、
もしこの機能が動作しても表示上目立ちにくくなっている

Fixes: 2fe8fa0e ("「APIコマンドを回避する」の設定を廃止")

OpenTween/Tween.Designer.cs
@@ -25,6 +25,7 @@
             this.HashStripSplitButton = new System.Windows.Forms.ToolStripSplitButton();
             this.ContextMenuPostMode = new System.Windows.Forms.ContextMenuStrip(this.components);
             this.ToolStripMenuItemUrlMultibyteSplit = new System.Windows.Forms.ToolStripMenuItem();
+            this.ToolStripMenuItemPreventSmsCommand = new System.Windows.Forms.ToolStripMenuItem();
             this.ToolStripMenuItemUrlAutoShorten = new System.Windows.Forms.ToolStripMenuItem();
             this.IdeographicSpaceToSpaceToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.MultiLineMenuItem = new System.Windows.Forms.ToolStripMenuItem();
@@ -408,6 +409,7 @@
             // 
             this.ContextMenuPostMode.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.ToolStripMenuItemUrlMultibyteSplit,
+            this.ToolStripMenuItemPreventSmsCommand,
             this.ToolStripMenuItemUrlAutoShorten,
             this.IdeographicSpaceToSpaceToolStripMenuItem,
             this.MultiLineMenuItem,
@@ -427,6 +429,14 @@
             this.ToolStripMenuItemUrlMultibyteSplit.CheckOnClick = true;
             this.ToolStripMenuItemUrlMultibyteSplit.Name = "ToolStripMenuItemUrlMultibyteSplit";
             resources.ApplyResources(this.ToolStripMenuItemUrlMultibyteSplit, "ToolStripMenuItemUrlMultibyteSplit");
+            // 
+            // ToolStripMenuItemPreventSmsCommand
+            // 
+            this.ToolStripMenuItemPreventSmsCommand.Checked = true;
+            this.ToolStripMenuItemPreventSmsCommand.CheckOnClick = true;
+            this.ToolStripMenuItemPreventSmsCommand.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.ToolStripMenuItemPreventSmsCommand.Name = "ToolStripMenuItemPreventSmsCommand";
+            resources.ApplyResources(this.ToolStripMenuItemPreventSmsCommand, "ToolStripMenuItemPreventSmsCommand");
             // 
             // ToolStripMenuItemUrlAutoShorten
             // 
@@ -2505,6 +2515,7 @@
         internal System.Windows.Forms.ToolStripSplitButton HashStripSplitButton;
         internal System.Windows.Forms.ContextMenuStrip ContextMenuPostMode;
         internal System.Windows.Forms.ToolStripMenuItem ToolStripMenuItemUrlMultibyteSplit;
+        internal System.Windows.Forms.ToolStripMenuItem ToolStripMenuItemPreventSmsCommand;
         internal System.Windows.Forms.ToolStripMenuItem ToolStripMenuItemUrlAutoShorten;
         internal System.Windows.Forms.ToolStripMenuItem IdeographicSpaceToSpaceToolStripMenuItem;
         internal System.Windows.Forms.ToolStripMenuItem MultiLineMenuItem;

OpenTween/Tween.cs
@@ -5253,6 +5253,19 @@ namespace OpenTween
 
             statusText = header + statusText + footer;
 
+            if (this.ToolStripMenuItemPreventSmsCommand.Checked)
+            {
+                // ツイートが意図せず SMS コマンドとして解釈されることを回避 (D, DM, M のみ)
+                // 参照: https://support.twitter.com/articles/14020
+
+                if (Regex.IsMatch(statusText, @"^[+\-\[\]\s\\.,*/(){}^~|='&%$#""<>?]*(d|dm|m)([+\-\[\]\s\\.,*/(){}^~|='&%$#""<>?]+|$)", RegexOptions.IgnoreCase)
+                    && !Twitter.DMSendTextRegex.IsMatch(statusText))
+                {
+                    // U+200B (ZERO WIDTH SPACE) を先頭に加えて回避
+                    statusText = '\u200b' + statusText;
+                }
+            }
+
             return statusText;
         }
 

OpenTween/Tween.en.resx
@@ -127,6 +127,12 @@
   <data name="ToolStripMenuItemUrlMultibyteSplit.Text" xml:space="preserve">
     <value>&amp;Separation of multibyte character string from URL</value>
   </data>
+  <data name="ToolStripMenuItemPreventSmsCommand.Size" type="System.Drawing.Size, System.Drawing">
+    <value>445, 24</value>
+  </data>
+  <data name="ToolStripMenuItemPreventSmsCommand.Text" xml:space="preserve">
+    <value>Prevent interpreting text as SMS command</value>
+  </data>
   <data name="ToolStripMenuItemUrlAutoShorten.Size" type="System.Drawing.Size, System.Drawing">
     <value>445, 24</value>
   </data>

OpenTween/Tween.resx
@@ -166,6 +166,12 @@
   <data name="ToolStripMenuItemUrlMultibyteSplit.Text" xml:space="preserve">
     <value>URLからの全角文字列の切り離し</value>
   </data>
+  <data name="ToolStripMenuItemPreventSmsCommand.Size" type="System.Drawing.Size, System.Drawing">
+    <value>257, 22</value>
+  </data>
+  <data name="ToolStripMenuItemPreventSmsCommand.Text" xml:space="preserve">
+    <value>SMSコマンドを回避する</value>
+  </data>
   <data name="ToolStripMenuItemUrlAutoShorten.Size" type="System.Drawing.Size, System.Drawing">
     <value>257, 22</value>
   </data>
@@ -2710,6 +2716,12 @@
     <value>ToolStripMenuItemUrlMultibyteSplit</value>
   </data>
   <data name="&gt;&gt;ToolStripMenuItemUrlMultibyteSplit.Type" xml:space="preserve">
+    <value>System.Windows.Forms.ToolStripMenuItem, System.Windows.Forms, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089</value>
+  </data>
+  <data name="&gt;&gt;ToolStripMenuItemPreventSmsCommand.Name" xml:space="preserve">
+    <value>ToolStripMenuItemPreventSmsCommand</value>
+  </data>
+  <data name="&gt;&gt;ToolStripMenuItemPreventSmsCommand.Type" xml:space="preserve">
     <value>System.Windows.Forms.ToolStripMenuItem, System.Windows.Forms, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089</value>
   </data>
   <data name="&gt;&gt;ToolStripMenuItemUrlAutoShorten.Name" xml:space="preserve">

OpenTween/Tween.zh-CHS.resx
@@ -124,6 +124,12 @@
   <data name="ToolStripMenuItemUrlMultibyteSplit.Text" xml:space="preserve">
     <value>排除URL中的全角字符</value>
   </data>
+  <data name="ToolStripMenuItemPreventSmsCommand.Size" type="System.Drawing.Size, System.Drawing">
+    <value>235, 22</value>
+  </data>
+  <data name="ToolStripMenuItemPreventSmsCommand.Text" xml:space="preserve">
+    <value>避免使用SMS命令</value>
+  </data>
   <data name="ToolStripMenuItemUrlAutoShorten.Size" type="System.Drawing.Size, System.Drawing">
     <value>235, 22</value>
   </data>