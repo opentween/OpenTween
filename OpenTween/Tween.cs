opentween/OpenTween

commit 6cff2aa92d5bc113a53e695958439e77e806a8f5

MyList_SelectedIndexChangedメソッドが呼ばれても選択中のアイテムのStatusIdが同一であれば無視する
RefreshTimeline から呼ばれる RestoreListViewSelection のように、SelectedIndex は変わるが
選択されている発言は同一のものである（移動しただけの）場合もあるため、これを正常に無視する

OpenTween/Tween.cs
@@ -1913,14 +1913,20 @@ namespace OpenTween
             _curItemIndex = _curList.SelectedIndices[0];
             if (_curItemIndex > _curList.VirtualListSize - 1) return;
 
+            PostClass selectedPost;
             try
             {
-                _curPost = GetCurTabPost(_curItemIndex);
+                selectedPost = GetCurTabPost(_curItemIndex);
             }
             catch (ArgumentException)
             {
                 return;
             }
+
+            if (selectedPost.StatusId == this._curPost?.StatusId)
+                return;
+
+            this._curPost = selectedPost;
 
             this.PushSelectPostChain();
 

commit 00817f01cbb2e341d9deeacfa138b121a2fb57e2

発言一覧の更新によって選択状態が失われたツイートを選択し直しても発言詳細部に反映されない不具合を修正 (thx @norick_v125!)
https://twitter.com/norick_v125/status/680656721379868672

Fixes: 6cff2aa ("MyList_SelectedIndexChangedメソッドが呼ばれても選択中のアイテムのStatusIdが同一であれば無視する")

OpenTween/Tween.cs
@@ -1677,7 +1677,15 @@ namespace OpenTween
             if (listSelection.SelectionMarkStatusId != null)
                 selectionMarkIndex = tab.IndexOf(listSelection.SelectionMarkStatusId.Value);
 
-            this.SelectListItem(listView, selectedIndices, focusedIndex, selectionMarkIndex);
+            listView.SelectedIndexChanged -= this.MyList_SelectedIndexChanged;
+            try
+            {
+                this.SelectListItem(listView, selectedIndices, focusedIndex, selectionMarkIndex);
+            }
+            finally
+            {
+                listView.SelectedIndexChanged += this.MyList_SelectedIndexChanged;
+            }
         }
 
         private bool BalloonRequired()
@@ -1940,9 +1948,6 @@ namespace OpenTween
             {
                 return;
             }
-
-            if (selectedPost.StatusId == this._curPost?.StatusId && selectedPost.IsRead)
-                return;
 
             this._curPost = selectedPost;
 