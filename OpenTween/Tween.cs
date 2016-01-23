opentween/OpenTween

commit 65565e2ab469c808bed76a31dbba695cdd9b201c

PostBrowserからのURLコピー時に「&」のエスケープが解除されない不具合を修正 (thx @pO_0q!)
https://twitter.com/pO_0q/status/652019666886373376

OpenTween/Tween.cs
@@ -10531,20 +10531,16 @@ namespace OpenTween
         {
             try
             {
-                MatchCollection mc = Regex.Matches(this.PostBrowser.DocumentText, @"<a[^>]*href=""(?<url>" + this._postBrowserStatusText.Replace(".", @"\.") + @")""[^>]*title=""(?<title>https?://[^""]+)""", RegexOptions.IgnoreCase);
-                foreach (Match m in mc)
+                foreach (var link in this.PostBrowser.Document.Links.Cast<HtmlElement>())
                 {
-                    if (m.Groups["url"].Value == this._postBrowserStatusText)
+                    if (link.GetAttribute("href") == this._postBrowserStatusText)
                     {
-                        Clipboard.SetDataObject(m.Groups["title"].Value, false, 5, 100);
-                        break;
+                        Clipboard.SetDataObject(link.GetAttribute("title"), false, 5, 100);
+                        return;
                     }
                 }
-                if (mc.Count == 0)
-                {
-                    Clipboard.SetDataObject(this._postBrowserStatusText, false, 5, 100);
-                }
-                //Clipboard.SetDataObject(this._postBrowserStatusText, false, 5, 100);
+
+                Clipboard.SetDataObject(this._postBrowserStatusText, false, 5, 100);
             }
             catch (Exception ex)
             {

commit f6b7e1130be05c7de2edced8f6faa6fe6fe5aa35

発言詳細部に表示されるハッシュタグやメンションで「URLをコピー」が機能しない不具合を修正 (thx @ufoprincess!)
https://twitter.com/ufoprincess/status/683266795088691200

Fixes: 65565e2a ("PostBrowserからのURLコピー時に「&」のエスケープが解除されない不具合を修正")

OpenTween/Tween.cs
@@ -10573,7 +10573,11 @@ namespace OpenTween
                 {
                     if (link.GetAttribute("href") == this._postBrowserStatusText)
                     {
-                        Clipboard.SetDataObject(link.GetAttribute("title"), false, 5, 100);
+                        var linkStr = link.GetAttribute("title");
+                        if (string.IsNullOrEmpty(linkStr))
+                            linkStr = link.GetAttribute("href");
+
+                        Clipboard.SetDataObject(linkStr, false, 5, 100);
                         return;
                     }
                 }