opentween/OpenTween

commit 00817f01cbb2e341d9deeacfa138b121a2fb57e2

発言一覧の更新によって選択状態が失われたツイートを選択し直しても発言詳細部に反映されない不具合を修正 (thx @norick_v125!)
https://twitter.com/norick_v125/status/680656721379868672

Fixes: 6cff2aa ("MyList_SelectedIndexChangedメソッドが呼ばれても選択中のアイテムのStatusIdが同一であれば無視する")

OpenTween/Tween.cs
@@ -1677,7 +1677,15 @@ namespace OpenTween
             if (listSelection.SelectionMarkStatusId != null)
                 selectionMarkIndex = tab.IndexOf(listSelection.SelectionMarkStatusId.Value);
 
-            this.SelectListItem(listView, selectedIndices, focusedIndex, selectionMarkIndex);
+            listView.SelectedIndexChanged -= this.MyList_SelectedIndexChanged;
+            try
+            {
+                this.SelectListItem(listView, selectedIndices, focusedIndex, selectionMarkIndex);
+            }
+            finally
+            {
+                listView.SelectedIndexChanged += this.MyList_SelectedIndexChanged;
+            }
         }
 
         private bool BalloonRequired()
@@ -1940,9 +1948,6 @@ namespace OpenTween
             {
                 return;
             }
-
-            if (selectedPost.StatusId == this._curPost?.StatusId && selectedPost.IsRead)
-                return;
 
             this._curPost = selectedPost;
 

commit d088686eff2687ab69fc2e38e42f8666b32af041

NVDAで発言一覧の名前以降の列が読み上げられない現象を回避 (thx @i_hate_myself92!)
Fixes: 00817f01 ("発言一覧の更新によって選択状態が失われたツイートを選択し直しても発言詳細部に反映されない不具合を修正")

OpenTween/Tween.cs
@@ -1678,15 +1678,7 @@ namespace OpenTween
             if (listSelection.SelectionMarkStatusId != null)
                 selectionMarkIndex = tab.IndexOf(listSelection.SelectionMarkStatusId.Value);
 
-            listView.SelectedIndexChanged -= this.MyList_SelectedIndexChanged;
-            try
-            {
-                this.SelectListItem(listView, selectedIndices, focusedIndex, selectionMarkIndex);
-            }
-            finally
-            {
-                listView.SelectedIndexChanged += this.MyList_SelectedIndexChanged;
-            }
+            this.SelectListItem(listView, selectedIndices, focusedIndex, selectionMarkIndex);
         }
 
         private bool BalloonRequired()
@@ -1940,17 +1932,14 @@ namespace OpenTween
             _curItemIndex = _curList.SelectedIndices[0];
             if (_curItemIndex > _curList.VirtualListSize - 1) return;
 
-            PostClass selectedPost;
             try
             {
-                selectedPost = GetCurTabPost(_curItemIndex);
+                this._curPost = GetCurTabPost(_curItemIndex);
             }
             catch (ArgumentException)
             {
                 return;
             }
-
-            this._curPost = selectedPost;
 
             this.PushSelectPostChain();
 