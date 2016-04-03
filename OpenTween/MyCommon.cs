opentween/OpenTween

commit 7bf814771bfd61baac95b5e6cfff4257e39d703d

DMを使用してエラーレポートを送信する機能を実装
送信するエラーレポートは gzip 圧縮し base64 エンコードされた上で DM で送られる

OpenTween/MyCommon.cs
@@ -386,48 +386,48 @@ namespace OpenTween
             {
                 var IsTerminatePermission = true;
 
+                var ident = WindowsIdentity.GetCurrent();
+                var princ = new WindowsPrincipal(ident);
+
+                var errorReport = string.Join(Environment.NewLine,
+                    string.Format(Properties.Resources.UnhandledExceptionText1, DateTime.Now.ToString()),
+
+                    // 権限書き出し
+                    string.Format(Properties.Resources.UnhandledExceptionText11 + princ.IsInRole(WindowsBuiltInRole.Administrator).ToString()),
+                    string.Format(Properties.Resources.UnhandledExceptionText12 + princ.IsInRole(WindowsBuiltInRole.User).ToString()),
+                    "",
+
+                    // OSVersion,AppVersion書き出し
+                    string.Format(Properties.Resources.UnhandledExceptionText4),
+                    string.Format(Properties.Resources.UnhandledExceptionText5, Environment.OSVersion.VersionString),
+                    string.Format(Properties.Resources.UnhandledExceptionText6, Environment.Version.ToString()),
+                    string.Format(Properties.Resources.UnhandledExceptionText7, MyCommon.GetAssemblyName(), FileVersion),
+
+                    ExceptionOutMessage(ex, ref IsTerminatePermission));
+
                 var logPath = MyCommon.GetErrorLogPath();
                 if (!Directory.Exists(logPath))
                     Directory.CreateDirectory(logPath);
 
                 var now = DateTime.Now;
                 var fileName = string.Format("{0}-{1:0000}{2:00}{3:00}-{4:00}{5:00}{6:00}.log", GetAssemblyName(), now.Year, now.Month, now.Day, now.Hour, now.Minute, now.Second);
-                fileName = Path.Combine(logPath, fileName);
-
-                using (var writer = new StreamWriter(fileName))
+                using (var writer = new StreamWriter(Path.Combine(logPath, fileName)))
                 {
-                    var ident = WindowsIdentity.GetCurrent();
-                    var princ = new WindowsPrincipal(ident);
-
-                    writer.WriteLine(Properties.Resources.UnhandledExceptionText1, DateTime.Now.ToString());
-                    writer.WriteLine(Properties.Resources.UnhandledExceptionText2, ApplicationSettings.FeedbackEmailAddress);
-                    writer.WriteLine(Properties.Resources.UnhandledExceptionText3, ApplicationSettings.FeedbackTwitterName);
-                    // 権限書き出し
-                    writer.WriteLine(Properties.Resources.UnhandledExceptionText11 + princ.IsInRole(WindowsBuiltInRole.Administrator).ToString());
-                    writer.WriteLine(Properties.Resources.UnhandledExceptionText12 + princ.IsInRole(WindowsBuiltInRole.User).ToString());
-                    writer.WriteLine();
-                    // OSVersion,AppVersion書き出し
-                    writer.WriteLine(Properties.Resources.UnhandledExceptionText4);
-                    writer.WriteLine(Properties.Resources.UnhandledExceptionText5, Environment.OSVersion.VersionString);
-                    writer.WriteLine(Properties.Resources.UnhandledExceptionText6, Environment.Version.ToString());
-                    writer.WriteLine(Properties.Resources.UnhandledExceptionText7, MyCommon.GetAssemblyName(), FileVersion);
-
-                    writer.Write(ExceptionOutMessage(ex, ref IsTerminatePermission));
-                    writer.Flush();
+                    writer.Write(errorReport);
                 }
 
-                switch (MessageBox.Show(MyCommon.ReplaceAppName(string.Format(Properties.Resources.UnhandledExceptionText9, fileName, ApplicationSettings.FeedbackEmailAddress, ApplicationSettings.FeedbackTwitterName, Environment.NewLine)),
-                                   Properties.Resources.UnhandledExceptionText10, MessageBoxButtons.YesNoCancel, MessageBoxIcon.Error))
+                using (var dialog = new SendErrorReportForm())
                 {
-                    case DialogResult.Yes:
-                        Process.Start(fileName);
-                        return false;
-                    case DialogResult.No:
-                        return false;
-                    case DialogResult.Cancel:
-                    default:
-                        return IsTerminatePermission;
+                    var mainForm = Application.OpenForms.Cast<Form>().FirstOrDefault() as TweenMain;
+                    if (mainForm != null)
+                        dialog.ErrorReport = new ErrorReport(mainForm.TwitterInstance, errorReport);
+                    else
+                        dialog.ErrorReport = new ErrorReport(errorReport);
+
+                    dialog.ShowDialog(mainForm);
                 }
+
+                return false;
             }
         }
 

OpenTween/ApplicationSettings.cs
@@ -48,6 +48,14 @@ namespace OpenTween
         /// </summary>
         public const string FeedbackTwitterName = "@kim_upsilon";
 
+        /// <summary>
+        /// FeedbackTwitterName のユーザー宛にエラーレポートの DM を送信可能であるか
+        /// </summary>
+        /// <remarks>
+        /// エラーレポートを DM で受け付ける場合は、フォロー外からの DM を受け付ける設定にする必要があります
+        /// </remarks>
+        public const bool AllowSendErrorReportByDM = true;
+
         //=====================================================================
         // Web サイト
 

OpenTween/ErrorReport.datasource
@@ -1,0 +1,10 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<!--
+    This file is automatically generated by Visual Studio .Net. It is 
+    used to store generic object data source configuration information.  
+    Renaming the file extension or editing the content of this file may   
+    cause the file to be unrecognizable by the program.
+-->
+<GenericObjectDataSource DisplayName="ErrorReport" Version="1.0" xmlns="urn:schemas-microsoft-com:xml-msdatasource">
+   <TypeInfo>OpenTween.ErrorReport, OpenTween, Version=0.1.0.0, Culture=neutral, PublicKeyToken=null</TypeInfo>
+</GenericObjectDataSource>

OpenTween/OpenTween.csproj
@@ -141,6 +141,12 @@
     <Compile Include="IndexedSortedSet.cs" />
     <Compile Include="MediaItem.cs" />
     <Compile Include="MouseWheelMessageFilter.cs" />
+    <Compile Include="SendErrorReportForm.cs">
+      <SubType>Form</SubType>
+    </Compile>
+    <Compile Include="SendErrorReportForm.Designer.cs">
+      <DependentUpon>SendErrorReportForm.cs</DependentUpon>
+    </Compile>
     <Compile Include="ShortcutCommand.cs" />
     <Compile Include="TweetExtractor.cs" />
     <Compile Include="WaitingDialog.cs">
@@ -500,6 +506,12 @@
     <EmbeddedResource Include="MediaSelector.zh-CHS.resx">
       <DependentUpon>MediaSelector.cs</DependentUpon>
     </EmbeddedResource>
+    <EmbeddedResource Include="SendErrorReportForm.en.resx">
+      <DependentUpon>SendErrorReportForm.cs</DependentUpon>
+    </EmbeddedResource>
+    <EmbeddedResource Include="SendErrorReportForm.resx">
+      <DependentUpon>SendErrorReportForm.cs</DependentUpon>
+    </EmbeddedResource>
     <EmbeddedResource Include="WaitingDialog.resx">
       <DependentUpon>WaitingDialog.cs</DependentUpon>
     </EmbeddedResource>
@@ -780,6 +792,7 @@
     <Content Include="Resources\InitialImage.png" />
     <Content Include="Resources\LogoImage.png" />
     <Content Include="Resources\MultiMediaImage.png" />
+    <None Include="ErrorReport.datasource" />
     <None Include="Resources\PlayableOverlayImage.png" />
     <Content Include="Resources\re.ico" />
     <Content Include="Resources\re2.ico" />

OpenTween/Properties/Resources.Designer.cs
@@ -2353,6 +2353,24 @@ namespace OpenTween.Properties {
         }
         
         /// <summary>
+        ///   DM の送信が完了しました に類似しているローカライズされた文字列を検索します。
+        /// </summary>
+        internal static string SendErrorReport_DmSendCompleted {
+            get {
+                return ResourceManager.GetString("SendErrorReport_DmSendCompleted", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   DM の送信に失敗しました に類似しているローカライズされた文字列を検索します。
+        /// </summary>
+        internal static string SendErrorReport_DmSendError {
+            get {
+                return ResourceManager.GetString("SendErrorReport_DmSendError", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   [ユーザ･･･{0}]   に類似しているローカライズされた文字列を検索します。
         /// </summary>
         internal static string SetFiltersText1 {

OpenTween/Properties/Resources.en.resx
@@ -1143,4 +1143,10 @@ Available service: {1}</value>
     (It may fail to authentication when clock is delayed.)
  * Please re-add the Twitter account in the settings dialog.</value>
   </data>
+  <data name="SendErrorReport_DmSendCompleted" xml:space="preserve">
+    <value>Succeeded to send DM.</value>
+  </data>
+  <data name="SendErrorReport_DmSendError" xml:space="preserve">
+    <value>Failed to send DM.</value>
+  </data>
 </root>

OpenTween/Properties/Resources.resx
@@ -1201,4 +1201,10 @@
  （大幅にずれていると認証に失敗することがあります）
 ・設定画面から Twitter アカウントをもう一度追加し直して下さい</value>
   </data>
+  <data name="SendErrorReport_DmSendCompleted" xml:space="preserve">
+    <value>DM の送信が完了しました</value>
+  </data>
+  <data name="SendErrorReport_DmSendError" xml:space="preserve">
+    <value>DM の送信に失敗しました</value>
+  </data>
 </root>

OpenTween/SendErrorReportForm.Designer.cs
@@ -1,0 +1,137 @@
+﻿namespace OpenTween
+{
+    partial class SendErrorReportForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SendErrorReportForm));
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.buttonSendByMail = new System.Windows.Forms.Button();
+            this.buttonSendByDM = new System.Windows.Forms.Button();
+            this.bindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.buttonNotSend = new System.Windows.Forms.Button();
+            this.textBoxErrorReport = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonReset = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            resources.ApplyResources(this.tableLayoutPanel1, "tableLayoutPanel1");
+            this.tableLayoutPanel1.Controls.Add(this.buttonSendByMail, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.buttonSendByDM, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.buttonNotSend, 2, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            // 
+            // buttonSendByMail
+            // 
+            resources.ApplyResources(this.buttonSendByMail, "buttonSendByMail");
+            this.buttonSendByMail.Name = "buttonSendByMail";
+            this.buttonSendByMail.UseVisualStyleBackColor = true;
+            this.buttonSendByMail.Click += new System.EventHandler(this.buttonSendByMail_Click);
+            // 
+            // buttonSendByDM
+            // 
+            this.buttonSendByDM.DataBindings.Add(new System.Windows.Forms.Binding("Enabled", this.bindingSource, "CanSendByDM", true));
+            resources.ApplyResources(this.buttonSendByDM, "buttonSendByDM");
+            this.buttonSendByDM.Name = "buttonSendByDM";
+            this.buttonSendByDM.UseVisualStyleBackColor = true;
+            this.buttonSendByDM.Click += new System.EventHandler(this.buttonSendByDM_Click);
+            // 
+            // bindingSource
+            // 
+            this.bindingSource.DataSource = typeof(OpenTween.ErrorReport);
+            // 
+            // buttonNotSend
+            // 
+            this.buttonNotSend.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            resources.ApplyResources(this.buttonNotSend, "buttonNotSend");
+            this.buttonNotSend.Name = "buttonNotSend";
+            this.buttonNotSend.UseVisualStyleBackColor = true;
+            this.buttonNotSend.Click += new System.EventHandler(this.buttonNotSend_Click);
+            // 
+            // textBoxErrorReport
+            // 
+            this.textBoxErrorReport.AcceptsReturn = true;
+            resources.ApplyResources(this.textBoxErrorReport, "textBoxErrorReport");
+            this.textBoxErrorReport.DataBindings.Add(new System.Windows.Forms.Binding("Text", this.bindingSource, "ReportText", true));
+            this.textBoxErrorReport.Name = "textBoxErrorReport";
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // buttonReset
+            // 
+            resources.ApplyResources(this.buttonReset, "buttonReset");
+            this.buttonReset.Name = "buttonReset";
+            this.buttonReset.UseVisualStyleBackColor = true;
+            this.buttonReset.Click += new System.EventHandler(this.buttonReset_Click);
+            // 
+            // SendErrorReportForm
+            // 
+            this.AcceptButton = this.buttonSendByDM;
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+            this.CancelButton = this.buttonNotSend;
+            this.Controls.Add(this.buttonReset);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxErrorReport);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "SendErrorReportForm";
+            this.Shown += new System.EventHandler(this.SendErrorReportForm_Shown);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button buttonSendByMail;
+        private System.Windows.Forms.Button buttonSendByDM;
+        private System.Windows.Forms.TextBox textBoxErrorReport;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonNotSend;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonReset;
+        private System.Windows.Forms.BindingSource bindingSource;
+    }
+}

commit 1eb6240faac4d9db81805f580aeee2f5fcc1d524

TweenMainがDisposeされた後にエラー報告画面を表示するとObjectDisposedExceptionが発生する不具合を修正
https://osdn.jp/ticket/browse.php?group_id=6526&tid=36231

Fixes: 7bf81477 ("DMを使用してエラーレポートを送信する機能を実装")

OpenTween/MyCommon.cs
@@ -420,7 +420,7 @@ namespace OpenTween
                 var mainForm = Application.OpenForms.OfType<TweenMain>().FirstOrDefault();
 
                 ErrorReport report;
-                if (mainForm != null)
+                if (mainForm != null && !mainForm.IsDisposed)
                     report = new ErrorReport(mainForm.TwitterInstance, errorReport);
                 else
                     report = new ErrorReport(errorReport);