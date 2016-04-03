opentween/OpenTween

commit 60291e0e7f01d45b04ddf1569f2b1a13dc1872a3

発言詳細部に表示される pic.twitter.com のリンクの title に alt_text を設定する

OpenTween/TweetFormatter.cs
@@ -134,8 +134,12 @@ namespace OpenTween
 
             expandedUrl = keepTco ? linkUrl : MyCommon.ConvertToReadableUrl(entity.ExpandedUrl);
 
+            var mediaEntity = entity as TwitterEntityMedia;
+
+            var titleText = mediaEntity?.AltText ?? expandedUrl;
+
             // twitter.com へのリンクは t.co を経由せずに直接リンクする (但し pic.twitter.com はそのまま)
-            if (!(entity is TwitterEntityMedia))
+            if (mediaEntity != null)
             {
                 if (entity.ExpandedUrl.StartsWith("https://twitter.com/", StringComparison.Ordinal) ||
                     entity.ExpandedUrl.StartsWith("http://twitter.com/", StringComparison.Ordinal))
@@ -144,7 +148,7 @@ namespace OpenTween
                 }
             }
 
-            return "<a href=\"" + e(linkUrl) + "\" title=\"" + e(expandedUrl) + "\">" + t(e(entity.DisplayUrl)) + "</a>";
+            return "<a href=\"" + e(linkUrl) + "\" title=\"" + e(titleText) + "\">" + t(e(entity.DisplayUrl)) + "</a>";
         }
 
         private static string FormatHashtagEntity(string targetText, TwitterEntityHashtag entity)

commit 96073ce7794ecb4f04bde3f796e8574fb78217fe

TweetFormatterの変更ミスを修正, テストコードを追加
Fixes: 60291e0e ("発言詳細部に表示される pic.twitter.com のリンクの title に alt_text を設定する")

OpenTween/TweetFormatter.cs
@@ -139,7 +139,7 @@ namespace OpenTween
             var titleText = mediaEntity?.AltText ?? expandedUrl;
 
             // twitter.com へのリンクは t.co を経由せずに直接リンクする (但し pic.twitter.com はそのまま)
-            if (mediaEntity != null)
+            if (mediaEntity == null)
             {
                 if (entity.ExpandedUrl.StartsWith("https://twitter.com/", StringComparison.Ordinal) ||
                     entity.ExpandedUrl.StartsWith("http://twitter.com/", StringComparison.Ordinal))

OpenTween.Tests/TweetFormatterTest.cs
@@ -137,6 +137,37 @@ namespace OpenTween
         }
 
         [Fact]
+        public void FormatMediaEntity_AltTextTest()
+        {
+            var text = "http://t.co/h5dCr4ftN4";
+            var entities = new[]
+            {
+                new TwitterEntityMedia
+                {
+                    Indices = new[] { 0, 22 },
+                    Sizes = new TwitterMediaSizes
+                    {
+                        Large = new TwitterMediaSizes.Size { Resize = "fit", Height = 329, Width = 1024 },
+                        Medium = new TwitterMediaSizes.Size { Resize = "fit", Height = 204, Width = 600 },
+                        Small = new TwitterMediaSizes.Size { Resize = "fit", Height = 116, Width = 340 },
+                        Thumb = new TwitterMediaSizes.Size { Resize = "crop", Height = 150, Width = 150 },
+                    },
+                    Type = "photo",
+                    Id = 426404550379986940L,
+                    MediaUrl = "http://pbs.twimg.com/media/BerkrewCYAAV4Kf.png",
+                    MediaUrlHttps = "https://pbs.twimg.com/media/BerkrewCYAAV4Kf.png",
+                    Url = "http://t.co/h5dCr4ftN4",
+                    DisplayUrl = "pic.twitter.com/h5dCr4ftN4",
+                    ExpandedUrl = "http://twitter.com/kim_upsilon/status/426404550371598337/photo/1",
+                    AltText = "ATOKの推測変換候補のスクリーンショット",
+                },
+            };
+
+            var expected = "<a href=\"http://t.co/h5dCr4ftN4\" title=\"ATOKの推測変換候補のスクリーンショット\">pic.twitter.com/h5dCr4ftN4</a>";
+            Assert.Equal(expected, TweetFormatter.AutoLinkHtml(text, entities));
+        }
+
+        [Fact]
         public void AutoLinkHtml_EntityNullTest()
         {
             var text = "てすとてすとー";