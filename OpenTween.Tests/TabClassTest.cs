opentween/OpenTween

commit 37389b4c96b0b669b568752aa1fea021f80308ac

検索ダイアログを書き直し
下記の機能を追加:
 * 検索ダイアログから振り分けタブを生成するボタンを追加
 * 検索ダイアログからPublicSearchタブを生成する画面を追加

OpenTween/Tween.cs
@@ -5677,69 +5677,113 @@ namespace OpenTween
 
         private void MenuItemSubSearch_Click(object sender, EventArgs e)
         {
-            //検索メニュー
-            SearchDialog.Owner = this;
-            if (SearchDialog.ShowDialog() == DialogResult.Cancel)
-            {
-                this.TopMost = SettingDialog.AlwaysTop;
-                return;
-            }
-            this.TopMost = SettingDialog.AlwaysTop;
-
-            if (!string.IsNullOrEmpty(SearchDialog.SWord))
-            {
-                DoTabSearch(SearchDialog.SWord,
-                            SearchDialog.CheckCaseSensitive,
-                            SearchDialog.CheckRegex,
-                            SEARCHTYPE.DialogSearch);
-            }
+            // 検索メニュー
+            this.ShowSearchDialog();
         }
 
         private void MenuItemSearchNext_Click(object sender, EventArgs e)
         {
-            //次を検索
-            if (string.IsNullOrEmpty(SearchDialog.SWord))
+            var previousSearch = this.SearchDialog.ResultOptions;
+            if (previousSearch == null || previousSearch.Type != SearchWordDialog.SearchType.Timeline)
             {
-                if (SearchDialog.ShowDialog() == DialogResult.Cancel)
-                {
-                    this.TopMost = SettingDialog.AlwaysTop;
-                    return;
-                }
-                this.TopMost = SettingDialog.AlwaysTop;
-                if (string.IsNullOrEmpty(SearchDialog.SWord)) return;
+                this.SearchDialog.Reset();
+                this.ShowSearchDialog();
+                return;
+            }
 
-                DoTabSearch(SearchDialog.SWord,
-                            SearchDialog.CheckCaseSensitive,
-                            SearchDialog.CheckRegex,
-                            SEARCHTYPE.DialogSearch);
-            }
-            else
-            {
-                DoTabSearch(SearchDialog.SWord,
-                            SearchDialog.CheckCaseSensitive,
-                            SearchDialog.CheckRegex,
-                            SEARCHTYPE.NextSearch);
-            }
+            // 次を検索
+            this.DoTabSearch(
+                previousSearch.Query,
+                previousSearch.CaseSensitive,
+                previousSearch.UseRegex,
+                SEARCHTYPE.NextSearch);
         }
 
         private void MenuItemSearchPrev_Click(object sender, EventArgs e)
         {
-            //前を検索
-            if (string.IsNullOrEmpty(SearchDialog.SWord))
+            var previousSearch = this.SearchDialog.ResultOptions;
+            if (previousSearch == null || previousSearch.Type != SearchWordDialog.SearchType.Timeline)
             {
-                if (SearchDialog.ShowDialog() == DialogResult.Cancel)
-                {
-                    this.TopMost = SettingDialog.AlwaysTop;
-                    return;
-                }
-                this.TopMost = SettingDialog.AlwaysTop;
-                if (string.IsNullOrEmpty(SearchDialog.SWord)) return;
+                this.SearchDialog.Reset();
+                this.ShowSearchDialog();
+                return;
             }
 
-            DoTabSearch(SearchDialog.SWord,
-                        SearchDialog.CheckCaseSensitive,
-                        SearchDialog.CheckRegex,
-                        SEARCHTYPE.PrevSearch);
+            // 前を検索
+            this.DoTabSearch(
+                previousSearch.Query,
+                previousSearch.CaseSensitive,
+                previousSearch.UseRegex,
+                SEARCHTYPE.PrevSearch);
+        }
+
+        /// <summary>
+        /// 検索ダイアログを表示し、検索を実行します
+        /// </summary>
+        private void ShowSearchDialog()
+        {
+            // Recentタブの検索時以外では「新規タブに表示」ボタンを無効化する
+            if (this._statuses.Tabs[this._curTab.Text].TabType == MyCommon.TabUsageType.Home)
+                this.SearchDialog.DisableNewTabButton = false;
+            else
+                this.SearchDialog.DisableNewTabButton = true;
+
+            if (this.SearchDialog.ShowDialog(this) != DialogResult.OK)
+            {
+                this.TopMost = this.SettingDialog.AlwaysTop;
+                return;
+            }
+            this.TopMost = this.SettingDialog.AlwaysTop;
+
+            var searchOptions = this.SearchDialog.ResultOptions;
+            if (searchOptions.Type == SearchWordDialog.SearchType.Timeline)
+            {
+                if (searchOptions.NewTab)
+                {
+                    var tabName = searchOptions.Query;
+
+                    try
+                    {
+                        tabName = this._statuses.MakeTabName(tabName);
+                    }
+                    catch (TabException ex)
+                    {
+                        MessageBox.Show(this, ex.Message, Application.ProductName, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+
+                    this.AddNewTab(tabName, false, MyCommon.TabUsageType.UserDefined);
+                    this._statuses.AddTab(tabName, MyCommon.TabUsageType.UserDefined, null);
+
+                    var filter = new PostFilterRule
+                    {
+                        FilterBody = new[] { searchOptions.Query },
+                        UseRegex = searchOptions.UseRegex,
+                        CaseSensitive = searchOptions.CaseSensitive,
+                    };
+                    this._statuses.Tabs[tabName].AddFilter(filter);
+
+                    var tabPage = this.ListTab.TabPages.Cast<TabPage>()
+                        .First(x => x.Text == tabName);
+
+                    this.ListTab.SelectedTab = tabPage;
+                    this.ListTabSelect(tabPage);
+
+                    this.ApplyPostFilters();
+                    this.SaveConfigsTabs();
+                }
+                else
+                {
+                    this.DoTabSearch(
+                        searchOptions.Query,
+                        searchOptions.CaseSensitive,
+                        searchOptions.UseRegex,
+                        SEARCHTYPE.DialogSearch);
+                }
+            }
+            else if (searchOptions.Type == SearchWordDialog.SearchType.Public)
+            {
+                this.AddNewTabForSearch(searchOptions.Query);
+            }
         }
 
         private void AboutMenuItem_Click(object sender, EventArgs e)
@@ -10348,14 +10392,20 @@ namespace OpenTween
 
             if (_selText != null)
             {
-                SearchDialog.SWord = _selText;
-                SearchDialog.CheckCaseSensitive = false;
-                SearchDialog.CheckRegex = false;
+                var searchOptions = new SearchWordDialog.SearchOptions(
+                    SearchWordDialog.SearchType.Timeline,
+                    _selText,
+                    newTab: false,
+                    caseSensitive: false,
+                    useRegex: false);
 
-                DoTabSearch(SearchDialog.SWord,
-                            SearchDialog.CheckCaseSensitive,
-                            SearchDialog.CheckRegex,
-                            SEARCHTYPE.NextSearch);
+                this.SearchDialog.ResultOptions = searchOptions;
+
+                this.DoTabSearch(
+                    searchOptions.Query,
+                    searchOptions.CaseSensitive,
+                    searchOptions.UseRegex,
+                    SEARCHTYPE.NextSearch);
             }
         }
 

commit e310c6e3e72c177598381d39ff4fe8f87c3af989

タイムライン検索で検索結果を新規タブに表示する場合とカーソル移動のみの場合で検索条件を同一にする
この変更により、検索結果を新規タブに表示した場合にスクリーンネームなどが検索対象にならない不具合も修正される
https://twitter.com/tests_peer/status/660777615150616576

Fixes: 37389b4c ("検索ダイアログを書き直し")

OpenTween/StatusDictionary.cs
@@ -2041,6 +2041,67 @@ namespace OpenTween
             return _ids.IndexOf(ID);
         }
 
+        public IEnumerable<int> SearchPostsAll(Func<string, bool> stringComparer)
+            => this.SearchPostsAll(stringComparer, reverse: false);
+
+        public IEnumerable<int> SearchPostsAll(Func<string, bool> stringComparer, int startIndex)
+            => this.SearchPostsAll(stringComparer, startIndex, reverse: false);
+
+        public IEnumerable<int> SearchPostsAll(Func<string, bool> stringComparer, bool reverse)
+        {
+            var startIndex = reverse ? this.AllCount - 1 : 0;
+
+            return this.SearchPostsAll(stringComparer, startIndex, reverse: false);
+        }
+
+        /// <summary>
+        /// タブ内の発言を指定された条件で検索します
+        /// </summary>
+        /// <param name="stringComparer">発言内容、スクリーン名、名前と比較する条件。マッチしたら true を返す</param>
+        /// <param name="startIndex">検索を開始する位置</param>
+        /// <param name="reverse">インデックスの昇順に検索する場合は false、降順の場合は true</param>
+        /// <returns></returns>
+        public IEnumerable<int> SearchPostsAll(Func<string, bool> stringComparer, int startIndex, bool reverse)
+        {
+            if (this.AllCount == 0)
+                yield break;
+
+            var searchIndices = Enumerable.Empty<int>();
+
+            if (!reverse)
+            {
+                // startindex ...末尾
+                if (startIndex != this.AllCount - 1)
+                    searchIndices = MyCommon.CountUp(startIndex, this.AllCount - 1);
+
+                // 先頭 ... (startIndex - 1)
+                if (startIndex != 0)
+                    searchIndices = searchIndices.Concat(MyCommon.CountUp(0, startIndex - 1));
+            }
+            else
+            {
+                // startIndex ... 先頭
+                if (startIndex != 0)
+                    searchIndices = MyCommon.CountDown(startIndex, 0);
+
+                // 末尾 ... (startIndex - 1)
+                if (startIndex != this.AllCount - 1)
+                    searchIndices = searchIndices.Concat(MyCommon.CountDown(this.AllCount - 1, startIndex - 1));
+            }
+
+            foreach (var index in searchIndices)
+            {
+                PostClass post;
+                if (!this.Posts.TryGetValue(this.GetId(index), out post))
+                    continue;
+
+                if (stringComparer(post.Nickname) || stringComparer(post.TextFromApi) || stringComparer(post.ScreenName))
+                {
+                    yield return index;
+                }
+            }
+        }
+
         [XmlIgnore]
         public bool FilterModified { get; set; }
 

OpenTween/Tween.cs
@@ -5699,152 +5699,65 @@ namespace OpenTween
         //    }
         //}
 
-        private void DoTabSearch(string _word,
-                                 bool CaseSensitive,
-                                 bool UseRegex,
-                                 SEARCHTYPE SType)
+        private void DoTabSearch(string searchWord, bool caseSensitive, bool useRegex, SEARCHTYPE searchType)
         {
-            int cidx = 0;
-            bool fnd = false;
-            int toIdx;
-            int stp = 1;
+            var tab = this._statuses.Tabs[this._curTab.Text];
 
-            if (_curList.VirtualListSize == 0)
+            if (tab.AllCount == 0)
             {
                 MessageBox.Show(Properties.Resources.DoTabSearchText2, Properties.Resources.DoTabSearchText3, MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
             }
 
-            if (_curList.SelectedIndices.Count > 0)
-            {
-                cidx = _curList.SelectedIndices[0];
-            }
-            toIdx = _curList.VirtualListSize;
+            var selectedIndex = this._curList.SelectedIndices.Count != 0 ? this._curList.SelectedIndices[0] : -1;
 
-            switch (SType)
+            int startIndex;
+            switch (searchType)
             {
-                case SEARCHTYPE.DialogSearch:    //ダイアログからの検索
-                    if (_curList.SelectedIndices.Count > 0)
-                        cidx = _curList.SelectedIndices[0];
+                case SEARCHTYPE.NextSearch: // 次を検索
+                    if (selectedIndex != -1)
+                        startIndex = Math.Min(selectedIndex + 1, tab.AllCount - 1);
                     else
-                        cidx = 0;
+                        startIndex = 0;
                     break;
-                case SEARCHTYPE.NextSearch:      //次を検索
-                    if (_curList.SelectedIndices.Count > 0)
-                    {
-                        cidx = _curList.SelectedIndices[0] + 1;
-                        if (cidx > toIdx) cidx = toIdx;
-                    }
+                case SEARCHTYPE.PrevSearch: // 前を検索
+                    if (selectedIndex != -1)
+                        startIndex = Math.Max(selectedIndex - 1, 0);
                     else
-                    {
-                        cidx = 0;
-                    }
+                        startIndex = tab.AllCount - 1;
                     break;
-                case SEARCHTYPE.PrevSearch:      //前を検索
-                    if (_curList.SelectedIndices.Count > 0)
-                    {
-                        cidx = _curList.SelectedIndices[0] - 1;
-                        if (cidx < 0) cidx = 0;
-                    }
+                case SEARCHTYPE.DialogSearch: // ダイアログからの検索
+                default:
+                    if (selectedIndex != -1)
+                        startIndex = selectedIndex;
                     else
-                    {
-                        cidx = toIdx;
-                    }
-                    toIdx = -1;
-                    stp = -1;
+                        startIndex = 0;
                     break;
             }
 
-            RegexOptions regOpt = RegexOptions.None;
-            StringComparison fndOpt = StringComparison.Ordinal;
-            if (!CaseSensitive)
-            {
-                regOpt = RegexOptions.IgnoreCase;
-                fndOpt = StringComparison.OrdinalIgnoreCase;
-            }
+            Func<string, bool> stringComparer;
             try
             {
-    RETRY:
-                if (UseRegex)
-                {
-                    // 正規表現検索
-                    Regex _search;
-                    try
-                    {
-                        _search = new Regex(_word, regOpt);
-                        for (int idx = cidx; idx != toIdx; idx += stp)
-                        {
-                            PostClass post;
-                            try
-                            {
-                                post = _statuses.Tabs[_curTab.Text][idx];
-                            }
-                            catch (Exception)
-                            {
-                                continue;
-                            }
-                            if (_search.IsMatch(post.Nickname)
-                                || _search.IsMatch(post.TextFromApi)
-                                || _search.IsMatch(post.ScreenName))
-                            {
-                                SelectListItem(_curList, idx);
-                                _curList.EnsureVisible(idx);
-                                return;
-                            }
-                        }
-                    }
-                    catch (ArgumentException)
-                    {
-                        MessageBox.Show(Properties.Resources.DoTabSearchText1, "", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                        return;
-                    }
-                }
-                else
-                {
-                    // 通常検索
-                    for (int idx = cidx; idx != toIdx; idx += stp)
-                    {
-                        PostClass post;
-                        try
-                        {
-                            post = _statuses.Tabs[_curTab.Text][idx];
-                        }
-                        catch (Exception)
-                        {
-                            continue;
-                        }
-                        if (post.Nickname.IndexOf(_word, fndOpt) > -1
-                            || post.TextFromApi.IndexOf(_word, fndOpt) > -1
-                            || post.ScreenName.IndexOf(_word, fndOpt) > -1)
-                        {
-                            SelectListItem(_curList, idx);
-                            _curList.EnsureVisible(idx);
-                            return;
-                        }
-                    }
-                }
-
-                if (!fnd)
-                {
-                    switch (SType)
-                    {
-                        case SEARCHTYPE.DialogSearch:
-                        case SEARCHTYPE.NextSearch:
-                            toIdx = cidx;
-                            cidx = 0;
-                            break;
-                        case SEARCHTYPE.PrevSearch:
-                            toIdx = cidx;
-                            cidx = _curList.VirtualListSize - 1;
-                            break;
-                    }
-                    fnd = true;
-                    goto RETRY;
-                }
+                stringComparer = this.CreateSearchComparer(searchWord, useRegex, caseSensitive);
             }
-            catch (ArgumentOutOfRangeException)
+            catch (ArgumentException)
             {
+                MessageBox.Show(Properties.Resources.DoTabSearchText1, Properties.Resources.DoTabSearchText3, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
             }
-            MessageBox.Show(Properties.Resources.DoTabSearchText2, Properties.Resources.DoTabSearchText3, MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            var reverse = searchType == SEARCHTYPE.PrevSearch;
+            var foundIndex = tab.SearchPostsAll(stringComparer, startIndex, reverse)
+                .DefaultIfEmpty(-1).First();
+
+            if (foundIndex == -1)
+            {
+                MessageBox.Show(Properties.Resources.DoTabSearchText2, Properties.Resources.DoTabSearchText3, MessageBoxButtons.OK, MessageBoxIcon.Information);
+                return;
+            }
+
+            this.SelectListItem(this._curList, foundIndex);
+            this._curList.EnsureVisible(foundIndex);
         }
 
         private void MenuItemSubSearch_Click(object sender, EventArgs e)
@@ -5920,20 +5833,30 @@ namespace OpenTween
                     this.AddNewTab(tabName, false, MyCommon.TabUsageType.SearchResults);
                     this._statuses.AddTab(tabName, MyCommon.TabUsageType.SearchResults, null);
 
-                    var filter = new PostFilterRule
-                    {
-                        FilterBody = new[] { searchOptions.Query },
-                        UseRegex = searchOptions.UseRegex,
-                        CaseSensitive = searchOptions.CaseSensitive,
-                    };
-
                     var targetTab = this._statuses.Tabs[this._curTab.Text];
-                    var posts = targetTab.Posts.Values
-                        .Where(x => filter.ExecFilter(x) == MyCommon.HITRESULT.CopyAndMark)
-                        .Where(x => targetTab.Contains(x.StatusId));
+
+                    Func<string, bool> stringComparer;
+                    try
+                    {
+                        stringComparer = this.CreateSearchComparer(searchOptions.Query, searchOptions.UseRegex, searchOptions.CaseSensitive);
+                    }
+                    catch (ArgumentException)
+                    {
+                        MessageBox.Show(Properties.Resources.DoTabSearchText1, Properties.Resources.DoTabSearchText3, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+                    }
+
+                    var foundIndices = targetTab.SearchPostsAll(stringComparer).ToArray();
+                    if (foundIndices.Length == 0)
+                    {
+                        MessageBox.Show(Properties.Resources.DoTabSearchText2, Properties.Resources.DoTabSearchText3, MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        return;
+                    }
+
+                    var foundPosts = foundIndices.Select(x => targetTab[x]);
 
                     var resultTab = this._statuses.Tabs[tabName];
-                    foreach (var post in posts)
+                    foreach (var post in foundPosts)
                     {
                         resultTab.AddPostToInnerStorage(post);
                     }
@@ -5958,6 +5881,27 @@ namespace OpenTween
             else if (searchOptions.Type == SearchWordDialog.SearchType.Public)
             {
                 this.AddNewTabForSearch(searchOptions.Query);
+            }
+        }
+
+        /// <summary>発言検索に使用するメソッドを生成します</summary>
+        /// <exception cref="ArgumentException">
+        /// <paramref name="useRegex"/> が true かつ、<paramref name="query"> が不正な正規表現な場合
+        /// </exception>
+        private Func<string, bool> CreateSearchComparer(string query, bool useRegex, bool caseSensitive)
+        {
+            if (useRegex)
+            {
+                var regexOption = caseSensitive ? RegexOptions.None : RegexOptions.IgnoreCase;
+                var regex = new Regex(query, regexOption);
+
+                return x => regex.IsMatch(x);
+            }
+            else
+            {
+                var comparisonType = caseSensitive ? StringComparison.CurrentCulture : StringComparison.CurrentCultureIgnoreCase;
+
+                return x => x.IndexOf(query, comparisonType) != -1;
             }
         }
 

OpenTween.Tests/TabClassTest.cs
@@ -387,6 +387,52 @@ namespace OpenTween
 
             Assert.False(tab.FilterModified);
         }
+
+        [Fact]
+        public void SearchPostsAll_Test()
+        {
+            var tab = new TabClass { TabType = MyCommon.TabUsageType.PublicSearch };
+
+            tab.AddPostToInnerStorage(new PostClass { StatusId = 100L, TextFromApi = "abcd", ScreenName = "", Nickname = "" }); // 0
+            tab.AddPostToInnerStorage(new PostClass { StatusId = 110L, TextFromApi = "efgh", ScreenName = "", Nickname = "" }); // 1
+            tab.AddPostToInnerStorage(new PostClass { StatusId = 120L, TextFromApi = "ijkl", ScreenName = "", Nickname = "" }); // 2
+            tab.AddPostToInnerStorage(new PostClass { StatusId = 130L, TextFromApi = "abc", ScreenName = "", Nickname = "" });  // 3
+            tab.AddPostToInnerStorage(new PostClass { StatusId = 140L, TextFromApi = "def", ScreenName = "", Nickname = "" });  // 4
+
+            tab.SetSortMode(ComparerMode.Id, SortOrder.Ascending);
+            tab.AddSubmit();
+
+            // インデックス番号 0 から開始 → 0, 1, 2, 3, 4 の順に検索
+            var result = tab.SearchPostsAll(x => x.Contains("a"), startIndex: 0);
+            Assert.Equal(new[] { 0, 3 }, result);
+
+            // インデックス番号 2 から開始 → 2, 3, 4, 0, 1 の順に検索
+            result = tab.SearchPostsAll(x => x.Contains("a"), startIndex: 2);
+            Assert.Equal(new[] { 3, 0 }, result);
+        }
+
+        [Fact]
+        public void SearchPostsAll_ReverseOrderTest()
+        {
+            var tab = new TabClass { TabType = MyCommon.TabUsageType.PublicSearch };
+
+            tab.AddPostToInnerStorage(new PostClass { StatusId = 100L, TextFromApi = "abcd", ScreenName = "", Nickname = "" }); // 0
+            tab.AddPostToInnerStorage(new PostClass { StatusId = 110L, TextFromApi = "efgh", ScreenName = "", Nickname = "" }); // 1
+            tab.AddPostToInnerStorage(new PostClass { StatusId = 120L, TextFromApi = "ijkl", ScreenName = "", Nickname = "" }); // 2
+            tab.AddPostToInnerStorage(new PostClass { StatusId = 130L, TextFromApi = "abc", ScreenName = "", Nickname = "" });  // 3
+            tab.AddPostToInnerStorage(new PostClass { StatusId = 140L, TextFromApi = "def", ScreenName = "", Nickname = "" });  // 4
+
+            tab.SetSortMode(ComparerMode.Id, SortOrder.Ascending);
+            tab.AddSubmit();
+
+            // インデックス番号 4 から逆順に開始 → 4, 3, 2, 1, 0 の順に検索
+            var result = tab.SearchPostsAll(x => x.Contains("a"), startIndex: 4, reverse: true);
+            Assert.Equal(new[] { 3, 0 }, result);
+
+            // インデックス番号 2 から逆順に開始 → 2, 1, 0, 4, 3 の順に検索
+            result = tab.SearchPostsAll(x => x.Contains("a"), startIndex: 2, reverse: true);
+            Assert.Equal(new[] { 0, 3 }, result);
+        }
     }
 
     public class TabUsageTypeExtTest