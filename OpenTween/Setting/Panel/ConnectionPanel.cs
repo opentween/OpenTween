opentween/OpenTween

commit 726a9016271b20f7967a26b607c2ba4e29ec8455

TwitterApiConnection.RestApiBaseをstaticプロパティに変更

OpenTween/Tween.cs
@@ -776,7 +776,7 @@ namespace OpenTween
             if (this._cfgCommon.AutoShortUrlFirst < 0)
                 this._cfgCommon.AutoShortUrlFirst = MyCommon.UrlConverter.Uxnu;
 
-            HttpTwitter.TwitterUrl = this._cfgCommon.TwitterUrl;
+            TwitterApiConnection.RestApiBase = new Uri(this._cfgCommon.TwitterUrl);
             this.tw = new Twitter(this.twitterApi);
 
             //認証関連
@@ -812,7 +812,7 @@ namespace OpenTween
                 this._cfgLocal.ProxyUser, this._cfgLocal.ProxyPassword);
             Networking.ForceIPv4 = this._cfgCommon.ForceIPv4;
 
-            HttpTwitter.TwitterUrl = this._cfgCommon.TwitterUrl;
+            TwitterApiConnection.RestApiBase = new Uri(this._cfgCommon.TwitterUrl);
             tw.RestrictFavCheck = this._cfgCommon.RestrictFavCheck;
             tw.ReadOwnPost = this._cfgCommon.ReadOwnPost;
             tw.TrackWord = this._cfgCommon.TrackWord;
@@ -4174,7 +4174,7 @@ namespace OpenTween
                     ShortUrl.Instance.DisableExpanding = !this._cfgCommon.TinyUrlResolve;
                     ShortUrl.Instance.BitlyId = this._cfgCommon.BilyUser;
                     ShortUrl.Instance.BitlyKey = this._cfgCommon.BitlyPwd;
-                    HttpTwitter.TwitterUrl = _cfgCommon.TwitterUrl;
+                    TwitterApiConnection.RestApiBase = new Uri(this._cfgCommon.TwitterUrl);
 
                     Networking.DefaultTimeout = TimeSpan.FromSeconds(this._cfgCommon.DefaultTimeOut);
                     Networking.SetWebProxy(this._cfgLocal.ProxyType,

OpenTween/AppendSettingDialog.cs
@@ -312,7 +312,7 @@ namespace OpenTween
 
             Networking.ForceIPv4 = this.ConnectionPanel.checkBoxForceIPv4.Checked;
 
-            HttpTwitter.TwitterUrl = this.ConnectionPanel.TwitterAPIText.Text.Trim();
+            TwitterApiConnection.RestApiBase = new Uri(this.ConnectionPanel.TwitterAPIText.Text.Trim());
         }
 
         private async Task<UserAccount> PinAuth()

OpenTween/Connection/TwitterApiConnection.cs
@@ -37,7 +37,7 @@ namespace OpenTween.Connection
 {
     public class TwitterApiConnection : IApiConnection, IDisposable
     {
-        public Uri RestApiBase { get; set; } = new Uri("https://api.twitter.com/1.1/");
+        public static Uri RestApiBase { get; set; } = new Uri("https://api.twitter.com/1.1/");
 
         public bool IsDisposed { get; private set; } = false;
 
@@ -60,7 +60,7 @@ namespace OpenTween.Connection
 
         public async Task<T> GetAsync<T>(Uri uri, IDictionary<string, string> param, string endpointName)
         {
-            var requestUri = new Uri(this.RestApiBase, uri);
+            var requestUri = new Uri(RestApiBase, uri);
 
             if (param != null)
                 requestUri = new Uri(requestUri, "?" + MyCommon.BuildQueryString(param));
@@ -102,7 +102,7 @@ namespace OpenTween.Connection
 
         public async Task<Stream> GetStreamAsync(Uri uri, IDictionary<string, string> param)
         {
-            var requestUri = new Uri(this.RestApiBase, uri);
+            var requestUri = new Uri(RestApiBase, uri);
 
             if (param != null)
                 requestUri = new Uri(requestUri, "?" + MyCommon.BuildQueryString(param));
@@ -120,7 +120,7 @@ namespace OpenTween.Connection
 
         public async Task<LazyJson<T>> PostLazyAsync<T>(Uri uri, IDictionary<string, string> param)
         {
-            var requestUri = new Uri(this.RestApiBase, uri);
+            var requestUri = new Uri(RestApiBase, uri);
             var request = new HttpRequestMessage(HttpMethod.Post, requestUri);
 
             using (var postContent = new FormUrlEncodedContent(param))
@@ -154,7 +154,7 @@ namespace OpenTween.Connection
 
         public async Task<LazyJson<T>> PostLazyAsync<T>(Uri uri, IDictionary<string, string> param, IDictionary<string, IMediaItem> media)
         {
-            var requestUri = new Uri(this.RestApiBase, uri);
+            var requestUri = new Uri(RestApiBase, uri);
             var request = new HttpRequestMessage(HttpMethod.Post, requestUri);
 
             using (var postContent = new MultipartFormDataContent())
@@ -239,7 +239,7 @@ namespace OpenTween.Connection
 
         public OAuthEchoHandler CreateOAuthEchoHandler(Uri authServiceProvider, Uri realm = null)
         {
-            var uri = new Uri(this.RestApiBase, authServiceProvider);
+            var uri = new Uri(RestApiBase, authServiceProvider);
 
             return OAuthEchoHandler.CreateHandler(Networking.CreateHttpClientHandler(), uri,
                 ApplicationSettings.TwitterConsumerKey, ApplicationSettings.TwitterConsumerSecret,

commit 17e713dae29e3076ed5edc944702691d798156a1

TwitterApiConnection.RestApiBaseの設定時に発生するエラーを修正
726a9016 で追加した RestApiBase が Uri を要求するのに対して SettingCommon.TwitterUrl は
api.twitter.com のホスト名のみを含んでいたため、Uri インスタンスを生成するためには
スキーム (https://) を追加する必要があった。

Fixes: 726a9016 ("TwitterApiConnection.RestApiBaseをstaticプロパティに変更")

OpenTween/Setting/Panel/ConnectionPanel.cs
@@ -46,14 +46,14 @@ namespace OpenTween.Setting.Panel
         {
             this.ConnectionTimeOut.Text = settingCommon.DefaultTimeOut.ToString();
             this.checkBoxForceIPv4.Checked = settingCommon.ForceIPv4;
-            this.TwitterAPIText.Text = settingCommon.TwitterUrl;
+            this.TwitterAPIText.Text = settingCommon.TwitterApiHost;
         }
 
         public void SaveConfig(SettingCommon settingCommon)
         {
             settingCommon.DefaultTimeOut = int.Parse(this.ConnectionTimeOut.Text);
             settingCommon.ForceIPv4 = this.checkBoxForceIPv4.Checked;
-            settingCommon.TwitterUrl = this.TwitterAPIText.Text.Trim();
+            settingCommon.TwitterApiHost = this.TwitterAPIText.Text.Trim();
         }
 
         private void ConnectionTimeOut_Validating(object sender, CancelEventArgs e)

OpenTween/Setting/SettingCommon.cs
@@ -202,7 +202,14 @@ namespace OpenTween
         public bool ShowGrid = false;
         public bool UseAtIdSupplement = true;
         public bool UseHashSupplement = true;
-        public string TwitterUrl = "api.twitter.com";
+
+        [XmlElement(ElementName = "TwitterUrl")]
+        public string TwitterApiHost = "api.twitter.com";
+
+        [XmlIgnore]
+        public Uri TwitterApiBaseUri
+            => new Uri($"https://{this.TwitterApiHost}/1.1/");
+
         public bool HotkeyEnabled = false;
         public Keys HotkeyModifier = Keys.None;
         public Keys HotkeyKey = Keys.None;

OpenTween/Tween.cs
@@ -776,7 +776,7 @@ namespace OpenTween
             if (this._cfgCommon.AutoShortUrlFirst < 0)
                 this._cfgCommon.AutoShortUrlFirst = MyCommon.UrlConverter.Uxnu;
 
-            TwitterApiConnection.RestApiBase = new Uri(this._cfgCommon.TwitterUrl);
+            TwitterApiConnection.RestApiBase = this._cfgCommon.TwitterApiBaseUri;
             this.tw = new Twitter(this.twitterApi);
 
             //認証関連
@@ -812,7 +812,7 @@ namespace OpenTween
                 this._cfgLocal.ProxyUser, this._cfgLocal.ProxyPassword);
             Networking.ForceIPv4 = this._cfgCommon.ForceIPv4;
 
-            TwitterApiConnection.RestApiBase = new Uri(this._cfgCommon.TwitterUrl);
+            TwitterApiConnection.RestApiBase = this._cfgCommon.TwitterApiBaseUri;
             tw.RestrictFavCheck = this._cfgCommon.RestrictFavCheck;
             tw.ReadOwnPost = this._cfgCommon.ReadOwnPost;
             tw.TrackWord = this._cfgCommon.TrackWord;
@@ -4174,7 +4174,7 @@ namespace OpenTween
                     ShortUrl.Instance.DisableExpanding = !this._cfgCommon.TinyUrlResolve;
                     ShortUrl.Instance.BitlyId = this._cfgCommon.BilyUser;
                     ShortUrl.Instance.BitlyKey = this._cfgCommon.BitlyPwd;
-                    TwitterApiConnection.RestApiBase = new Uri(this._cfgCommon.TwitterUrl);
+                    TwitterApiConnection.RestApiBase = this._cfgCommon.TwitterApiBaseUri;
 
                     Networking.DefaultTimeout = TimeSpan.FromSeconds(this._cfgCommon.DefaultTimeOut);
                     Networking.SetWebProxy(this._cfgLocal.ProxyType,