opentween/OpenTween

commit f0d946bc3e680e01d153786836c46e232d8139c5

発言詳細欄をTweetDetailsViewクラスに分離

OpenTween/TweetDetailsView.cs
@@ -1,0 +1,46 @@
+﻿// OpenTween - Client of Twitter
+// Copyright (c) 2007-2011 kiri_feather (@kiri_feather) <kiri.feather@gmail.com>
+//           (c) 2008-2011 Moz (@syo68k)
+//           (c) 2008-2011 takeshik (@takeshik) <http://www.takeshik.org/>
+//           (c) 2010-2011 anis774 (@anis774) <http://d.hatena.ne.jp/anis774/>
+//           (c) 2010-2011 fantasticswallow (@f_swallow) <http://twitter.com/f_swallow>
+//           (c) 2011      kim_upsilon (@kim_upsilon) <https://upsilo.net/~upsilon/>
+// All rights reserved.
+//
+// This file is part of OpenTween.
+//
+// This program is free software; you can redistribute it and/or modify it
+// under the terms of the GNU General public License as published by the Free
+// Software Foundation; either version 3 of the License, or (at your option)
+// any later version.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General public License
+// for more details.
+//
+// You should have received a copy of the GNU General public License along
+// with this program. If not, see <http://www.gnu.org/licenses/>, or write to
+// the Free Software Foundation, Inc., 51 Franklin Street - Fifth Floor,
+// Boston, MA 02110-1301, USA.
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OpenTween
+{
+    public partial class TweetDetailsView : UserControl
+    {
+        public TweetDetailsView()
+        {
+            this.InitializeComponent();
+        }
+    }
+}

OpenTween/OpenTween.csproj
@@ -168,6 +168,12 @@
     </Compile>
     <Compile Include="ShortcutCommand.cs" />
     <Compile Include="Thumbnail\Services\PbsTwimgCom.cs" />
+    <Compile Include="TweetDetailsView.cs">
+      <SubType>UserControl</SubType>
+    </Compile>
+    <Compile Include="TweetDetailsView.Designer.cs">
+      <DependentUpon>TweetDetailsView.cs</DependentUpon>
+    </Compile>
     <Compile Include="TweetExtractor.cs" />
     <Compile Include="WaitingDialog.cs">
       <SubType>Form</SubType>
@@ -527,6 +533,17 @@
     </EmbeddedResource>
     <EmbeddedResource Include="SendErrorReportForm.resx">
       <DependentUpon>SendErrorReportForm.cs</DependentUpon>
+    </EmbeddedResource>
+    <EmbeddedResource Include="TweetDetailsView.en.resx">
+      <DependentUpon>TweetDetailsView.cs</DependentUpon>
+      <SubType>Designer</SubType>
+    </EmbeddedResource>
+    <EmbeddedResource Include="TweetDetailsView.resx">
+      <DependentUpon>TweetDetailsView.cs</DependentUpon>
+    </EmbeddedResource>
+    <EmbeddedResource Include="TweetDetailsView.zh-CHT.resx">
+      <DependentUpon>TweetDetailsView.cs</DependentUpon>
+      <SubType>Designer</SubType>
     </EmbeddedResource>
     <EmbeddedResource Include="WaitingDialog.resx">
       <DependentUpon>WaitingDialog.cs</DependentUpon>

OpenTween/Tween.en.resx
@@ -304,264 +304,6 @@
   <data name="SplitContainer1.Panel1.ToolTip" xml:space="preserve">
     <value />
   </data>
-  <data name="TableLayoutPanel1.AccessibleName" xml:space="preserve">
-    <value>PostDetailsArea</value>
-  </data>
-  <data name="UserPicture.AccessibleName" xml:space="preserve">
-    <value>ProfileImage</value>
-  </data>
-  <data name="FollowToolStripMenuItem.Size" type="System.Drawing.Size, System.Drawing">
-    <value>286, 24</value>
-  </data>
-  <data name="FollowToolStripMenuItem.Text" xml:space="preserve">
-    <value>&amp;Follow</value>
-  </data>
-  <data name="UnFollowToolStripMenuItem.Size" type="System.Drawing.Size, System.Drawing">
-    <value>286, 24</value>
-  </data>
-  <data name="UnFollowToolStripMenuItem.Text" xml:space="preserve">
-    <value>&amp;Unfollow</value>
-  </data>
-  <data name="ShowFriendShipToolStripMenuItem.Size" type="System.Drawing.Size, System.Drawing">
-    <value>286, 24</value>
-  </data>
-  <data name="ShowFriendShipToolStripMenuItem.Text" xml:space="preserve">
-    <value>Friendship &amp;Relation</value>
-  </data>
-  <data name="ListManageUserContextToolStripMenuItem3.Size" type="System.Drawing.Size, System.Drawing">
-    <value>286, 24</value>
-  </data>
-  <data name="ListManageUserContextToolStripMenuItem3.Text" xml:space="preserve">
-    <value>Add to &amp;Lists</value>
-  </data>
-  <data name="ToolStripSeparator37.Size" type="System.Drawing.Size, System.Drawing">
-    <value>283, 6</value>
-  </data>
-  <data name="ShowUserStatusToolStripMenuItem.Size" type="System.Drawing.Size, System.Drawing">
-    <value>286, 24</value>
-  </data>
-  <data name="ShowUserStatusToolStripMenuItem.Text" xml:space="preserve">
-    <value>User &amp;Profile</value>
-  </data>
-  <data name="SearchPostsDetailNameToolStripMenuItem.Size" type="System.Drawing.Size, System.Drawing">
-    <value>286, 24</value>
-  </data>
-  <data name="SearchPostsDetailNameToolStripMenuItem.Text" xml:space="preserve">
-    <value>User Timeline</value>
-  </data>
-  <data name="SearchAtPostsDetailNameToolStripMenuItem.Size" type="System.Drawing.Size, System.Drawing">
-    <value>286, 24</value>
-  </data>
-  <data name="SearchAtPostsDetailNameToolStripMenuItem.Text" xml:space="preserve">
-    <value>Search &amp;Mentions to this user</value>
-  </data>
-  <data name="ToolStripMenuItem1.Size" type="System.Drawing.Size, System.Drawing">
-    <value>283, 6</value>
-  </data>
-  <data name="IconNameToolStripMenuItem.Size" type="System.Drawing.Size, System.Drawing">
-    <value>286, 24</value>
-  </data>
-  <data name="ReloadIconToolStripMenuItem.Text" xml:space="preserve">
-    <value>&amp;Reload Icon</value>
-  </data>
-  <data name="SaveIconPictureToolStripMenuItem.Size" type="System.Drawing.Size, System.Drawing">
-    <value>286, 24</value>
-  </data>
-  <data name="SaveIconPictureToolStripMenuItem.Text" xml:space="preserve">
-    <value>Save &amp;Icon...</value>
-  </data>
-  <data name="ContextMenuUserPicture.Size" type="System.Drawing.Size, System.Drawing">
-    <value>287, 232</value>
-  </data>
-  <data name="ContextMenuUserPicture.ToolTip" xml:space="preserve">
-    <value />
-  </data>
-  <data name="UserPicture.ToolTip" xml:space="preserve">
-    <value />
-  </data>
-  <data name="NameLabel.Size" type="System.Drawing.Size, System.Drawing">
-    <value>250, 17</value>
-  </data>
-  <data name="NameLabel.ToolTip" xml:space="preserve">
-    <value />
-  </data>
-  <data name="PostBrowser.AccessibleName" xml:space="preserve">
-    <value>PostBody</value>
-  </data>
-  <data name="SearchGoogleContextMenuItem.Size" type="System.Drawing.Size, System.Drawing">
-    <value>216, 24</value>
-  </data>
-  <data name="SearchWikipediaContextMenuItem.Size" type="System.Drawing.Size, System.Drawing">
-    <value>216, 24</value>
-  </data>
-  <data name="SearchPublicSearchContextMenuItem.Size" type="System.Drawing.Size, System.Drawing">
-    <value>216, 24</value>
-  </data>
-  <data name="CurrentTabToolStripMenuItem.Size" type="System.Drawing.Size, System.Drawing">
-    <value>216, 24</value>
-  </data>
-  <data name="CurrentTabToolStripMenuItem.Text" xml:space="preserve">
-    <value>Current Tab(&amp;Local)</value>
-  </data>
-  <data name="SelectionSearchContextMenuItem.Size" type="System.Drawing.Size, System.Drawing">
-    <value>313, 24</value>
-  </data>
-  <data name="SelectionSearchContextMenuItem.Text" xml:space="preserve">
-    <value>&amp;Search selecting words</value>
-  </data>
-  <data name="ToolStripSeparator13.Size" type="System.Drawing.Size, System.Drawing">
-    <value>310, 6</value>
-  </data>
-  <data name="SelectionCopyContextMenuItem.Size" type="System.Drawing.Size, System.Drawing">
-    <value>313, 24</value>
-  </data>
-  <data name="SelectionCopyContextMenuItem.Text" xml:space="preserve">
-    <value>&amp;Copy</value>
-  </data>
-  <data name="UrlCopyContextMenuItem.Size" type="System.Drawing.Size, System.Drawing">
-    <value>313, 24</value>
-  </data>
-  <data name="UrlCopyContextMenuItem.Text" xml:space="preserve">
-    <value>Copy &amp;URL</value>
-  </data>
-  <data name="SelectionAllContextMenuItem.Size" type="System.Drawing.Size, System.Drawing">
-    <value>313, 24</value>
-  </data>
-  <data name="SelectionAllContextMenuItem.Text" xml:space="preserve">
-    <value>Select &amp;All</value>
-  </data>
-  <data name="ToolStripSeparator5.Size" type="System.Drawing.Size, System.Drawing">
-    <value>310, 6</value>
-  </data>
-  <data name="FollowContextMenuItem.Size" type="System.Drawing.Size, System.Drawing">
-    <value>313, 24</value>
-  </data>
-  <data name="FollowContextMenuItem.Text" xml:space="preserve">
-    <value>&amp;Follow</value>
-  </data>
-  <data name="RemoveContextMenuItem.Size" type="System.Drawing.Size, System.Drawing">
-    <value>313, 24</value>
-  </data>
-  <data name="RemoveContextMenuItem.Text" xml:space="preserve">
-    <value>&amp;Unfollow</value>
-  </data>
-  <data name="FriendshipContextMenuItem.Size" type="System.Drawing.Size, System.Drawing">
-    <value>313, 24</value>
-  </data>
-  <data name="FriendshipContextMenuItem.Text" xml:space="preserve">
-    <value>Friendship &amp;Relation</value>
-  </data>
-  <data name="FriendshipAllMenuItem.Size" type="System.Drawing.Size, System.Drawing">
-    <value>313, 24</value>
-  </data>
-  <data name="FriendshipAllMenuItem.Text" xml:space="preserve">
-    <value>&amp;All User Friendship in this Tweet</value>
-  </data>
-  <data name="ToolStripSeparator36.Size" type="System.Drawing.Size, System.Drawing">
-    <value>310, 6</value>
-  </data>
-  <data name="ShowUserStatusContextMenuItem.Size" type="System.Drawing.Size, System.Drawing">
-    <value>313, 24</value>
-  </data>
-  <data name="ShowUserStatusContextMenuItem.Text" xml:space="preserve">
-    <value>User &amp;Profile</value>
-  </data>
-  <data name="SearchPostsDetailToolStripMenuItem.Size" type="System.Drawing.Size, System.Drawing">
-    <value>313, 24</value>
-  </data>
-  <data name="SearchPostsDetailToolStripMenuItem.Text" xml:space="preserve">
-    <value>User Timeline(&amp;F)</value>
-  </data>
-  <data name="SearchAtPostsDetailToolStripMenuItem.Size" type="System.Drawing.Size, System.Drawing">
-    <value>313, 24</value>
-  </data>
-  <data name="SearchAtPostsDetailToolStripMenuItem.Text" xml:space="preserve">
-    <value>Search &amp;Mentions to this user</value>
-  </data>
-  <data name="ToolStripSeparator32.Size" type="System.Drawing.Size, System.Drawing">
-    <value>310, 6</value>
-  </data>
-  <data name="IdFilterAddMenuItem.Size" type="System.Drawing.Size, System.Drawing">
-    <value>313, 24</value>
-  </data>
-  <data name="IdFilterAddMenuItem.Text" xml:space="preserve">
-    <value>Filter by &amp;Id ...</value>
-  </data>
-  <data name="ListManageUserContextToolStripMenuItem.Size" type="System.Drawing.Size, System.Drawing">
-    <value>313, 24</value>
-  </data>
-  <data name="ListManageUserContextToolStripMenuItem.Text" xml:space="preserve">
-    <value>Add to &amp;Lists</value>
-  </data>
-  <data name="ToolStripSeparator33.Size" type="System.Drawing.Size, System.Drawing">
-    <value>310, 6</value>
-  </data>
-  <data name="UseHashtagMenuItem.Size" type="System.Drawing.Size, System.Drawing">
-    <value>313, 24</value>
-  </data>
-  <data name="UseHashtagMenuItem.Text" xml:space="preserve">
-    <value>Use this &amp;Hashtag</value>
-  </data>
-  <data name="SelectionTranslationToolStripMenuItem.Size" type="System.Drawing.Size, System.Drawing">
-    <value>313, 24</value>
-  </data>
-  <data name="SelectionTranslationToolStripMenuItem.Text" xml:space="preserve">
-    <value>&amp;Translate</value>
-  </data>
-  <data name="TranslationToolStripMenuItem.Size" type="System.Drawing.Size, System.Drawing">
-    <value>313, 24</value>
-  </data>
-  <data name="TranslationToolStripMenuItem.Text" xml:space="preserve">
-    <value>Translate</value>
-  </data>
-  <data name="ContextMenuPostBrowser.Size" type="System.Drawing.Size, System.Drawing">
-    <value>314, 418</value>
-  </data>
-  <data name="ContextMenuPostBrowser.ToolTip" xml:space="preserve">
-    <value />
-  </data>
-  <data name="PostBrowser.Size" type="System.Drawing.Size, System.Drawing">
-    <value>492, 112</value>
-  </data>
-  <data name="PostBrowser.ToolTip" xml:space="preserve">
-    <value />
-  </data>
-  <data name="DateTimeLabel.Location" type="System.Drawing.Point, System.Drawing">
-    <value>436, 4</value>
-  </data>
-  <data name="DateTimeLabel.ToolTip" xml:space="preserve">
-    <value />
-  </data>
-  <data name="SourceCopyMenuItem.Size" type="System.Drawing.Size, System.Drawing">
-    <value>204, 24</value>
-  </data>
-  <data name="SourceCopyMenuItem.Text" xml:space="preserve">
-    <value>Copy &amp;Source</value>
-  </data>
-  <data name="SourceUrlCopyMenuItem.Size" type="System.Drawing.Size, System.Drawing">
-    <value>204, 24</value>
-  </data>
-  <data name="SourceUrlCopyMenuItem.Text" xml:space="preserve">
-    <value>Copy Source &amp;URL</value>
-  </data>
-  <data name="ContextMenuSource.Size" type="System.Drawing.Size, System.Drawing">
-    <value>205, 52</value>
-  </data>
-  <data name="ContextMenuSource.ToolTip" xml:space="preserve">
-    <value />
-  </data>
-  <data name="SourceLinkLabel.Location" type="System.Drawing.Point, System.Drawing">
-    <value>492, 4</value>
-  </data>
-  <data name="SourceLinkLabel.ToolTip" xml:space="preserve">
-    <value />
-  </data>
-  <data name="TableLayoutPanel1.Size" type="System.Drawing.Size, System.Drawing">
-    <value>570, 141</value>
-  </data>
-  <data name="TableLayoutPanel1.ToolTip" xml:space="preserve">
-    <value />
-  </data>
   <data name="SplitContainer2.Panel1.ToolTip" xml:space="preserve">
     <value />
   </data>

OpenTween/Tween.zh-CHS.resx
@@ -241,161 +241,8 @@
   <data name="ContextMenuTabProperty.Size" type="System.Drawing.Size, System.Drawing">
     <value>186, 207</value>
   </data>
-  <data name="FollowToolStripMenuItem.Size" type="System.Drawing.Size, System.Drawing">
-    <value>170, 22</value>
-  </data>
-  <data name="FollowToolStripMenuItem.Text" xml:space="preserve">
-    <value>关注</value>
-  </data>
-  <data name="UnFollowToolStripMenuItem.Size" type="System.Drawing.Size, System.Drawing">
-    <value>170, 22</value>
-  </data>
-  <data name="UnFollowToolStripMenuItem.Text" xml:space="preserve">
-    <value>取消关注</value>
-  </data>
-  <data name="ShowFriendShipToolStripMenuItem.Size" type="System.Drawing.Size, System.Drawing">
-    <value>170, 22</value>
-  </data>
-  <data name="ShowFriendShipToolStripMenuItem.Text" xml:space="preserve">
-    <value>显示相互关注状态</value>
-  </data>
-  <data name="ListManageUserContextToolStripMenuItem3.Size" type="System.Drawing.Size, System.Drawing">
-    <value>170, 22</value>
-  </data>
-  <data name="ListManageUserContextToolStripMenuItem3.Text" xml:space="preserve">
-    <value>列表管理(&amp;L)</value>
-  </data>
-  <data name="ToolStripSeparator37.Size" type="System.Drawing.Size, System.Drawing">
-    <value>167, 6</value>
-  </data>
-  <data name="ShowUserStatusToolStripMenuItem.Size" type="System.Drawing.Size, System.Drawing">
-    <value>170, 22</value>
-  </data>
-  <data name="ShowUserStatusToolStripMenuItem.Text" xml:space="preserve">
-    <value>显示用户信息</value>
-  </data>
-  <data name="SearchPostsDetailNameToolStripMenuItem.Size" type="System.Drawing.Size, System.Drawing">
-    <value>170, 22</value>
-  </data>
-  <data name="SearchPostsDetailNameToolStripMenuItem.Text" xml:space="preserve">
-    <value>搜索该用户的消息</value>
-  </data>
-  <data name="ToolStripMenuItem1.Size" type="System.Drawing.Size, System.Drawing">
-    <value>167, 6</value>
-  </data>
-  <data name="IconNameToolStripMenuItem.Size" type="System.Drawing.Size, System.Drawing">
-    <value>170, 22</value>
-  </data>
-  <data name="SaveIconPictureToolStripMenuItem.Size" type="System.Drawing.Size, System.Drawing">
-    <value>170, 22</value>
-  </data>
-  <data name="ContextMenuUserPicture.Size" type="System.Drawing.Size, System.Drawing">
-    <value>171, 192</value>
-  </data>
-  <data name="NameLabel.Font" type="System.Drawing.Font, System.Drawing">
-    <value>宋体, 9pt, style=Bold</value>
-  </data>
   <data name="SearchYatsContextMenuItem.Text" xml:space="preserve">
     <value>Twitter(&amp;Y)</value>
-  </data>
-  <data name="CurrentTabToolStripMenuItem.Text" xml:space="preserve">
-    <value>当前标签(&amp;L)</value>
-  </data>
-  <data name="SelectionSearchContextMenuItem.Size" type="System.Drawing.Size, System.Drawing">
-    <value>209, 22</value>
-  </data>
-  <data name="SelectionSearchContextMenuItem.Text" xml:space="preserve">
-    <value>搜索选中文本(&amp;S)</value>
-  </data>
-  <data name="ToolStripSeparator13.Size" type="System.Drawing.Size, System.Drawing">
-    <value>206, 6</value>
-  </data>
-  <data name="SelectionCopyContextMenuItem.Size" type="System.Drawing.Size, System.Drawing">
-    <value>209, 22</value>
-  </data>
-  <data name="SelectionCopyContextMenuItem.Text" xml:space="preserve">
-    <value>复制选中文本(&amp;C)</value>
-  </data>
-  <data name="UrlCopyContextMenuItem.Size" type="System.Drawing.Size, System.Drawing">
-    <value>209, 22</value>
-  </data>
-  <data name="UrlCopyContextMenuItem.Text" xml:space="preserve">
-    <value>复制网址(&amp;U)</value>
-  </data>
-  <data name="SelectionAllContextMenuItem.Size" type="System.Drawing.Size, System.Drawing">
-    <value>209, 22</value>
-  </data>
-  <data name="SelectionAllContextMenuItem.Text" xml:space="preserve">
-    <value>选择全部(&amp;A)</value>
-  </data>
-  <data name="ToolStripSeparator5.Size" type="System.Drawing.Size, System.Drawing">
-    <value>206, 6</value>
-  </data>
-  <data name="FollowContextMenuItem.Size" type="System.Drawing.Size, System.Drawing">
-    <value>209, 22</value>
-  </data>
-  <data name="FollowContextMenuItem.Text" xml:space="preserve">
-    <value>关注(&amp;F)</value>
-  </data>
-  <data name="RemoveContextMenuItem.Size" type="System.Drawing.Size, System.Drawing">
-    <value>209, 22</value>
-  </data>
-  <data name="RemoveContextMenuItem.Text" xml:space="preserve">
-    <value>取消关注(&amp;N)</value>
-  </data>
-  <data name="FriendshipContextMenuItem.Size" type="System.Drawing.Size, System.Drawing">
-    <value>209, 22</value>
-  </data>
-  <data name="FriendshipContextMenuItem.Text" xml:space="preserve">
-    <value>显示相互关注状态(&amp;R)</value>
-  </data>
-  <data name="FriendshipAllMenuItem.Size" type="System.Drawing.Size, System.Drawing">
-    <value>209, 22</value>
-  </data>
-  <data name="FriendshipAllMenuItem.Text" xml:space="preserve">
-    <value>显示所有人的关注状态(&amp;A)</value>
-  </data>
-  <data name="ToolStripSeparator36.Size" type="System.Drawing.Size, System.Drawing">
-    <value>206, 6</value>
-  </data>
-  <data name="ShowUserStatusContextMenuItem.Size" type="System.Drawing.Size, System.Drawing">
-    <value>209, 22</value>
-  </data>
-  <data name="ShowUserStatusContextMenuItem.Text" xml:space="preserve">
-    <value>显示用户信息(&amp;P)</value>
-  </data>
-  <data name="SearchPostsDetailToolStripMenuItem.Size" type="System.Drawing.Size, System.Drawing">
-    <value>209, 22</value>
-  </data>
-  <data name="SearchPostsDetailToolStripMenuItem.Text" xml:space="preserve">
-    <value>搜索该用户的消息</value>
-  </data>
-  <data name="ToolStripSeparator32.Size" type="System.Drawing.Size, System.Drawing">
-    <value>206, 6</value>
-  </data>
-  <data name="IdFilterAddMenuItem.Size" type="System.Drawing.Size, System.Drawing">
-    <value>209, 22</value>
-  </data>
-  <data name="IdFilterAddMenuItem.Text" xml:space="preserve">
-    <value>新建ID过滤规则(&amp;I)</value>
-  </data>
-  <data name="ListManageUserContextToolStripMenuItem.Size" type="System.Drawing.Size, System.Drawing">
-    <value>209, 22</value>
-  </data>
-  <data name="ListManageUserContextToolStripMenuItem.Text" xml:space="preserve">
-    <value>列表管理(&amp;L)</value>
-  </data>
-  <data name="ToolStripSeparator33.Size" type="System.Drawing.Size, System.Drawing">
-    <value>206, 6</value>
-  </data>
-  <data name="UseHashtagMenuItem.Size" type="System.Drawing.Size, System.Drawing">
-    <value>209, 22</value>
-  </data>
-  <data name="UseHashtagMenuItem.Text" xml:space="preserve">
-    <value>固定Hash Tag(&amp;H)</value>
-  </data>
-  <data name="ContextMenuPostBrowser.Size" type="System.Drawing.Size, System.Drawing">
-    <value>210, 320</value>
   </data>
   <data name="PostButton.Text" xml:space="preserve">
     <value>发送</value>

commit 1ab8f24610036147c7afe29e1d92570e5b57737c

TweetDetailsView.TabStopをデフォルトでfalseとなるように変更
Fixes: f0d946bc ("発言詳細欄をTweetDetailsViewクラスに分離")

OpenTween/TweetDetailsView.cs
@@ -55,6 +55,13 @@ namespace OpenTween
         /// <summary>現在表示中の発言</summary>
         public PostClass CurrentPost { get; private set; }
 
+        [DefaultValue(false)]
+        public new bool TabStop
+        {
+            get { return base.TabStop; }
+            set { base.TabStop = value; }
+        }
+
         /// <summary>ステータスバーに表示するテキストの変化を通知するイベント</summary>
         public event EventHandler<TweetDetailsViewStatusChengedEventArgs> StatusChanged;
 
@@ -64,6 +71,8 @@ namespace OpenTween
         public TweetDetailsView()
         {
             this.InitializeComponent();
+
+            this.TabStop = false;
 
             //発言詳細部の初期化
             NameLabel.Text = "";