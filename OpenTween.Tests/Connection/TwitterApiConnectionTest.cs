opentween/OpenTween

commit 4359037e04e28b63184eb9650bc0a1284150ab24

IApiConnectionにmultipart/form-dataを送信するためのメソッドを追加

OpenTween/Connection/TwitterApiConnection.cs
@@ -125,6 +125,46 @@ namespace OpenTween.Connection
             }
         }
 
+        public async Task<LazyJson<T>> PostLazyAsync<T>(Uri uri, IDictionary<string, string> param, IDictionary<string, IMediaItem> media)
+        {
+            var requestUri = new Uri(this.RestApiBase, uri);
+            var request = new HttpRequestMessage(HttpMethod.Post, requestUri);
+
+            using (var postContent = new MultipartFormDataContent())
+            {
+                foreach (var kv in param)
+                    postContent.Add(new StringContent(kv.Value), kv.Key);
+
+                foreach (var kv in media)
+                    postContent.Add(new StreamContent(kv.Value.OpenRead()), kv.Key);
+
+                request.Content = postContent;
+
+                HttpResponseMessage response = null;
+                try
+                {
+                    response = await this.http.SendAsync(request, HttpCompletionOption.ResponseHeadersRead)
+                        .ConfigureAwait(false);
+
+                    await this.CheckStatusCode(response)
+                        .ConfigureAwait(false);
+
+                    var result = new LazyJson<T>(response);
+                    response = null;
+
+                    return result;
+                }
+                catch (OperationCanceledException ex)
+                {
+                    throw TwitterApiException.CreateFromException(ex);
+                }
+                finally
+                {
+                    response?.Dispose();
+                }
+            }
+        }
+
         protected async Task CheckStatusCode(HttpResponseMessage response)
         {
             var statusCode = response.StatusCode;

OpenTween.Tests/Connection/TwitterApiConnectionTest.cs
@@ -24,6 +24,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Text;
@@ -196,6 +197,64 @@ namespace OpenTween.Connection
                     .ConfigureAwait(false));
 
                 Assert.Equal(0, mockHandler.QueueCount);
+            }
+        }
+
+        [Fact]
+        public async Task PostLazyAsync_MultipartTest()
+        {
+            using (var mockHandler = new HttpMessageHandlerMock())
+            using (var http = new HttpClient(mockHandler))
+            using (var apiConnection = new TwitterApiConnection("", ""))
+            {
+                apiConnection.http = http;
+
+                using (var image = TestUtils.CreateDummyImage())
+                using (var media = new MemoryImageMediaItem(image))
+                {
+                    mockHandler.Enqueue(async x =>
+                    {
+                        Assert.Equal(HttpMethod.Post, x.Method);
+                        Assert.Equal("https://api.twitter.com/1.1/hoge/tetete.json",
+                            x.RequestUri.AbsoluteUri);
+
+                        Assert.IsType<MultipartFormDataContent>(x.Content);
+
+                        var boundary = x.Content.Headers.ContentType.Parameters.Cast<NameValueHeaderValue>()
+                            .First(y => y.Name == "boundary").Value;
+
+                        // 前後のダブルクオーテーションを除去
+                        boundary = boundary.Substring(1, boundary.Length - 2);
+
+                        var body = await x.Content.ReadAsStringAsync()
+                            .ConfigureAwait(false);
+                        Assert.True(body.StartsWith("--" + boundary + "\r\n", StringComparison.Ordinal));
+
+                        return new HttpResponseMessage(HttpStatusCode.OK)
+                        {
+                            Content = new StringContent("\"hogehoge\""),
+                        };
+                    });
+
+                    var endpoint = new Uri("hoge/tetete.json", UriKind.Relative);
+                    var param = new Dictionary<string, string>
+                    {
+                        ["aaaa"] = "1111",
+                        ["bbbb"] = "2222",
+                    };
+                    var mediaParam = new Dictionary<string, IMediaItem>
+                    {
+                        ["media1"] = media,
+                    };
+
+                    var result = await apiConnection.PostLazyAsync<string>(endpoint, param, mediaParam)
+                        .ConfigureAwait(false);
+
+                    Assert.Equal("hogehoge", await result.LoadJsonAsync()
+                        .ConfigureAwait(false));
+
+                    Assert.Equal(0, mockHandler.QueueCount);
+                }
             }
         }
     }

OpenTween/Connection/IApiConnection.cs
@@ -32,5 +32,7 @@ namespace OpenTween.Connection
         Task<T> GetAsync<T>(Uri uri, IDictionary<string, string> param);
 
         Task<LazyJson<T>> PostLazyAsync<T>(Uri uri, IDictionary<string, string> param);
+
+        Task<LazyJson<T>> PostLazyAsync<T>(Uri uri, IDictionary<string, string> param, IDictionary<string, IMediaItem> media);
     }
 }

commit 4147cb79222f60ad4544bc594a4e62e1a6a92230

PostLazyAsync(uri, param, media) のparam, mediaがnullの場合に例外が発生する不具合を修正
Fixes: 4359037e ("IApiConnectionにmultipart/form-dataを送信するためのメソッドを追加")

OpenTween/Connection/TwitterApiConnection.cs
@@ -175,11 +175,16 @@ namespace OpenTween.Connection
 
             using (var postContent = new MultipartFormDataContent())
             {
-                foreach (var kv in param)
-                    postContent.Add(new StringContent(kv.Value), kv.Key);
-
-                foreach (var kv in media)
-                    postContent.Add(new StreamContent(kv.Value.OpenRead()), kv.Key, kv.Value.Name);
+                if (param != null)
+                {
+                    foreach (var kv in param)
+                        postContent.Add(new StringContent(kv.Value), kv.Key);
+                }
+                if (media != null)
+                {
+                    foreach (var kv in media)
+                        postContent.Add(new StreamContent(kv.Value.OpenRead()), kv.Key, kv.Value.Name);
+                }
 
                 request.Content = postContent;
 

OpenTween.Tests/Connection/TwitterApiConnectionTest.cs
@@ -407,5 +407,54 @@ namespace OpenTween.Connection
                 }
             }
         }
+
+        [Fact]
+        public async Task PostLazyAsync_Multipart_NullTest()
+        {
+            using (var mockHandler = new HttpMessageHandlerMock())
+            using (var http = new HttpClient(mockHandler))
+            using (var apiConnection = new TwitterApiConnection("", ""))
+            {
+                apiConnection.http = http;
+
+                mockHandler.Enqueue(async x =>
+                {
+                    Assert.Equal(HttpMethod.Post, x.Method);
+                    Assert.Equal("https://api.twitter.com/1.1/hoge/tetete.json",
+                        x.RequestUri.AbsoluteUri);
+
+                    Assert.IsType<MultipartFormDataContent>(x.Content);
+
+                    var boundary = x.Content.Headers.ContentType.Parameters.Cast<NameValueHeaderValue>()
+                        .First(y => y.Name == "boundary").Value;
+
+                    // 前後のダブルクオーテーションを除去
+                    boundary = boundary.Substring(1, boundary.Length - 2);
+
+                    var expectedText =
+                        $"--{boundary}\r\n" +
+                        $"\r\n--{boundary}--\r\n";
+
+                    var expected = Encoding.UTF8.GetBytes(expectedText);
+
+                    Assert.Equal(expected, await x.Content.ReadAsByteArrayAsync().ConfigureAwait(false));
+
+                    return new HttpResponseMessage(HttpStatusCode.OK)
+                    {
+                        Content = new StringContent("\"hogehoge\""),
+                    };
+                });
+
+                var endpoint = new Uri("hoge/tetete.json", UriKind.Relative);
+
+                var result = await apiConnection.PostLazyAsync<string>(endpoint, param: null, media: null)
+                    .ConfigureAwait(false);
+
+                Assert.Equal("hogehoge", await result.LoadJsonAsync()
+                    .ConfigureAwait(false));
+
+                Assert.Equal(0, mockHandler.QueueCount);
+            }
+        }
     }
 }