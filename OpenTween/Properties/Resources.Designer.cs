opentween/OpenTween

commit 25f0899859945d9205951c8ea32c7a1dab0b2107

TweenMain内の各コントロールのAccessibleNameに適切な名前をセット
デフォルトの AccessibleName に何がセットされるかはタブオーダー順も影響するため TabIndex の順序も修正

OpenTween/Properties/Resources.Designer.cs
@@ -61,6 +61,15 @@ namespace OpenTween.Properties {
         }
         
         /// <summary>
+        ///   発言一覧 に類似しているローカライズされた文字列を検索します。
+        /// </summary>
+        internal static string AddNewTab_ListView_AccessibleName {
+            get {
+                return ResourceManager.GetString("AddNewTab_ListView_AccessibleName", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   (新規タブ) に類似しているローカライズされた文字列を検索します。
         /// </summary>
         internal static string AddNewTabText1 {

OpenTween/Properties/Resources.en.resx
@@ -1134,4 +1134,7 @@ Available service: {1}</value>
   <data name="ImageAltText" xml:space="preserve">
     <value>[Image: {0}]</value>
   </data>
+  <data name="AddNewTab_ListView_AccessibleName" xml:space="preserve">
+    <value>PostsList</value>
+  </data>
 </root>

OpenTween/Properties/Resources.resx
@@ -1192,4 +1192,7 @@
   <data name="ImageAltText" xml:space="preserve">
     <value>[画像: {0}]</value>
   </data>
+  <data name="AddNewTab_ListView_AccessibleName" xml:space="preserve">
+    <value>発言一覧</value>
+  </data>
 </root>

OpenTween/Tween.cs
@@ -1011,6 +1011,11 @@ namespace OpenTween
             CopyURLMenuItem.ShortcutKeyDisplayString = "Ctrl+Shift+C";
             CopyUserIdStripMenuItem.ShortcutKeyDisplayString = "Shift+Alt+C";
 
+            // SourceLinkLabel のテキストが SplitContainer2.Panel2.AccessibleName にセットされるのを防ぐ
+            // （タブオーダー順で SourceLinkLabel の次にある PostBrowser が TabStop = false となっているため、
+            // さらに次のコントロールである SplitContainer2.Panel2 の AccessibleName がデフォルトで SourceLinkLabel のテキストになってしまう)
+            this.SplitContainer2.Panel2.AccessibleName = "";
+
             ////////////////////////////////////////////////////////////////////////////////
             var sortOrder = (SortOrder)_cfgCommon.SortOrder;
             var mode = ComparerMode.Id;
@@ -4617,6 +4622,7 @@ namespace OpenTween
                     var label = new Label();
                     label.Dock = DockStyle.Top;
                     label.Name = "labelUser";
+                    label.TabIndex = 0;
                     if (tabType == MyCommon.TabUsageType.Lists)
                     {
                         label.Text = listInfo.ToString();
@@ -4649,6 +4655,7 @@ namespace OpenTween
                         pnl.Controls.Add(btn);
                         pnl.Controls.Add(lbl);
                         pnl.Name = "panelSearch";
+                        pnl.TabIndex = 0;
                         pnl.Dock = DockStyle.Top;
                         pnl.Height = cmb.Height;
                         pnl.Enter += SearchControls_Enter;
@@ -4661,6 +4668,7 @@ namespace OpenTween
                         cmb.DropDownStyle = ComboBoxStyle.DropDown;
                         cmb.ImeMode = ImeMode.NoControl;
                         cmb.TabStop = false;
+                        cmb.TabIndex = 1;
                         cmb.AutoCompleteMode = AutoCompleteMode.None;
                         cmb.KeyDown += SearchComboBox_KeyDown;
 
@@ -4671,6 +4679,7 @@ namespace OpenTween
                         cmbLang.Name = "comboLang";
                         cmbLang.DropDownStyle = ComboBoxStyle.DropDownList;
                         cmbLang.TabStop = false;
+                        cmbLang.TabIndex = 2;
                         cmbLang.Items.Add("");
                         cmbLang.Items.Add("ja");
                         cmbLang.Items.Add("en");
@@ -4698,12 +4707,14 @@ namespace OpenTween
                         lbl.Width = 90;
                         lbl.Height = cmb.Height;
                         lbl.TextAlign = ContentAlignment.MiddleLeft;
+                        lbl.TabIndex = 0;
 
                         btn.Text = "Search";
                         btn.Name = "buttonSearch";
                         btn.UseVisualStyleBackColor = true;
                         btn.Dock = DockStyle.Right;
                         btn.TabStop = false;
+                        btn.TabIndex = 3;
                         btn.Click += SearchButton_Click;
 
                         TabClass tab;
@@ -4733,6 +4744,8 @@ namespace OpenTween
                 _tabPage.UseVisualStyleBackColor = true;
                 _tabPage.AccessibleRole = AccessibleRole.PageTab;
 
+                _listCustom.AccessibleName = Properties.Resources.AddNewTab_ListView_AccessibleName;
+                _listCustom.TabIndex = 1;
                 _listCustom.AllowColumnReorder = true;
                 _listCustom.ContextMenuStrip = this.ContextMenuOperate;
                 _listCustom.ColumnHeaderContextMenuStrip = this.ContextMenuColumnHeader;

OpenTween/Tween.resx
@@ -295,6 +295,9 @@
   <data name="SplitContainer1.Orientation" type="System.Windows.Forms.Orientation, System.Windows.Forms">
     <value>Horizontal</value>
   </data>
+  <data name="TimelinePanel.AccessibleName" xml:space="preserve">
+    <value>発言一覧表示部</value>
+  </data>
   <data name="ListTab.Alignment" type="System.Windows.Forms.TabAlignment, System.Windows.Forms">
     <value>Bottom</value>
   </data>
@@ -520,8 +523,14 @@
   <data name="SplitContainer2.Orientation" type="System.Windows.Forms.Orientation, System.Windows.Forms">
     <value>Horizontal</value>
   </data>
+  <data name="TableLayoutPanel1.AccessibleName" xml:space="preserve">
+    <value>発言詳細部</value>
+  </data>
   <data name="TableLayoutPanel1.ColumnCount" type="System.Int32, mscorlib">
     <value>4</value>
+  </data>
+  <data name="UserPicture.AccessibleName" xml:space="preserve">
+    <value>プロフィール画像</value>
   </data>
   <metadata name="ContextMenuUserPicture.TrayLocation" type="System.Drawing.Point, System.Drawing, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a">
     <value>17, 93</value>
@@ -617,7 +626,7 @@
     <value>Zoom</value>
   </data>
   <data name="UserPicture.TabIndex" type="System.Int32, mscorlib">
-    <value>5</value>
+    <value>0</value>
   </data>
   <data name="&gt;&gt;UserPicture.Name" xml:space="preserve">
     <value>UserPicture</value>
@@ -650,7 +659,7 @@
     <value>340, 14</value>
   </data>
   <data name="NameLabel.TabIndex" type="System.Int32, mscorlib">
-    <value>0</value>
+    <value>1</value>
   </data>
   <data name="NameLabel.Text" xml:space="preserve">
     <value>LblName</value>
@@ -669,6 +678,9 @@
   </data>
   <data name="&gt;&gt;NameLabel.ZOrder" xml:space="preserve">
     <value>1</value>
+  </data>
+  <data name="PostBrowser.AccessibleName" xml:space="preserve">
+    <value>発言本文</value>
   </data>
   <metadata name="ContextMenuPostBrowser.TrayLocation" type="System.Drawing.Point, System.Drawing, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b03f5f7f11d50a3a">
     <value>189, 55</value>
@@ -836,7 +848,7 @@
     <value>449, 60</value>
   </data>
   <data name="PostBrowser.TabIndex" type="System.Int32, mscorlib">
-    <value>6</value>
+    <value>4</value>
   </data>
   <data name="&gt;&gt;PostBrowser.Name" xml:space="preserve">
     <value>PostBrowser</value>
@@ -997,8 +1009,14 @@
   <data name="SplitContainer2.Panel1MinSize" type="System.Int32, mscorlib">
     <value>0</value>
   </data>
+  <data name="TableLayoutPanel2.AccessibleName" xml:space="preserve">
+    <value>発言投稿部</value>
+  </data>
   <data name="TableLayoutPanel2.ColumnCount" type="System.Int32, mscorlib">
     <value>3</value>
+  </data>
+  <data name="StatusText.AccessibleName" xml:space="preserve">
+    <value>本文入力欄</value>
   </data>
   <data name="StatusText.Dock" type="System.Windows.Forms.DockStyle, System.Windows.Forms">
     <value>Top</value>
@@ -1059,6 +1077,9 @@
   </data>
   <data name="&gt;&gt;PostButton.ZOrder" xml:space="preserve">
     <value>1</value>
+  </data>
+  <data name="lblLen.AccessibleName" xml:space="preserve">
+    <value>残り文字数</value>
   </data>
   <data name="lblLen.Dock" type="System.Windows.Forms.DockStyle, System.Windows.Forms">
     <value>Fill</value>
@@ -1170,6 +1191,9 @@
   </data>
   <data name="&gt;&gt;SplitContainer3.Panel1.ZOrder" xml:space="preserve">
     <value>0</value>
+  </data>
+  <data name="tweetThumbnail1.AccessibleName" xml:space="preserve">
+    <value>サムネイル表示部</value>
   </data>
   <data name="tweetThumbnail1.Dock" type="System.Windows.Forms.DockStyle, System.Windows.Forms">
     <value>Fill</value>

OpenTween/Tween.Designer.cs
@@ -714,6 +714,7 @@
             // 
             // UserPicture
             // 
+            this.UserPicture.AccessibleRole = System.Windows.Forms.AccessibleRole.Graphic;
             this.UserPicture.BackColor = System.Drawing.Color.White;
             this.UserPicture.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.UserPicture.ContextMenuStrip = this.ContextMenuUserPicture;

OpenTween/Tween.en.resx
@@ -211,6 +211,9 @@
   <data name="ToolStripContainer1.BottomToolStripPanel.ToolTip" xml:space="preserve">
     <value />
   </data>
+  <data name="TimelinePanel.AccessibleName" xml:space="preserve">
+    <value>PostsListArea</value>
+  </data>
   <data name="AddTabMenuItem.Size" type="System.Drawing.Size, System.Drawing">
     <value>247, 24</value>
   </data>
@@ -301,6 +304,12 @@
   <data name="SplitContainer1.Panel1.ToolTip" xml:space="preserve">
     <value />
   </data>
+  <data name="TableLayoutPanel1.AccessibleName" xml:space="preserve">
+    <value>PostDetailsArea</value>
+  </data>
+  <data name="UserPicture.AccessibleName" xml:space="preserve">
+    <value>ProfileImage</value>
+  </data>
   <data name="FollowToolStripMenuItem.Size" type="System.Drawing.Size, System.Drawing">
     <value>286, 24</value>
   </data>
@@ -375,6 +384,9 @@
   </data>
   <data name="NameLabel.ToolTip" xml:space="preserve">
     <value />
+  </data>
+  <data name="PostBrowser.AccessibleName" xml:space="preserve">
+    <value>PostBody</value>
   </data>
   <data name="SearchGoogleContextMenuItem.Size" type="System.Drawing.Size, System.Drawing">
     <value>216, 24</value>
@@ -553,14 +565,23 @@
   <data name="SplitContainer2.Panel1.ToolTip" xml:space="preserve">
     <value />
   </data>
+  <data name="TableLayoutPanel2.AccessibleName" xml:space="preserve">
+    <value>PostComposeArea</value>
+  </data>
   <data name="StatusText.Size" type="System.Drawing.Size, System.Drawing">
     <value>452, 22</value>
+  </data>
+  <data name="StatusText.AccessibleName" xml:space="preserve">
+    <value>PostBodyInput</value>
   </data>
   <data name="StatusText.ToolTip" xml:space="preserve">
     <value />
   </data>
   <data name="lblLen.Location" type="System.Drawing.Point, System.Drawing">
     <value>452, 0</value>
+  </data>
+  <data name="lblLen.AccessibleName" xml:space="preserve">
+    <value>RemainCharacterLength</value>
   </data>
   <data name="lblLen.ToolTip" xml:space="preserve">
     <value />
@@ -586,6 +607,9 @@
   </data>
   <data name="SplitContainer3.Panel1.ToolTip" xml:space="preserve">
     <value />
+  </data>
+  <data name="tweetThumbnail1.AccessibleName" xml:space="preserve">
+    <value>ThumbnailArea</value>
   </data>
   <data name="tweetThumbnail1.Size" type="System.Drawing.Size, System.Drawing">
     <value>55, 170</value>

commit 60011d91b95fc4cd9fd7bf5000b18fffd25648db

本文入力欄のAccessibleDescriptionに残り文字数のテキストを追加
以前のバージョンではタブオーダーが lblLen の次に StatusText が来る順序となっていたが、
25f08998 の修正以降はこれが表示上の順序通りとなるように変更されていた。
しかし、その修正により StatusText にフォーカスを移した際にスクリーンリーダーで
残り文字数が読み上げられなくなり、かえって不便な状態となっていた。
このコミットではタブオーダーを元に戻すのではなく、StatusText の AccessibleText に
残り文字数に関するテキストを含めることでフォーカス時に読み上げられる状態にした。

Fixes: 25f08998 ("TweenMain内の各コントロールのAccessibleNameに適切な名前をセット")

OpenTween/Properties/Resources.Designer.cs
@@ -2662,6 +2662,15 @@ namespace OpenTween.Properties {
         }
         
         /// <summary>
+        ///   残り{0}文字 に類似しているローカライズされた文字列を検索します。
+        /// </summary>
+        internal static string StatusText_AccessibleDescription {
+            get {
+                return ResourceManager.GetString("StatusText_AccessibleDescription", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   投稿に失敗しました に類似しているローカライズされた文字列を検索します。
         /// </summary>
         internal static string StatusUpdateFailed1 {

OpenTween/Properties/Resources.en.resx
@@ -1140,4 +1140,7 @@ Available service: {1}</value>
   <data name="QuoteStatus_AccessibleText" xml:space="preserve">
     <value>[Quote @{0}: {1}]</value>
   </data>
+  <data name="StatusText_AccessibleDescription" xml:space="preserve">
+    <value>{0} characters remaining</value>
+  </data>
 </root>

OpenTween/Properties/Resources.resx
@@ -1198,4 +1198,7 @@
   <data name="QuoteStatus_AccessibleText" xml:space="preserve">
     <value>[引用 @{0}: {1}]</value>
   </data>
+  <data name="StatusText_AccessibleDescription" xml:space="preserve">
+    <value>残り{0}文字</value>
+  </data>
 </root>

OpenTween/Tween.cs
@@ -4699,6 +4699,9 @@ namespace OpenTween
             {
                 StatusText.ForeColor = _clInputFont;
             }
+
+            this.StatusText.AccessibleDescription = string.Format(Properties.Resources.StatusText_AccessibleDescription, pLen);
+
             if (string.IsNullOrEmpty(StatusText.Text))
             {
                 this.inReplyTo = null;

OpenTween/Tween.resx
@@ -634,7 +634,7 @@
   <data name="&gt;&gt;PostButton.ZOrder" xml:space="preserve">
     <value>1</value>
   </data>
-  <data name="lblLen.AccessibleName" xml:space="preserve">
+  <data name="lblLen.AccessibleDescription" xml:space="preserve">
     <value>残り文字数</value>
   </data>
   <data name="lblLen.Dock" type="System.Windows.Forms.DockStyle, System.Windows.Forms">