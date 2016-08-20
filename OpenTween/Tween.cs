opentween/OpenTween

commit 8ef95e5a008f8e43da8d0bddc29afa259b3c80a8

TweenMain.RefreshTimelineの未読タブのアイコン表示に関するコードを整理

OpenTween/Tween.cs
@@ -1424,22 +1424,18 @@ namespace OpenTween
 
             if (MyCommon._endingFlag) return;
 
-            try
+            if (this._cfgCommon.TabIconDisp)
             {
-                foreach (TabPage tab in ListTab.TabPages)
+                foreach (var tabPage in this.ListTab.TabPages.Cast<TabPage>())
                 {
-                    TabModel tabInfo = _statuses.Tabs[tab.Text];
-
-                    if (tabInfo.UnreadCount > 0)
-                        if (this._cfgCommon.TabIconDisp)
-                            if (tab.ImageIndex == -1) tab.ImageIndex = 0; //タブアイコン
+                    var tabModel = this._statuses.Tabs[tabPage.Text];
+                    if (tabModel.UnreadCount > 0)
+                        tabPage.ImageIndex = 0; // 未読アイコン
                 }
-                if (!this._cfgCommon.TabIconDisp) ListTab.Refresh();
             }
-            catch (Exception)
+            else
             {
-                //ex.Data["Msg"] = "Ref1, UseAPI=" + SettingDialog.UseAPI.ToString();
-                //throw;
+                this.ListTab.Refresh();
             }
 
             // リストに反映＆選択状態復元

commit f585e76041b798afb621c626b7c6726e2d379873

新着ツイートを受信するたびにタブの表示がちらつく不具合を修正
Fixes: 8ef95e5a ("TweenMain.RefreshTimelineの未読タブのアイコン表示に関するコードを整理")

OpenTween/Tween.cs
@@ -1424,18 +1424,21 @@ namespace OpenTween
 
             if (MyCommon._endingFlag) return;
 
-            if (this._cfgCommon.TabIconDisp)
+            if (addCount > 0)
             {
-                foreach (var tabPage in this.ListTab.TabPages.Cast<TabPage>())
+                if (this._cfgCommon.TabIconDisp)
                 {
-                    var tabModel = this._statuses.Tabs[tabPage.Text];
-                    if (tabModel.UnreadCount > 0)
-                        tabPage.ImageIndex = 0; // 未読アイコン
+                    foreach (var tabPage in this.ListTab.TabPages.Cast<TabPage>())
+                    {
+                        var tabModel = this._statuses.Tabs[tabPage.Text];
+                        if (tabModel.UnreadCount > 0 && tabPage.ImageIndex != 0)
+                            tabPage.ImageIndex = 0; // 未読アイコン
+                    }
                 }
-            }
-            else
-            {
-                this.ListTab.Refresh();
+                else
+                {
+                    this.ListTab.Refresh();
+                }
             }
 
             // リストに反映＆選択状態復元