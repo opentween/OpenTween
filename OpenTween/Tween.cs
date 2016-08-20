opentween/OpenTween

commit 420ec654a6bf47da8bdcfa86eacf1805d45fffef

POST欄にfocusがあるときに Ctrl + V を押した際、clipboardに画像があるとその画像をテンポラリに保存して投稿する処理を追加 #27

OpenTween/Tween.cs
@@ -6811,6 +6811,12 @@ namespace OpenTween
                             // Webページを開く動作
                             OpenURLMenuItem_Click(null, null);
                             return true;
+                        case Keys.V:
+                            if( Focused == FocusedControl.StatusText ) {
+                                ProcClipboardFromStatusTextWhenCtrlPlusV();
+                                return true;
+                            }
+                            break;
                     }
                     //フォーカスList
                     if (Focused == FocusedControl.ListTab)
@@ -12720,6 +12726,28 @@ namespace OpenTween
         private void ImageSelector_VisibleChanged(object sender, EventArgs e)
         {
             this.StatusText_TextChanged(null, null);
+        }
+
+        /// <summary>
+        /// StatusTextでCtrl+Vが押下された時の処理
+        /// </summary>
+        private void ProcClipboardFromStatusTextWhenCtrlPlusV() {
+            if( Clipboard.ContainsText() ) {
+                // clipboardにテキストがある場合は貼り付け処理
+                this.StatusText.Paste( Clipboard.GetText() );
+            } else if( Clipboard.ContainsImage() ) {
+                // 画像があるので投稿処理を行う
+
+                // clipboardから画像を取得
+                var image = Clipboard.GetImage();
+                // 一時的に保存するためのパスを取得し、一旦保存を行う(png)
+                var path = string.Format( "{0}.png", Path.GetTempFileName() );
+                using( var fs = new FileStream( path, FileMode.Create ) ) {
+                    image.Save( fs, System.Drawing.Imaging.ImageFormat.Png );
+                }
+                // 保存したパスをImageSelectorに投げる
+                this.ImageSelector.BeginSelection( new string[] { path } );
+            }
         }
 #endregion
 

commit 48b5057fd29f94c6a8089611cbf365dc522612d9

StatusTextでクリップボードの内容を貼り付けた際のExternalExceptionが考慮されていない不具合を修正
https://osdn.jp/ticket/browse.php?group_id=6526&tid=35970

Fixes: 420ec654 ("POST欄にfocusがあるときに Ctrl + V を押した際...")

OpenTween/Tween.cs
@@ -41,6 +41,7 @@ using System.Media;
 using System.Net;
 using System.Net.Http;
 using System.Reflection;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading;
@@ -11364,27 +11365,34 @@ namespace OpenTween
         /// </summary>
         private void ProcClipboardFromStatusTextWhenCtrlPlusV()
         {
-            if (Clipboard.ContainsText())
+            try
             {
-                // clipboardにテキストがある場合は貼り付け処理
-                this.StatusText.Paste(Clipboard.GetText());
-            }
-            else if (Clipboard.ContainsImage())
-            {
-                // 画像があるので投稿処理を行う
-                if (MessageBox.Show(Properties.Resources.PostPictureConfirm3,
-                                   Properties.Resources.PostPictureWarn4,
-                                   MessageBoxButtons.OKCancel,
-                                   MessageBoxIcon.Question,
-                                   MessageBoxDefaultButton.Button2)
-                               == DialogResult.OK)
+                if (Clipboard.ContainsText())
                 {
-                    // clipboardから画像を取得
-                    using (var image = Clipboard.GetImage())
+                    // clipboardにテキストがある場合は貼り付け処理
+                    this.StatusText.Paste(Clipboard.GetText());
+                }
+                else if (Clipboard.ContainsImage())
+                {
+                    // 画像があるので投稿処理を行う
+                    if (MessageBox.Show(Properties.Resources.PostPictureConfirm3,
+                                       Properties.Resources.PostPictureWarn4,
+                                       MessageBoxButtons.OKCancel,
+                                       MessageBoxIcon.Question,
+                                       MessageBoxDefaultButton.Button2)
+                                   == DialogResult.OK)
                     {
-                        this.ImageSelector.BeginSelection(image);
+                        // clipboardから画像を取得
+                        using (var image = Clipboard.GetImage())
+                        {
+                            this.ImageSelector.BeginSelection(image);
+                        }
                     }
                 }
+            }
+            catch (ExternalException ex)
+            {
+                MessageBox.Show(ex.Message);
             }
         }
 #endregion