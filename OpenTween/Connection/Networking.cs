opentween/OpenTween

commit f7958f7d660c86fb0c622041a0a2275decd302e4

WebRequestでのリクエスト時にデフォルトでキャッシュを有効にする
内部で HttpWebRequest を使用する HttpClient も影響を受ける

OpenTween/Connection/Networking.cs
@@ -24,6 +24,7 @@ using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Net;
+using System.Net.Cache;
 using System.Net.Http;
 using System.Text;
 using System.Threading;
@@ -103,6 +104,7 @@ namespace OpenTween.Connection
             Networking.initialized = true;
 
             ServicePointManager.Expect100Continue = false;
+            WebRequest.DefaultCachePolicy = new RequestCachePolicy(RequestCacheLevel.Default);
         }
 
         public static void SetWebProxy(ProxyType proxyType, string proxyAddress, int proxyPort,

commit 1065fa5c12c05bb72f87e2ce89bf35e04857aa8f

WebRequest.DefaultCachePolicyを使用したキャッシュを再び無効化
下記のような原因不明のエラーが多数報告されたため:

  * https://osdn.jp/ticket/browse.php?group_id=6526&tid=36295
  * https://osdn.jp/ticket/browse.php?group_id=6526&tid=35968
  * https://osdn.jp/ticket/browse.php?group_id=6526&tid=35967
  * https://osdn.jp/ticket/browse.php?group_id=6526&tid=35886

Fixes: f7958f7d ("WebRequestでのリクエスト時にデフォルトでキャッシュを有効にする")

OpenTween/Connection/Networking.cs
@@ -104,7 +104,6 @@ namespace OpenTween.Connection
             Networking.initialized = true;
 
             ServicePointManager.Expect100Continue = false;
-            WebRequest.DefaultCachePolicy = new RequestCachePolicy(RequestCacheLevel.Default);
         }
 
         public static void SetWebProxy(ProxyType proxyType, string proxyAddress, int proxyPort,