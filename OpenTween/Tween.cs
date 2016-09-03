opentween/OpenTween

commit ce713ec26034d8e2c88d662269389847eeb7e633

RefreshTimelineでListViewの更新を現在表示中のリストに対してのみ行う
RefreshTimeline メソッドによって ListView の更新を行う対象を _curList
のみに変更した。これにより、現在表示されていないバックグラウンドの
ListView のスクロール位置・選択状態および VirtualListSize は、
そのタブに切り替わるまでは更新されない (TabModel の状態と一致しない)
状態になる。

OpenTween/Tween.cs
@@ -259,6 +259,10 @@ namespace OpenTween
         private PostClass _curPost;
         private bool _isColumnChanged = false;
 
+        // 各タブの発言一覧のスクロール位置・選択状態を保持するフィールド
+        private IDictionary<string, ListViewScroll> listViewScroll = new Dictionary<string, ListViewScroll>();
+        private IDictionary<string, ListViewSelection> listViewSelection = new Dictionary<string, ListViewSelection>();
+
         private const int MAX_WORKER_THREADS = 20;
         private SemaphoreSlim workerSemaphore = new SemaphoreSlim(MAX_WORKER_THREADS);
         private CancellationTokenSource workerCts = new CancellationTokenSource();
@@ -1402,11 +1406,13 @@ namespace OpenTween
 
         private void RefreshTimeline()
         {
-            // 現在表示中のタブのスクロール位置を退避
-            var curListScroll = this.SaveListViewScroll(this._curList, this._statuses.Tabs[this._curTab.Text]);
+            var curTabModel = this._statuses.Tabs[this._curTab.Text];
 
-            // 各タブのリスト上の選択位置などを退避
-            var listSelections = this.SaveListViewSelection();
+            // 現在表示中のタブのスクロール位置を退避
+            var curListScroll = this.SaveListViewScroll(this._curList, curTabModel);
+
+            // 現在表示中のタブのリスト上の選択位置などを退避
+            var curListSelection = this.SaveListViewSelection(this._curList, curTabModel);
 
             //更新確定
             PostClass[] notifyPosts;
@@ -1418,34 +1424,12 @@ namespace OpenTween
 
             if (MyCommon._endingFlag) return;
 
-            //リストに反映＆選択状態復元
             try
             {
                 foreach (TabPage tab in ListTab.TabPages)
                 {
-                    DetailsListView lst = (DetailsListView)tab.Tag;
                     TabModel tabInfo = _statuses.Tabs[tab.Text];
-                    if (isDelete || lst.VirtualListSize != tabInfo.AllCount)
-                    {
-                        using (ControlTransaction.Update(lst))
-                        {
-                            if (lst.Equals(_curList))
-                            {
-                                this.PurgeListViewItemCache();
-                            }
-                            try
-                            {
-                                lst.VirtualListSize = tabInfo.AllCount; //リスト件数更新
-                            }
-                            catch (Exception)
-                            {
-                                //アイコン描画不具合あり？
-                            }
 
-                            // 選択位置などを復元
-                            this.RestoreListViewSelection(lst, tabInfo, listSelections[tabInfo.TabName]);
-                        }
-                    }
                     if (tabInfo.UnreadCount > 0)
                         if (this._cfgCommon.TabIconDisp)
                             if (tab.ImageIndex == -1) tab.ImageIndex = 0; //タブアイコン
@@ -1458,8 +1442,24 @@ namespace OpenTween
                 //throw;
             }
 
-            // スクロール位置を復元
-            this.RestoreListViewScroll(this._curList, this._statuses.Tabs[this._curTab.Text], curListScroll);
+            // リストに反映＆選択状態復元
+            if (this._curList.VirtualListSize != curTabModel.AllCount || isDelete)
+            {
+                using (ControlTransaction.Update(this._curList))
+                {
+                    this.PurgeListViewItemCache();
+
+                    // リスト件数更新
+                    this._curList.VirtualListSize = curTabModel.AllCount;
+
+                    // 選択位置などを復元
+                    this.RestoreListViewSelection(this._curList, curTabModel, curListSelection);
+                }
+
+                // スクロール位置の復元は Begin/EndUpdate の外で行う
+                // 参照: https://github.com/opentween/OpenTween/commit/7dbf6491
+                this.RestoreListViewScroll(this._curList, curTabModel, curListScroll);
+            }
 
             //新着通知
             NotifyNewPosts(notifyPosts, soundFile, addCount, newMentionOrDm);
@@ -9822,10 +9822,35 @@ namespace OpenTween
         {
             SetListProperty();
 
+            if (this._curList != null)
+            {
+                var beforeSelectedList = this._curList;
+                var beforeSelectedTabModel = this._statuses.Tabs[this._curTab.Text];
+
+                // 発言一覧のスクロール位置・選択状態を退避
+                this.listViewScroll[beforeSelectedTabModel.TabName] = this.SaveListViewScroll(beforeSelectedList, beforeSelectedTabModel);
+                this.listViewSelection[beforeSelectedTabModel.TabName] = this.SaveListViewSelection(beforeSelectedList, beforeSelectedTabModel);
+            }
+
             this.PurgeListViewItemCache();
 
             _curTab = _tab;
             _curList = (DetailsListView)_tab.Tag;
+
+            var curTabModel = this._statuses.Tabs[this._curTab.Text];
+
+            this._curList.VirtualListSize = curTabModel.AllCount;
+
+            // 発言一覧のスクロール位置を復元
+            ListViewScroll scrollInfo;
+            if (this.listViewScroll.TryGetValue(curTabModel.TabName, out scrollInfo))
+                this.RestoreListViewScroll(this._curList, curTabModel, scrollInfo);
+
+            // 発言一覧の選択状態を復元
+            ListViewSelection selectionInfo;
+            if (this.listViewSelection.TryGetValue(curTabModel.TabName, out selectionInfo))
+                this.RestoreListViewSelection(this._curList, curTabModel, selectionInfo);
+
             if (_curList.SelectedIndices.Count > 0)
             {
                 _curItemIndex = _curList.SelectedIndices[0];

commit 63b867e1b6cce4a59e5193c61360e82118b7e5bb

Alt+←/→による移動時にエラーが発生する不具合を修正
ce713ec2 以降、表示中でない ListView の件数は更新されなくなったため、
_curList でない VirtualListSize を参照せず TabModel.AllCount を使用
しなければならない

https://osdn.jp/ticket/browse.php?group_id=6526&tid=36606

Fixes: ce713ec2 ("RefreshTimelineでListViewの更新を現在表示中のリストに対してのみ行う")

OpenTween/Tween.cs
@@ -6503,16 +6503,19 @@ namespace OpenTween
 
         private void GoSamePostToAnotherTab(bool left)
         {
-            if (_curList.VirtualListSize == 0) return;
-            int fIdx = 0;
-            int toIdx = 0;
-            int stp = 1;
-            long targetId = 0;
+            if (this._curList.SelectedIndices.Count == 0)
+                return;
 
-            if (_statuses.Tabs[_curTab.Text].TabType == MyCommon.TabUsageType.DirectMessage) return; // Directタブは対象外（見つかるはずがない）
-            if (_curList.SelectedIndices.Count == 0) return; //未選択も処理しない
+            var tab = this._statuses.Tabs[this._curTab.Text];
 
-            targetId = GetCurTabPost(_curList.SelectedIndices[0]).StatusId;
+            // Directタブは対象外（見つかるはずがない）
+            if (tab.TabType == MyCommon.TabUsageType.DirectMessage)
+                return;
+
+            var selectedIndex = this._curList.SelectedIndices[0];
+            var selectedStatusId = tab.GetStatusIdAt(selectedIndex);
+
+            int fIdx, toIdx, stp;
 
             if (left)
             {
@@ -6543,22 +6546,22 @@ namespace OpenTween
                 stp = 1;
             }
 
-            bool found = false;
             for (int tabidx = fIdx; tabidx != toIdx; tabidx += stp)
             {
-                if (_statuses.Tabs[ListTab.TabPages[tabidx].Text].TabType == MyCommon.TabUsageType.DirectMessage) continue; // Directタブは対象外
-                for (int idx = 0; idx < ((DetailsListView)ListTab.TabPages[tabidx].Tag).VirtualListSize; idx++)
+                var targetTab = this._statuses.Tabs[this.ListTab.TabPages[tabidx].Text];
+
+                // Directタブは対象外
+                if (targetTab.TabType == MyCommon.TabUsageType.DirectMessage)
+                    continue;
+
+                var foundIndex = targetTab.IndexOf(selectedStatusId);
+                if (foundIndex != -1)
                 {
-                    if (_statuses.Tabs[ListTab.TabPages[tabidx].Text][idx].StatusId == targetId)
-                    {
-                        ListTab.SelectedIndex = tabidx;
-                        SelectListItem(_curList, idx);
-                        _curList.EnsureVisible(idx);
-                        found = true;
-                        break;
-                    }
+                    ListTab.SelectedIndex = tabidx;
+                    SelectListItem(_curList, foundIndex);
+                    _curList.EnsureVisible(foundIndex);
+                    return;
                 }
-                if (found) break;
             }
         }
 