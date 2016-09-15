opentween/OpenTween

commit 6b42467183a1cfcfebd1758663cabc31e2a23a0c

TweenMain.doStatusDelete()メソッドを書き直し / 他人に公式RTされた自分のツイートの削除に対応

OpenTween/Tween.cs
@@ -3387,7 +3387,6 @@ namespace OpenTween
                 ReadedStripMenuItem.Enabled = true;
                 UnreadStripMenuItem.Enabled = true;
             }
-            DeleteStripMenuItem.Text = Properties.Resources.DeleteMenuText1;
             if (_statuses.Tabs[ListTab.SelectedTab.Text].TabType == MyCommon.TabUsageType.DirectMessage || !this.ExistCurrentPost || _curPost.IsDm)
             {
                 FavAddToolStripMenuItem.Enabled = false;
@@ -3401,10 +3400,6 @@ namespace OpenTween
                 QuoteStripMenuItem.Enabled = false;
                 FavoriteRetweetContextMenu.Enabled = false;
                 FavoriteRetweetUnofficialContextMenu.Enabled = false;
-                if (this.ExistCurrentPost && _curPost.IsDm)
-                    DeleteStripMenuItem.Enabled = true;
-                else
-                    DeleteStripMenuItem.Enabled = false;
             }
             else
             {
@@ -3418,27 +3413,9 @@ namespace OpenTween
                 {
                     ReTweetOriginalStripMenuItem.Enabled = false;
                     FavoriteRetweetContextMenu.Enabled = false;
-                    if (string.IsNullOrEmpty(_curPost.RetweetedBy))
-                    {
-                        DeleteStripMenuItem.Text = Properties.Resources.DeleteMenuText1;
-                    }
-                    else
-                    {
-                        DeleteStripMenuItem.Text = Properties.Resources.DeleteMenuText2;
-                    }
-                    DeleteStripMenuItem.Enabled = true;
                 }
                 else
                 {
-                    if (string.IsNullOrEmpty(_curPost.RetweetedBy))
-                    {
-                        DeleteStripMenuItem.Text = Properties.Resources.DeleteMenuText1;
-                    }
-                    else
-                    {
-                        DeleteStripMenuItem.Text = Properties.Resources.DeleteMenuText2;
-                    }
-                    DeleteStripMenuItem.Enabled = false;
                     if (_curPost.IsProtect)
                     {
                         ReTweetOriginalStripMenuItem.Enabled = false;
@@ -3482,6 +3459,15 @@ namespace OpenTween
             {
                 MoveToRTHomeMenuItem.Enabled = true;
             }
+
+            if (this.ExistCurrentPost)
+            {
+                this.DeleteStripMenuItem.Enabled = this._curPost.CanDeleteBy(this.tw.UserId);
+                if (this._curPost.RetweetedByUserId == this.tw.UserId)
+                    this.DeleteStripMenuItem.Text = Properties.Resources.DeleteMenuText2;
+                else
+                    this.DeleteStripMenuItem.Text = Properties.Resources.DeleteMenuText1;
+            }
         }
 
         private void ReplyStripMenuItem_Click(object sender, EventArgs e)
@@ -3496,113 +3482,116 @@ namespace OpenTween
 
         private void doStatusDelete()
         {
-            if (_curTab == null || _curList == null) return;
-            if (_statuses.Tabs[_curTab.Text].TabType != MyCommon.TabUsageType.DirectMessage)
-            {
-                bool myPost = false;
-                foreach (int idx in _curList.SelectedIndices)
-                {
-                    if (GetCurTabPost(idx).IsMe ||
-                       GetCurTabPost(idx).RetweetedBy.ToLower() == tw.Username.ToLower())
-                    {
-                        myPost = true;
-                        break;
-                    }
-                }
-                if (!myPost) return;
-            }
+            if (this._curTab == null || this._curList == null)
+                return;
+
+            if (this._curList.SelectedIndices.Count == 0)
+                return;
+
+            var posts = this._curList.SelectedIndices.Cast<int>()
+                .Select(x => this.GetCurTabPost(x))
+                .ToArray();
+
+            // 選択されたツイートの中に削除可能なものが一つでもあるか
+            if (!posts.Any(x => x.CanDeleteBy(this.tw.UserId)))
+                return;
+
+            var ret = MessageBox.Show(this,
+                string.Format(Properties.Resources.DeleteStripMenuItem_ClickText1, Environment.NewLine),
+                Properties.Resources.DeleteStripMenuItem_ClickText2,
+                MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+
+            if (ret != DialogResult.OK)
+                return;
+
+            int focusedIndex;
+            if (this._curList.FocusedItem != null)
+                focusedIndex = this._curList.FocusedItem.Index;
+            else if (this._curList.TopItem != null)
+                focusedIndex = this._curList.TopItem.Index;
             else
-            {
-                if (_curList.SelectedIndices.Count == 0)
-                    return;
-            }
-
-            string tmp = string.Format(Properties.Resources.DeleteStripMenuItem_ClickText1, Environment.NewLine);
-
-            if (MessageBox.Show(tmp, Properties.Resources.DeleteStripMenuItem_ClickText2,
-                  MessageBoxButtons.OKCancel,
-                  MessageBoxIcon.Question) == DialogResult.Cancel) return;
-
-            int fidx;
-            if (_curList.FocusedItem != null)
-                fidx = _curList.FocusedItem.Index;
-            else if (_curList.TopItem != null)
-                fidx = _curList.TopItem.Index;
-            else
-                fidx = 0;
+                focusedIndex = 0;
 
             try
             {
                 this.Cursor = Cursors.WaitCursor;
 
-                bool rslt = true;
-                foreach (long Id in _statuses.GetId(_curTab.Text, _curList.SelectedIndices))
+                string lastError = null;
+                foreach (var post in posts)
                 {
-                    string rtn = "";
-                    if (_statuses.Tabs[_curTab.Text].TabType == MyCommon.TabUsageType.DirectMessage)
+                    if (!post.CanDeleteBy(this.tw.UserId))
+                        continue;
+
+                    string err;
+                    if (post.IsDm)
                     {
-                        rtn = tw.RemoveDirectMessage(Id, _statuses[Id]);
+                        err = this.tw.RemoveDirectMessage(post.StatusId, post);
                     }
                     else
                     {
-                        if (_statuses[Id].IsMe || _statuses[Id].RetweetedBy.ToLower() == tw.Username.ToLower())
-                            rtn = tw.RemoveStatus(Id);
+                        if (post.RetweetedId != null && post.UserId == this.tw.UserId)
+                            // 他人に RT された自分のツイート
+                            err = this.tw.RemoveStatus(post.RetweetedId.Value);
                         else
-                            continue;
+                            // 自分のツイート or 自分が RT したツイート
+                            err = this.tw.RemoveStatus(post.StatusId);
                     }
-                    if (rtn.Length > 0)
+
+                    if (!string.IsNullOrEmpty(err))
                     {
-                        //エラー
-                        rslt = false;
+                        lastError = err;
+                        continue;
                     }
-                    else
-                    {
-                        _statuses.RemovePost(Id);
-                    }
+
+                    this._statuses.RemovePost(post.StatusId);
                 }
 
-                if (!rslt)
-                    StatusLabel.Text = Properties.Resources.DeleteStripMenuItem_ClickText3;  //失敗
+                if (lastError == null)
+                    this.StatusLabel.Text = Properties.Resources.DeleteStripMenuItem_ClickText4; // 成功
                 else
-                    StatusLabel.Text = Properties.Resources.DeleteStripMenuItem_ClickText4;  //成功
+                    this.StatusLabel.Text = Properties.Resources.DeleteStripMenuItem_ClickText3; // 失敗
 
                 this.PurgeListViewItemCache();
-                _curPost = null;
-                _curItemIndex = -1;
-                foreach (TabPage tb in ListTab.TabPages)
+                this._curPost = null;
+                this._curItemIndex = -1;
+
+                foreach (var tabPage in this.ListTab.TabPages.Cast<TabPage>())
                 {
-                    ((DetailsListView)tb.Tag).VirtualListSize = _statuses.Tabs[tb.Text].AllCount;
-                    if (_curTab.Equals(tb))
+                    var listView = (DetailsListView)tabPage.Tag;
+                    var tab = this._statuses.Tabs[tabPage.Text];
+
+                    using (ControlTransaction.Update(listView))
                     {
-                        do
-                        {
-                            _curList.SelectedIndices.Clear();
-                        }
-                        while (_curList.SelectedIndices.Count > 0);
+                        listView.VirtualListSize = tab.AllCount;
 
-                        if (_statuses.Tabs[tb.Text].AllCount > 0)
+                        if (tabPage == this._curTab)
                         {
-                            if (_statuses.Tabs[tb.Text].AllCount - 1 > fidx && fidx > -1)
-                                _curList.SelectedIndices.Add(fidx);
-                            else
-                                _curList.SelectedIndices.Add(_statuses.Tabs[tb.Text].AllCount - 1);
+                            listView.SelectedIndices.Clear();
 
-                            if (_curList.SelectedIndices.Count > 0)
+                            if (tab.AllCount != 0)
                             {
-                                _curList.EnsureVisible(_curList.SelectedIndices[0]);
-                                _curList.FocusedItem = _curList.Items[_curList.SelectedIndices[0]];
+                                int selectedIndex;
+                                if (tab.AllCount - 1 > focusedIndex && focusedIndex > -1)
+                                    selectedIndex = focusedIndex;
+                                else
+                                    selectedIndex = tab.AllCount - 1;
+
+                                listView.SelectedIndices.Add(selectedIndex);
+                                listView.EnsureVisible(selectedIndex);
+                                listView.FocusedItem = listView.Items[selectedIndex];
                             }
                         }
                     }
-                    if (_statuses.Tabs[tb.Text].UnreadCount == 0)
+
+                    if (this._cfgCommon.TabIconDisp && tab.UnreadCount == 0)
                     {
-                        if (this._cfgCommon.TabIconDisp)
-                        {
-                            if (tb.ImageIndex == 0) tb.ImageIndex = -1; //タブアイコン
-                        }
+                        if (tabPage.ImageIndex == 0)
+                            tabPage.ImageIndex = -1; // タブアイコン
                     }
                 }
-                if (!this._cfgCommon.TabIconDisp) ListTab.Refresh();
+
+                if (!this._cfgCommon.TabIconDisp)
+                    this.ListTab.Refresh();
             }
             finally
             {
@@ -11755,7 +11744,6 @@ namespace OpenTween
                 this.QtOpMenuItem.Enabled = false;
                 this.FavoriteRetweetMenuItem.Enabled = false;
                 this.FavoriteRetweetUnofficialMenuItem.Enabled = false;
-                if (this.ExistCurrentPost && _curPost.IsDm) this.DelOpMenuItem.Enabled = true;
             }
             else
             {
@@ -11769,11 +11757,9 @@ namespace OpenTween
                 {
                     this.RtOpMenuItem.Enabled = false;
                     this.FavoriteRetweetMenuItem.Enabled = false;
-                    this.DelOpMenuItem.Enabled = true;
                 }
                 else
                 {
-                    this.DelOpMenuItem.Enabled = false;
                     if (_curPost.IsProtect)
                     {
                         this.RtOpMenuItem.Enabled = false;
@@ -11817,6 +11803,11 @@ namespace OpenTween
             else
             {
                 OpenRterHomeMenuItem.Enabled = true;
+            }
+
+            if (this.ExistCurrentPost)
+            {
+                this.DelOpMenuItem.Enabled = this._curPost.CanDeleteBy(this.tw.UserId);
             }
         }
 

OpenTween.Tests/PostClassTest.cs
@@ -182,5 +182,89 @@ namespace OpenTween
             Assert.Empty(post.ReplyToList);
             Assert.Equal(-1, post.StateIndex);
         }
+
+        [Fact]
+        public void CanDeleteBy_SentDMTest()
+        {
+            var post = new TestPostClass
+            {
+                IsDm = true,
+                IsMe = true, // 自分が送信した DM
+                UserId = 222L, // 送信先ユーザーID
+            };
+
+            Assert.True(post.CanDeleteBy(selfUserId: 111L));
+        }
+
+        [Fact]
+        public void CanDeleteBy_ReceivedDMTest()
+        {
+            var post = new TestPostClass
+            {
+                IsDm = true,
+                IsMe = false, // 自分が受け取った DM
+                UserId = 222L, // 送信元ユーザーID
+            };
+
+            Assert.True(post.CanDeleteBy(selfUserId: 111L));
+        }
+
+        [Fact]
+        public void CanDeleteBy_MyTweetTest()
+        {
+            var post = new TestPostClass
+            {
+                UserId = 111L, // 自分のツイート
+            };
+
+            Assert.True(post.CanDeleteBy(selfUserId: 111L));
+        }
+
+        [Fact]
+        public void CanDeleteBy_OthersTweetTest()
+        {
+            var post = new TestPostClass
+            {
+                UserId = 222L, // 他人のツイート
+            };
+
+            Assert.False(post.CanDeleteBy(selfUserId: 111L));
+        }
+
+        [Fact]
+        public void CanDeleteBy_RetweetedByMeTest()
+        {
+            var post = new TestPostClass
+            {
+                RetweetedByUserId = 111L, // 自分がリツイートした
+                UserId = 222L, // 他人のツイート
+            };
+
+            Assert.True(post.CanDeleteBy(selfUserId: 111L));
+        }
+
+        [Fact]
+        public void CanDeleteBy_RetweetedByOthersTest()
+        {
+            var post = new TestPostClass
+            {
+                RetweetedByUserId = 333L, // 他人がリツイートした
+                UserId = 222L, // 他人のツイート
+            };
+
+            Assert.False(post.CanDeleteBy(selfUserId: 111L));
+        }
+
+        [Fact]
+        public void CanDeleteBy_MyTweetHaveBeenRetweetedByOthersTest()
+        {
+            var post = new TestPostClass
+            {
+                RetweetedByUserId = 222L, // 他人がリツイートした
+                UserId = 111L, // 自分のツイート
+            };
+
+            Assert.True(post.CanDeleteBy(selfUserId: 111L));
+        }
     }
 }

OpenTween/StatusDictionary.cs
@@ -310,6 +310,28 @@ namespace OpenTween
             }
         }
 
+        /// <summary>
+        /// このツイートが指定したユーザーによって削除可能であるかを判定します
+        /// </summary>
+        /// <param name="selfUserId">ツイートを削除しようとするユーザーのID</param>
+        /// <returns>削除可能であれば true、そうでなければ false</returns>
+        public bool CanDeleteBy(long selfUserId)
+        {
+            // 自分が送った DM と自分に届いた DM のどちらも削除可能
+            if (this.IsDm)
+                return true;
+
+            // 自分のツイート or 他人に RT された自分のツイート
+            if (this.UserId == selfUserId)
+                return true;
+
+            // 自分が RT したツイート
+            if (this.RetweetedByUserId == selfUserId)
+                return true;
+
+            return false;
+        }
+
         protected virtual PostClass GetRetweetSource(long statusId)
         {
             return TabInformations.GetInstance().RetweetSource(statusId);

commit fc9f64e2c0c6cbeffee48d157b80b31b74c04e33

「自分がRTした自分のツイート」に対して削除機能を実行すると元ツイートが削除される不具合を修正
修正前は「自分が RT した自分のツイート」に対して削除を行うと、RT 取り消しよりも
元ツイート (RetweetedId) の削除が優先して実行されていた。

Fixes: 6b424671 ("TweenMain.doStatusDelete()メソッドを書き直し / 他人に公式RTされた自分のツイートの削除に対応")

OpenTween/Tween.cs
@@ -3474,14 +3474,25 @@ namespace OpenTween
                         }
                         else
                         {
-                            if (post.RetweetedId != null && post.UserId == this.tw.UserId)
-                                // 他人に RT された自分のツイート
-                                await this.twitterApi.StatusesDestroy(post.RetweetedId.Value)
-                                    .IgnoreResponse();
-                            else
-                                // 自分のツイート or 自分が RT したツイート
+                            if (post.RetweetedByUserId == this.tw.UserId)
+                                // 自分が RT したツイート (自分が RT した自分のツイートも含む)
+                                //   => RT を取り消し
                                 await this.twitterApi.StatusesDestroy(post.StatusId)
                                     .IgnoreResponse();
+
+                            if (post.UserId == this.tw.UserId)
+                            {
+                                if (post.RetweetedId != null)
+                                    // 他人に RT された自分のツイート
+                                    //   => RT 元の自分のツイートを削除
+                                    await this.twitterApi.StatusesDestroy(post.RetweetedId.Value)
+                                        .IgnoreResponse();
+                                else
+                                    // 自分のツイート
+                                    //   => ツイートを削除
+                                    await this.twitterApi.StatusesDestroy(post.StatusId)
+                                        .IgnoreResponse();
+                            }
                         }
                     }
                     catch (WebApiException ex)