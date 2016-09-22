opentween/OpenTween

commit 363ae2884f9a32f2af8b8fbb770bf9033de7be7b

異字体セレクタ U+FE0F を含む絵文字が正しく表示されない不具合を修正
Fixes: 98015d9c ("twemoji V2 に対応")

OpenTween/EmojiFormatter.cs
@@ -48,6 +48,9 @@ namespace OpenTween
             string input = m.Value;
             string codes = "";
 
+            // 異字体セレクタ U+FE0F (emoji style) は除去する
+            input = input.Replace("\uFE0F", "");
+
             for (var i = 0; i < input.Length; i += char.IsSurrogatePair(input, i) ? 2 : 1)
             {
                 var codepoint = char.ConvertToUtf32(input, i);
@@ -60,7 +63,7 @@ namespace OpenTween
                 codes += string.Format("{0:x}", codepoint);
             }
 
-            return "<img class=\"emoji\" src=\"https://twemoji.maxcdn.com/2/72x72/" + codes + ".png\" alt=\"" + m.Value + "\" />";
+            return "<img class=\"emoji\" src=\"https://twemoji.maxcdn.com/2/72x72/" + codes + ".png\" alt=\"" + input + "\" />";
         }
     }
 }

OpenTween.Tests/EmojiFormatterTest.cs
@@ -42,6 +42,30 @@ namespace OpenTween
         }
 
         [Fact]
+        public void ReplaceEmojiToImg_VariationSelector_TextStyleTest()
+        {
+            // 異字体セレクタを使用して明示的にテキストスタイルで表示させている文字
+            var origText = "©\uFE0E"; // U+00A9 + U+FE0E (text style)
+
+            var result = EmojiFormatter.ReplaceEmojiToImg(origText);
+            var expected = "©\uFE0E";
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void ReplaceEmojiToImg_VariationSelector_EmojiStyleTest()
+        {
+            // 異字体セレクタを使用して明示的に絵文字スタイルで表示させている文字
+            var origText = "©\uFE0F"; // U+00A9 + U+FE0F (emoji style)
+
+            var result = EmojiFormatter.ReplaceEmojiToImg(origText);
+            var expected = "<img class=\"emoji\" src=\"https://twemoji.maxcdn.com/2/72x72/a9.png\" alt=\"©\" />";
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
         public void ReplaceEmojiToImg_SurrogatePairTest()
         {
             var origText = "🍣"; // U+1F363

commit 512c7bc81e229acde83057a87adfc4f953c42ae4

ZWJ (U+200D) を含む絵文字では異字体セレクタ U+FE0F を除去しない
Fixes: 363ae288 ("異字体セレクタ U+FE0F を含む絵文字が正しく表示されない不具合を修正")

OpenTween/EmojiFormatter.cs
@@ -48,8 +48,9 @@ namespace OpenTween
             string input = m.Value;
             string codes = "";
 
-            // 異字体セレクタ U+FE0F (emoji style) は除去する
-            input = input.Replace("\uFE0F", "");
+            // 異字体セレクタ U+FE0F (emoji style) は除去する (ZWJ を含まない場合のみ)
+            if (!input.Contains('\u200D'))
+                input = input.Replace("\uFE0F", "");
 
             for (var i = 0; i < input.Length; i += char.IsSurrogatePair(input, i) ? 2 : 1)
             {

OpenTween.Tests/EmojiFormatterTest.cs
@@ -124,6 +124,31 @@ namespace OpenTween
         }
 
         [Fact]
+        public void ReplaceEmojiToImg_EmojiZWJSequenceTest()
+        {
+            // 複数の絵文字を U+200D (ZERO WIDTH JOINER) で繋げて表現する絵文字
+            var origText = "👨\u200D🎨"; // U+1F468 (MAN) + U+200D + U+1F3A8 (ARTIST PALETTE)
+
+            var result = EmojiFormatter.ReplaceEmojiToImg(origText);
+            var expected = "<img class=\"emoji\" src=\"https://twemoji.maxcdn.com/2/72x72/1f468-200d-1f3a8.png\" alt=\"👨\u200D🎨\" />";
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void ReplaceEmojiToImg_EmojiZWJSequenceWithVariationSelectorTest()
+        {
+            // 複数の絵文字を U+200D (ZERO WIDTH JOINER) で繋げて表現 + 異字体セレクタ U+FE0F を含む絵文字
+            // この場合は URL 生成時に異字体セレクタ U+FE0F を除去しない
+            var origText = "🏃\u200D♀\uFE0F"; // U+1F3C3 (RUNNER) + U+200D + U+2640 (FEMARE SIGN) + U+FE0F
+
+            var result = EmojiFormatter.ReplaceEmojiToImg(origText);
+            var expected = "<img class=\"emoji\" src=\"https://twemoji.maxcdn.com/2/72x72/1f3c3-200d-2640-fe0f.png\" alt=\"🏃\u200D♀\uFE0F\" />";
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
         public void ReplaceEmojiToImg_NotEmojiTest()
         {
             var origText = "123ABC";