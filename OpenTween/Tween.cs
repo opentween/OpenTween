opentween/OpenTween

commit 194d3fa12c9f5677aaff44ee0e8ad8b58d265c54

ツイートの入力可能文字数に pic.twitter.com のURLをカウントしない
c93eecae の修正では pic.twitter.com 以外の画像共有サービスへのアップロードが考慮されていなかった

Fixes: c93eecae ("ツイートの入力可能文字数に pic.twitter.com のURLをカウントしない")

OpenTween/Tween.cs
@@ -4816,9 +4816,11 @@ namespace OpenTween
             //文字数カウント
             var remainCount = this.tw.GetTextLengthRemain(statusText);
 
-            if (this.ImageSelector.Visible && !string.IsNullOrEmpty(this.ImageSelector.ServiceName))
+            var uploadService = this.ImageSelector.SelectedService;
+            if (this.ImageSelector.Visible && uploadService != null)
             {
-                remainCount -= this.tw.Configuration.CharactersReservedPerMedia;
+                // TODO: ImageSelector で選択中の画像の枚数が mediaCount 引数に渡るようにする
+                remainCount -= uploadService.GetReservedTextLength(1);
             }
 
             return remainCount;

OpenTween/Connection/TwitterPhoto.cs
@@ -105,11 +105,9 @@ namespace OpenTween.Connection
                 .ConfigureAwait(false);
         }
 
+        // pic.twitter.com の URL は文字数にカウントされない
         public int GetReservedTextLength(int mediaCount)
-        {
-            // 枚数に関わらず文字数は一定
-            return this.twitterConfig.ShortUrlLength;
-        }
+            => 0;
 
         public void UpdateTwitterConfiguration(TwitterConfiguration config)
         {

commit ed4b0950d52fec1adc8cd99b1d441b17a814f363

画像投稿先のサービスを変更した際にも残り文字数のカウントを更新する
Fixes: 194d3fa1 ("ツイートの入力可能文字数に pic.twitter.com のURLをカウントしない")

OpenTween/Tween.cs
@@ -11372,8 +11372,7 @@ namespace OpenTween
                 ModifySettingCommon = true;
                 SaveConfigsAll(true);
 
-                if (ImageSelector.ServiceName.Equals("Twitter"))
-                    this.StatusText_TextChanged(null, null);
+                this.StatusText_TextChanged(null, null);
             }
         }
 