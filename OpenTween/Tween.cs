opentween/OpenTween

commit fc9f64e2c0c6cbeffee48d157b80b31b74c04e33

「自分がRTした自分のツイート」に対して削除機能を実行すると元ツイートが削除される不具合を修正
修正前は「自分が RT した自分のツイート」に対して削除を行うと、RT 取り消しよりも
元ツイート (RetweetedId) の削除が優先して実行されていた。

Fixes: 6b424671 ("TweenMain.doStatusDelete()メソッドを書き直し / 他人に公式RTされた自分のツイートの削除に対応")

OpenTween/Tween.cs
@@ -3474,14 +3474,25 @@ namespace OpenTween
                         }
                         else
                         {
-                            if (post.RetweetedId != null && post.UserId == this.tw.UserId)
-                                // 他人に RT された自分のツイート
-                                await this.twitterApi.StatusesDestroy(post.RetweetedId.Value)
-                                    .IgnoreResponse();
-                            else
-                                // 自分のツイート or 自分が RT したツイート
+                            if (post.RetweetedByUserId == this.tw.UserId)
+                                // 自分が RT したツイート (自分が RT した自分のツイートも含む)
+                                //   => RT を取り消し
                                 await this.twitterApi.StatusesDestroy(post.StatusId)
                                     .IgnoreResponse();
+
+                            if (post.UserId == this.tw.UserId)
+                            {
+                                if (post.RetweetedId != null)
+                                    // 他人に RT された自分のツイート
+                                    //   => RT 元の自分のツイートを削除
+                                    await this.twitterApi.StatusesDestroy(post.RetweetedId.Value)
+                                        .IgnoreResponse();
+                                else
+                                    // 自分のツイート
+                                    //   => ツイートを削除
+                                    await this.twitterApi.StatusesDestroy(post.StatusId)
+                                        .IgnoreResponse();
+                            }
                         }
                     }
                     catch (WebApiException ex)

commit b6df46afd1802ac3288b64d32c64f8db02d70153

「自分がRTした自分のツイート」に対して削除機能を実行すると元ツイートが削除される不具合を修正 (thx @a4lg!)
fc9f64e2 の修正ミスで post.StatusId と post.RetweetedId の両方に対して削除が行われていた
https://osdn.net/ticket/browse.php?group_id=6526&tid=36679

Fixes: fc9f64e2 ("「自分がRTした自分のツイート」に対して削除機能を実行すると元ツイートが削除される不具合を修正")

OpenTween/Tween.cs
@@ -3475,23 +3475,27 @@ namespace OpenTween
                         else
                         {
                             if (post.RetweetedByUserId == this.tw.UserId)
+                            {
                                 // 自分が RT したツイート (自分が RT した自分のツイートも含む)
                                 //   => RT を取り消し
                                 await this.twitterApi.StatusesDestroy(post.StatusId)
                                     .IgnoreResponse();
-
-                            if (post.UserId == this.tw.UserId)
+                            }
+                            else
                             {
-                                if (post.RetweetedId != null)
-                                    // 他人に RT された自分のツイート
-                                    //   => RT 元の自分のツイートを削除
-                                    await this.twitterApi.StatusesDestroy(post.RetweetedId.Value)
-                                        .IgnoreResponse();
-                                else
-                                    // 自分のツイート
-                                    //   => ツイートを削除
-                                    await this.twitterApi.StatusesDestroy(post.StatusId)
-                                        .IgnoreResponse();
+                                if (post.UserId == this.tw.UserId)
+                                {
+                                    if (post.RetweetedId != null)
+                                        // 他人に RT された自分のツイート
+                                        //   => RT 元の自分のツイートを削除
+                                        await this.twitterApi.StatusesDestroy(post.RetweetedId.Value)
+                                            .IgnoreResponse();
+                                    else
+                                        // 自分のツイート
+                                        //   => ツイートを削除
+                                        await this.twitterApi.StatusesDestroy(post.StatusId)
+                                            .IgnoreResponse();
+                                }
                             }
                         }
                     }