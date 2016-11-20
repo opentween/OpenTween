opentween/OpenTween

commit 8538894ced670c4ceb07bbfd8694281fe1ce996d

タブ名の変更時にlistView{Scroll,Selection}のキーも合わせて変更する
Fixes: ce713ec2 ("RefreshTimelineでListViewの更新を現在表示中のリストに対してのみ行う")

OpenTween/Tween.cs
@@ -7446,15 +7446,26 @@ namespace OpenTween
                         return false;
                     }
                 }
-                //タブ名を変更
-                for (int i = 0; i < ListTab.TabCount; i++)
+
+                var tabPage = this.ListTab.TabPages.Cast<TabPage>()
+                    .First(x => x.Text == origTabName);
+
+                ListViewScroll scrollInfo;
+                if (this.listViewScroll.TryGetValue(origTabName, out scrollInfo))
                 {
-                    if (ListTab.TabPages[i].Text == origTabName)
-                    {
-                        ListTab.TabPages[i].Text = newTabName;
-                        break;
-                    }
+                    this.listViewScroll.Remove(origTabName);
+                    this.listViewScroll[newTabName] = scrollInfo;
                 }
+
+                ListViewSelection selectionInfo;
+                if (this.listViewSelection.TryGetValue(origTabName, out selectionInfo))
+                {
+                    this.listViewSelection.Remove(origTabName);
+                    this.listViewSelection[newTabName] = selectionInfo;
+                }
+
+                //タブ名を変更
+                tabPage.Text = newTabName;
                 _statuses.RenameTab(origTabName, newTabName);
 
                 SaveConfigsCommon();

commit 1651fd98fd927dae76ec04932306512e1c81e391

ミュートタブの名前を変更するとエラーが発生する不具合を修正
https://osdn.net/ticket/browse.php?group_id=6526&tid=36805

Fixes: 8538894c ("タブ名の変更時にlistView{Scroll,Selection}のキーも合わせて変更する")

OpenTween/Tween.cs
@@ -7468,24 +7468,28 @@ namespace OpenTween
                 }
 
                 var tabPage = this.ListTab.TabPages.Cast<TabPage>()
-                    .First(x => x.Text == origTabName);
+                    .FirstOrDefault(x => x.Text == origTabName);
 
-                ListViewScroll scrollInfo;
-                if (this.listViewScroll.TryGetValue(origTabName, out scrollInfo))
+                if (tabPage != null)
                 {
-                    this.listViewScroll.Remove(origTabName);
-                    this.listViewScroll[newTabName] = scrollInfo;
+                    ListViewScroll scrollInfo;
+                    if (this.listViewScroll.TryGetValue(origTabName, out scrollInfo))
+                    {
+                        this.listViewScroll.Remove(origTabName);
+                        this.listViewScroll[newTabName] = scrollInfo;
+                    }
+
+                    ListViewSelection selectionInfo;
+                    if (this.listViewSelection.TryGetValue(origTabName, out selectionInfo))
+                    {
+                        this.listViewSelection.Remove(origTabName);
+                        this.listViewSelection[newTabName] = selectionInfo;
+                    }
+
+                    //タブ名を変更
+                    tabPage.Text = newTabName;
                 }
 
-                ListViewSelection selectionInfo;
-                if (this.listViewSelection.TryGetValue(origTabName, out selectionInfo))
-                {
-                    this.listViewSelection.Remove(origTabName);
-                    this.listViewSelection[newTabName] = selectionInfo;
-                }
-
-                //タブ名を変更
-                tabPage.Text = newTabName;
                 _statuses.RenameTab(origTabName, newTabName);
 
                 SaveConfigsCommon();