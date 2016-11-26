opentween/OpenTween

commit 74df0fc982192cfcb137f4423bf527e85ed903fa

動作しなくなっていたFoursquareのサムネイル表示を再実装
Foursquareチェックイン時のツイートのうち位置情報が付与されていない
ツイートに対してのみサムネイル情報を返します

OpenTween/Thumbnail/Services/FoursquareCheckin.cs
@@ -1,0 +1,137 @@
+﻿// OpenTween - Client of Twitter
+// Copyright (c) 2012 kim_upsilon (@kim_upsilon) <https://upsilo.net/~upsilon/>
+// All rights reserved.
+//
+// This file is part of OpenTween.
+//
+// This program is free software; you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the Free
+// Software Foundation; either version 3 of the License, or (at your option)
+// any later version.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+// for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program. If not, see <http://www.gnu.org/licenses/>, or write to
+// the Free Software Foundation, Inc., 51 Franklin Street - Fifth Floor,
+// Boston, MA 02110-1301, USA.
+
+using System;
+using System.Net.Http;
+using System.Runtime.Serialization.Json;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.XPath;
+
+namespace OpenTween.Thumbnail.Services
+{
+    class FoursquareCheckin : IThumbnailService
+    {
+        public static readonly Regex UrlPatternRegex =
+            new Regex(@"^https?://foursquare\.com/.+?/checkin/(?<checkin_id>[0-9a-z]+)(?:\?s=(?<signature>[^&]+))?");
+
+        public static readonly string ApiBase = "https://api.foursquare.com/v2";
+
+        protected readonly HttpClient http;
+
+        public FoursquareCheckin()
+            : this(null)
+        {
+        }
+
+        public FoursquareCheckin(HttpClient http)
+        {
+            this.http = http ?? MyCommon.CreateHttpClient();
+        }
+
+        public override async Task<ThumbnailInfo> GetThumbnailInfoAsync(string url, PostClass post, CancellationToken token)
+        {
+            if (!AppendSettingDialog.Instance.IsPreviewFoursquare)
+                return null;
+
+            // ツイートに位置情報が付与されている場合は何もしない
+            if (post.PostGeo.Lat != 0 || post.PostGeo.Lng != 0)
+                return null;
+
+            var match = UrlPatternRegex.Match(url);
+
+            if (!match.Success)
+                return null;
+
+            var checkinIdGroup = match.Groups["checkin_id"];
+            var signatureGroup = match.Groups["signature"];
+
+            try
+            {
+                // Foursquare のベニュー情報を取得
+                // 参照: https://developer.foursquare.com/docs/venues/venues
+
+                var query = HttpUtility.ParseQueryString(string.Empty);
+                query["client_id"] = ApplicationSettings.FoursquareClientId;
+                query["client_secret"] = ApplicationSettings.FoursquareClientSecret;
+                query["v"] = "20140419"; // https://developer.foursquare.com/overview/versioning
+
+                if (signatureGroup.Success)
+                    query["signature"] = signatureGroup.Value;
+
+                var apiUrl = new Uri(ApiBase + "/checkins/" + checkinIdGroup.Value + "?" + query);
+
+                using (var response = await this.http.GetAsync(apiUrl, token).ConfigureAwait(false))
+                {
+                    response.EnsureSuccessStatusCode();
+
+                    var jsonBytes = await response.Content.ReadAsByteArrayAsync()
+                        .ConfigureAwait(false);
+
+                    var location = ParseIntoLocation(jsonBytes);
+                    if (location == null)
+                        return null;
+
+                    var map = MapThumb.GetDefaultInstance();
+
+                    return new ThumbnailInfo(this.http)
+                    {
+                        ImageUrl = map.CreateMapLinkUrl(location.Latitude, location.Longitude),
+                        ThumbnailUrl = map.CreateStaticMapUrl(location.Latitude, location.Longitude),
+                        TooltipText = null,
+                    };
+                }
+            }
+            catch (HttpRequestException) { }
+
+            return null;
+        }
+
+        internal static GlobalLocation ParseIntoLocation(byte[] jsonBytes)
+        {
+            using (var jsonReader = JsonReaderWriterFactory.CreateJsonReader(jsonBytes, XmlDictionaryReaderQuotas.Max))
+            {
+                var xElm = XElement.Load(jsonReader);
+
+                var locationElm = xElm.XPathSelectElement("/response/checkin/venue/location");
+
+                // 座標が得られなかった場合
+                if (locationElm == null)
+                    return null;
+
+                // 月など、地球以外の星の座標である場合
+                var planetElm = locationElm.Element("planet");
+                if (planetElm != null && planetElm.Value != "earth")
+                    return null;
+
+                return new GlobalLocation
+                {
+                    Latitude = double.Parse(locationElm.Element("lat").Value),
+                    Longitude = double.Parse(locationElm.Element("lng").Value),
+                };
+            }
+        }
+    }
+}

OpenTween.Tests/Thumbnail/Services/FoursquareCheckinTest.cs
@@ -1,0 +1,259 @@
+﻿// OpenTween - Client of Twitter
+// Copyright (c) 2014 kim_upsilon (@kim_upsilon) <https://upsilo.net/~upsilon/>
+// All rights reserved.
+//
+// This file is part of OpenTween.
+//
+// This program is free software; you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the Free
+// Software Foundation; either version 3 of the License, or (at your option)
+// any later version.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+// for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program. If not, see <http://www.gnu.org/licenses/>, or write to
+// the Free Software Foundation, Inc., 51 Franklin Street - Fifth Floor,
+// Boston, MA 02110-1301, USA.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using NSubstitute;
+using Xunit;
+using Xunit.Extensions;
+
+#pragma warning disable 1998 // awaitが無いasyncラムダ式に対する警告を抑制
+
+namespace OpenTween.Thumbnail.Services
+{
+    public class FoursquareCheckinTest
+    {
+        public FoursquareCheckinTest()
+        {
+            this.SettingsSetup();
+            this.MyCommonSetup();
+        }
+
+        public void SettingsSetup()
+        {
+            AppendSettingDialog.Instance.IsPreviewFoursquare = true;
+        }
+
+        public void MyCommonSetup()
+        {
+            var mockAssembly = Substitute.For<_Assembly>();
+            mockAssembly.GetName().Returns(new AssemblyName("OpenTween"));
+            MyCommon.EntryAssembly = mockAssembly;
+
+            MyCommon.fileVersion = "1.0.0.0";
+        }
+
+        [Fact]
+        public async Task GetThumbnailInfoAsync_RequestTest()
+        {
+            var handler = new HttpMessageHandlerMock();
+            var service = new FoursquareCheckin(new HttpClient(handler));
+
+            handler.Queue.Enqueue(async x =>
+            {
+                Assert.Equal(HttpMethod.Get, x.Method);
+                Assert.Equal("https://api.foursquare.com/v2/checkins/xxxxxxxx",
+                    x.RequestUri.GetLeftPart(UriPartial.Path));
+
+                var query = HttpUtility.ParseQueryString(x.RequestUri.Query);
+
+                Assert.Equal(ApplicationSettings.FoursquareClientId, query["client_id"]);
+                Assert.Equal(ApplicationSettings.FoursquareClientSecret, query["client_secret"]);
+                Assert.NotNull(query["v"]);
+                Assert.Null(query["signature"]);
+
+                // リクエストに対するテストなのでレスポンスは適当に返す
+                return new HttpResponseMessage(HttpStatusCode.NotFound);
+            });
+
+            var post = new PostClass
+            {
+                PostGeo = new PostClass.StatusGeo { },
+            };
+
+            var thumb = await service.GetThumbnailInfoAsync(
+                "https://foursquare.com/checkin/hogehoge/xxxxxxxx",
+                post, CancellationToken.None);
+        }
+
+        [Fact]
+        public async Task GetThumbnailInfoAsync_RequestWithSignatureTest()
+        {
+            var handler = new HttpMessageHandlerMock();
+            var service = new FoursquareCheckin(new HttpClient(handler));
+
+            handler.Queue.Enqueue(async x =>
+            {
+                Assert.Equal(HttpMethod.Get, x.Method);
+                Assert.Equal("https://api.foursquare.com/v2/checkins/xxxxxxxx",
+                    x.RequestUri.GetLeftPart(UriPartial.Path));
+
+                var query = HttpUtility.ParseQueryString(x.RequestUri.Query);
+
+                Assert.Equal(ApplicationSettings.FoursquareClientId, query["client_id"]);
+                Assert.Equal(ApplicationSettings.FoursquareClientSecret, query["client_secret"]);
+                Assert.NotNull(query["v"]);
+                Assert.Equal("aaaaaaa", query["signature"]);
+
+                // リクエストに対するテストなのでレスポンスは適当に返す
+                return new HttpResponseMessage(HttpStatusCode.NotFound);
+            });
+
+            var post = new PostClass
+            {
+                PostGeo = new PostClass.StatusGeo { },
+            };
+
+            var thumb = await service.GetThumbnailInfoAsync(
+                "https://foursquare.com/checkin/hogehoge/xxxxxxxx?s=aaaaaaa",
+                post, CancellationToken.None);
+        }
+
+        [Fact]
+        public async Task GetThumbnailInfoAsync_GeoLocatedTweetTest()
+        {
+            var handler = new HttpMessageHandlerMock();
+            var service = new FoursquareCheckin(new HttpClient(handler));
+
+            handler.Queue.Enqueue(async x =>
+            {
+                // このリクエストは実行されないはず
+                Assert.True(false);
+                return new HttpResponseMessage(HttpStatusCode.NotFound);
+            });
+
+            // 既にジオタグが付いているツイートに対しては何もしない
+            var post = new PostClass
+            {
+                PostGeo = new PostClass.StatusGeo
+                {
+                    Lat = 34.35067978344854,
+                    Lng = 134.04693603515625,
+                },
+            };
+
+            var thumb = await service.GetThumbnailInfoAsync(
+                "https://foursquare.com/checkin/hogehoge/xxxxxxxx?s=aaaaaaa",
+                post, CancellationToken.None);
+        }
+
+        [Fact]
+        public async Task GetThumbnailInfoAsync_DisabledTest()
+        {
+            var handler = new HttpMessageHandlerMock();
+            var service = new FoursquareCheckin(new HttpClient(handler));
+
+            handler.Queue.Enqueue(async x =>
+            {
+                // このリクエストは実行されないはず
+                Assert.True(false);
+                return new HttpResponseMessage(HttpStatusCode.NotFound);
+            });
+
+            var post = new PostClass
+            {
+                PostGeo = new PostClass.StatusGeo { },
+            };
+
+            // 設定により無効化されている場合は何もしない
+            AppendSettingDialog.Instance.IsPreviewFoursquare = false;
+
+            var thumb = await service.GetThumbnailInfoAsync(
+                "https://foursquare.com/checkin/hogehoge/xxxxxxxx",
+                post, CancellationToken.None);
+        }
+
+        [Fact]
+        public void ParseInLocation_Test()
+        {
+            var json = @"{
+  ""meta"": { ""code"": 200 },
+  ""response"": {
+    ""checkin"": {
+      ""id"": ""xxxxxxxxx"",
+      ""type"": ""checkin"",
+      ""venue"": {
+        ""id"": ""4b73dedcf964a5206bbe2de3"",
+        ""name"": ""高松駅 (Takamatsu Sta.)"",
+        ""location"": {
+          ""lat"": 34.35067978344854,
+          ""lng"": 134.04693603515625
+        }
+      }
+    }
+  }
+}";
+            var jsonBytes = Encoding.UTF8.GetBytes(json);
+            var location = FoursquareCheckin.ParseIntoLocation(jsonBytes);
+
+            Assert.NotNull(location);
+            Assert.Equal(34.35067978344854, location.Latitude);
+            Assert.Equal(134.04693603515625, location.Longitude);
+        }
+
+        [Fact]
+        public void ParseInLocation_PlanetTest()
+        {
+            var json = @"{
+  ""meta"": { ""code"": 200 },
+  ""response"": {
+    ""checkin"": {
+      ""id"": ""xxxxxxxxx"",
+      ""type"": ""checkin"",
+      ""venue"": {
+        ""id"": ""5069d8bdc640385aa7711fe4"",
+        ""name"": ""Gale Crater"",
+        ""location"": {
+          ""planet"": ""mars"",
+          ""lat"": 34.201694,
+          ""lng"": -118.17166
+        }
+      }
+    }
+  }
+}";
+            var jsonBytes = Encoding.UTF8.GetBytes(json);
+            var location = FoursquareCheckin.ParseIntoLocation(jsonBytes);
+
+            // 地球以外の位置にあるベニューに対しては null を返す
+            Assert.Null(location);
+        }
+
+        [Fact]
+        public void ParseInLocation_VenueNullTest()
+        {
+            var json = @"{
+  ""meta"": { ""code"": 200 },
+  ""response"": {
+    ""checkin"": {
+      ""id"": ""xxxxxxxxx"",
+      ""type"": ""checkin"",
+      ""venue"": null
+    }
+  }
+}";
+            var jsonBytes = Encoding.UTF8.GetBytes(json);
+            var location = FoursquareCheckin.ParseIntoLocation(jsonBytes);
+
+            // ベニュー情報が得られなかった場合は null を返す
+            Assert.Null(location);
+        }
+    }
+}

OpenTween.Tests/FoursquareTest.cs
@@ -1,53 +1,0 @@
-﻿// OpenTween - Client of Twitter
-// Copyright (c) 2012 the40san <http://sourceforge.jp/users/the40san/>
-// All rights reserved.
-//
-// This file is part of OpenTween.
-//
-// This program is free software; you can redistribute it and/or modify it
-// under the terms of the GNU General public License as published by the Free
-// Software Foundation; either version 3 of the License, or (at your option)
-// any later version.
-//
-// This program is distributed in the hope that it will be useful, but
-// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
-// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General public License
-// for more details.
-//
-// You should have received a copy of the GNU General public License along
-// with this program. If not, see <http://www.gnu.org/licenses/>, or write to
-// the Free Software Foundation, Inc., 51 Franklin Street - Fifth Floor,
-// Boston, MA 02110-1301, USA.
-
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using OpenTween;
-using Xunit;
-using Xunit.Extensions;
-
-namespace OpenTween
-{
-    /// <summary>
-    /// OpenTween.Foursquareクラステスト用
-    /// </summary>
-    class FoursquareTest
-    {
-        [Fact]
-        public void Test_GetInstance()
-        {
-            Assert.IsType<Foursquare>(Foursquare.GetInstance);
-            Assert.NotNull(Foursquare.GetInstance);
-        }
-
-        //[InlineData("https://ja.foursquare.com/v/starbucks-coffee-jr%E6%9D%B1%E6%B5%B7-%E5%93%81%E5%B7%9D%E9%A7%85%E5%BA%97/4b5fd527f964a52036ce29e3", Result = "")]
-        public string Test_GetMapsUri(string url)
-        {
-            AppendSettingDialog.Instance.IsPreviewFoursquare = true;
-            string refText ="";
-            return Foursquare.GetInstance.GetMapsUri(url, ref refText);
-        }
-        
-    }
-}

OpenTween.Tests/OpenTween.Tests.csproj
@@ -44,6 +44,7 @@
     <Reference Include="System.Drawing" />
     <Reference Include="System.Net.Http" />
     <Reference Include="System.Runtime.Serialization" />
+    <Reference Include="System.Web" />
     <Reference Include="System.Windows.Forms" />
     <Reference Include="System.Xml.Linq" />
     <Reference Include="System.Data.DataSetExtensions" />
@@ -69,7 +70,6 @@
     <Compile Include="MyApplicationTest.cs" />
     <Compile Include="OTPictureBoxTest.cs" />
     <Compile Include="PostFilterRuleTest.cs" />
-    <Compile Include="FoursquareTest.cs" />
     <Compile Include="LRUCacheDictionaryTest.cs" />
     <Compile Include="MyCommonTest.cs" />
     <Compile Include="PostClassTest.cs" />
@@ -80,6 +80,7 @@
     <Compile Include="TabInformationTest.cs" />
     <Compile Include="TabsDialogTest.cs" />
     <Compile Include="TestUtils.cs" />
+    <Compile Include="Thumbnail\Services\FoursquareCheckinTest.cs" />
     <Compile Include="Thumbnail\Services\ImgAzyobuziNetTest.cs" />
     <Compile Include="Thumbnail\Services\MetaThumbnailServiceTest.cs" />
     <Compile Include="Thumbnail\Services\SimpleThumbnailServiceTest.cs" />

OpenTween/Foursquare.cs
@@ -1,246 +1,0 @@
-﻿// OpenTween - Client of Twitter
-// Copyright (c) 2007-2011 kiri_feather (@kiri_feather) <kiri.feather@gmail.com>
-//           (c) 2008-2011 Moz (@syo68k)
-//           (c) 2008-2011 takeshik (@takeshik) <http://www.takeshik.org/>
-//           (c) 2010-2011 anis774 (@anis774) <http://d.hatena.ne.jp/anis774/>
-//           (c) 2010-2011 fantasticswallow (@f_swallow) <http://twitter.com/f_swallow>
-//           (c) 2011      Egtra (@egtra) <http://dev.activebasic.com/egtra/>
-// All rights reserved.
-//
-// This file is part of OpenTween.
-//
-// This program is free software; you can redistribute it and/or modify it
-// under the terms of the GNU General Public License as published by the Free
-// Software Foundation; either version 3 of the License, or (at your option)
-// any later version.
-//
-// This program is distributed in the hope that it will be useful, but
-// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
-// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
-// for more details.
-//
-// You should have received a copy of the GNU General Public License along
-// with this program. If not, see <http://www.gnu.org/licenses/>, or write to
-// the Free Software Foundation, Inc., 51 Franklin Street - Fifth Floor,
-// Boston, MA 02110-1301, USA.
-
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Net;
-using System.Text.RegularExpressions;
-using System.Runtime.Serialization;
-using OpenTween.Thumbnail;
-
-namespace OpenTween
-{
-    public class Foursquare : HttpConnection
-    {
-        private static Foursquare _instance = new Foursquare();
-        public static Foursquare GetInstance
-        {
-            get
-            {
-                return _instance;
-            }
-        }
-
-        private Dictionary<string, string> _authKey = new Dictionary<string, string>
-        {
-            {"client_id", ApplicationSettings.FoursquareClientId},
-            {"client_secret", ApplicationSettings.FoursquareClientSecret},
-        };
-
-        private Dictionary<string, GlobalLocation> CheckInUrlsVenueCollection = new Dictionary<string, GlobalLocation>();
-
-        private string GetVenueId(string url)
-        {
-            var content = "";
-            try
-            {
-                var res = GetContent("GET", new Uri(url), null, ref content);
-                if (res != HttpStatusCode.OK) return "";
-            }
-            catch (Exception)
-            {
-                return "";
-            }
-            var mc = Regex.Match(content, "/venue/(?<venueId>[0-9]+)", RegexOptions.IgnoreCase);
-            if (mc.Success)
-            {
-                var vId = mc.Result("${venueId}");
-                return vId;
-            }
-            else
-            {
-                return "";
-            }
-        }
-
-        private FourSquareDataModel.Venue GetVenueInfo(string venueId)
-        {
-            var content = "";
-            try
-            {
-                var res = GetContent("GET",
-                                     new Uri("https://api.foursquare.com/v2/venues/" + venueId),
-                                     _authKey,
-                                     ref content);
-
-                if (res == HttpStatusCode.OK)
-                {
-                    FourSquareDataModel.FourSquareData curData = null;
-                    try
-                    {
-                        curData = MyCommon.CreateDataFromJson<FourSquareDataModel.FourSquareData>(content);
-                    }
-                    catch (Exception)
-                    {
-                        return null;
-                    }
-
-                    return curData.Response.Venue;
-                }
-                else
-                {
-                    //FourSquareDataModel.FourSquareData curData = null;
-                    //try
-                    //{
-                    //    curData = CreateDataFromJson<FourSquareDataModel.FourSquareData>(content);
-                    //}
-                    //catch (Exception)
-                    //{
-                    //    return null;;
-                    //}
-                    //MessageBox.Show(res.ToString() + Environment.NewLine + curData.Meta.ErrorType + Environment.NewLine + curData.Meta.ErrorDetail);
-                    return null;
-                }
-            }
-            catch (Exception)
-            {
-                return null;
-            }
-        }
-
-        public string GetMapsUri(string url, ref string refText)
-        {
-            if (!AppendSettingDialog.Instance.IsPreviewFoursquare) return null;
-
-            var urlId = Regex.Replace(url, @"https?://(4sq|foursquare)\.com/", "");
-
-            GlobalLocation location;
-            if (CheckInUrlsVenueCollection.TryGetValue(urlId, out location))
-            {
-                refText = location.LocateInfo;
-                return MapThumb.GetDefaultInstance().CreateStaticMapUrl(location);
-            }
-
-            FourSquareDataModel.Venue curVenue = null;
-            var venueId = GetVenueId(url);
-            if (string.IsNullOrEmpty(venueId)) return null;
-
-            curVenue = GetVenueInfo(venueId);
-            if (curVenue == null) return null;
-
-            var curLocation = new GlobalLocation {Latitude = curVenue.Location.Latitude, Longitude = curVenue.Location.Longitude, LocateInfo = CreateVenueInfoText(curVenue)};
-            //例外発生の場合があるため
-            if (!CheckInUrlsVenueCollection.ContainsKey(urlId)) CheckInUrlsVenueCollection.Add(urlId, curLocation);
-            refText = curLocation.LocateInfo;
-            return MapThumb.GetDefaultInstance().CreateStaticMapUrl(curLocation);
-        }
-
-        private string CreateVenueInfoText(FourSquareDataModel.Venue info)
-        {
-            return info.Name + Environment.NewLine + info.Stats.UsersCount.ToString() + "/" + info.Stats.CheckinsCount.ToString() + Environment.NewLine + info.Location.Address + Environment.NewLine + info.Location.City + info.Location.State + Environment.NewLine + info.Location.Latitude.ToString() + Environment.NewLine + info.Location.Longitude.ToString();
-        }
-        public HttpStatusCode GetContent(string method,
-                    Uri requestUri,
-                    Dictionary<string, string> param,
-                    ref string content)
-        {
-            var webReq  = CreateRequest(method,
-                                        requestUri,
-                                        param);
-            var code = GetResponse(webReq, out content, null);
-            return code;
-        }
-
-    #region "FourSquare DataModel"
-
-        public class FourSquareDataModel
-        {
-            [DataContract]
-            public class FourSquareData
-            {
-                [DataMember(Name = "meta", IsRequired = false)] public Meta Meta;
-                [DataMember(Name = "response", IsRequired = false)] public Response Response;
-            }
-
-            [DataContract]
-            public class Response
-            {
-                [DataMember(Name = "venue", IsRequired = false)] public Venue Venue;
-            }
-
-            [DataContract]
-            public class Venue
-            {
-                [DataMember(Name = "id")] public string Id;
-                [DataMember(Name = "name")] public string Name;
-                [DataMember(Name = "location")] public Location Location;
-                [DataMember(Name = "verified")] public bool Verified;
-                [DataMember(Name = "stats")] public Stats Stats;
-                [DataMember(Name = "mayor")] public Mayor Mayor;
-                [DataMember(Name = "shortUrl")] public string ShortUrl;
-                [DataMember(Name = "timeZone")] public string TimeZone;
-            }
-
-            [DataContract]
-            public class Location
-            {
-                [DataMember(Name = "address")] public string Address;
-                [DataMember(Name = "city")] public string City;
-                [DataMember(Name = "state")] public string State;
-                [DataMember(Name = "lat")] public double Latitude;
-                [DataMember(Name = "lng")] public double Longitude;
-            }
-
-            [DataContract]
-            public class Stats
-            {
-                [DataMember(Name = "checkinsCount")] public int CheckinsCount;
-                [DataMember(Name = "usersCount")] public int UsersCount;
-            }
-
-
-            [DataContract]
-            public class Mayor
-            {
-                [DataMember(Name = "count")] public int Count;
-                [DataMember(Name = "user", IsRequired = false)] public FoursquareUser User;
-            }
-
-            [DataContract]
-            public class FoursquareUser
-            {
-                [DataMember(Name = "id")] public int Id;
-                [DataMember(Name = "firstName")] public string FirstName;
-                [DataMember(Name = "photo")] public string Photo;
-                [DataMember(Name = "gender")] public string Gender;
-                [DataMember(Name = "homeCity")] public string HomeCity;
-            }
-
-            [DataContract]
-            public class Meta
-            {
-                [DataMember(Name = "code")] public int Code;
-                [DataMember(Name = "errorType", IsRequired = false)] public string ErrorType;
-                [DataMember(Name = "errorDetail", IsRequired = false)] public string ErrorDetail;
-            }
-        }
-    #endregion
-
-
-    }
-}

commit e6a3a3dfdad189a7aa70985698da16a3c5e11a83

小数点にピリオド以外の記号を用いるカルチャで double.Parse に失敗する不具合を修正 (thx @Xiatian!)
https://osdn.net/ticket/browse.php?group_id=6526&tid=35878

Fixes: 74df0fc9 ("動作しなくなっていたFoursquareのサムネイル表示を再実装")

OpenTween/Thumbnail/Services/FoursquareCheckin.cs
@@ -21,6 +21,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Net.Http;
 using System.Runtime.Serialization.Json;
 using System.Text.RegularExpressions;
@@ -195,8 +196,8 @@ namespace OpenTween.Thumbnail.Services
 
                 return new GlobalLocation
                 {
-                    Latitude = double.Parse(locationElm.Element("lat").Value),
-                    Longitude = double.Parse(locationElm.Element("lng").Value),
+                    Latitude = double.Parse(locationElm.Element("lat").Value, CultureInfo.InvariantCulture),
+                    Longitude = double.Parse(locationElm.Element("lng").Value, CultureInfo.InvariantCulture),
                 };
             }
         }

OpenTween.Tests/Thumbnail/Services/FoursquareCheckinTest.cs
@@ -21,6 +21,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -229,6 +230,39 @@ namespace OpenTween.Thumbnail.Services
 }";
             var jsonBytes = Encoding.UTF8.GetBytes(json);
             var location = FoursquareCheckin.ParseIntoLocation(jsonBytes);
+
+            Assert.NotNull(location);
+            Assert.Equal(34.35067978344854, location.Latitude);
+            Assert.Equal(134.04693603515625, location.Longitude);
+        }
+
+        [Fact]
+        public void ParseInLocation_CultureTest()
+        {
+            var json = @"{
+  ""meta"": { ""code"": 200 },
+  ""response"": {
+    ""checkin"": {
+      ""id"": ""xxxxxxxxx"",
+      ""type"": ""checkin"",
+      ""venue"": {
+        ""id"": ""4b73dedcf964a5206bbe2de3"",
+        ""name"": ""高松駅 (Takamatsu Sta.)"",
+        ""location"": {
+          ""lat"": 34.35067978344854,
+          ""lng"": 134.04693603515625
+        }
+      }
+    }
+  }
+}";
+            var origCulture = Thread.CurrentThread.CurrentCulture;
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("ru-RU");
+
+            var jsonBytes = Encoding.UTF8.GetBytes(json);
+            var location = FoursquareCheckin.ParseIntoLocation(jsonBytes);
+
+            Thread.CurrentThread.CurrentCulture = origCulture;
 
             Assert.NotNull(location);
             Assert.Equal(34.35067978344854, location.Latitude);