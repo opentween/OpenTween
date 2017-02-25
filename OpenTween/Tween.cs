opentween/OpenTween

commit 8dbad640e515030bb6dbfc9c680482486c9a9179

HttpClientを使用してShortUrlクラスの実装を修正

OpenTween/Tween.cs
@@ -2178,7 +2178,7 @@ namespace OpenTween
             return cl;
         }
 
-        private void PostButton_Click(object sender, EventArgs e)
+        private async void PostButton_Click(object sender, EventArgs e)
         {
             if (StatusText.Text.Trim().Length == 0)
             {
@@ -2211,7 +2211,7 @@ namespace OpenTween
             if (SettingDialog.Nicoms)
             {
                 StatusText.SelectionStart = StatusText.Text.Length;
-                UrlConvert(MyCommon.UrlConverter.Nicoms);
+                await UrlConvertAsync(MyCommon.UrlConverter.Nicoms);
             }
             //if (SettingDialog.UrlConvertAuto)
             //{
@@ -9923,7 +9923,7 @@ namespace OpenTween
             _modifySettingLocal = true;
         }
 
-        private bool UrlConvert(MyCommon.UrlConverter Converter_Type)
+        private async Task<bool> UrlConvertAsync(MyCommon.UrlConverter Converter_Type)
         {
             //t.coで投稿時自動短縮する場合は、外部サービスでの短縮禁止
             //if (SettingDialog.UrlConvertAuto && SettingDialog.ShortenTco) return;
@@ -9955,7 +9955,9 @@ namespace OpenTween
                         //短縮URL変換 日本語を含むかもしれないのでURLエンコードする
                         try
                         {
-                            result = ShortUrl.Instance.ShortenUrl(Converter_Type, tmp);
+                            var srcUri = new Uri(MyCommon.urlEncodeMultibyteChar(tmp));
+                            var resultUri = await ShortUrl.Instance.ShortenUrlAsync(Converter_Type, srcUri);
+                            result = resultUri.ToString();
                         }
                         catch (WebApiException e)
                         {
@@ -10022,7 +10024,9 @@ namespace OpenTween
                         //短縮URL変換 日本語を含むかもしれないのでURLエンコードする
                         try
                         {
-                            result = ShortUrl.Instance.ShortenUrl(Converter_Type, tmp);
+                            var srcUri = new Uri(MyCommon.urlEncodeMultibyteChar(tmp));
+                            var resultUri = await ShortUrl.Instance.ShortenUrlAsync(Converter_Type, srcUri);
+                            result = resultUri.ToString();
                         }
                         catch (WebApiException e)
                         {
@@ -10079,29 +10083,29 @@ namespace OpenTween
             }
         }
 
-        private void TinyURLToolStripMenuItem_Click(object sender, EventArgs e)
+        private async void TinyURLToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            UrlConvert(MyCommon.UrlConverter.TinyUrl);
+            await UrlConvertAsync(MyCommon.UrlConverter.TinyUrl);
         }
 
-        private void IsgdToolStripMenuItem_Click(object sender, EventArgs e)
+        private async void IsgdToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            UrlConvert(MyCommon.UrlConverter.Isgd);
+            await UrlConvertAsync(MyCommon.UrlConverter.Isgd);
         }
 
-        private void TwurlnlToolStripMenuItem_Click(object sender, EventArgs e)
+        private async void TwurlnlToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            UrlConvert(MyCommon.UrlConverter.Twurl);
+            await UrlConvertAsync(MyCommon.UrlConverter.Twurl);
         }
 
-        private void UxnuMenuItem_Click(object sender, EventArgs e)
+        private async void UxnuMenuItem_Click(object sender, EventArgs e)
         {
-            UrlConvert(MyCommon.UrlConverter.Uxnu);
+            await UrlConvertAsync(MyCommon.UrlConverter.Uxnu);
         }
 
-        private void UrlConvertAutoToolStripMenuItem_Click(object sender, EventArgs e) 
+        private async void UrlConvertAutoToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            if (!UrlConvert(SettingDialog.AutoShortUrlFirst))
+            if (!await UrlConvertAsync(SettingDialog.AutoShortUrlFirst))
             {
                 MyCommon.UrlConverter svc = SettingDialog.AutoShortUrlFirst;
                 Random rnd = new Random();
@@ -10111,7 +10115,7 @@ namespace OpenTween
                     svc = (MyCommon.UrlConverter)rnd.Next(System.Enum.GetNames(typeof(MyCommon.UrlConverter)).Length);
                 }
                 while (svc == SettingDialog.AutoShortUrlFirst || svc == MyCommon.UrlConverter.Nicoms || svc == MyCommon.UrlConverter.Unu);
-                UrlConvert(svc);
+                await UrlConvertAsync(svc);
             }
         }
 
@@ -11166,14 +11170,14 @@ namespace OpenTween
             TabRename(ref _rclickTabName);
         }
 
-        private void BitlyToolStripMenuItem_Click(object sender, EventArgs e)
+        private async void BitlyToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            UrlConvert(MyCommon.UrlConverter.Bitly);
+            await UrlConvertAsync(MyCommon.UrlConverter.Bitly);
         }
 
-        private void JmpToolStripMenuItem_Click(object sender, EventArgs e)
+        private async void JmpToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            UrlConvert(MyCommon.UrlConverter.Jmp);
+            await UrlConvertAsync(MyCommon.UrlConverter.Jmp);
         }
 
 

OpenTween.Tests/OpenTween.Tests.csproj
@@ -42,6 +42,7 @@
     <Reference Include="System" />
     <Reference Include="System.Core" />
     <Reference Include="System.Drawing" />
+    <Reference Include="System.Net.Http" />
     <Reference Include="System.Runtime.Serialization" />
     <Reference Include="System.Windows.Forms" />
     <Reference Include="System.Xml.Linq" />
@@ -73,6 +74,8 @@
     <Compile Include="PostClassTest.cs" />
     <Compile Include="PostFilterRuleVersion113DeserializeTest.cs" />
     <Compile Include="Properties\AssemblyInfo.cs" />
+    <Compile Include="RegexAsyncTest.cs" />
+    <Compile Include="ShortUrlTest.cs" />
     <Compile Include="TabInformationTest.cs" />
     <Compile Include="TabsDialogTest.cs" />
     <Compile Include="TestUtils.cs" />

OpenTween.Tests/RegexAsyncTest.cs
@@ -1,0 +1,46 @@
+﻿// OpenTween - Client of Twitter
+// Copyright (c) 2014 kim_upsilon (@kim_upsilon) <https://upsilo.net/~upsilon/>
+// All rights reserved.
+//
+// This file is part of OpenTween.
+//
+// This program is free software; you can redistribute it and/or modify it
+// under the terms of the GNU General public License as published by the Free
+// Software Foundation; either version 3 of the License, or (at your option)
+// any later version.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General public License
+// for more details.
+//
+// You should have received a copy of the GNU General public License along
+// with this program. if (not, see <http://www.gnu.org/licenses/>, or write to
+// the Free Software Foundation, Inc., 51 Franklin Street - Fifth Floor,
+// Boston, MA 02110-1301, USA.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Extensions;
+
+namespace OpenTween
+{
+    public class RegexAsyncTest
+    {
+        [Fact]
+        public async Task ReplaceAsync_Test()
+        {
+            var origText = "123aaa456bbb";
+
+            var regex = new Regex(@"[A-Za-z]");
+            var result = await regex.ReplaceAsync(origText, x => Task.FromResult(x.Value.ToUpper()));
+
+            Assert.Equal("123AAA456BBB", result);
+        }
+    }
+}

OpenTween.Tests/ShortUrlTest.cs
@@ -1,0 +1,240 @@
+﻿// OpenTween - Client of Twitter
+// Copyright (c) 2014 kim_upsilon (@kim_upsilon) <https://upsilo.net/~upsilon/>
+// All rights reserved.
+//
+// This file is part of OpenTween.
+//
+// This program is free software; you can redistribute it and/or modify it
+// under the terms of the GNU General public License as published by the Free
+// Software Foundation; either version 3 of the License, or (at your option)
+// any later version.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General public License
+// for more details.
+//
+// You should have received a copy of the GNU General public License along
+// with this program. if (not, see <http://www.gnu.org/licenses/>, or write to
+// the Free Software Foundation, Inc., 51 Franklin Street - Fifth Floor,
+// Boston, MA 02110-1301, USA.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+using Xunit.Extensions;
+
+namespace OpenTween
+{
+    public class ShortUrlTest
+    {
+        class HttpMessageHandlerMock : HttpMessageHandler
+        {
+            public readonly Queue<Func<HttpRequestMessage, Task<HttpResponseMessage>>> Queue =
+                new Queue<Func<HttpRequestMessage, Task<HttpResponseMessage>>>();
+
+            protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+            {
+                var handler = this.Queue.Dequeue();
+                return handler(request);
+            }
+        }
+
+#pragma warning disable 1998 // awaitが無いasyncラムダ式に対する警告を抑制
+
+        [Fact]
+        public async Task ExpandUrlAsync_Test()
+        {
+            var handler = new HttpMessageHandlerMock();
+            var shortUrl = new ShortUrl(new HttpClient(handler));
+
+            // http://t.co/hoge1 -> http://example.com/hoge2
+            handler.Queue.Enqueue(async x =>
+            {
+                Assert.Equal(HttpMethod.Head, x.Method);
+                Assert.Equal(new Uri("http://t.co/hoge1"), x.RequestUri);
+
+                return this.CreateRedirectResponse("http://example.com/hoge2");
+            });
+
+            Assert.Equal(new Uri("http://example.com/hoge2"),
+                await shortUrl.ExpandUrlAsync(new Uri("http://t.co/hoge1")));
+        }
+
+        [Fact]
+        public async Task ExpandUrlAsync_DisableExpandingTest()
+        {
+            var handler = new HttpMessageHandlerMock();
+            var shortUrl = new ShortUrl(new HttpClient(handler));
+
+            shortUrl.DisableExpanding = true;
+
+            // http://t.co/hoge1 -> http://example.com/hoge2
+            handler.Queue.Enqueue(async x =>
+            {
+                // このリクエストは実行されないはず
+                Assert.True(false);
+                return this.CreateRedirectResponse("http://example.com/hoge2");
+            });
+
+            Assert.Equal(new Uri("http://t.co/hoge1"),
+                await shortUrl.ExpandUrlAsync(new Uri("http://t.co/hoge1")));
+        }
+
+        [Fact]
+        public async Task ExpandUrlAsync_RecursiveTest()
+        {
+            var handler = new HttpMessageHandlerMock();
+            var shortUrl = new ShortUrl(new HttpClient(handler));
+
+            // http://t.co/hoge1 -> http://bit.ly/hoge2
+            handler.Queue.Enqueue(async x =>
+            {
+                Assert.Equal(HttpMethod.Head, x.Method);
+                Assert.Equal(new Uri("http://t.co/hoge1"), x.RequestUri);
+
+                return this.CreateRedirectResponse("http://bit.ly/hoge2");
+            });
+
+            // http://bit.ly/hoge2 -> http://example.com/hoge3
+            handler.Queue.Enqueue(async x =>
+            {
+                Assert.Equal(HttpMethod.Head, x.Method);
+                Assert.Equal(new Uri("http://bit.ly/hoge2"), x.RequestUri);
+
+                return this.CreateRedirectResponse("http://example.com/hoge3");
+            });
+
+            Assert.Equal(new Uri("http://example.com/hoge3"),
+                await shortUrl.ExpandUrlAsync(new Uri("http://t.co/hoge1")));
+        }
+
+        [Fact]
+        public async Task ExpandUrlAsync_RecursiveLimitTest()
+        {
+            var handler = new HttpMessageHandlerMock();
+            var shortUrl = new ShortUrl(new HttpClient(handler));
+
+            // http://t.co/hoge1 -> http://bit.ly/hoge2
+            handler.Queue.Enqueue(async x =>
+            {
+                Assert.Equal(HttpMethod.Head, x.Method);
+                Assert.Equal(new Uri("http://t.co/hoge1"), x.RequestUri);
+
+                return this.CreateRedirectResponse("http://bit.ly/hoge2");
+            });
+
+            // http://bit.ly/hoge2 -> http://tinyurl.com/hoge3
+            handler.Queue.Enqueue(async x =>
+            {
+                Assert.Equal(HttpMethod.Head, x.Method);
+                Assert.Equal(new Uri("http://bit.ly/hoge2"), x.RequestUri);
+
+                return this.CreateRedirectResponse("http://tinyurl.com/hoge3");
+            });
+
+            // http://tinyurl.com/hoge3 -> http://example.com/hoge4
+            handler.Queue.Enqueue(async x =>
+            {
+                // このリクエストは実行されないはず
+                Assert.True(false);
+                return this.CreateRedirectResponse("http://example.com/hoge4");
+            });
+
+            Assert.Equal(new Uri("http://tinyurl.com/hoge3"),
+                await shortUrl.ExpandUrlAsync(new Uri("http://t.co/hoge1"), redirectLimit: 2));
+        }
+
+        [Fact]
+        public async Task ExpandUrlAsync_HttpErrorTest()
+        {
+            var handler = new HttpMessageHandlerMock();
+            var shortUrl = new ShortUrl(new HttpClient(handler));
+
+            // http://t.co/hoge1 -> 503 Service Unavailable
+            handler.Queue.Enqueue(async x =>
+            {
+                return new HttpResponseMessage(HttpStatusCode.ServiceUnavailable);
+            });
+
+            Assert.Equal(new Uri("http://t.co/hoge1"),
+                await shortUrl.ExpandUrlAsync(new Uri("http://t.co/hoge1")));
+        }
+
+        [Fact]
+        public async Task ExpandUrlHtmlAsync_Test()
+        {
+            var handler = new HttpMessageHandlerMock();
+            var shortUrl = new ShortUrl(new HttpClient(handler));
+
+            // http://t.co/hoge1 -> http://example.com/hoge2
+            handler.Queue.Enqueue(async x =>
+            {
+                Assert.Equal(HttpMethod.Head, x.Method);
+                Assert.Equal(new Uri("http://t.co/hoge1"), x.RequestUri);
+
+                return this.CreateRedirectResponse("http://example.com/hoge2");
+            });
+
+            Assert.Equal("<a href=\"http://example.com/hoge2\">hogehoge</a>",
+                await shortUrl.ExpandUrlHtmlAsync("<a href=\"http://t.co/hoge1\">hogehoge</a>"));
+        }
+
+        private HttpResponseMessage CreateRedirectResponse(string uriStr)
+        {
+            var response = new HttpResponseMessage(HttpStatusCode.TemporaryRedirect);
+            response.Headers.Location = new Uri(uriStr);
+            return response;
+        }
+
+        [Fact]
+        public async Task ShortenUrlAsync_TinyUrlTest()
+        {
+            var handler = new HttpMessageHandlerMock();
+            var shortUrl = new ShortUrl(new HttpClient(handler));
+
+            handler.Queue.Enqueue(async x =>
+            {
+                Assert.Equal(HttpMethod.Post, x.Method);
+                Assert.Equal(new Uri("http://tinyurl.com/api-create.php"), x.RequestUri);
+                Assert.Equal("url=http%3A%2F%2Fexample.com%2Fhogehoge", await x.Content.ReadAsStringAsync());
+
+                return new HttpResponseMessage(HttpStatusCode.OK)
+                {
+                    Content = new ByteArrayContent(Encoding.UTF8.GetBytes("http://tinyurl.com/hoge")),
+                };
+            });
+
+            Assert.Equal(new Uri("http://tinyurl.com/hoge"),
+                await shortUrl.ShortenUrlAsync(MyCommon.UrlConverter.TinyUrl, new Uri("http://example.com/hogehoge")));
+        }
+
+        [Fact]
+        public async Task ShortenUrlAsync_UxnuUrlTest()
+        {
+            var handler = new HttpMessageHandlerMock();
+            var shortUrl = new ShortUrl(new HttpClient(handler));
+
+            handler.Queue.Enqueue(async x =>
+            {
+                Assert.Equal(HttpMethod.Get, x.Method);
+                Assert.Equal("http://ux.nu/api/short?format=plain&url=http://example.com/hogehoge",
+                    x.RequestUri.ToString());
+
+                return new HttpResponseMessage(HttpStatusCode.OK)
+                {
+                    Content = new ByteArrayContent(Encoding.UTF8.GetBytes("http://ux.nu/hoge")),
+                };
+            });
+
+            Assert.Equal(new Uri("http://ux.nu/hoge"),
+                await shortUrl.ShortenUrlAsync(MyCommon.UrlConverter.Uxnu, new Uri("http://example.com/hogehoge")));
+        }
+    }
+}

OpenTween/MyCommon.cs
@@ -42,6 +42,8 @@ using System.Runtime.Serialization.Json;
 using System.Reflection;
 using System.Diagnostics;
 using System.Text.RegularExpressions;
+using System.Net;
+using System.Net.Http;
 using System.Net.NetworkInformation;
 using System.Runtime.InteropServices;
 using OpenTween.Api;
@@ -962,6 +964,41 @@ namespace OpenTween
         public static string GetStatusUrl(string screenName, long statusId)
         {
             return TwitterUrl + screenName + "/status/" + statusId.ToString();
+        }
+
+        /// <summary>
+        /// OpenTween 内で共通して使う設定を施した HttpClient インスタンスを作成します
+        /// </summary>
+        public static HttpClient CreateHttpClient()
+        {
+            return CreateHttpClient(new HttpClientHandler());
+        }
+
+        /// <summary>
+        /// OpenTween 内で共通して使う設定を施した HttpClient インスタンスを作成します
+        /// </summary>
+        public static HttpClient CreateHttpClient(HttpClientHandler handler)
+        {
+            switch (HttpConnection.proxyKind)
+            {
+                case HttpConnection.ProxyType.None:
+                    handler.UseProxy = false;
+                    break;
+                case HttpConnection.ProxyType.Specified:
+                    handler.UseProxy = true;
+                    handler.Proxy = HttpConnection.proxy;
+                    break;
+                case HttpConnection.ProxyType.IE:
+                default:
+                    handler.UseProxy = true;
+                    handler.Proxy = WebRequest.GetSystemWebProxy();
+                    break;
+            }
+
+            var client = new HttpClient(handler);
+            client.DefaultRequestHeaders.Add("User-Agent", MyCommon.GetUserAgentString());
+
+            return client;
         }
     }
 }

OpenTween/OpenTween.csproj
@@ -50,7 +50,9 @@
     <Reference Include="System" />
     <Reference Include="System.Core" />
     <Reference Include="System.Drawing" />
+    <Reference Include="System.Net.Http" />
     <Reference Include="System.Runtime.Serialization" />
+    <Reference Include="System.Web" />
     <Reference Include="System.Windows.Forms" />
     <Reference Include="System.Xml.Linq" />
     <Reference Include="System.Data.DataSetExtensions" />
@@ -153,6 +155,7 @@
     <Compile Include="OTWebClient.cs">
       <SubType>Component</SubType>
     </Compile>
+    <Compile Include="RegexAsync.cs" />
     <Compile Include="Setting\Panel\ActionPanel.cs">
       <SubType>UserControl</SubType>
     </Compile>

OpenTween/RegexAsync.cs
@@ -1,0 +1,52 @@
+﻿// OpenTween - Client of Twitter
+// Copyright (c) 2014 kim_upsilon (@kim_upsilon) <https://upsilo.net/~upsilon/>
+// All rights reserved.
+//
+// This file is part of OpenTween.
+//
+// This program is free software; you can redistribute it and/or modify it
+// under the terms of the GNU General public License as published by the Free
+// Software Foundation; either version 3 of the License, or (at your option)
+// any later version.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General public License
+// for more details.
+//
+// You should have received a copy of the GNU General public License along
+// with this program. if (not, see <http://www.gnu.org/licenses/>, or write to
+// the Free Software Foundation, Inc., 51 Franklin Street - Fifth Floor,
+// Boston, MA 02110-1301, USA.
+
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace OpenTween
+{
+    public static class RegexAsync
+    {
+        public static async Task<string> ReplaceAsync(this Regex regex, string input, Func<Match, Task<string>> evaluator)
+        {
+            var matches = regex.Matches(input).Cast<Match>();
+
+            var evaluatorTasks = matches
+                .GroupBy(x => x.Value) // ToDictionaryする際に key が重複しないようにする
+                .Select(async x => new
+                {
+                    Key = x.Key,
+                    Value = await evaluator(x.First()).ConfigureAwait(false),
+                });
+
+            var replacements = (await Task.WhenAll(evaluatorTasks).ConfigureAwait(false))
+                .ToDictionary(x => x.Key, x => x.Value);
+
+            return regex.Replace(input, x => replacements[x.Value]);
+        }
+    }
+}

commit 00011c55db151746847ea9cfb4ce3b344b5fdbb8

短縮URLが生成されるまでの間に投稿欄のテキストが変更されるとエラーが発生する不具合を修正
https://osdn.net/ticket/browse.php?group_id=6526&tid=36440

Fixes: 8dbad640 ("HttpClientを使用してShortUrlクラスの実装を修正")

OpenTween/Tween.cs
@@ -9286,7 +9286,12 @@ namespace OpenTween
                     {
                         urlUndo undotmp = new urlUndo();
 
-                        StatusText.Select(StatusText.Text.IndexOf(tmp, StringComparison.Ordinal), tmp.Length);
+                        // 短縮 URL が生成されるまでの間に投稿欄から元の URL が削除されていたら中断する
+                        var origUrlIndex = this.StatusText.Text.IndexOf(tmp, StringComparison.Ordinal);
+                        if (origUrlIndex == -1)
+                            return false;
+
+                        StatusText.Select(origUrlIndex, tmp.Length);
                         StatusText.SelectedText = result;
 
                         //undoバッファにセット
@@ -9362,7 +9367,12 @@ namespace OpenTween
 
                     if (!string.IsNullOrEmpty(result))
                     {
-                        StatusText.Select(StatusText.Text.IndexOf(mt.Result("${url}"), StringComparison.Ordinal), mt.Result("${url}").Length);
+                        // 短縮 URL が生成されるまでの間に投稿欄から元の URL が削除されていたら中断する
+                        var origUrlIndex = this.StatusText.Text.IndexOf(mt.Result("${url}"), StringComparison.Ordinal);
+                        if (origUrlIndex == -1)
+                            return false;
+
+                        StatusText.Select(origUrlIndex, mt.Result("${url}").Length);
                         StatusText.SelectedText = result;
                         //undoバッファにセット
                         undotmp.Before = mt.Result("${url}");