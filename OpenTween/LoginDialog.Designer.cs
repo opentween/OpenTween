bcb587d00cfc8978f2440423f29499640d87095e
Kimura Youichi <kim.upsilon@bucyou.net>
2017-04-28 02:06:08 +0900

    bit.ly のアクセストークン取得に使用する認証ダイアログを追加

---
+++ OpenTween/LoginDialog.Designer.cs
@@ -0,0 +1,107 @@
+﻿namespace OpenTween
+{
+    partial class LoginDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LoginDialog));
+            this.label1 = new System.Windows.Forms.Label();
+            this.textboxLoginName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textboxPassword = new System.Windows.Forms.TextBox();
+            this.buttonLogin = new System.Windows.Forms.Button();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // textboxLoginName
+            // 
+            resources.ApplyResources(this.textboxLoginName, "textboxLoginName");
+            this.textboxLoginName.Name = "textboxLoginName";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // textboxPassword
+            // 
+            resources.ApplyResources(this.textboxPassword, "textboxPassword");
+            this.textboxPassword.Name = "textboxPassword";
+            this.textboxPassword.UseSystemPasswordChar = true;
+            // 
+            // buttonLogin
+            // 
+            resources.ApplyResources(this.buttonLogin, "buttonLogin");
+            this.buttonLogin.Name = "buttonLogin";
+            this.buttonLogin.UseVisualStyleBackColor = true;
+            this.buttonLogin.Click += new System.EventHandler(this.buttonLogin_Click);
+            // 
+            // buttonCancel
+            // 
+            resources.ApplyResources(this.buttonCancel, "buttonCancel");
+            this.buttonCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            // 
+            // LoginDialog
+            // 
+            this.AcceptButton = this.buttonLogin;
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.buttonCancel;
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.buttonLogin);
+            this.Controls.Add(this.textboxPassword);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textboxLoginName);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "LoginDialog";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textboxLoginName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textboxPassword;
+        private System.Windows.Forms.Button buttonLogin;
+        private System.Windows.Forms.Button buttonCancel;
+    }
+}
---
+++ OpenTween/LoginDialog.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OpenTween
+{
+    public partial class LoginDialog : OTBaseForm
+    {
+        public string LoginName => this.textboxLoginName.Text;
+        public string Password => this.textboxPassword.Text;
+
+        public Func<Task<bool>> LoginCallback { get; set; } = null;
+        public bool LoginSuccessed { get; set; } = false;
+
+        public LoginDialog()
+            => this.InitializeComponent();
+
+        private async void buttonLogin_Click(object sender, EventArgs e)
+        {
+            if (this.LoginCallback == null)
+                return;
+
+            try
+            {
+                using (ControlTransaction.Disabled(this))
+                {
+                    // AcceptButton によって自動でフォームが閉じられるのを抑制する
+                    this.AcceptButton = null;
+
+                    this.LoginSuccessed = await this.LoginCallback();
+                    if (this.LoginSuccessed)
+                        this.DialogResult = DialogResult.OK;
+                }
+            }
+            finally
+            {
+                this.AcceptButton = this.buttonLogin;
+            }
+        }
+    }
+}
