opentween/OpenTween

commit 1da38617c0530ae5dcf3417cf78b01eda589db33

発言一覧の表示位置の固定に ListView.TopItem を使用する
TopItem を使用した場合、内部でスクロール位置の差分を計算してスクロールされるため、
EnsureVisible を 2 回実行する場合と比べて余分なスクロールを軽減できる。

http://referencesource.microsoft.com/System.Windows.Forms/winforms/Managed/System/WinForms/ListView.cs.html#1283588d0806650a

OpenTween/Tween.cs
@@ -1435,11 +1435,9 @@ namespace OpenTween
                                 break;
                             default:
                                 //表示位置キープ
-                                if (_curList.VirtualListSize > 0 && _statuses.Tabs[_curTab.Text].IndexOf(topId) > -1)
-                                {
-                                    _curList.EnsureVisible(_curList.VirtualListSize - 1);
-                                    _curList.EnsureVisible(_statuses.Tabs[_curTab.Text].IndexOf(topId));
-                                }
+                                var topIndex = _statuses.Tabs[_curTab.Text].IndexOf(topId);
+                                if (_curList.VirtualListSize > 0 && topIndex > -1)
+                                    this._curList.TopItem = _curList.Items[topIndex];
                                 break;
                         }
                     }

commit ca3e80a7816850cbe2fcd6f6bf1c84d4223d4374

ListView.set_TopItem内部でNullReferenceExceptionが発生する問題に対処
原因は未だ不明なものの、内部で ListView.get_TopItem が null を返していると考えられる。
例外が発生した場合は EnsureVisible を二回呼び出してスクロールする。

https://osdn.net/ticket/browse.php?group_id=6526&tid=36219

Fixes: 1da38617 ("発言一覧の表示位置の固定に ListView.TopItem を使用する")

OpenTween/Tween.cs
@@ -1630,7 +1630,18 @@ namespace OpenTween
                 case ScrollLockMode.FixedToItem:
                     var topIndex = listScroll.TopItemStatusId != null ? tab.IndexOf(listScroll.TopItemStatusId.Value) : -1;
                     if (topIndex != -1)
-                        listView.TopItem = listView.Items[topIndex];
+                    {
+                        var topItem = listView.Items[topIndex];
+                        try
+                        {
+                            listView.TopItem = topItem;
+                        }
+                        catch (NullReferenceException)
+                        {
+                            listView.EnsureVisible(listView.VirtualListSize - 1);
+                            listView.EnsureVisible(topIndex);
+                        }
+                    }
                     break;
                 case ScrollLockMode.None:
                 default: