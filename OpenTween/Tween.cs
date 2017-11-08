opentween/OpenTween

commit ba530b422553c169bdce155de26fcf7372119fc6

自分宛のリプライの場合に先頭の「@... 」を除去する (in_reply_to_status_id は保持する)

OpenTween/Tween.cs
@@ -4757,6 +4757,14 @@ namespace OpenTween
             if (statusText.Contains("RT @"))
                 disableFooter = true;
 
+            // 自分宛のリプライの場合は先頭の「@screen_name 」の部分を除去する (in_reply_to_status_id は維持される)
+            if (this.inReplyTo != null && this.inReplyTo.Item2 == this.tw.Username)
+            {
+                var mentionSelf = $"@{this.tw.Username} ";
+                if (statusText.Length > mentionSelf.Length && statusText.StartsWith(mentionSelf, StringComparison.OrdinalIgnoreCase))
+                    statusText = statusText.Substring(mentionSelf.Length);
+            }
+
             var header = "";
             var footer = "";
 

commit 68b8120d149b84528589d7a0298bd541d55681f4

自分宛のリプライかつ画像のみのツイートを投稿した場合に先頭の「@... 」が除去されない不具合を修正 (thx @mino90h!)
https://twitter.com/mino90h/status/927570750029971457

Fixes: ba530b42 ("自分宛のリプライの場合に先頭の「@... 」を除去する (in_reply_to_status_id は保持する)")

OpenTween/Tween.cs
@@ -4761,8 +4761,11 @@ namespace OpenTween
             if (this.inReplyTo != null && this.inReplyTo.Item2 == this.tw.Username)
             {
                 var mentionSelf = $"@{this.tw.Username} ";
-                if (statusText.Length > mentionSelf.Length && statusText.StartsWith(mentionSelf, StringComparison.OrdinalIgnoreCase))
-                    statusText = statusText.Substring(mentionSelf.Length);
+                if (statusText.StartsWith(mentionSelf, StringComparison.OrdinalIgnoreCase))
+                {
+                    if (statusText.Length > mentionSelf.Length || this.GetSelectedImageService() != null)
+                        statusText = statusText.Substring(mentionSelf.Length);
+                }
             }
 
             var header = "";
@@ -4817,8 +4820,8 @@ namespace OpenTween
             //文字数カウント
             var remainCount = this.tw.GetTextLengthRemain(statusText);
 
-            var uploadService = this.ImageSelector.SelectedService;
-            if (this.ImageSelector.Visible && uploadService != null)
+            var uploadService = this.GetSelectedImageService();
+            if (uploadService != null)
             {
                 // TODO: ImageSelector で選択中の画像の枚数が mediaCount 引数に渡るようにする
                 remainCount -= uploadService.GetReservedTextLength(1);
@@ -4826,6 +4829,9 @@ namespace OpenTween
 
             return remainCount;
         }
+
+        private IMediaUploadService GetSelectedImageService()
+            => this.ImageSelector.Visible ? this.ImageSelector.SelectedService : null;
 
         private void MyList_CacheVirtualItems(object sender, CacheVirtualItemsEventArgs e)
         {