opentween/OpenTween

commit 33ea8c0ba0d1fefeef89c31a24ba9e72845d71df

ツイート文字数の280文字への上限緩和 (weightedLength) に対応
https://developer.twitter.com/en/docs/developer-utilities/twitter-text

OpenTween/Twitter.cs
@@ -149,6 +149,7 @@ namespace OpenTween
 
         public TwitterApi Api { get; }
         public TwitterConfiguration Configuration { get; private set; }
+        public TwitterTextConfiguration TextConfiguration { get; private set; }
 
         delegate void GetIconImageDelegate(PostClass post);
         private readonly object LockObj = new object();
@@ -181,6 +182,7 @@ namespace OpenTween
         {
             this.Api = api;
             this.Configuration = TwitterConfiguration.DefaultConfiguration();
+            this.TextConfiguration = TwitterTextConfiguration.DefaultConfiguration();
         }
 
         public TwitterApiAccessLevel AccessLevel
@@ -1708,12 +1710,12 @@ namespace OpenTween
         {
             var matchDm = Twitter.DMSendTextRegex.Match(postText);
             if (matchDm.Success)
-                return this.GetTextLengthRemainInternal(matchDm.Groups["body"].Value, isDm: true);
+                return this.GetTextLengthRemainDM(matchDm.Groups["body"].Value);
 
-            return this.GetTextLengthRemainInternal(postText, isDm: false);
+            return this.GetTextLengthRemainWeighted(postText);
         }
 
-        private int GetTextLengthRemainInternal(string postText, bool isDm)
+        private int GetTextLengthRemainDM(string postText)
         {
             var textLength = 0;
 
@@ -1738,10 +1740,54 @@ namespace OpenTween
                 textLength += shortUrlLength - url.Length;
             }
 
-            if (isDm)
-                return this.Configuration.DmTextCharacterLimit - textLength;
-            else
-                return 140 - textLength;
+            return this.Configuration.DmTextCharacterLimit - textLength;
+        }
+
+        private int GetTextLengthRemainWeighted(string postText)
+        {
+            var config = this.TextConfiguration;
+            var totalWeight = 0;
+
+            var urls = TweetExtractor.ExtractUrlEntities(postText).ToArray();
+
+            var pos = 0;
+            while (pos < postText.Length)
+            {
+                var urlEntity = urls.FirstOrDefault(x => x.Indices[0] == pos);
+                if (urlEntity != null)
+                {
+                    totalWeight += config.TransformedURLLength * config.Scale;
+
+                    var urlLength = urlEntity.Indices[1] - urlEntity.Indices[0];
+                    pos += urlLength;
+
+                    continue;
+                }
+
+                var codepoint = char.ConvertToUtf32(postText, pos);
+                var weight = config.DefaultWeight;
+
+                foreach (var weightRange in config.Ranges)
+                {
+                    if (codepoint >= weightRange.Start && codepoint <= weightRange.End)
+                    {
+                        weight = weightRange.Weight;
+                        break;
+                    }
+                }
+
+                totalWeight += weight;
+
+                var isSurrogatePair = codepoint > 0xffff;
+                if (isSurrogatePair)
+                    pos += 2; // サロゲートペアの場合は2文字分進める
+                else
+                    pos++;
+            }
+
+            var remainWeight = config.MaxWeightedTweetLength * config.Scale - totalWeight;
+
+            return remainWeight / config.Scale;
         }
 
 

OpenTween.Tests/TwitterTest.cs
@@ -327,8 +327,8 @@ namespace OpenTween
         {
             using (var twitter = new Twitter())
             {
-                Assert.Equal(140, twitter.GetTextLengthRemain(""));
-                Assert.Equal(132, twitter.GetTextLengthRemain("hogehoge"));
+                Assert.Equal(280, twitter.GetTextLengthRemain(""));
+                Assert.Equal(272, twitter.GetTextLengthRemain("hogehoge"));
             }
         }
 
@@ -343,6 +343,13 @@ namespace OpenTween
 
                 Assert.Equal(10000, twitter.GetTextLengthRemain("D twitter "));
                 Assert.Equal(9992, twitter.GetTextLengthRemain("D twitter hogehoge"));
+
+                // t.co に短縮される分の文字数を考慮
+                twitter.Configuration.ShortUrlLength = 20;
+                Assert.Equal(9971, twitter.GetTextLengthRemain("D twitter hogehoge http://example.com/"));
+
+                twitter.Configuration.ShortUrlLengthHttps = 21;
+                Assert.Equal(9970, twitter.GetTextLengthRemain("D twitter hogehoge https://example.com/"));
             }
         }
 
@@ -352,15 +359,14 @@ namespace OpenTween
             using (var twitter = new Twitter())
             {
                 // t.co に短縮される分の文字数を考慮
-                twitter.Configuration.ShortUrlLength = 20;
-                Assert.Equal(120, twitter.GetTextLengthRemain("http://example.com/"));
-                Assert.Equal(120, twitter.GetTextLengthRemain("http://example.com/hogehoge"));
-                Assert.Equal(111, twitter.GetTextLengthRemain("hogehoge http://example.com/"));
+                twitter.TextConfiguration.TransformedURLLength = 20;
+                Assert.Equal(260, twitter.GetTextLengthRemain("http://example.com/"));
+                Assert.Equal(260, twitter.GetTextLengthRemain("http://example.com/hogehoge"));
+                Assert.Equal(251, twitter.GetTextLengthRemain("hogehoge http://example.com/"));
 
-                twitter.Configuration.ShortUrlLengthHttps = 21;
-                Assert.Equal(119, twitter.GetTextLengthRemain("https://example.com/"));
-                Assert.Equal(119, twitter.GetTextLengthRemain("https://example.com/hogehoge"));
-                Assert.Equal(110, twitter.GetTextLengthRemain("hogehoge https://example.com/"));
+                Assert.Equal(260, twitter.GetTextLengthRemain("https://example.com/"));
+                Assert.Equal(260, twitter.GetTextLengthRemain("https://example.com/hogehoge"));
+                Assert.Equal(251, twitter.GetTextLengthRemain("hogehoge https://example.com/"));
             }
         }
 
@@ -370,15 +376,15 @@ namespace OpenTween
             using (var twitter = new Twitter())
             {
                 // t.co に短縮される分の文字数を考慮
-                twitter.Configuration.ShortUrlLength = 20;
-                Assert.Equal(120, twitter.GetTextLengthRemain("example.com"));
-                Assert.Equal(120, twitter.GetTextLengthRemain("example.com/hogehoge"));
-                Assert.Equal(111, twitter.GetTextLengthRemain("hogehoge example.com"));
+                twitter.TextConfiguration.TransformedURLLength = 20;
+                Assert.Equal(260, twitter.GetTextLengthRemain("example.com"));
+                Assert.Equal(260, twitter.GetTextLengthRemain("example.com/hogehoge"));
+                Assert.Equal(251, twitter.GetTextLengthRemain("hogehoge example.com"));
 
                 // スキーム (http://) を省略かつ末尾が ccTLD の場合は t.co に短縮されない
-                Assert.Equal(130, twitter.GetTextLengthRemain("example.jp"));
+                Assert.Equal(270, twitter.GetTextLengthRemain("example.jp"));
                 // ただし、末尾にパスが続く場合は t.co に短縮される
-                Assert.Equal(120, twitter.GetTextLengthRemain("example.jp/hogehoge"));
+                Assert.Equal(260, twitter.GetTextLengthRemain("example.jp/hogehoge"));
             }
         }
 
@@ -387,8 +393,8 @@ namespace OpenTween
         {
             using (var twitter = new Twitter())
             {
-                Assert.Equal(139, twitter.GetTextLengthRemain("🍣"));
-                Assert.Equal(133, twitter.GetTextLengthRemain("🔥🐔🔥 焼き鳥"));
+                Assert.Equal(278, twitter.GetTextLengthRemain("🍣"));
+                Assert.Equal(267, twitter.GetTextLengthRemain("🔥🐔🔥 焼き鳥"));
             }
         }
     }

OpenTween/Api/DataModel/TwitterTextConfiguration.cs
@@ -1,0 +1,86 @@
+﻿// OpenTween - Client of Twitter
+// Copyright (c) 2017 kim_upsilon (@kim_upsilon) <https://upsilo.net/~upsilon/>
+// All rights reserved.
+//
+// This file is part of OpenTween.
+//
+// This program is free software; you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the Free
+// Software Foundation; either version 3 of the License, or (at your option)
+// any later version.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+// for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program. If not, see <http://www.gnu.org/licenses/>, or write to
+// the Free Software Foundation, Inc., 51 Franklin Street - Fifth Floor,
+// Boston, MA 02110-1301, USA.
+
+using System.Runtime.Serialization;
+
+namespace OpenTween.Api.DataModel
+{
+    [DataContract]
+    public class TwitterTextConfiguration
+    {
+        [DataMember(Name = "version")]
+        public string Version { get; set; }
+
+        [DataMember(Name = "maxWeightedTweetLength")]
+        public int MaxWeightedTweetLength { get; set; }
+
+        [DataMember(Name = "scale")]
+        public int Scale { get; set; }
+
+        [DataMember(Name = "defaultWeight")]
+        public int DefaultWeight { get; set; }
+
+        [DataMember(Name = "transformedURLLength")]
+        public int TransformedURLLength { get; set; }
+
+        [DataContract]
+        public class CodepointRange
+        {
+            [DataMember(Name = "start")]
+            public int Start { get; set; }
+
+            [DataMember(Name = "end")]
+            public int End { get; set; }
+
+            [DataMember(Name = "weight")]
+            public int Weight { get; set; }
+        }
+
+        [DataMember(Name = "ranges")]
+        public CodepointRange[] Ranges { get; set; }
+
+        /// <exception cref="SerializationException"/>
+        public static TwitterTextConfiguration ParseJson(string json)
+        {
+            return MyCommon.CreateDataFromJson<TwitterTextConfiguration>(json);
+        }
+
+        public static TwitterTextConfiguration DefaultConfiguration()
+        {
+            // 参照: https://developer.twitter.com/en/docs/developer-utilities/twitter-text
+            return new TwitterTextConfiguration
+            {
+                Version = "2",
+                MaxWeightedTweetLength = 280,
+                Scale = 100,
+                DefaultWeight = 200,
+                TransformedURLLength = 23,
+                Ranges = new[]
+                {
+                    new CodepointRange { Start = 0, End = 4351, Weight = 100 },
+                    new CodepointRange { Start = 8192, End = 8205, Weight = 100 },
+                    new CodepointRange { Start = 8208, End = 8223, Weight = 100 },
+                    new CodepointRange { Start = 8242, End = 8247, Weight = 100 },
+                },
+            };
+        }
+    }
+}

OpenTween/OpenTween.csproj
@@ -84,6 +84,7 @@
     <Compile Include="Api\DataModel\TwitterSearchResult.cs" />
     <Compile Include="Api\DataModel\TwitterStatus.cs" />
     <Compile Include="Api\DataModel\TwitterStreamEvent.cs" />
+    <Compile Include="Api\DataModel\TwitterTextConfiguration.cs" />
     <Compile Include="Api\DataModel\TwitterUploadMediaResult.cs" />
     <Compile Include="Api\DataModel\TwitterUser.cs" />
     <Compile Include="Api\DataModel\TwitterApiAccessLevel.cs" />

commit adb3f418cd301c2aaa1819706a6d15ff48c41a2f

投稿欄にIME経由で絵文字を入力するとエラーが発生する問題を回避
TextBox に IME 経由 (ATOK2017で確認) で絵文字を入力した場合に、サロゲートペアのうち
LowSurrogate が入力される前に TextChanged イベントが発生することによって
壊れたエンコーディングの文字列が Twitter.GetTextLengthRemain に渡される場合があった

Fixes: 33ea8c0b ("ツイート文字数の280文字への上限緩和 (weightedLength) に対応")

OpenTween/Extensions.cs
@@ -73,5 +73,17 @@ namespace OpenTween
             key = kvp.Key;
             value = kvp.Value;
         }
+
+        /// <summary>
+        /// 文字列中の指定された位置にある文字のコードポイントを返します
+        /// </summary>
+        public static int GetCodepointAtSafe(this string s, int index)
+        {
+            // IsSurrogatePair が true を返す場合のみ ConvertToUtf32 メソッドを使用する
+            if (char.IsSurrogatePair(s, index))
+                return char.ConvertToUtf32(s, index);
+
+            return s[index];
+        }
     }
 }

OpenTween/Twitter.cs
@@ -1767,7 +1767,7 @@ namespace OpenTween
                     continue;
                 }
 
-                var codepoint = char.ConvertToUtf32(postText, pos);
+                var codepoint = postText.GetCodepointAtSafe(pos);
                 var weight = config.DefaultWeight;
 
                 foreach (var weightRange in config.Ranges)

OpenTween.Tests/ExtensionsTest.cs
@@ -55,5 +55,34 @@ namespace OpenTween
             Assert.True(CultureInfo.InvariantCulture.Contains(new CultureInfo("ja")));
             Assert.True(CultureInfo.InvariantCulture.Contains(CultureInfo.InvariantCulture));
         }
+
+        [Theory]
+        [InlineData("abc", 0, (int)'a')]
+        [InlineData("abc", 1, (int)'b')]
+        [InlineData("abc", 2, (int)'c')]
+        [InlineData("🍣", 0, 0x1f363)] // サロゲートペア
+        public void GetCodepointAtSafe_Test(string s, int index, int expected)
+        {
+            Assert.Equal(expected, s.GetCodepointAtSafe(index));
+        }
+
+        [Theory]
+        // char.ConvertToUtf32 をそのまま使用するとエラーになるパターン
+        [InlineData(new[] { '\ud83c', '\udf63' }, 1, 0xdf63)] // pos がサロゲートペアの後半部分を指している
+        [InlineData(new[] { '\ud83c' }, 0, 0xd83c)] // 壊れたサロゲートペア (LowSurrogate が無い)
+        [InlineData(new[] { '\udf63' }, 0, 0xdf63)] // 壊れたサロゲートペア (HighSurrogate が無い)
+        public void GetCodepointAtSafe_BrokenSurrogateTest(char[] s, int index, int expected)
+        {
+            // InlineDataAttribute で壊れたサロゲートペアの string を扱えないため char[] を使う
+            Assert.Equal(expected, new string(s).GetCodepointAtSafe(index));
+        }
+
+        [Fact]
+        public void GetCodepointAtSafe_ErrorTest()
+        {
+            Assert.Throws<ArgumentNullException>(() => ((string)null).GetCodepointAtSafe(0));
+            Assert.Throws<ArgumentOutOfRangeException>(() => "a".GetCodepointAtSafe(-1));
+            Assert.Throws<ArgumentOutOfRangeException>(() => "a".GetCodepointAtSafe(1));
+        }
     }
 }

OpenTween.Tests/TwitterTest.cs
@@ -397,5 +397,16 @@ namespace OpenTween
                 Assert.Equal(267, twitter.GetTextLengthRemain("🔥🐔🔥 焼き鳥"));
             }
         }
+
+        [Fact]
+        public void GetTextLengthRemain_BrokenSurrogateTest()
+        {
+            using (var twitter = new Twitter())
+            {
+                // 投稿欄に IME から絵文字を入力すると HighSurrogate のみ入力された状態で TextChanged イベントが呼ばれることがある
+                Assert.Equal(278, twitter.GetTextLengthRemain("\ud83d"));
+                Assert.Equal(9999, twitter.GetTextLengthRemain("D twitter \ud83d"));
+            }
+        }
     }
 }