29c5191bd2df007d79e8733b3515d345c65002b1
Kimura Youichi <kim.upsilon@bucyou.net>
2018-05-06 09:55:52 +0900

    DateTimeUtc構造体を追加
    日付の内部表現として UTC で表現された DateTime を扱うためのラッパーとして使用する。
    この構造体を使用する場合、画面上に表示するタイミングでその都度ローカルのタイムゾーンに変換して出力する方針とする。

---
+++ OpenTween.Tests/DateTimeUtcTest.cs
@@ -0,0 +1,202 @@
+﻿// OpenTween - Client of Twitter
+// Copyright (c) 2018 kim_upsilon (@kim_upsilon) <https://upsilo.net/~upsilon/>
+// All rights reserved.
+//
+// This file is part of OpenTween.
+//
+// This program is free software; you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the Free
+// Software Foundation; either version 3 of the License, or (at your option)
+// any later version.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+// for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program. If not, see <http://www.gnu.org/licenses/>, or write to
+// the Free Software Foundation, Inc., 51 Franklin Street - Fifth Floor,
+// Boston, MA 02110-1301, USA.
+
+using System;
+using Xunit;
+
+namespace OpenTween
+{
+    public class DateTimeUtcTest
+    {
+        [Fact]
+        public void Constructor_DateTest()
+        {
+            var utc = new DateTimeUtc(2018, 5, 6);
+
+            Assert.Equal(new DateTime(2018, 5, 6, 0, 0, 0, 0, DateTimeKind.Utc),
+                utc.ToDateTimeUnsafe());
+        }
+
+        [Fact]
+        public void Constructor_DateAndTimeTest()
+        {
+            var utc = new DateTimeUtc(2018, 5, 6, 11, 22, 33);
+
+            Assert.Equal(new DateTime(2018, 5, 6, 11, 22, 33, 0, DateTimeKind.Utc),
+                utc.ToDateTimeUnsafe());
+        }
+
+        [Fact]
+        public void Constructor_DateAndTimeMillisecondsTest()
+        {
+            var utc = new DateTimeUtc(2018, 5, 6, 11, 22, 33, 456);
+
+            Assert.Equal(new DateTime(2018, 5, 6, 11, 22, 33, 456, DateTimeKind.Utc),
+                utc.ToDateTimeUnsafe());
+        }
+
+        [Fact]
+        public void Constructor_DateTimeOffsetTest()
+        {
+            var datetimeOffset = new DateTimeOffset(2018, 5, 6, 11, 22, 33, 456, TimeSpan.FromHours(9));
+            var utc = new DateTimeUtc(datetimeOffset);
+
+            Assert.Equal(new DateTime(2018, 5, 6, 2, 22, 33, 456, DateTimeKind.Utc),
+                utc.ToDateTimeUnsafe());
+        }
+
+        [Fact]
+        public void Constructor_DateTimeTest()
+        {
+            var datetime = new DateTime(2018, 5, 6, 11, 22, 33, DateTimeKind.Utc);
+            var utc = new DateTimeUtc(datetime);
+
+            Assert.Equal(datetime, utc.ToDateTimeUnsafe());
+        }
+
+        [Fact]
+        public void Constructor_LocalDateTimeTest()
+        {
+            Assert.Throws<ArgumentException>(
+                () => new DateTimeUtc(new DateTime(2018, 5, 6, 12, 0, 0, DateTimeKind.Local)));
+
+            Assert.Throws<ArgumentException>(
+                () => new DateTimeUtc(new DateTime(2018, 5, 6, 12, 0, 0, DateTimeKind.Unspecified)));
+        }
+
+        [Fact]
+        public void ToDateTimeOffset()
+        {
+            var utc = new DateTimeUtc(2018, 5, 6, 11, 22, 33, 111);
+
+            Assert.Equal(new DateTimeOffset(2018, 5, 6, 11, 22, 33, 111, TimeSpan.Zero),
+                utc.ToDateTimeOffset());
+        }
+
+        [Fact]
+        public void CompareTo_Test()
+        {
+            var utc1 = new DateTimeUtc(2018, 5, 6, 11, 22, 33, 111);
+            var utc2 = new DateTimeUtc(2018, 5, 6, 11, 22, 33, 111);
+            var utc3 = new DateTimeUtc(2018, 5, 6, 11, 22, 33, 222);
+
+            Assert.Equal(0, utc1.CompareTo(utc2));
+            Assert.True(utc1.CompareTo(utc3) < 0);
+            Assert.True(utc3.CompareTo(utc1) > 0);
+        }
+
+        [Fact]
+        public void Equals_Test()
+        {
+            var utc1 = new DateTimeUtc(2018, 5, 6, 11, 22, 33, 111);
+            var utc2 = new DateTimeUtc(2018, 5, 6, 11, 22, 33, 111);
+            var utc3 = new DateTimeUtc(2018, 5, 6, 11, 22, 33, 222);
+
+            Assert.True(utc1.Equals(utc2));
+            Assert.True(utc1.Equals((object)utc2));
+
+            Assert.False(utc1.Equals(utc3));
+            Assert.False(utc1.Equals((object)utc3));
+        }
+
+        [Fact]
+        public void GetHashCode_Test()
+        {
+            var utc1 = new DateTimeUtc(2018, 5, 6, 11, 22, 33, 111);
+            var utc2 = new DateTimeUtc(2018, 5, 6, 11, 22, 33, 111);
+            var utc3 = new DateTimeUtc(2018, 5, 6, 11, 22, 33, 222);
+
+            Assert.Equal(utc1.GetHashCode(), utc2.GetHashCode());
+            Assert.NotEqual(utc1.GetHashCode(), utc3.GetHashCode());
+        }
+
+        [Fact]
+        public void OperatorPlus_Test()
+        {
+            var utc = new DateTimeUtc(2018, 5, 6, 11, 22, 33, 111);
+            var diff = TimeSpan.FromDays(1);
+
+            Assert.Equal(new DateTime(2018, 5, 7, 11, 22, 33, 111, DateTimeKind.Utc),
+                (utc + diff).ToDateTimeUnsafe());
+        }
+
+        [Fact]
+        public void OperatorMinus_TimeSpanTest()
+        {
+            var utc = new DateTimeUtc(2018, 5, 6, 11, 22, 33, 111);
+            var diff = TimeSpan.FromDays(1);
+
+            Assert.Equal(new DateTime(2018, 5, 5, 11, 22, 33, 111, DateTimeKind.Utc),
+                (utc - diff).ToDateTimeUnsafe());
+        }
+
+        [Fact]
+        public void OperatorMinus_DateTimeTest()
+        {
+            var utc1 = new DateTimeUtc(2018, 5, 6, 11, 22, 33, 111);
+            var utc2 = new DateTimeUtc(2018, 5, 7, 11, 22, 33, 111);
+
+            Assert.Equal(TimeSpan.Zero, utc1 - utc1);
+            Assert.Equal(TimeSpan.FromDays(-1), utc1 - utc2);
+            Assert.Equal(TimeSpan.FromDays(1), utc2 - utc1);
+        }
+
+        [Fact]
+        public void OperatorEqual_Test()
+        {
+            var utc1 = new DateTimeUtc(2018, 5, 6, 11, 22, 33, 111);
+            var utc2 = new DateTimeUtc(2018, 5, 6, 11, 22, 33, 222);
+
+#pragma warning disable CS1718
+            Assert.True(utc1 == utc1);
+            Assert.False(utc1 == utc2);
+
+            Assert.False(utc1 != utc1);
+            Assert.True(utc1 != utc2);
+#pragma warning restore CS1718
+        }
+
+        [Fact]
+        public void OperatorCompare_Test()
+        {
+            var utc1 = new DateTimeUtc(2018, 5, 6, 11, 22, 33, 111);
+            var utc2 = new DateTimeUtc(2018, 5, 6, 11, 22, 33, 222);
+
+#pragma warning disable CS1718
+            Assert.False(utc1 < utc1);
+            Assert.True(utc1 < utc2);
+            Assert.False(utc2 < utc1);
+
+            Assert.True(utc1 <= utc1);
+            Assert.True(utc1 <= utc2);
+            Assert.False(utc2 <= utc1);
+
+            Assert.False(utc1 > utc1);
+            Assert.False(utc1 > utc2);
+            Assert.True(utc2 > utc1);
+
+            Assert.True(utc1 >= utc1);
+            Assert.False(utc1 >= utc2);
+            Assert.True(utc2 >= utc1);
+#pragma warning restore CS1718
+        }
+    }
+}
---
+++ OpenTween/DateTimeUtc.cs
@@ -0,0 +1,109 @@
+﻿// OpenTween - Client of Twitter
+// Copyright (c) 2018 kim_upsilon (@kim_upsilon) <https://upsilo.net/~upsilon/>
+// All rights reserved.
+//
+// This file is part of OpenTween.
+//
+// This program is free software; you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the Free
+// Software Foundation; either version 3 of the License, or (at your option)
+// any later version.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+// for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program. If not, see <http://www.gnu.org/licenses/>, or write to
+// the Free Software Foundation, Inc., 51 Franklin Street - Fifth Floor,
+// Boston, MA 02110-1301, USA.
+
+using System;
+
+namespace OpenTween
+{
+    /// <summary>
+    /// <see cref="DateTimeKind.Utc"/> に固定された <see cref="DateTime"/> を扱うための構造体
+    /// </summary>
+    public struct DateTimeUtc : IComparable<DateTimeUtc>, IEquatable<DateTimeUtc>
+    {
+        public static DateTimeUtc Now
+            => new DateTimeUtc(DateTime.UtcNow);
+
+        private readonly DateTime datetime;
+
+        public DateTimeUtc(int year, int month, int day)
+            : this(year, month, day, hour: 0, minute: 0, second: 0)
+        {
+        }
+
+        public DateTimeUtc(int year, int month, int day, int hour, int minute, int second)
+            : this(year, month, day, hour, minute, second, millisecond: 0)
+        {
+        }
+
+        public DateTimeUtc(int year, int month, int day, int hour, int minute, int second, int millisecond)
+            : this(new DateTime(year, month, day, hour, minute, second, millisecond, DateTimeKind.Utc))
+        {
+        }
+
+        public DateTimeUtc(DateTimeOffset datetimeOffset)
+            : this(datetimeOffset.UtcDateTime)
+        {
+        }
+
+        public DateTimeUtc(DateTime datetime)
+        {
+            if (datetime.Kind != DateTimeKind.Utc)
+                throw new ArgumentException("datetime には UTC に変換された時刻が必須です", nameof(datetime));
+
+            this.datetime = datetime;
+        }
+
+        public DateTimeOffset ToDateTimeOffset()
+            => new DateTimeOffset(this.datetime);
+
+        public DateTime ToDateTimeUnsafe()
+            => this.datetime;
+
+        public int CompareTo(DateTimeUtc other)
+            => this.datetime.CompareTo(other.datetime);
+
+        public bool Equals(DateTimeUtc other)
+            => this == other;
+
+        public override bool Equals(object obj)
+            => obj is DateTimeUtc other && this.Equals(other);
+
+        public override int GetHashCode()
+            => this.datetime.GetHashCode();
+
+        public static DateTimeUtc operator +(DateTimeUtc a, TimeSpan b)
+            => new DateTimeUtc(a.datetime + b);
+
+        public static DateTimeUtc operator -(DateTimeUtc a, TimeSpan b)
+            => new DateTimeUtc(a.datetime - b);
+
+        public static TimeSpan operator -(DateTimeUtc a, DateTimeUtc b)
+            => a.datetime - b.datetime;
+
+        public static bool operator ==(DateTimeUtc a, DateTimeUtc b)
+            => a.datetime == b.datetime;
+
+        public static bool operator !=(DateTimeUtc a, DateTimeUtc b)
+            => a.datetime != b.datetime;
+
+        public static bool operator <(DateTimeUtc a, DateTimeUtc b)
+            => a.datetime < b.datetime;
+
+        public static bool operator <=(DateTimeUtc a, DateTimeUtc b)
+            => a.datetime <= b.datetime;
+
+        public static bool operator >(DateTimeUtc a, DateTimeUtc b)
+            => a.datetime > b.datetime;
+
+        public static bool operator >=(DateTimeUtc a, DateTimeUtc b)
+            => a.datetime >= b.datetime;
+    }
+}
