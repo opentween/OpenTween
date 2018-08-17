opentween/OpenTween

commit 1a38ae79089ae6949ff75bc8e446f0723e95f7f5

/direct_messages/events/destroy.json によるDMの削除に対応

OpenTween/Api/TwitterApi.cs
@@ -421,17 +421,6 @@ namespace OpenTween.Api
             return this.apiConnection.GetAsync<TwitterDirectMessage[]>(endpoint, param, "/direct_messages/sent");
         }
 
-        public Task<LazyJson<TwitterDirectMessage>> DirectMessagesDestroy(long statusId)
-        {
-            var endpoint = new Uri("direct_messages/destroy.json", UriKind.Relative);
-            var param = new Dictionary<string, string>
-            {
-                ["id"] = statusId.ToString(),
-            };
-
-            return this.apiConnection.PostLazyAsync<TwitterDirectMessage>(endpoint, param);
-        }
-
         public Task<TwitterMessageEventList> DirectMessagesEventsList(int? count = null, string cursor = null)
         {
             var endpoint = new Uri("direct_messages/events/list.json", UriKind.Relative);
@@ -476,6 +465,17 @@ namespace OpenTween.Api
 }}";
 
             return this.apiConnection.PostJsonAsync(endpoint, json);
+        }
+
+        public Task DirectMessagesEventsDestroy(string eventId)
+        {
+            var endpoint = new Uri("direct_messages/events/destroy.json", UriKind.Relative);
+            var param = new Dictionary<string, string>
+            {
+                ["id"] = eventId.ToString(),
+            };
+
+            return this.apiConnection.DeleteAsync(endpoint, param);
         }
 
         public Task<TwitterUser> UsersShow(string screenName)

OpenTween/Connection/IApiConnection.cs
@@ -43,5 +43,7 @@ namespace OpenTween.Connection
         Task PostAsync(Uri uri, IDictionary<string, string> param, IDictionary<string, IMediaItem> media);
 
         Task PostJsonAsync(Uri uri, string json);
+
+        Task DeleteAsync(Uri uri, IDictionary<string, string> param);
     }
 }

OpenTween/Connection/TwitterApiConnection.cs
@@ -329,6 +329,35 @@ namespace OpenTween.Connection
             }
         }
 
+        public async Task DeleteAsync(Uri uri, IDictionary<string, string> param)
+        {
+            var requestUri = new Uri(RestApiBase, uri);
+            var request = new HttpRequestMessage(HttpMethod.Delete, requestUri);
+
+            using (var postContent = new FormUrlEncodedContent(param))
+            {
+                request.Content = postContent;
+
+                try
+                {
+                    using (var response = await this.http.SendAsync(request, HttpCompletionOption.ResponseHeadersRead)
+                        .ConfigureAwait(false))
+                    {
+                        await this.CheckStatusCode(response)
+                            .ConfigureAwait(false);
+                    }
+                }
+                catch (HttpRequestException ex)
+                {
+                    throw TwitterApiException.CreateFromException(ex);
+                }
+                catch (OperationCanceledException ex)
+                {
+                    throw TwitterApiException.CreateFromException(ex);
+                }
+            }
+        }
+
         protected async Task CheckStatusCode(HttpResponseMessage response)
         {
             var statusCode = response.StatusCode;

OpenTween.Tests/Connection/TwitterApiConnectionTest.cs
@@ -493,5 +493,47 @@ namespace OpenTween.Connection
                 Assert.Equal(0, mockHandler.QueueCount);
             }
         }
+
+        [Fact]
+        public async Task DeleteAsync_Test()
+        {
+            using (var mockHandler = new HttpMessageHandlerMock())
+            using (var http = new HttpClient(mockHandler))
+            using (var apiConnection = new TwitterApiConnection("", ""))
+            {
+                apiConnection.http = http;
+
+                mockHandler.Enqueue(async x =>
+                {
+                    Assert.Equal(HttpMethod.Delete, x.Method);
+                    Assert.Equal("https://api.twitter.com/1.1/hoge/tetete.json",
+                        x.RequestUri.AbsoluteUri);
+
+                    var body = await x.Content.ReadAsStringAsync()
+                        .ConfigureAwait(false);
+                    var query = HttpUtility.ParseQueryString(body);
+
+                    Assert.Equal("1111", query["aaaa"]);
+                    Assert.Equal("2222", query["bbbb"]);
+
+                    return new HttpResponseMessage(HttpStatusCode.OK)
+                    {
+                        Content = new StringContent("\"hogehoge\""),
+                    };
+                });
+
+                var endpoint = new Uri("hoge/tetete.json", UriKind.Relative);
+                var param = new Dictionary<string, string>
+                {
+                    ["aaaa"] = "1111",
+                    ["bbbb"] = "2222",
+                };
+
+                await apiConnection.DeleteAsync(endpoint, param)
+                    .ConfigureAwait(false);
+
+                Assert.Equal(0, mockHandler.QueueCount);
+            }
+        }
     }
 }

OpenTween.Tests/Api/TwitterApiTest.cs
@@ -724,29 +724,6 @@ namespace OpenTween.Api
         }
 
         [Fact]
-        public async Task DirectMessagesDestroy_Test()
-        {
-            using (var twitterApi = new TwitterApi())
-            {
-                var mock = new Mock<IApiConnection>();
-                mock.Setup(x =>
-                    x.PostLazyAsync<TwitterDirectMessage>(
-                        new Uri("direct_messages/destroy.json", UriKind.Relative),
-                        new Dictionary<string, string> { { "id", "100" } })
-                )
-                .ReturnsAsync(LazyJson.Create(new TwitterDirectMessage { Id = 100L }));
-
-                twitterApi.apiConnection = mock.Object;
-
-                await twitterApi.DirectMessagesDestroy(statusId: 100L)
-                    .IgnoreResponse()
-                    .ConfigureAwait(false);
-
-                mock.VerifyAll();
-            }
-        }
-
-        [Fact]
         public async Task DirectMessagesEventsList_Test()
         {
             using (var twitterApi = new TwitterApi())
@@ -806,6 +783,28 @@ namespace OpenTween.Api
                 twitterApi.apiConnection = mock.Object;
 
                 await twitterApi.DirectMessagesEventsNew(recipientId: 12345L, text: "hogehoge", mediaId: 67890L)
+                    .ConfigureAwait(false);
+
+                mock.VerifyAll();
+            }
+        }
+
+        [Fact]
+        public async Task DirectMessagesEventsDestroy_Test()
+        {
+            using (var twitterApi = new TwitterApi())
+            {
+                var mock = new Mock<IApiConnection>();
+                mock.Setup(x =>
+                    x.DeleteAsync(
+                        new Uri("direct_messages/events/destroy.json", UriKind.Relative),
+                        new Dictionary<string, string> { { "id", "100" } })
+                )
+                .Returns(Task.CompletedTask);
+
+                twitterApi.apiConnection = mock.Object;
+
+                await twitterApi.DirectMessagesEventsDestroy(eventId: "100")
                     .ConfigureAwait(false);
 
                 mock.VerifyAll();

OpenTween/Tween.cs
@@ -3314,8 +3314,7 @@ namespace OpenTween
                     {
                         if (post.IsDm)
                         {
-                            await this.twitterApi.DirectMessagesDestroy(post.StatusId)
-                                .IgnoreResponse();
+                            await this.twitterApi.DirectMessagesEventsDestroy(post.StatusId.ToString(CultureInfo.InvariantCulture));
                         }
                         else
                         {

commit caef00dd6514268f4ad20f4660e7ca7a1a4f9515

/direct_messages/events/destroy.json のリクエスト形式の誤りを修正
Fixes: 1a38ae79 ("/direct_messages/events/destroy.json によるDMの削除に対応")

OpenTween/Api/TwitterApi.cs
@@ -475,7 +475,10 @@ namespace OpenTween.Api
                 ["id"] = eventId.ToString(),
             };
 
-            return this.apiConnection.DeleteAsync(endpoint, param);
+            // なぜか application/x-www-form-urlencoded でパラメーターを送ると Bad Request になる謎仕様
+            endpoint = new Uri(endpoint.OriginalString + "?" + MyCommon.BuildQueryString(param), UriKind.Relative);
+
+            return this.apiConnection.DeleteAsync(endpoint);
         }
 
         public Task<TwitterUser> UsersShow(string screenName)

OpenTween/Connection/IApiConnection.cs
@@ -44,6 +44,6 @@ namespace OpenTween.Connection
 
         Task PostJsonAsync(Uri uri, string json);
 
-        Task DeleteAsync(Uri uri, IDictionary<string, string> param);
+        Task DeleteAsync(Uri uri);
     }
 }

OpenTween/Connection/TwitterApiConnection.cs
@@ -329,32 +329,27 @@ namespace OpenTween.Connection
             }
         }
 
-        public async Task DeleteAsync(Uri uri, IDictionary<string, string> param)
+        public async Task DeleteAsync(Uri uri)
         {
             var requestUri = new Uri(RestApiBase, uri);
             var request = new HttpRequestMessage(HttpMethod.Delete, requestUri);
 
-            using (var postContent = new FormUrlEncodedContent(param))
+            try
             {
-                request.Content = postContent;
-
-                try
+                using (var response = await this.http.SendAsync(request, HttpCompletionOption.ResponseHeadersRead)
+                    .ConfigureAwait(false))
                 {
-                    using (var response = await this.http.SendAsync(request, HttpCompletionOption.ResponseHeadersRead)
-                        .ConfigureAwait(false))
-                    {
-                        await this.CheckStatusCode(response)
-                            .ConfigureAwait(false);
-                    }
+                    await this.CheckStatusCode(response)
+                        .ConfigureAwait(false);
                 }
-                catch (HttpRequestException ex)
-                {
-                    throw TwitterApiException.CreateFromException(ex);
-                }
-                catch (OperationCanceledException ex)
-                {
-                    throw TwitterApiException.CreateFromException(ex);
-                }
+            }
+            catch (HttpRequestException ex)
+            {
+                throw TwitterApiException.CreateFromException(ex);
+            }
+            catch (OperationCanceledException ex)
+            {
+                throw TwitterApiException.CreateFromException(ex);
             }
         }
 

OpenTween.Tests/Connection/TwitterApiConnectionTest.cs
@@ -503,33 +503,18 @@ namespace OpenTween.Connection
             {
                 apiConnection.http = http;
 
-                mockHandler.Enqueue(async x =>
+                mockHandler.Enqueue(x =>
                 {
                     Assert.Equal(HttpMethod.Delete, x.Method);
                     Assert.Equal("https://api.twitter.com/1.1/hoge/tetete.json",
                         x.RequestUri.AbsoluteUri);
 
-                    var body = await x.Content.ReadAsStringAsync()
-                        .ConfigureAwait(false);
-                    var query = HttpUtility.ParseQueryString(body);
-
-                    Assert.Equal("1111", query["aaaa"]);
-                    Assert.Equal("2222", query["bbbb"]);
-
-                    return new HttpResponseMessage(HttpStatusCode.OK)
-                    {
-                        Content = new StringContent("\"hogehoge\""),
-                    };
+                    return new HttpResponseMessage(HttpStatusCode.NoContent);
                 });
 
                 var endpoint = new Uri("hoge/tetete.json", UriKind.Relative);
-                var param = new Dictionary<string, string>
-                {
-                    ["aaaa"] = "1111",
-                    ["bbbb"] = "2222",
-                };
 
-                await apiConnection.DeleteAsync(endpoint, param)
+                await apiConnection.DeleteAsync(endpoint)
                     .ConfigureAwait(false);
 
                 Assert.Equal(0, mockHandler.QueueCount);