opentween/OpenTween

commit f862ef84f91a894e773e9d9a2b5d3b7c36c87b9e

TweetExtractorの返すエンティティのインデックスがサロゲートペアを考慮できていない不具合を修正
テストケースに「✨」を使用していたが、これは U+2728 で基本多言語面にある文字なのでサロゲートペアのテストとして適切ではなかった

Fixes: 33d5a834 ("テキストからURLを抽出してTwitterEntityUrlとして出力するExtractUrlEntitiesメソッドを追加")

OpenTween/Extensions.cs
@@ -86,6 +86,30 @@ namespace OpenTween
             return s[index];
         }
 
+        /// <summary>
+        /// 指定された部分文字列のコードポイント単位での文字数を返す
+        /// </summary>
+        /// <param name="s">文字列</param>
+        /// <param name="start">開始位置</param>
+        /// <param name="end">終了位置</param>
+        public static int GetCodepointCount(this string s, int start, int end)
+        {
+            if (s == null)
+                throw new ArgumentNullException(nameof(s));
+            if (start < 0 || start > s.Length)
+                throw new ArgumentOutOfRangeException(nameof(start));
+            if (end < 0 || end > s.Length)
+                throw new ArgumentOutOfRangeException(nameof(end));
+            if (start > end)
+                throw new ArgumentOutOfRangeException(nameof(start));
+
+            var count = 0;
+            for (var i = start; i < end; i += char.IsSurrogatePair(s, i) ? 2 : 1)
+                count++;
+
+            return count;
+        }
+
         public static Task ForEachAsync<T>(this IObservable<T> observable, Action<T> subscriber)
             => ForEachAsync(observable, value => { subscriber(value); return Task.CompletedTask; });
 

OpenTween.Tests/ExtensionsTest.cs
@@ -88,6 +88,26 @@ namespace OpenTween
             Assert.Throws<ArgumentOutOfRangeException>(() => "a".GetCodepointAtSafe(1));
         }
 
+        [Theory]
+        [InlineData("", 0, 0, 0)]
+        [InlineData("sushi 🍣", 0, 8, 7)]
+        [InlineData("sushi 🍣", 0, 5, 5)]
+        [InlineData("sushi 🍣", 6, 8, 1)]
+        [InlineData("sushi 🍣", 6, 7, 1)] // サロゲートペアの境界を跨ぐ範囲 (LowSurrogate が無い)
+        [InlineData("sushi 🍣", 7, 8, 1)] // サロゲートペアの境界を跨ぐ範囲 (HighSurrogate が無い)
+        public void GetCodepointCount_Test(string str, int start, int end, int expected)
+            => Assert.Equal(expected, str.GetCodepointCount(start, end));
+
+        [Fact]
+        public void GetCodepointCount_ErrorTest()
+        {
+            Assert.Throws<ArgumentNullException>(() => ((string)null).GetCodepointCount(0, 0));
+            Assert.Throws<ArgumentOutOfRangeException>(() => "abc".GetCodepointCount(-1, 3));
+            Assert.Throws<ArgumentOutOfRangeException>(() => "abc".GetCodepointCount(0, 4));
+            Assert.Throws<ArgumentOutOfRangeException>(() => "abc".GetCodepointCount(4, 5));
+            Assert.Throws<ArgumentOutOfRangeException>(() => "abc".GetCodepointCount(2, 1));
+        }
+
         [Fact]
         public async Task ForEachAsync_Test()
         {

OpenTween.Tests/TweetExtractorTest.cs
@@ -78,7 +78,7 @@ namespace OpenTween
         [Fact]
         public void ExtractUrlEntities_SurrogatePairTest()
         {
-            var entity = TweetExtractor.ExtractUrlEntities("✨ http://example.com/ ✨").Single();
+            var entity = TweetExtractor.ExtractUrlEntities("🍣 http://example.com/ 🍣").Single();
 
             Assert.Equal(new[] { 2, 21 }, entity.Indices);
             Assert.Equal("http://example.com/", entity.Url);
@@ -145,6 +145,15 @@ namespace OpenTween
         }
 
         [Fact]
+        public void ExtractMentionEntities_SurrogatePairTest()
+        {
+            var entity = TweetExtractor.ExtractMentionEntities("🍣 @twitterapi").Single();
+
+            Assert.Equal(new[] { 2, 13 }, entity.Indices);
+            Assert.Equal("twitterapi", entity.ScreenName);
+        }
+
+        [Fact]
         public void ExtractHashtagEntities_Test()
         {
             var entity = TweetExtractor.ExtractHashtagEntities("hogehoge #test").Single();
@@ -164,6 +173,15 @@ namespace OpenTween
             Assert.Equal("test", entities[0].Text);
             Assert.Equal(new[] { 15, 21 }, entities[1].Indices);
             Assert.Equal("test2", entities[1].Text);
+        }
+
+        [Fact]
+        public void ExtractHashtagEntities_SurrogatePairTest()
+        {
+            var entity = TweetExtractor.ExtractHashtagEntities("🍣 #sushi").Single();
+
+            Assert.Equal(new[] { 2, 8 }, entity.Indices);
+            Assert.Equal("sushi", entity.Text);
         }
     }
 }

OpenTween/TweetExtractor.cs
@@ -83,8 +83,9 @@ namespace OpenTween
 
                 if (validUrl)
                 {
-                    var startPos = m.Groups["url"].Index;
-                    var endPos = startPos + m.Groups["url"].Length;
+                    var urlGroup = m.Groups["url"];
+                    var startPos = text.GetCodepointCount(0, urlGroup.Index);
+                    var endPos = startPos + text.GetCodepointCount(urlGroup.Index, urlGroup.Index + urlGroup.Length);
 
                     yield return new TwitterEntityUrl
                     {
@@ -106,8 +107,8 @@ namespace OpenTween
             var matchesAtList = Regex.Matches(text, @"(?<=^|[^a-zA-Z0-9_/])([@＠][a-zA-Z0-9_]{1,20}/[a-zA-Z][a-zA-Z0-9\p{IsLatin-1Supplement}\-]{0,79})");
             foreach (var match in matchesAtList.Cast<Match>())
             {
-                var startPos = match.Index;
-                var endPos = startPos + match.Length;
+                var startPos = text.GetCodepointCount(0, match.Index);
+                var endPos = startPos + text.GetCodepointCount(match.Index, match.Index + match.Length);
 
                 yield return new TwitterEntityMention
                 {
@@ -120,8 +121,8 @@ namespace OpenTween
             var matchesAtUser = Regex.Matches(text, "(?<=^|[^a-zA-Z0-9_/])([@＠][a-zA-Z0-9_]{1,20})(?=[^a-zA-Z0-9_/]|$)");
             foreach (var match in matchesAtUser.Cast<Match>())
             {
-                var startPos = match.Index;
-                var endPos = startPos + match.Length;
+                var startPos = text.GetCodepointCount(0, match.Index);
+                var endPos = startPos + text.GetCodepointCount(match.Index, match.Index + match.Length);
 
                 yield return new TwitterEntityMention
                 {
@@ -141,8 +142,8 @@ namespace OpenTween
             {
                 var groupHashtagSharp = match.Groups[2];
                 var groupHashtagText = match.Groups[3];
-                var startPos = groupHashtagSharp.Index;
-                var endPos = startPos + groupHashtagSharp.Length + groupHashtagText.Length;
+                var startPos = text.GetCodepointCount(0, groupHashtagSharp.Index);
+                var endPos = startPos + text.GetCodepointCount(groupHashtagSharp.Index, groupHashtagSharp.Index + groupHashtagSharp.Length + groupHashtagText.Length);
 
                 yield return new TwitterEntityHashtag
                 {

commit 59cfadafb820ee61847513e4ecfdc886b6f6ab40

GetTextLengthRemainWeightedの内部で使用するインデックスをコードポイント単位となるように変更
f862ef84 によって TweetExtractor が返す Entity のインデックスがコードポイント単位となったため、バイト数単位の pos と一致しなくなっていた

Fixes: f862ef84 ("TweetExtractorの返すエンティティのインデックスがサロゲートペアを考慮できていない不具合を修正")

OpenTween/Extensions.cs
@@ -75,15 +75,33 @@ namespace OpenTween
         }
 
         /// <summary>
-        /// 文字列中の指定された位置にある文字のコードポイントを返します
+        /// 文字列をコードポイント単位に分割して返します
         /// </summary>
-        public static int GetCodepointAtSafe(this string s, int index)
+        public static IEnumerable<int> ToCodepoints(this string s)
         {
-            // IsSurrogatePair が true を返す場合のみ ConvertToUtf32 メソッドを使用する
-            if (char.IsSurrogatePair(s, index))
-                return char.ConvertToUtf32(s, index);
+            if (s == null)
+                throw new ArgumentNullException(nameof(s));
 
-            return s[index];
+            IEnumerable<int> GetEnumerable(string input)
+            {
+                var i = 0;
+                var length = input.Length;
+                while (i < length)
+                {
+                    if (char.IsSurrogatePair(input, i))
+                    {
+                        yield return char.ConvertToUtf32(input, i);
+                        i += 2;
+                    }
+                    else
+                    {
+                        yield return input[i];
+                        i++;
+                    }
+                }
+            }
+
+            return GetEnumerable(s);
         }
 
         /// <summary>

OpenTween/Twitter.cs
@@ -1812,21 +1812,19 @@ namespace OpenTween
 
             var urls = TweetExtractor.ExtractUrlEntities(postText).ToArray();
 
-            var pos = 0;
-            while (pos < postText.Length)
+            var codepoints = postText.ToCodepoints().ToArray();
+            var index = 0;
+            while (index < codepoints.Length)
             {
-                var urlEntity = urls.FirstOrDefault(x => x.Indices[0] == pos);
+                var urlEntity = urls.FirstOrDefault(x => x.Indices[0] == index);
                 if (urlEntity != null)
                 {
                     totalWeight += config.TransformedURLLength * config.Scale;
-
-                    var urlLength = urlEntity.Indices[1] - urlEntity.Indices[0];
-                    pos += urlLength;
-
+                    index = urlEntity.Indices[1];
                     continue;
                 }
 
-                var codepoint = postText.GetCodepointAtSafe(pos);
+                var codepoint = codepoints[index];
                 var weight = config.DefaultWeight;
 
                 foreach (var weightRange in config.Ranges)
@@ -1839,12 +1837,7 @@ namespace OpenTween
                 }
 
                 totalWeight += weight;
-
-                var isSurrogatePair = codepoint > 0xffff;
-                if (isSurrogatePair)
-                    pos += 2; // サロゲートペアの場合は2文字分進める
-                else
-                    pos++;
+                index++;
             }
 
             var remainWeight = config.MaxWeightedTweetLength * config.Scale - totalWeight;

OpenTween.Tests/ExtensionsTest.cs
@@ -60,33 +60,24 @@ namespace OpenTween
         }
 
         [Theory]
-        [InlineData("abc", 0, (int)'a')]
-        [InlineData("abc", 1, (int)'b')]
-        [InlineData("abc", 2, (int)'c')]
-        [InlineData("🍣", 0, 0x1f363)] // サロゲートペア
-        public void GetCodepointAtSafe_Test(string s, int index, int expected)
-        {
-            Assert.Equal(expected, s.GetCodepointAtSafe(index));
-        }
+        [InlineData("abc", new int[] { 'a', 'b', 'c' })]
+        [InlineData("🍣", new int[] { 0x1f363 })] // サロゲートペア
+        public void ToCodepoints_Test(string s, int[] expected)
+            => Assert.Equal(expected, s.ToCodepoints());
 
         [Theory]
         // char.ConvertToUtf32 をそのまま使用するとエラーになるパターン
-        [InlineData(new[] { '\ud83c', '\udf63' }, 1, 0xdf63)] // pos がサロゲートペアの後半部分を指している
-        [InlineData(new[] { '\ud83c' }, 0, 0xd83c)] // 壊れたサロゲートペア (LowSurrogate が無い)
-        [InlineData(new[] { '\udf63' }, 0, 0xdf63)] // 壊れたサロゲートペア (HighSurrogate が無い)
-        public void GetCodepointAtSafe_BrokenSurrogateTest(char[] s, int index, int expected)
+        [InlineData(new[] { '\ud83c' }, new[] { 0xd83c })] // 壊れたサロゲートペア (LowSurrogate が無い)
+        [InlineData(new[] { '\udf63' }, new[] { 0xdf63 })] // 壊れたサロゲートペア (HighSurrogate が無い)
+        public void ToCodepoints_BrokenSurrogateTest(char[] s, int[] expected)
         {
             // InlineDataAttribute で壊れたサロゲートペアの string を扱えないため char[] を使う
-            Assert.Equal(expected, new string(s).GetCodepointAtSafe(index));
+            Assert.Equal(expected, new string(s).ToCodepoints());
         }
 
         [Fact]
-        public void GetCodepointAtSafe_ErrorTest()
-        {
-            Assert.Throws<ArgumentNullException>(() => ((string)null).GetCodepointAtSafe(0));
-            Assert.Throws<ArgumentOutOfRangeException>(() => "a".GetCodepointAtSafe(-1));
-            Assert.Throws<ArgumentOutOfRangeException>(() => "a".GetCodepointAtSafe(1));
-        }
+        public void ToCodepoints_ErrorTest()
+            => Assert.Throws<ArgumentNullException>(() => ((string)null).ToCodepoints());
 
         [Theory]
         [InlineData("", 0, 0, 0)]