opentween/OpenTween

commit 8a1d17d52b68bf56b05f76abe89bdaccf0ba0769

投稿時取得がオフでも投稿した内容を即TLに反映させる

OpenTween/Tween.cs
@@ -2602,6 +2602,7 @@ namespace OpenTween
 
             p.Report("Posting...");
 
+            PostClass post = null;
             var errMsg = "";
 
             try
@@ -2616,7 +2617,7 @@ namespace OpenTween
                             .ConfigureAwait(false);
                     }
 
-                    await this.tw.PostStatus(postParamsWithMedia)
+                    post = await this.tw.PostStatus(postParamsWithMedia)
                         .ConfigureAwait(false);
                 });
 
@@ -2695,12 +2696,20 @@ namespace OpenTween
 
             this.SetMainWindowTitle();
 
-            if (SettingManager.Common.PostAndGet)
+            // TLに反映
+            if (!this.tw.UserStreamActive)
             {
-                if (this.tw.UserStreamActive)
-                    this.RefreshTimeline();
-                else
+                if (SettingManager.Common.PostAndGet)
                     await this.RefreshTabAsync<HomeTabModel>();
+                else
+                {
+                    if (post != null)
+                    {
+                        this._statuses.AddPost(post);
+                        this._statuses.DistributePosts();
+                        this.RefreshTimeline();
+                    }
+                }
             }
         }
 
@@ -2713,9 +2722,6 @@ namespace OpenTween
                 var progress = new Progress<string>(x => this.StatusLabel.Text = x);
 
                 await this.RetweetAsyncInternal(progress, this.workerCts.Token, statusIds);
-
-                if (SettingManager.Common.PostAndGet && !this.tw.UserStreamActive)
-                    await this.RefreshTabAsync<HomeTabModel>();
             }
             catch (WebApiException ex)
             {
@@ -2744,10 +2750,16 @@ namespace OpenTween
 
             p.Report("Posting...");
 
-            foreach (var statusId in statusIds)
+            var posts = new List<PostClass>();
+
+            await Task.Run(async () =>
             {
-                await this.tw.PostRetweet(statusId, read).ConfigureAwait(false);
-            }
+                foreach (var statusId in statusIds)
+                {
+                    var post = await this.tw.PostRetweet(statusId, read).ConfigureAwait(false);
+                    if (post != null) posts.Add(post);
+                }
+            });
 
             if (ct.IsCancellationRequested)
                 return;
@@ -2761,6 +2773,22 @@ namespace OpenTween
             {
                 if (this._postTimestamps[i] < oneHour)
                     this._postTimestamps.RemoveAt(i);
+            }
+
+            // TLに反映
+            if (!this.tw.UserStreamActive)
+            {
+                if (SettingManager.Common.PostAndGet)
+                    await this.RefreshTabAsync<HomeTabModel>();
+                else
+                {
+                    if (posts.Count > 0)
+                    {
+                        posts.ForEach(post => this._statuses.AddPost(post));
+                        this._statuses.DistributePosts();
+                        this.RefreshTimeline();
+                    }
+                }
             }
         }
 

OpenTween/Twitter.cs
@@ -270,7 +270,7 @@ namespace OpenTween
             return orgData;
         }
 
-        public async Task PostStatus(PostStatusParams param)
+        public async Task<PostClass> PostStatus(PostStatusParams param)
         {
             this.CheckAccountState();
 
@@ -280,7 +280,7 @@ namespace OpenTween
 
                 await this.SendDirectMessage(param.Text, mediaId)
                     .ConfigureAwait(false);
-                return;
+                return null;
             }
 
             var response = await this.Api.StatusesUpdate(param.Text, param.InReplyToStatusId, param.MediaIds,
@@ -296,6 +296,11 @@ namespace OpenTween
                 throw new WebApiException("OK:Delaying?");
 
             this.previousStatusId = status.Id;
+
+            //投稿したものを返す
+            var post = CreatePostsFromStatusData(status);
+            if (this.ReadOwnPost) post.IsRead = true;
+            return post;
         }
 
         public async Task<long> UploadMedia(IMediaItem item, string mediaCategory = null)
@@ -382,7 +387,7 @@ namespace OpenTween
                 .ConfigureAwait(false);
         }
 
-        public async Task PostRetweet(long id, bool read)
+        public async Task<PostClass> PostRetweet(long id, bool read)
         {
             this.CheckAccountState();
 
@@ -403,16 +408,16 @@ namespace OpenTween
             lock (LockObj)
             {
                 if (TabInformations.GetInstance().ContainsKey(status.Id))
-                    return;
+                    return null;
             }
 
             //Retweet判定
             if (status.RetweetedStatus == null)
                 throw new WebApiException("Invalid Json!");
 
-            //ReTweetしたものをTLに追加
+            //Retweetしたものを返す
             post = CreatePostsFromStatusData(status);
-            
+
             //ユーザー情報
             post.IsMe = true;
 
@@ -421,7 +426,7 @@ namespace OpenTween
             if (this.ReadOwnPost) post.IsRead = true;
             post.IsDm = false;
 
-            TabInformations.GetInstance().AddPost(post);
+            return post;
         }
 
         public string Username

commit 5fde82ac28723d36dcc72d24d1882abe67faec8c

投稿時取得がオンの場合でもRTを即TLに反映させる
TL上の直近のツイートやRTをRTした場合、単体のツイートとしては流れてこないため、自前で追加する必要がある

Fixes: 8a1d17d5 ("投稿時取得がオフでも投稿した内容を即TLに反映させる")

OpenTween/Tween.cs
@@ -2772,16 +2772,16 @@ namespace OpenTween
             // TLに反映
             if (!this.tw.UserStreamActive)
             {
+                // 自分のRTはTLの更新では取得できない場合があるので、
+                // 投稿時取得の有無に関わらず追加しておく
+                posts.ForEach(post => this._statuses.AddPost(post));
+
                 if (SettingManager.Common.PostAndGet)
                     await this.RefreshTabAsync<HomeTabModel>();
                 else
                 {
-                    if (posts.Count > 0)
-                    {
-                        posts.ForEach(post => this._statuses.AddPost(post));
-                        this._statuses.DistributePosts();
-                        this.RefreshTimeline();
-                    }
+                    this._statuses.DistributePosts();
+                    this.RefreshTimeline();
                 }
             }
         }