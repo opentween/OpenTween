opentween/OpenTween

commit 91c593311960adea212667bd137228a52019e636

TabInformations.MuteTabプロパティを追加しTabsからミュートタブを除外

OpenTween/FilterDialog.cs
@@ -102,21 +102,7 @@ namespace OpenTween
 
         private void RefreshListTabs()
         {
-            var tabs = new List<TabModel>();
-
-            foreach (var tab in this._sts.Tabs)
-            {
-                if (tab.TabType == MyCommon.TabUsageType.Mute)
-                    continue;
-
-                tabs.Add(tab);
-            }
-
-            var muteTab = this._sts.GetTabByType(MyCommon.TabUsageType.Mute);
-            if (muteTab != null)
-                tabs.Add(muteTab);
-
-            this.tabs = tabs;
+            this.tabs = this._sts.Tabs.Append(this._sts.MuteTab).ToList();
 
             using (ControlTransaction.Update(this.ListTabs))
             {

OpenTween/Models/TabInformations.cs
@@ -41,6 +41,8 @@ namespace OpenTween.Models
     {
         //個別タブの情報をDictionaryで保持
         public TabCollection Tabs { get; } = new TabCollection();
+        public MuteTabModel MuteTab { get; private set; }
+
         public ConcurrentDictionary<long, PostClass> Posts { get; } = new ConcurrentDictionary<long, PostClass>();
 
         private Dictionary<long, PostClass> _quotes = new Dictionary<long, PostClass>();
@@ -113,6 +115,15 @@ namespace OpenTween.Models
         {
             lock (this.LockObj)
             {
+                if (tab is MuteTabModel muteTab)
+                {
+                    if (this.MuteTab != null)
+                        return false;
+
+                    this.MuteTab = muteTab;
+                    return true;
+                }
+
                 if (this.Tabs.Contains(tab.TabName))
                     return false;
 
@@ -548,7 +559,7 @@ namespace OpenTween.Models
 
         public bool IsMuted(PostClass post, bool isHomeTimeline)
         {
-            var muteTab = this.GetTabByType<MuteTabModel>();
+            var muteTab = this.MuteTab;
             if (muteTab != null && muteTab.AddFiltered(post) == MyCommon.HITRESULT.Move)
                 return true;
 
@@ -686,9 +697,6 @@ namespace OpenTween.Models
 
                 foreach (var tab in this.Tabs.OfType<FilterTabModel>().ToArray())
                 {
-                    if (tab.TabType == MyCommon.TabUsageType.Mute)
-                        continue;
-
                     // フィルタに変更のあったタブのみを対象とする
                     if (!tab.FilterModified)
                         continue;

OpenTween/Tween.cs
@@ -1057,15 +1057,11 @@ namespace OpenTween
             if (this._statuses.GetTabByType<FavoritesTabModel>() == null)
                 this._statuses.AddTab(new FavoritesTabModel());
 
-            if (this._statuses.GetTabByType<MuteTabModel>() == null)
+            if (this._statuses.MuteTab == null)
                 this._statuses.AddTab(new MuteTabModel());
 
             foreach (var tab in _statuses.Tabs)
             {
-                // ミュートタブは表示しない
-                if (tab.TabType == MyCommon.TabUsageType.Mute)
-                    continue;
-
                 if (!AddNewTab(tab, startup: true))
                     throw new TabException(Properties.Resources.TweenMain_LoadText1);
             }
@@ -7197,7 +7193,7 @@ namespace OpenTween
 
             var tabs = this.ListTab.TabPages.Cast<TabPage>()
                 .Select(x => this._statuses.Tabs[x.Text])
-                .Append(this._statuses.GetTabByType(MyCommon.TabUsageType.Mute));
+                .Append(this._statuses.MuteTab);
 
             foreach (var tab in tabs)
             {

commit 5f6deb229c4cad36b31eeb4dc5a98c0eb1ac9326

FilterDialogでミュートタブを選択するとKeyNotFoundExceptionが発生する不具合を修正
Fixes: 91c59331 ("TabInformations.MuteTabプロパティを追加しTabsからミュートタブを除外")

OpenTween/FilterDialog.cs
@@ -140,8 +140,13 @@ namespace OpenTween
                 CheckProtected.Enabled = true;
             }
 
+            CheckManageRead.CheckedChanged -= this.CheckManageRead_CheckedChanged;
             CheckManageRead.Checked = tab.UnreadManage;
+            CheckManageRead.CheckedChanged += this.CheckManageRead_CheckedChanged;
+
+            CheckNotifyNew.CheckedChanged -= this.CheckNotifyNew_CheckedChanged;
             CheckNotifyNew.Checked = tab.Notify;
+            CheckNotifyNew.CheckedChanged += this.CheckNotifyNew_CheckedChanged;
 
             var idx = ComboSound.Items.IndexOf(tab.SoundFile);
             if (idx == -1) idx = 0;
@@ -151,6 +156,7 @@ namespace OpenTween
 
             if (tab.TabType == MyCommon.TabUsageType.Mute)
             {
+                this.ButtonRenameTab.Enabled = false;
                 this.CheckManageRead.Enabled = false;
                 this.CheckNotifyNew.Enabled = false;
                 this.ComboSound.Enabled = false;
@@ -160,6 +166,7 @@ namespace OpenTween
             }
             else
             {
+                this.ButtonRenameTab.Enabled = true;
                 this.CheckManageRead.Enabled = true;
                 this.CheckNotifyNew.Enabled = true;
                 this.ComboSound.Enabled = true;
@@ -225,7 +232,6 @@ namespace OpenTween
                 _ => "UNKNOWN",
             };
 
-            ButtonRenameTab.Enabled = true;
             if (tab.IsDefaultTabType || tab.Protected)
             {
                 ButtonDeleteTab.Enabled = false;