opentween/OpenTween

commit 91c593311960adea212667bd137228a52019e636

TabInformations.MuteTabプロパティを追加しTabsからミュートタブを除外

OpenTween/Tween.cs
@@ -1057,15 +1057,11 @@ namespace OpenTween
             if (this._statuses.GetTabByType<FavoritesTabModel>() == null)
                 this._statuses.AddTab(new FavoritesTabModel());
 
-            if (this._statuses.GetTabByType<MuteTabModel>() == null)
+            if (this._statuses.MuteTab == null)
                 this._statuses.AddTab(new MuteTabModel());
 
             foreach (var tab in _statuses.Tabs)
             {
-                // ミュートタブは表示しない
-                if (tab.TabType == MyCommon.TabUsageType.Mute)
-                    continue;
-
                 if (!AddNewTab(tab, startup: true))
                     throw new TabException(Properties.Resources.TweenMain_LoadText1);
             }
@@ -7197,7 +7193,7 @@ namespace OpenTween
 
             var tabs = this.ListTab.TabPages.Cast<TabPage>()
                 .Select(x => this._statuses.Tabs[x.Text])
-                .Append(this._statuses.GetTabByType(MyCommon.TabUsageType.Mute));
+                .Append(this._statuses.MuteTab);
 
             foreach (var tab in tabs)
             {

OpenTween/FilterDialog.cs
@@ -102,21 +102,7 @@ namespace OpenTween
 
         private void RefreshListTabs()
         {
-            var tabs = new List<TabModel>();
-
-            foreach (var tab in this._sts.Tabs)
-            {
-                if (tab.TabType == MyCommon.TabUsageType.Mute)
-                    continue;
-
-                tabs.Add(tab);
-            }
-
-            var muteTab = this._sts.GetTabByType(MyCommon.TabUsageType.Mute);
-            if (muteTab != null)
-                tabs.Add(muteTab);
-
-            this.tabs = tabs;
+            this.tabs = this._sts.Tabs.Append(this._sts.MuteTab).ToList();
 
             using (ControlTransaction.Update(this.ListTabs))
             {

OpenTween/Models/TabInformations.cs
@@ -41,6 +41,8 @@ namespace OpenTween.Models
     {
         //個別タブの情報をDictionaryで保持
         public TabCollection Tabs { get; } = new TabCollection();
+        public MuteTabModel MuteTab { get; private set; }
+
         public ConcurrentDictionary<long, PostClass> Posts { get; } = new ConcurrentDictionary<long, PostClass>();
 
         private Dictionary<long, PostClass> _quotes = new Dictionary<long, PostClass>();
@@ -113,6 +115,15 @@ namespace OpenTween.Models
         {
             lock (this.LockObj)
             {
+                if (tab is MuteTabModel muteTab)
+                {
+                    if (this.MuteTab != null)
+                        return false;
+
+                    this.MuteTab = muteTab;
+                    return true;
+                }
+
                 if (this.Tabs.Contains(tab.TabName))
                     return false;
 
@@ -548,7 +559,7 @@ namespace OpenTween.Models
 
         public bool IsMuted(PostClass post, bool isHomeTimeline)
         {
-            var muteTab = this.GetTabByType<MuteTabModel>();
+            var muteTab = this.MuteTab;
             if (muteTab != null && muteTab.AddFiltered(post) == MyCommon.HITRESULT.Move)
                 return true;
 
@@ -686,9 +697,6 @@ namespace OpenTween.Models
 
                 foreach (var tab in this.Tabs.OfType<FilterTabModel>().ToArray())
                 {
-                    if (tab.TabType == MyCommon.TabUsageType.Mute)
-                        continue;
-
                     // フィルタに変更のあったタブのみを対象とする
                     if (!tab.FilterModified)
                         continue;

commit 69d5cc6b9e64c894e4245ebc06fc8bef0c42aeed

ID/Source振り分けルール作成でKeyNotFoundExceptionが発生する不具合を修正
Fixes: 91c59331 ("TabInformations.MuteTabプロパティを追加しTabsからミュートタブを除外")

OpenTween/TabsDialog.cs
@@ -46,7 +46,7 @@ namespace OpenTween
 
         protected internal class TabListItem
         {
-            public TabModel? Tab { get; set; }
+            public FilterTabModel? Tab { get; set; }
             public string Label { get; set; } = "";
 
             public override string ToString()
@@ -82,10 +82,8 @@ namespace OpenTween
 
             var tabs = this.TabInfo.Tabs.Append(this.TabInfo.MuteTab);
 
-            foreach (var tab in tabs)
+            foreach (var tab in tabs.OfType<FilterTabModel>())
             {
-                if (!tab.IsDistributableTabType) continue;
-
                 this.TabList.Items.Add(new TabListItem
                 {
                     Label = tab.TabName,
@@ -110,14 +108,14 @@ namespace OpenTween
                 this.OK_Button.Enabled = true;
         }
 
-        public TabModel? SelectedTab
+        public FilterTabModel? SelectedTab
             => this.TabList.SelectedItem is TabListItem item ? item.Tab : null;
 
-        public TabModel[] SelectedTabs
+        public FilterTabModel[] SelectedTabs
             => this.TabList.SelectedItems
                     .Cast<TabListItem>()
                     .Select(x => x.Tab)
-                    .OfType<TabModel>()
+                    .OfType<FilterTabModel>()
                     .ToArray();
     }
 }

OpenTween/Tween.cs
@@ -8091,9 +8091,9 @@ namespace OpenTween
                 foreach (var post in this.CurrentTab.SelectedPosts)
                 {
                     //タブ選択（or追加）
-                    if (!SelectTab(out var tabName)) return;
+                    if (!SelectTab(out var tab)) return;
 
-                    fltDialog.SetCurrent(tabName);
+                    fltDialog.SetCurrent(tab.TabName);
 
                     if (post.RetweetedBy == null)
                     {
@@ -8251,9 +8251,7 @@ namespace OpenTween
         public void AddFilterRuleByScreenName(params string[] screenNameArray)
         {
             //タブ選択（or追加）
-            if (!SelectTab(out var tabName)) return;
-
-            var tab = (FilterTabModel)this._statuses.Tabs[tabName];
+            if (!SelectTab(out var tab)) return;
 
             bool mv;
             bool mk;
@@ -8288,10 +8286,8 @@ namespace OpenTween
         public void AddFilterRuleBySource(params string[] sourceArray)
         {
             // タブ選択ダイアログを表示（or追加）
-            if (!this.SelectTab(out var tabName))
+            if (!this.SelectTab(out var filterTab))
                 return;
-
-            var filterTab = (FilterTabModel)this._statuses.Tabs[tabName];
 
             bool mv;
             bool mk;
@@ -8324,24 +8320,25 @@ namespace OpenTween
             this.SaveConfigsTabs();
         }
 
-        private bool SelectTab([NotNullWhen(true)] out string? tabName)
+        private bool SelectTab([NotNullWhen(true)] out FilterTabModel? tab)
         {
             do
             {
-                tabName = null;
+                tab = null;
 
                 //振り分け先タブ選択
                 using (var dialog = new TabsDialog(_statuses))
                 {
                     if (dialog.ShowDialog(this) == DialogResult.Cancel) return false;
 
-                    tabName = dialog.SelectedTab?.TabName;
+                    tab = dialog.SelectedTab;
                 }
 
                 this.CurrentTabPage.Focus();
                 //新規タブを選択→タブ作成
-                if (tabName == null)
+                if (tab == null)
                 {
+                    string tabName;
                     using (var inputName = new InputTabName())
                     {
                         inputName.TabName = _statuses.MakeTabName("MyTab");
@@ -8352,8 +8349,8 @@ namespace OpenTween
                     this.TopMost = SettingManager.Common.AlwaysTop;
                     if (!string.IsNullOrEmpty(tabName))
                     {
-                        var tab = new FilterTabModel(tabName);
-                        if (!_statuses.AddTab(tab) || !AddNewTab(tab, startup: false))
+                        var newTab = new FilterTabModel(tabName);
+                        if (!_statuses.AddTab(newTab) || !AddNewTab(newTab, startup: false))
                         {
                             var tmp = string.Format(Properties.Resources.IDRuleMenuItem_ClickText2, tabName);
                             MessageBox.Show(tmp, Properties.Resources.IDRuleMenuItem_ClickText3, MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
@@ -8361,6 +8358,7 @@ namespace OpenTween
                         }
                         else
                         {
+                            tab = newTab;
                             return true;
                         }
                     }