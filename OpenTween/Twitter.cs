opentween/OpenTween

commit a53f5b1afd7402ff06fb9db689ceb44679d93956

PostClass.Cloneメソッドを廃止
RelTabName を廃止したため InnerStorage なタブに追加する際に Clone する必要が無くなった
ただし IsRead や FavoritesCount などが存在するため PostClass は完全に不変ではない

OpenTween/Twitter.cs
@@ -1910,7 +1910,7 @@ namespace OpenTween
                     tab.RelationTargetPost = p;
                 }
             }
-            relPosts.Add(tab.RelationTargetPost.StatusId, tab.RelationTargetPost.Clone());
+            relPosts.Add(tab.RelationTargetPost.StatusId, tab.RelationTargetPost);
 
             Exception lastException = null;
 
@@ -1922,11 +1922,7 @@ namespace OpenTween
                 var inReplyToId = nextPost.InReplyToStatusId.Value;
 
                 var inReplyToPost = TabInformations.GetInstance()[inReplyToId];
-                if (inReplyToPost != null)
-                {
-                    inReplyToPost = inReplyToPost.Clone();
-                }
-                else
+                if (inReplyToPost == null)
                 {
                     try
                     {
@@ -1956,9 +1952,8 @@ namespace OpenTween
                     if (relPosts.ContainsKey(_statusId))
                         continue;
 
-                    PostClass p = null;
-                    var _post = TabInformations.GetInstance()[_statusId];
-                    if (_post == null)
+                    var p = TabInformations.GetInstance()[_statusId];
+                    if (p == null)
                     {
                         try
                         {
@@ -1969,10 +1964,6 @@ namespace OpenTween
                             lastException = ex;
                             break;
                         }
-                    }
-                    else
-                    {
-                        p = _post.Clone();
                     }
 
                     if (p != null)

OpenTween.Tests/PostClassTest.cs
@@ -91,15 +91,6 @@ namespace OpenTween
             };
         }
 
-        [Fact]
-        public void CloneTest()
-        {
-            var post = new PostClass();
-            var clonePost = post.Clone();
-
-            TestUtils.CheckDeepCloning(post, clonePost);
-        }
-
         [Theory]
         [InlineData(null,  null)]
         [InlineData("", "")]

OpenTween/StatusDictionary.cs
@@ -41,7 +41,7 @@ using System.Xml.Serialization;
 
 namespace OpenTween
 {
-    public class PostClass : ICloneable
+    public class PostClass
     {
         public struct StatusGeo : IEquatable<StatusGeo>
         {
@@ -367,17 +367,6 @@ namespace OpenTween
             return false;
         }
 
-        [Obsolete("Use PostClass.Clone() instead.")]
-        public PostClass Copy()
-        {
-            return this.Clone();
-        }
-
-        public PostClass Clone()
-        {
-            return (PostClass)((ICloneable)this).Clone();
-        }
-
         public override bool Equals(object obj)
         {
             if (obj == null || this.GetType() != obj.GetType()) return false;
@@ -421,18 +410,6 @@ namespace OpenTween
         {
             return this.StatusId.GetHashCode();
         }
-
-#region "IClonable.Clone"
-        object ICloneable.Clone()
-        {
-            var clone = (PostClass)this.MemberwiseClone();
-            clone.ReplyToList = new List<string>(this.ReplyToList);
-            clone.Media = new List<MediaInfo>(this.Media);
-            clone.QuoteStatusIds = this.QuoteStatusIds.ToArray();
-
-            return clone;
-        }
-#endregion
     }
 
     public class MediaInfo

OpenTween/Tween.cs
@@ -5906,7 +5906,7 @@ namespace OpenTween
                     var resultTab = this._statuses.Tabs[tabName];
                     foreach (var post in posts)
                     {
-                        resultTab.AddPostToInnerStorage(post.Clone());
+                        resultTab.AddPostToInnerStorage(post);
                     }
 
                     this._statuses.DistributePosts();

commit b49a4041a1b3ce05d823f8e9b8568d744d440542

関連発言表示によって内部の未読状態と未読表示が一致しない状態になる不具合を修正 (thx @wal_san!)
https://twitter.com/wal_san/status/1489886234990178304

Fixes: a53f5b1a ("PostClass.Cloneメソッドを廃止")

OpenTween/Twitter.cs
@@ -1069,12 +1069,13 @@ namespace OpenTween
 
             relPosts.Values.ToList().ForEach(p =>
             {
-                if (p.IsMe && !read && this.ReadOwnPost)
-                    p.IsRead = true;
+                var post = p.Clone();
+                if (post.IsMe && !read && this.ReadOwnPost)
+                    post.IsRead = true;
                 else
-                    p.IsRead = read;
+                    post.IsRead = read;
 
-                tab.AddPostQueue(p);
+                tab.AddPostQueue(post);
             });
 
             if (lastException != null)