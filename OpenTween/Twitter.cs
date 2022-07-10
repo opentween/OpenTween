opentween/OpenTween

commit e03de8b09129ced44db9f2269fd557f9f5cb7261

Recentタブのツイート取得にTwitter API v2を使用する

OpenTween/Twitter.cs
@@ -44,6 +44,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using OpenTween.Api;
 using OpenTween.Api.DataModel;
+using OpenTween.Api.TwitterV2;
 using OpenTween.Connection;
 using OpenTween.Models;
 using OpenTween.Setting;
@@ -547,17 +548,19 @@ namespace OpenTween
 
             var count = GetApiResultCount(MyCommon.WORKERTYPE.Timeline, more, startup);
 
-            TwitterStatus[] statuses;
-            if (more)
+            var request = new GetTimelineRequest(this.UserId)
             {
-                statuses = await this.Api.StatusesHomeTimeline(count, maxId: tab.OldestId)
-                    .ConfigureAwait(false);
-            }
-            else
-            {
-                statuses = await this.Api.StatusesHomeTimeline(count)
-                    .ConfigureAwait(false);
-            }
+                MaxResults = count,
+                UntilId = more ? tab.OldestId.ToString() : null,
+            };
+
+            var response = await request.Send(this.Api.Connection)
+                .ConfigureAwait(false);
+
+            var tweetIds = response.Data.Select(x => x.Id).ToList();
+
+            var statuses = await this.Api.StatusesLookup(tweetIds)
+                .ConfigureAwait(false);
 
             var minimumId = this.CreatePostsFromJson(statuses, MyCommon.WORKERTYPE.Timeline, tab, read);
             if (minimumId != null)

OpenTween.Tests/Api/TwitterApiTest.cs
@@ -198,6 +198,34 @@ namespace OpenTween.Api
         }
 
         [Fact]
+        public async Task StatusesLookup_Test()
+        {
+            var mock = new Mock<IApiConnection>();
+            mock.Setup(x =>
+                x.GetAsync<TwitterStatus[]>(
+                    new Uri("statuses/lookup.json", UriKind.Relative),
+                    new Dictionary<string, string>
+                    {
+                        { "id", "100,200" },
+                        { "include_entities", "true" },
+                        { "include_ext_alt_text", "true" },
+                        { "tweet_mode", "extended" },
+                    },
+                    "/statuses/lookup"
+                )
+            )
+            .ReturnsAsync(Array.Empty<TwitterStatus>());
+
+            using var twitterApi = new TwitterApi(ApiKey.Create("fake_consumer_key"), ApiKey.Create("fake_consumer_secret"));
+            twitterApi.ApiConnection = mock.Object;
+
+            await twitterApi.StatusesLookup(statusIds: new[] { "100", "200" })
+                .ConfigureAwait(false);
+
+            mock.VerifyAll();
+        }
+
+        [Fact]
         public async Task StatusesUpdate_Test()
         {
             var mock = new Mock<IApiConnection>();

OpenTween.Tests/Api/TwitterV2/GetTimelineRequestTest.cs
@@ -1,0 +1,65 @@
+﻿// OpenTween - Client of Twitter
+// Copyright (c) 2922 kim_upsilon (@kim_upsilon) <https://upsilo.net/~upsilon/>
+// All rights reserved.
+//
+// This file is part of OpenTween.
+//
+// This program is free software; you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the Free
+// Software Foundation; either version 3 of the License, or (at your option)
+// any later version.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+// for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program. If not, see <http://www.gnu.org/licenses/>, or write to
+// the Free Software Foundation, Inc., 51 Franklin Street - Fifth Floor,
+// Boston, MA 02110-1301, USA.
+
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Moq;
+using OpenTween.Api.DataModel;
+using OpenTween.Connection;
+using Xunit;
+
+namespace OpenTween.Api.TwitterV2
+{
+    public class GetTimelineRequestTest
+    {
+        [Fact]
+        public async Task StatusesMentionsTimeline_Test()
+        {
+            var mock = new Mock<IApiConnection>();
+            mock.Setup(x =>
+                x.GetAsync<TwitterV2TweetIds>(
+                    new Uri("/2/users/100/timelines/reverse_chronological", UriKind.Relative),
+                    new Dictionary<string, string>
+                    {
+                        { "tweet.fields", "id" },
+                        { "max_results", "200" },
+                        { "until_id", "900" },
+                        { "since_id", "100" },
+                    },
+                    "/2/users/:id/timelines/reverse_chronological"
+                )
+            )
+            .ReturnsAsync(new TwitterV2TweetIds());
+
+            var request = new GetTimelineRequest(userId: 100L)
+            {
+                MaxResults = 200,
+                SinceId = "100",
+                UntilId = "900",
+            };
+
+            await request.Send(mock.Object).ConfigureAwait(false);
+
+            mock.VerifyAll();
+        }
+    }
+}

OpenTween/Api/DataModel/TwitterV2TweetId.cs
@@ -1,0 +1,34 @@
+﻿// OpenTween - Client of Twitter
+// Copyright (c) 2022 kim_upsilon (@kim_upsilon) <https://upsilo.net/~upsilon/>
+// All rights reserved.
+//
+// This file is part of OpenTween.
+//
+// This program is free software; you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the Free
+// Software Foundation; either version 3 of the License, or (at your option)
+// any later version.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+// for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program. If not, see <http://www.gnu.org/licenses/>, or write to
+// the Free Software Foundation, Inc., 51 Franklin Street - Fifth Floor,
+// Boston, MA 02110-1301, USA.
+
+#nullable enable annotations
+
+using System.Runtime.Serialization;
+
+namespace OpenTween.Api.DataModel
+{
+    [DataContract]
+    public class TwitterV2TweetId
+    {
+        [DataMember(Name = "id")]
+        public string Id { get; set; }
+    }
+}

OpenTween/Api/DataModel/TwitterV2TweetIds.cs
@@ -1,0 +1,34 @@
+﻿// OpenTween - Client of Twitter
+// Copyright (c) 2022 kim_upsilon (@kim_upsilon) <https://upsilo.net/~upsilon/>
+// All rights reserved.
+//
+// This file is part of OpenTween.
+//
+// This program is free software; you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the Free
+// Software Foundation; either version 3 of the License, or (at your option)
+// any later version.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+// for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program. If not, see <http://www.gnu.org/licenses/>, or write to
+// the Free Software Foundation, Inc., 51 Franklin Street - Fifth Floor,
+// Boston, MA 02110-1301, USA.
+
+#nullable enable annotations
+
+using System.Runtime.Serialization;
+
+namespace OpenTween.Api.DataModel
+{
+    [DataContract]
+    public class TwitterV2TweetIds
+    {
+        [DataMember(Name = "data")]
+        public TwitterV2TweetId[] Data { get; set; }
+    }
+}

OpenTween/Api/TwitterApi.cs
@@ -138,6 +138,20 @@ namespace OpenTween.Api
             return this.Connection.GetAsync<TwitterStatus>(endpoint, param, "/statuses/show/:id");
         }
 
+        public Task<TwitterStatus[]> StatusesLookup(IReadOnlyList<string> statusIds)
+        {
+            var endpoint = new Uri("statuses/lookup.json", UriKind.Relative);
+            var param = new Dictionary<string, string>
+            {
+                ["id"] = string.Join(",", statusIds),
+                ["include_entities"] = "true",
+                ["include_ext_alt_text"] = "true",
+                ["tweet_mode"] = "extended",
+            };
+
+            return this.Connection.GetAsync<TwitterStatus[]>(endpoint, param, "/statuses/lookup");
+        }
+
         public Task<LazyJson<TwitterStatus>> StatusesUpdate(
             string status,
             long? replyToId,

OpenTween/Api/TwitterV2/GetTimelineRequest.cs
@@ -1,0 +1,79 @@
+﻿// OpenTween - Client of Twitter
+// Copyright (c) 2022 kim_upsilon (@kim_upsilon) <https://upsilo.net/~upsilon/>
+// All rights reserved.
+//
+// This file is part of OpenTween.
+//
+// This program is free software; you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the Free
+// Software Foundation; either version 3 of the License, or (at your option)
+// any later version.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+// for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program. If not, see <http://www.gnu.org/licenses/>, or write to
+// the Free Software Foundation, Inc., 51 Franklin Street - Fifth Floor,
+// Boston, MA 02110-1301, USA.
+
+#nullable enable
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTween.Api.DataModel;
+using OpenTween.Connection;
+
+namespace OpenTween.Api.TwitterV2
+{
+    public class GetTimelineRequest
+    {
+        public static readonly string EndpointName = "/2/users/:id/timelines/reverse_chronological";
+
+        public long UserId { get; set; }
+
+        public int? MaxResults { get; set; }
+
+        public string? UntilId { get; set; }
+
+        public string? SinceId { get; set; }
+
+        public GetTimelineRequest(long userId)
+            => this.UserId = userId;
+
+        private Uri CreateEndpointUri()
+            => new($"/2/users/{this.UserId}/timelines/reverse_chronological", UriKind.Relative);
+
+        private Dictionary<string, string> CreateParameters()
+        {
+            var param = new Dictionary<string, string>
+            {
+                ["tweet.fields"] = "id",
+            };
+
+            if (this.MaxResults != null)
+                param["max_results"] = this.MaxResults.ToString();
+
+            if (this.UntilId != null)
+                param["until_id"] = this.UntilId;
+
+            if (this.SinceId != null)
+                param["since_id"] = this.SinceId;
+
+            return param;
+        }
+
+        public Task<TwitterV2TweetIds> Send(IApiConnection apiConnection)
+        {
+            var uri = this.CreateEndpointUri();
+            var param = this.CreateParameters();
+
+            return apiConnection.GetAsync<TwitterV2TweetIds>(uri, param, EndpointName);
+        }
+    }
+}

OpenTween/ApiInfoDialog.cs
@@ -31,6 +31,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using OpenTween.Api;
+using OpenTween.Api.TwitterV2;
 
 namespace OpenTween
 {
@@ -41,7 +42,7 @@ namespace OpenTween
 
         private readonly List<string> tlEndpoints = new()
         {
-            "/statuses/home_timeline",
+            GetTimelineRequest.EndpointName,
             "/statuses/mentions_timeline",
             "/statuses/show/:id",
             "/statuses/user_timeline",

commit cf867226e81ca2d7d92496c9455b8339cc425b37

RecentタブのAPI v2移行による取得件数上限の変更を反映
Fixes: e03de8b0 ("Recentタブのツイート取得にTwitter API v2を使用する")

OpenTween/Twitter.cs
@@ -449,7 +449,7 @@ namespace OpenTween
             // https://dev.twitter.com/rest/public
             return type switch
             {
-                MyCommon.WORKERTYPE.Timeline => 200,
+                MyCommon.WORKERTYPE.Timeline => 100,
                 MyCommon.WORKERTYPE.Reply => 200,
                 MyCommon.WORKERTYPE.UserTimeline => 200,
                 MyCommon.WORKERTYPE.Favorites => 200,

OpenTween.Tests/TwitterTest.cs
@@ -166,7 +166,7 @@ namespace OpenTween
 
             // Timeline
             Assert.Equal(timeline, Twitter.GetApiResultCount(MyCommon.WORKERTYPE.Timeline, false, false));
-            Assert.Equal(more, Twitter.GetApiResultCount(MyCommon.WORKERTYPE.Timeline, true, false));
+            Assert.Equal(100, Twitter.GetApiResultCount(MyCommon.WORKERTYPE.Timeline, true, false)); // 100 件が上限
             Assert.Equal(startup, Twitter.GetApiResultCount(MyCommon.WORKERTYPE.Timeline, false, true));
 
             // Reply