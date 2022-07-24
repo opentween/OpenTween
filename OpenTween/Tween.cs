opentween/OpenTween

commit 7f2fa759272630b26ebe9840f9e8565454868bed

ファイルメニューに「ツイートURLを開く」を追加

OpenTween/Tween.cs
@@ -7742,6 +7742,51 @@ namespace OpenTween
             tabSetting.Save();
         }
 
+        private /* async */ void OpenURLFileMenuItem_Click(object sender, EventArgs e)
+        {
+            string inputText;
+            var ret = InputDialog.Show(this, Properties.Resources.OpenURL_InputText, Properties.Resources.OpenURL_Caption, out inputText);
+            if (ret != DialogResult.OK)
+                return;
+
+            var match = Twitter.StatusUrlRegex.Match(inputText);
+            if (!match.Success)
+            {
+                MessageBox.Show(this, Properties.Resources.OpenURL_InvalidFormat,
+                    Properties.Resources.OpenURL_Caption, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            var statusId = long.Parse(match.Groups["StatusId"].Value);
+            var uiScheduler = TaskScheduler.FromCurrentSynchronizationContext();
+
+            Task.Factory.StartNew(() =>
+                {
+                    var post = this._statuses[statusId];
+                    if (post == null)
+                    {
+                        var err = this.tw.GetStatusApi(false, statusId, ref post);
+                        if (!string.IsNullOrEmpty(err))
+                            throw new WebApiException(err);
+                    }
+                    return post;
+                }, CancellationToken.None, TaskCreationOptions.None, TaskScheduler.Default)
+                .ContinueWith(t =>
+                {
+                    if (t.IsFaulted)
+                    {
+                        t.Exception.Flatten().Handle(x => x is WebApiException);
+
+                        var message = t.Exception.InnerException.Message;
+                        MessageBox.Show(this, string.Format(Properties.Resources.OpenURL_LoadFailed, message),
+                            Properties.Resources.OpenURL_Caption, MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                        return;
+                    }
+                    this.OpenRelatedTab(t.Result);
+                }, uiScheduler);
+        }
+
         private void SaveLogMenuItem_Click(object sender, EventArgs e)
         {
             DialogResult rslt = MessageBox.Show(string.Format(Properties.Resources.SaveLogMenuItem_ClickText1, Environment.NewLine),
@@ -12450,49 +12495,43 @@ namespace OpenTween
 
         private void ShowRelatedStatusesMenuItem_Click(object sender, EventArgs e) // Handles ShowRelatedStatusesMenuItem.Click, ShowRelatedStatusesMenuItem2.Click
         {
-            TabClass backToTab = _curTab == null ? _statuses.Tabs[ListTab.SelectedTab.Text] : _statuses.Tabs[_curTab.Text];
             if (this.ExistCurrentPost && !_curPost.IsDm)
             {
-                //PublicSearchも除外した方がよい？
-                if (_statuses.GetTabByType(MyCommon.TabUsageType.Related) == null)
-                {
-                    const string TabName = "Related Tweets";
-                    string tName = TabName;
-                    if (!this.AddNewTab(tName, false, MyCommon.TabUsageType.Related))
-                    {
-                        for (int i = 2; i <= 100; i++)
-                        {
-                            tName = TabName + i.ToString();
-                            if (this.AddNewTab(tName, false, MyCommon.TabUsageType.Related))
-                            {
-                                _statuses.AddTab(tName, MyCommon.TabUsageType.Related, null);
-                                break;
-                            }
-                        }
-                    }
-                    else
-                    {
-                        _statuses.AddTab(tName, MyCommon.TabUsageType.Related, null);
-                    }
-                    _statuses.GetTabByName(tName).UnreadManage = false;
-                    _statuses.GetTabByName(tName).Notify = false;
-                }
-
-                TabClass tb = _statuses.GetTabByType(MyCommon.TabUsageType.Related);
-                tb.RelationTargetPost = _curPost;
-                this.ClearTab(tb.TabName, false);
-                for (int i = 0; i < ListTab.TabPages.Count; i++)
-                {
-                    if (tb.TabName == ListTab.TabPages[i].Text)
-                    {
-                        ListTab.SelectedIndex = i;
-                        ListTabSelect(ListTab.TabPages[i]);
-                        break;
-                    }
-                }
-
-                GetTimeline(MyCommon.WORKERTYPE.Related, 1, 1, tb.TabName);
+                this.OpenRelatedTab(this._curPost);
             }
+        }
+
+        private void OpenRelatedTab(PostClass post)
+        {
+            const string tabName = "Related Tweets";
+
+            var tabRelated = this._statuses.GetTabByType(MyCommon.TabUsageType.Related);
+
+            if (tabRelated == null)
+            {
+                this.AddNewTab(tabName, false, MyCommon.TabUsageType.Related);
+                this._statuses.AddTab(tabName, MyCommon.TabUsageType.Related, null);
+
+                tabRelated = this._statuses.GetTabByType(MyCommon.TabUsageType.Related);
+                tabRelated.UnreadManage = false;
+                tabRelated.Notify = false;
+            }
+
+            tabRelated.RelationTargetPost = post;
+            this.ClearTab(tabName, false);
+
+            for (int i = 0; i < this.ListTab.TabPages.Count; i++)
+            {
+                var tabPage = this.ListTab.TabPages[i];
+                if (tabName == tabPage.Text)
+                {
+                    this.ListTab.SelectedIndex = i;
+                    this.ListTabSelect(tabPage);
+                    break;
+                }
+            }
+
+            this.GetTimeline(MyCommon.WORKERTYPE.Related, 1, 1, tabName);
         }
 
         private void CacheInfoMenuItem_Click(object sender, EventArgs e)

OpenTween/Properties/Resources.Designer.cs
@@ -1713,6 +1713,42 @@ namespace OpenTween.Properties {
         }
         
         /// <summary>
+        ///   ツイートURLを開く に類似しているローカライズされた文字列を検索します。
+        /// </summary>
+        internal static string OpenURL_Caption {
+            get {
+                return ResourceManager.GetString("OpenURL_Caption", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   ツイートの URL を入力して下さい。 に類似しているローカライズされた文字列を検索します。
+        /// </summary>
+        internal static string OpenURL_InputText {
+            get {
+                return ResourceManager.GetString("OpenURL_InputText", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   入力された URL が正しくありません。 に類似しているローカライズされた文字列を検索します。
+        /// </summary>
+        internal static string OpenURL_InvalidFormat {
+            get {
+                return ResourceManager.GetString("OpenURL_InvalidFormat", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   ツイートの取得に失敗しました ({0}) に類似しているローカライズされた文字列を検索します。
+        /// </summary>
+        internal static string OpenURL_LoadFailed {
+            get {
+                return ResourceManager.GetString("OpenURL_LoadFailed", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   発言 {0}  フォロー {1}  フォロワー {2}({3}) に類似しているローカライズされた文字列を検索します。
         /// </summary>
         internal static string OwnStatusTitle {

OpenTween/Properties/Resources.en.resx
@@ -1074,4 +1074,16 @@
   <data name="ConfigDirectoryNotExist" xml:space="preserve">
     <value>The configuration directory ({0}) specified by the "/configDir" option does not exist or is not accessible.</value>
   </data>
+  <data name="OpenURL_Caption" xml:space="preserve">
+    <value>Open URL</value>
+  </data>
+  <data name="OpenURL_InputText" xml:space="preserve">
+    <value>Enter the URL of the tweet you want to open.</value>
+  </data>
+  <data name="OpenURL_InvalidFormat" xml:space="preserve">
+    <value>The input URL is not valid.</value>
+  </data>
+  <data name="OpenURL_LoadFailed" xml:space="preserve">
+    <value>Failed to load the tweet. ({0})</value>
+  </data>
 </root>

OpenTween/Properties/Resources.resx
@@ -1132,4 +1132,16 @@
   <data name="ConfigDirectoryNotExist" xml:space="preserve">
     <value>"/configDir" で指定されたディレクトリ ({0}) は存在しないかアクセスできません。</value>
   </data>
+  <data name="OpenURL_Caption" xml:space="preserve">
+    <value>ツイートURLを開く</value>
+  </data>
+  <data name="OpenURL_InputText" xml:space="preserve">
+    <value>ツイートの URL を入力して下さい。</value>
+  </data>
+  <data name="OpenURL_InvalidFormat" xml:space="preserve">
+    <value>入力された URL が正しくありません。</value>
+  </data>
+  <data name="OpenURL_LoadFailed" xml:space="preserve">
+    <value>ツイートの取得に失敗しました ({0})</value>
+  </data>
 </root>

OpenTween/Tween.Designer.cs
@@ -133,6 +133,8 @@
             this.MenuStrip1 = new System.Windows.Forms.MenuStrip();
             this.MenuItemFile = new System.Windows.Forms.ToolStripMenuItem();
             this.SettingFileMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator44 = new System.Windows.Forms.ToolStripSeparator();
+            this.OpenURLFileMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.ToolStripSeparator21 = new System.Windows.Forms.ToolStripSeparator();
             this.SaveFileMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.ToolStripSeparator23 = new System.Windows.Forms.ToolStripSeparator();
@@ -1239,6 +1241,8 @@
             resources.ApplyResources(this.MenuItemFile, "MenuItemFile");
             this.MenuItemFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.SettingFileMenuItem,
+            this.toolStripSeparator44,
+            this.OpenURLFileMenuItem,
             this.ToolStripSeparator21,
             this.SaveFileMenuItem,
             this.ToolStripSeparator23,
@@ -1257,6 +1261,17 @@
             resources.ApplyResources(this.SettingFileMenuItem, "SettingFileMenuItem");
             this.SettingFileMenuItem.Name = "SettingFileMenuItem";
             this.SettingFileMenuItem.Click += new System.EventHandler(this.SettingStripMenuItem_Click);
+            // 
+            // toolStripSeparator44
+            // 
+            this.toolStripSeparator44.Name = "toolStripSeparator44";
+            resources.ApplyResources(this.toolStripSeparator44, "toolStripSeparator44");
+            // 
+            // OpenURLFileMenuItem
+            // 
+            this.OpenURLFileMenuItem.Name = "OpenURLFileMenuItem";
+            resources.ApplyResources(this.OpenURLFileMenuItem, "OpenURLFileMenuItem");
+            this.OpenURLFileMenuItem.Click += new System.EventHandler(this.OpenURLFileMenuItem_Click);
             // 
             // ToolStripSeparator21
             // 
@@ -2844,6 +2859,8 @@
         private TweetThumbnail tweetThumbnail1;
         private System.Windows.Forms.ToolStripMenuItem TwitterApiStatusToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem ReloadIconToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator44;
+        private System.Windows.Forms.ToolStripMenuItem OpenURLFileMenuItem;
 
     }
 }

OpenTween/Tween.en.resx
@@ -960,6 +960,9 @@
   <data name="SettingFileMenuItem.Text" xml:space="preserve">
     <value>Settings(&amp;O)...</value>
   </data>
+  <data name="OpenURLFileMenuItem.Text" xml:space="preserve">
+    <value>Open URL(&amp;L)...</value>
+  </data>
   <data name="ToolStripSeparator21.Size" type="System.Drawing.Size, System.Drawing">
     <value>239, 6</value>
   </data>

OpenTween/Tween.resx
@@ -1565,6 +1565,15 @@
   <data name="SettingFileMenuItem.Text" xml:space="preserve">
     <value>設定(&amp;O)</value>
   </data>
+  <data name="toolStripSeparator44.Size" type="System.Drawing.Size, System.Drawing">
+    <value>187, 6</value>
+  </data>
+  <data name="OpenURLFileMenuItem.Size" type="System.Drawing.Size, System.Drawing">
+    <value>190, 22</value>
+  </data>
+  <data name="OpenURLFileMenuItem.Text" xml:space="preserve">
+    <value>ツイートURLを開く(&amp;L)...</value>
+  </data>
   <data name="&gt;&gt;MenuItemHelp.Name" xml:space="preserve">
     <value>MenuItemHelp</value>
   </data>
@@ -4195,6 +4204,18 @@
     <value>2</value>
   </data>
   <data name="&gt;&gt;SettingFileMenuItem.Type" xml:space="preserve">
+    <value>System.Windows.Forms.ToolStripMenuItem, System.Windows.Forms, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089</value>
+  </data>
+  <data name="&gt;&gt;toolStripSeparator44.Name" xml:space="preserve">
+    <value>toolStripSeparator44</value>
+  </data>
+  <data name="&gt;&gt;toolStripSeparator44.Type" xml:space="preserve">
+    <value>System.Windows.Forms.ToolStripSeparator, System.Windows.Forms, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089</value>
+  </data>
+  <data name="&gt;&gt;OpenURLFileMenuItem.Name" xml:space="preserve">
+    <value>OpenURLFileMenuItem</value>
+  </data>
+  <data name="&gt;&gt;OpenURLFileMenuItem.Type" xml:space="preserve">
     <value>System.Windows.Forms.ToolStripMenuItem, System.Windows.Forms, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089</value>
   </data>
   <data name="MultiLineMenuItem.ShortcutKeys" type="System.Windows.Forms.Keys, System.Windows.Forms">

commit 9d69549373b5df8c322e3eb11f0a417047cfaf35

「ツイートURLを開く」でOverflowExceptionが発生する不具合を修正
https://osdn.net/projects/opentween/ticket/45204

Fixes: 7f2fa759 ("ファイルメニューに「ツイートURLを開く」を追加")

OpenTween/Tween.cs
@@ -5799,6 +5799,17 @@ namespace OpenTween
 
         private async void OpenURLFileMenuItem_Click(object sender, EventArgs e)
         {
+            static void ShowFormatErrorDialog(IWin32Window owner)
+            {
+                MessageBox.Show(
+                    owner,
+                    Properties.Resources.OpenURL_InvalidFormat,
+                    Properties.Resources.OpenURL_Caption,
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error
+                );
+            }
+
             var ret = InputDialog.Show(this, Properties.Resources.OpenURL_InputText, Properties.Resources.OpenURL_Caption, out var inputText);
             if (ret != DialogResult.OK)
                 return;
@@ -5806,12 +5817,7 @@ namespace OpenTween
             var match = Twitter.StatusUrlRegex.Match(inputText);
             if (!match.Success)
             {
-                MessageBox.Show(
-                    this,
-                    Properties.Resources.OpenURL_InvalidFormat,
-                    Properties.Resources.OpenURL_Caption,
-                    MessageBoxButtons.OK,
-                    MessageBoxIcon.Error);
+                ShowFormatErrorDialog(this);
                 return;
             }
 
@@ -5819,6 +5825,10 @@ namespace OpenTween
             {
                 var statusId = long.Parse(match.Groups["StatusId"].Value);
                 await this.OpenRelatedTab(statusId);
+            }
+            catch (OverflowException)
+            {
+                ShowFormatErrorDialog(this);
             }
             catch (TabException ex)
             {