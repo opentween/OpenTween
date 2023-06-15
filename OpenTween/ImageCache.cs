opentween/OpenTween

commit 519d4f3e994385e21a1dd873232d7a2a178a07cf

アイコンのキャッシュ機構を書き直し
 * LRU によるキャッシュを実装
 * 副産物として System.Runtime.Caching.dll 依存が無くなりました
   (まだ .NET Framework 4 Full に依存する箇所は残っています)
 * 同一アイコンに対するリクエストが複数同時に行われた場合、
   一本の非同期タスクに纏められるようになりました

OpenTween/ImageCache.cs
@@ -1,0 +1,182 @@
+﻿// OpenTween - Client of Twitter
+// Copyright (c) 2013 kim_upsilon (@kim_upsilon) <https://upsilo.net/~upsilon/>
+// All rights reserved.
+//
+// This file is part of OpenTween.
+//
+// This program is free software; you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the Free
+// Software Foundation; either version 3 of the License, or (at your option)
+// any later version.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+// for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program. If not, see <http://www.gnu.org/licenses/>, or write to
+// the Free Software Foundation, Inc., 51 Franklin Street - Fifth Floor,
+// Boston, MA 02110-1301, USA.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Threading;
+
+namespace OpenTween
+{
+    public class ImageCache : IDisposable
+    {
+        /// <summary>
+        /// キャッシュとして URL と取得した画像を対に保持する辞書
+        /// </summary>
+        internal LRUCacheDictionary<string, Task<MemoryImage>> innerDictionary;
+
+        /// <summary>
+        /// 非同期タスクをキャンセルするためのトークンのもと
+        /// </summary>
+        private CancellationTokenSource cancelTokenSource;
+
+        /// <summary>
+        /// innerDictionary の排他制御のためのロックオブジェクト
+        /// </summary>
+        private object lockObject = new object();
+
+        /// <summary>
+        /// オブジェクトが破棄された否か
+        /// </summary>
+        private bool disposed = false;
+
+        public ImageCache()
+        {
+            lock (this.lockObject)
+            {
+                this.innerDictionary = new LRUCacheDictionary<string, Task<MemoryImage>>(trimLimit: 300, autoTrimCount: 100);
+                
+                this.innerDictionary.CacheRemoved += (s, e) => {
+                    var imageTask = e.Item.Value;
+
+                    if (imageTask.Status == TaskStatus.RanToCompletion)
+                        imageTask.Result.Dispose();
+
+                    imageTask.Dispose();
+
+                    this.CacheRemoveCount++;
+                };
+
+                this.cancelTokenSource = new CancellationTokenSource();
+            }
+        }
+
+        /// <summary>
+        /// 保持しているキャッシュの件数
+        /// </summary>
+        public long CacheCount
+        {
+            get { return this.innerDictionary.Count; }
+        }
+
+        /// <summary>
+        /// 破棄されたキャッシュの件数
+        /// </summary>
+        public int CacheRemoveCount { get; private set; }
+
+        /// <summary>
+        /// 指定された URL にある画像を非同期に取得するメソッド
+        /// </summary>
+        /// <param name="address">取得先の URL</param>
+        /// <param name="force">キャッシュを使用せずに取得する場合は true</param>
+        /// <returns>非同期に画像を取得するタスク</returns>
+        public Task<MemoryImage> DownloadImageAsync(string address, bool force = false)
+        {
+            var cancelToken = this.cancelTokenSource.Token;
+
+            return Task.Factory.StartNew(() =>
+            {
+                Task<MemoryImage> cachedImageTask = null;
+                lock (this.lockObject)
+                {
+                    if (force)
+                        this.innerDictionary.Remove(address);
+
+                    if (this.innerDictionary.ContainsKey(address) && !this.innerDictionary[address].IsFaulted)
+                        cachedImageTask = this.innerDictionary[address];
+                }
+
+                if (cachedImageTask != null)
+                    return cachedImageTask;
+
+                cancelToken.ThrowIfCancellationRequested();
+
+                using (var client = new OTWebClient() { Timeout = 10000 })
+                {
+                    var imageTask = client.DownloadDataAsync(new Uri(address), cancelToken).ContinueWith(t =>
+                    {
+                        MemoryImage image = null;
+                        if (t.Status == TaskStatus.RanToCompletion)
+                        {
+                            image = MemoryImage.CopyFromBytes(t.Result);
+                        }
+
+                        if (t.Exception != null)
+                            t.Exception.Handle(e => e is WebException);
+
+                        // FIXME: MemoryImage.Dispose() が正しいタイミングで呼ばれるように修正すべき
+                        return image;
+                    }, cancelToken);
+
+                    lock (this.lockObject)
+                    {
+                        this.innerDictionary[address] = imageTask;
+                    }
+
+                    return imageTask;
+                }
+            }, cancelToken).Unwrap();
+        }
+
+        public MemoryImage TryGetFromCache(string address)
+        {
+            lock (this.lockObject)
+            {
+                if (!this.innerDictionary.ContainsKey(address))
+                    return null;
+
+                var imageTask = this.innerDictionary[address];
+                if (imageTask.Status != TaskStatus.RanToCompletion)
+                    return null;
+
+                return imageTask.Result;
+            }
+        }
+
+        public void CancelAsync()
+        {
+            lock (this.lockObject)
+            {
+                this.cancelTokenSource.Cancel();
+                this.cancelTokenSource = new CancellationTokenSource();
+            }
+        }
+
+        public void Dispose()
+        {
+            if (this.disposed) return;
+
+            this.CancelAsync();
+
+            lock (this.lockObject)
+            {
+                foreach (var item in this.innerDictionary)
+                    item.Value.Dispose();
+
+                this.innerDictionary.Clear();
+                this.cancelTokenSource.Dispose();
+            }
+        }
+    }
+}

OpenTween.Tests/LRUCacheDictionaryTest.cs
@@ -1,0 +1,376 @@
+﻿// OpenTween - Client of Twitter
+// Copyright (c) 2013 kim_upsilon (@kim_upsilon) <https://upsilo.net/~upsilon/>
+// All rights reserved.
+//
+// This file is part of OpenTween.
+//
+// This program is free software; you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the Free
+// Software Foundation; either version 3 of the License, or (at your option)
+// any later version.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+// for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program. If not, see <http://www.gnu.org/licenses/>, or write to
+// the Free Software Foundation, Inc., 51 Franklin Street - Fifth Floor,
+// Boston, MA 02110-1301, USA.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace OpenTween
+{
+    [TestFixture]
+    class LRUCacheDictionaryTest
+    {
+        [Test]
+        public void InnerListTest()
+        {
+            var dict = new LRUCacheDictionary<string, string>
+            {
+                {"key1", "value1"},
+                {"key2", "value2"},
+                {"key3", "value3"},
+            };
+
+            var node = dict.innerList.First;
+            Assert.That(node.Value.Key, Is.EqualTo("key3"));
+            node = node.Next;
+            Assert.That(node.Value.Key, Is.EqualTo("key2"));
+            node = node.Next;
+            Assert.That(node.Value.Key, Is.EqualTo("key1"));
+
+            // 2 -> 3 -> 1 の順に値を参照
+            var x = dict["key2"];
+            x = dict["key3"];
+            x = dict["key1"];
+
+            // 直近に参照した順で並んでいるかテスト
+            node = dict.innerList.First;
+            Assert.That(node.Value.Key, Is.EqualTo("key1"));
+            node = node.Next;
+            Assert.That(node.Value.Key, Is.EqualTo("key3"));
+            node = node.Next;
+            Assert.That(node.Value.Key, Is.EqualTo("key2"));
+        }
+
+        [Test]
+        public void TrimLimitTest([Range(1, 5)] int trimLimit)
+        {
+            var dict = new LRUCacheDictionary<string, string>()
+            {
+                {"key1", "value1"},
+                {"key2", "value2"},
+                {"key3", "value3"},
+            };
+
+            dict.TrimLimit = trimLimit;
+            var ret = dict.Trim();
+
+            if (trimLimit >= 3)
+            {
+                // trimLimit がアイテムの件数より大きい場合、Trim メソッドは動作せずに false を返す。
+                Assert.That(ret, Is.False);
+                Assert.That(dict.Count, Is.EqualTo(3));
+            }
+            else
+            {
+                Assert.That(ret, Is.True);
+                Assert.That(dict.Count, Is.EqualTo(trimLimit));
+            }
+        }
+
+        [Test]
+        public void TrimOrderTest()
+        {
+            var dict = new LRUCacheDictionary<string, string>()
+            {
+                {"key1", "value1"},
+                {"key2", "value2"},
+                {"key3", "value3"},
+                {"key4", "value4"},
+                {"key5", "value5"},
+            };
+
+            // 4 -> 2 -> 3 -> 1 -> 5 の順で参照
+            var x = dict["key4"];
+            x = dict["key2"];
+            x = dict["key3"];
+            x = dict["key1"];
+            x = dict["key5"];
+
+            // 3 個までに縮小
+            dict.TrimLimit = 3;
+            dict.Trim();
+
+            // 直近に参照された 3 -> 1 -> 5 のみ残っているはず
+            Assert.That(dict.ContainsKey("key1"), Is.True);
+            Assert.That(dict.ContainsKey("key2"), Is.False);
+            Assert.That(dict.ContainsKey("key3"), Is.True);
+            Assert.That(dict.ContainsKey("key4"), Is.False);
+            Assert.That(dict.ContainsKey("key5"), Is.True);
+        }
+
+        [Test]
+        public void AutoTrimTest()
+        {
+            var dict = new LRUCacheDictionary<string, string>();
+
+            dict.AutoTrimCount = 4; // 4アクセス毎に
+            dict.TrimLimit = 3; // 3個を越えるアイテムを削除する
+
+            dict["key1"] = "value1"; // 1アクセス目
+            dict["key2"] = "value2"; // 2アクセス目
+            dict["key3"] = "value3"; // 3アクセス目
+            dict["key4"] = "value4"; // 4アクセス目 (この直後にTrim)
+
+            // 1 -> 2 -> 3 -> 4 の順にアクセスしたため、直近 3 件の 2, 3, 4 だけが残る
+            Assert.That(dict.innerDict.Keys, Is.EquivalentTo(new[] { "key2", "key3", "key4" }));
+
+            dict["key5"] = "value5";         // 5アクセス目
+            dict.Add("key6", "value6");      // 6アクセス目
+            var x = dict["key2"];            // 7アクセス目
+            dict.TryGetValue("key4", out x); // 8アクセス目 (この直後にTrim)
+
+            // 5 -> 6 -> 2 -> 4 の順でアクセスしたため、直近 3 件の 6, 2, 4 だけが残る
+            Assert.That(dict.innerDict.Keys, Is.EquivalentTo(new[] { "key6", "key2", "key4" }));
+        }
+
+        [Test]
+        public void CacheRemovedEventTest()
+        {
+            var dict = new LRUCacheDictionary<string, string>();
+
+            dict["key1"] = "value1";
+            dict["key2"] = "value2";
+            dict["key3"] = "value3";
+            dict["key4"] = "value4";
+
+            // イベント設定
+            var removedList = new List<string>();
+            dict.CacheRemoved += (s, e) =>
+            {
+                removedList.Add(e.Item.Key);
+            };
+
+            // 2 個までに縮小
+            dict.TrimLimit = 2;
+            dict.Trim();
+
+            // 直近に参照された 3, 4 以外のアイテムに対してイベントが発生しているはず
+            Assert.That(removedList, Is.EquivalentTo(new[] { "key1", "key2" }));
+        }
+
+        // ここから下は IDictionary としての機能が正しく動作するかのテスト
+
+        [Test]
+        public void AddTest()
+        {
+            var dict = new LRUCacheDictionary<string, string>();
+
+            dict.Add("key1", "value1");
+
+            Assert.That(dict.innerDict.Count, Is.EqualTo(1));
+            Assert.That(dict.innerDict.ContainsKey("key1"), Is.True);
+            var internalNode = dict.innerDict["key1"];
+            Assert.That(internalNode.Value.Key, Is.EqualTo("key1"));
+            Assert.That(internalNode.Value.Value, Is.EqualTo("value1"));
+
+            dict.Add("key2", "value2");
+
+            Assert.That(dict.innerDict.Count, Is.EqualTo(2));
+            Assert.That(dict.innerDict.ContainsKey("key2"), Is.True);
+            internalNode = dict.innerDict["key2"];
+            Assert.That(internalNode.Value.Key, Is.EqualTo("key2"));
+            Assert.That(internalNode.Value.Value, Is.EqualTo("value2"));
+        }
+
+        [Test]
+        public void ContainsKeyTest()
+        {
+            var dict = new LRUCacheDictionary<string, string>
+            {
+                {"key1", "value1"},
+                {"key2", "value2"},
+                {"key3", "value3"},
+            };
+
+            Assert.That(dict.ContainsKey("key1"), Is.True);
+            Assert.That(dict.ContainsKey("key3"), Is.True);
+            Assert.That(dict.ContainsKey("value1"), Is.False);
+            Assert.That(dict.ContainsKey("hogehoge"), Is.False);
+        }
+
+        [Test]
+        public void RemoveTest()
+        {
+            var dict = new LRUCacheDictionary<string, string>
+            {
+                {"key1", "value1"},
+                {"key2", "value2"},
+                {"key3", "value3"},
+            };
+
+            dict.Remove("key1");
+
+            Assert.That(dict.innerDict.Count, Is.EqualTo(2));
+            Assert.That(dict.innerList.Count, Is.EqualTo(2));
+            Assert.That(dict.innerDict.ContainsKey("key1"), Is.False);
+            Assert.That(dict.innerDict.ContainsKey("key2"), Is.True);
+            Assert.That(dict.innerDict.ContainsKey("key3"), Is.True);
+
+            dict.Remove("key2");
+            dict.Remove("key3");
+
+            Assert.That(dict.innerDict, Is.Empty);
+            Assert.That(dict.innerList, Is.Empty);
+        }
+
+        [Test]
+        public void GetterTest()
+        {
+            var dict = new LRUCacheDictionary<string, string>
+            {
+                {"key1", "value1"},
+                {"key2", "value2"},
+                {"key3", "value3"},
+            };
+
+            Assert.That(dict["key1"], Is.EqualTo("value1"));
+            Assert.That(dict["key2"], Is.EqualTo("value2"));
+            Assert.That(dict["key3"], Is.EqualTo("value3"));
+
+            Assert.Throws<KeyNotFoundException>(() => { var x = dict["hogehoge"]; });
+        }
+
+        [Test]
+        public void SetterTest()
+        {
+            var dict = new LRUCacheDictionary<string, string>
+            {
+                {"key1", "value1"},
+                {"key2", "value2"},
+                {"key3", "value3"},
+            };
+
+            dict["key1"] = "foo";
+            Assert.That(dict.innerDict["key1"].Value.Value, Is.EqualTo("foo"));
+
+            dict["hogehoge"] = "bar";
+            Assert.That(dict.innerDict.ContainsKey("hogehoge"), Is.True);
+            Assert.That(dict.innerDict["hogehoge"].Value.Value, Is.EqualTo("bar"));
+        }
+
+        [Test]
+        public void TryGetValueTest()
+        {
+            var dict = new LRUCacheDictionary<string, string>
+            {
+                {"key1", "value1"},
+                {"key2", "value2"},
+                {"key3", "value3"},
+            };
+
+            string value;
+            var ret = dict.TryGetValue("key1", out value);
+            Assert.That(ret, Is.True);
+            Assert.That(value, Is.EqualTo("value1"));
+
+            ret = dict.TryGetValue("hogehoge", out value);
+            Assert.That(ret, Is.False);
+            Assert.That(value, Is.Null);
+        }
+
+        [Test]
+        public void KeysTest()
+        {
+            var dict = new LRUCacheDictionary<string, string>
+            {
+                {"key1", "value1"},
+                {"key2", "value2"},
+                {"key3", "value3"},
+            };
+
+            Assert.That(dict.Keys, Is.EquivalentTo(new[] { "key1", "key2", "key3" }));
+
+            dict.Add("foo", "bar");
+            Assert.That(dict.Keys, Is.EquivalentTo(new[] { "key1", "key2", "key3", "foo" }));
+
+            dict.Remove("key2");
+            Assert.That(dict.Keys, Is.EquivalentTo(new[] { "key1", "key3", "foo" }));
+
+            dict.Clear();
+            Assert.That(dict.Keys, Is.Empty);
+        }
+
+        [Test]
+        public void ValuesTest()
+        {
+            var dict = new LRUCacheDictionary<string, string>
+            {
+                {"key1", "value1"},
+                {"key2", "value2"},
+                {"key3", "value3"},
+            };
+
+            Assert.That(dict.Values, Is.EquivalentTo(new[] { "value1", "value2", "value3" }));
+
+            dict.Add("foo", "bar");
+            Assert.That(dict.Values, Is.EquivalentTo(new[] { "value1", "value2", "value3", "bar" }));
+
+            dict.Remove("key2");
+            Assert.That(dict.Values, Is.EquivalentTo(new[] { "value1", "value3", "bar" }));
+
+            dict.Clear();
+            Assert.That(dict.Values, Is.Empty);
+        }
+
+        [Test]
+        public void CountTest()
+        {
+            var dict = new LRUCacheDictionary<string, string>();
+
+            Assert.That(dict.Count, Is.EqualTo(0));
+
+            dict.Add("key1", "value1");
+            Assert.That(dict.Count, Is.EqualTo(1));
+
+            dict.Add("key2", "value2");
+            Assert.That(dict.Count, Is.EqualTo(2));
+
+            dict.Remove("key1");
+            Assert.That(dict.Count, Is.EqualTo(1));
+
+            dict.Clear();
+            Assert.That(dict.Count, Is.EqualTo(0));
+        }
+
+        [Test]
+        public void EnumeratorTest()
+        {
+            var dict = new LRUCacheDictionary<string, string>
+            {
+                {"key1", "value1"},
+                {"key2", "value2"},
+                {"key3", "value3"},
+            };
+
+            var enumerator = dict.GetEnumerator();
+
+            Assert.That(enumerator.MoveNext(), Is.True);
+            Assert.That(enumerator.Current, Is.EqualTo(new KeyValuePair<string, string>("key1", "value1")));
+            Assert.That(enumerator.MoveNext(), Is.True);
+            Assert.That(enumerator.Current, Is.EqualTo(new KeyValuePair<string, string>("key2", "value2")));
+            Assert.That(enumerator.MoveNext(), Is.True);
+            Assert.That(enumerator.Current, Is.EqualTo(new KeyValuePair<string, string>("key3", "value3")));
+            Assert.That(enumerator.MoveNext(), Is.False);
+        }
+    }
+}

OpenTween.Tests/OpenTween.Tests.csproj
@@ -53,6 +53,7 @@
     <Compile Include="ApiInformationTest.cs" />
     <Compile Include="BingTest.cs" />
     <Compile Include="FoursquareTest.cs" />
+    <Compile Include="LRUCacheDictionaryTest.cs" />
     <Compile Include="MyCommonTest.cs" />
     <Compile Include="PostClassTest.cs" />
     <Compile Include="Properties\AssemblyInfo.cs" />

commit 8849845403bfe9824942071ecd6c41bca79a9962

ImageCache内のTaskで発生したエラーがハンドルされずに破棄されてしまう場合がある不具合の修正
Fixes: 519d4f3e ("アイコンのキャッシュ機構を書き直し")

OpenTween/ImageCache.cs
@@ -58,6 +58,23 @@ namespace OpenTween
             {
                 // まだ参照されている場合もあるのでDisposeはファイナライザ任せ
                 this.CacheRemoveCount++;
+
+                var task = e.Item.Value;
+                if (task.Status != TaskStatus.RanToCompletion || task.IsFaulted)
+                {
+                    // Task の例外がハンドルされないまま破棄されると AggregateException が発生するため try-catch で処理する Task を挟む
+                    static async Task HandleException<T>(Task<T> t)
+                    {
+                        try
+                        {
+                            _ = await t.ConfigureAwait(false);
+                        }
+                        catch
+                        {
+                        }
+                    }
+                    _ = HandleException(task);
+                }
             };
 
             this.cancelTokenSource = new CancellationTokenSource();