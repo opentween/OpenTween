opentween/OpenTween

commit 4df2ef945c1f91f209ee0015d5d34f0066ecc767

TweenMain.ListTab.TabPagesとTabInformations.Tabsの順序を同期させる

OpenTween/Models/TabInformations.cs
@@ -164,6 +164,25 @@ namespace OpenTween.Models
             }
         }
 
+        public void ReplaceTab(TabModel tab)
+        {
+            if (!this.ContainsTab(tab.TabName))
+                throw new ArgumentOutOfRangeException(nameof(tab));
+
+            var index = this.Tabs.IndexOf(tab);
+            this.Tabs.RemoveAt(index);
+            this.Tabs.Insert(index, tab);
+        }
+
+        public void MoveTab(int newIndex, TabModel tab)
+        {
+            if (!this.ContainsTab(tab))
+                throw new ArgumentOutOfRangeException(nameof(tab));
+
+            this.Tabs.Remove(tab);
+            this.Tabs.Insert(newIndex, tab);
+        }
+
         public bool ContainsTab(string TabText)
             => this.Tabs.Contains(TabText);
 
@@ -650,10 +669,11 @@ namespace OpenTween.Models
         {
             lock (this.LockObj)
             {
+                var index = this.Tabs.IndexOf(Original);
                 var tb = this.Tabs[Original];
-                this.Tabs.Remove(Original);
+                this.Tabs.RemoveAt(index);
                 tb.TabName = NewName;
-                this.Tabs.Add(tb);
+                this.Tabs.Insert(index, tb);
             }
         }
 

OpenTween/Tween.cs
@@ -7466,16 +7466,18 @@ namespace OpenTween
 
             using (ControlTransaction.Layout(this.ListTab))
             {
-                var mTp = this.ListTab.TabPages[targetIndex];
-                this.ListTab.TabPages.Remove(mTp);
+                var tab = this._statuses.Tabs[targetIndex];
+                var tabPage = this.ListTab.TabPages[targetIndex];
+                this.ListTab.TabPages.Remove(tabPage);
 
                 if (targetIndex < baseIndex)
                     baseIndex--;
 
-                if (isBeforeBaseTab)
-                    ListTab.TabPages.Insert(baseIndex, mTp);
-                else
-                    ListTab.TabPages.Insert(baseIndex + 1, mTp);
+                if (!isBeforeBaseTab)
+                    baseIndex++;
+
+                this._statuses.MoveTab(baseIndex, tab);
+                ListTab.TabPages.Insert(baseIndex, tabPage);
             }
 
             SaveConfigsTabs();
@@ -10572,8 +10574,8 @@ namespace OpenTween
                         // 関連発言なら既存のタブを置き換える
                         tb.TabName = relatedTab.TabName;
                         this.ClearTab(tb.TabName, false);
-                        _statuses.Tabs.Remove(tb.TabName);
-                        _statuses.Tabs.Add(tb);
+
+                        this._statuses.ReplaceTab(tb);
 
                         for (int i = 0; i < ListTab.TabPages.Count; i++)
                         {

OpenTween/Models/TabCollection.cs
@@ -25,6 +25,9 @@ namespace OpenTween.Models
 {
     public class TabCollection : KeyedCollection<string, TabModel>
     {
+        public int IndexOf(string tabName)
+            => this.IndexOf(this[tabName]);
+
         public bool TryGetValue(string tabName, out TabModel tab)
             => this.Dictionary.TryGetValue(tabName, out tab);
 

commit d3ad88571356a3251a7d6c33a78098ce43a1b0db

削除したタブの取消時に同名のタブが存在した場合のエラーが適切に処理されない不具合を修正
関連発言タブの削除を取り消した時に同名のタブが存在すると ArgumentOutOfRangeException が発生する問題の修正。
この変更により、タブ削除の取消時に同名のタブがある場合はタブ名の変更を試みずにエラーメッセージを表示するように変更している。

Fixes: 4df2ef94 ("TweenMain.ListTab.TabPagesとTabInformations.Tabsの順序を同期させる")

OpenTween/Models/TabInformations.cs
@@ -176,16 +176,6 @@ namespace OpenTween.Models
             }
         }
 
-        public void ReplaceTab(TabModel tab)
-        {
-            if (!this.ContainsTab(tab.TabName))
-                throw new ArgumentOutOfRangeException(nameof(tab));
-
-            var index = this.tabs.IndexOf(tab);
-            this.tabs.RemoveAt(index);
-            this.tabs.Insert(index, tab);
-        }
-
         public void MoveTab(int newIndex, TabModel tab)
         {
             if (!this.ContainsTab(tab))

OpenTween/Properties/Resources.Designer.cs
@@ -3033,6 +3033,15 @@ namespace OpenTween.Properties {
         }
         
         /// <summary>
+        ///   削除したタブの取消に失敗しました。「{0}」と同じ名前のタブが存在します に類似しているローカライズされた文字列を検索します。
+        /// </summary>
+        internal static string UndoRemovedTab_DuplicateError {
+            get {
+                return ResourceManager.GetString("UndoRemovedTab_DuplicateError", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   **** エラー ログ: {0} **** に類似しているローカライズされた文字列を検索します。
         /// </summary>
         internal static string UnhandledExceptionText1 {

OpenTween/Properties/Resources.en.resx
@@ -314,6 +314,7 @@ Do you retry to update status?</value></data>
 	<data name="TweenAboutBox_LoadText2"><value>Version {0}</value></data>
 	<data name="TweenMain_LoadText1"><value>Failed to add tab.</value></data>
 	<data name="Unauthorized"><value>You are not authorized.</value></data>
+	<data name="UndoRemovedTab_DuplicateError"><value>Failed to undo the removed tab. A tab with the same name as "{0}" exists.</value></data>
 	<data name="UnhandledExceptionText1"><value>**** Error Log: {0} ****</value></data>
 	<data name="UnhandledExceptionText10"><value>Error</value></data>
 	<data name="UnhandledExceptionText11"><value>Administrator  authority：</value></data>

OpenTween/Properties/Resources.resx
@@ -348,6 +348,7 @@
 	<data name="TweenAboutBox_LoadText2"><value>バージョン {0}</value></data>
 	<data name="TweenMain_LoadText1"><value>タブ作成エラー</value></data>
 	<data name="Unauthorized"><value>You are not authorized.</value></data>
+	<data name="UndoRemovedTab_DuplicateError"><value>削除したタブの取消に失敗しました。「{0}」と同じ名前のタブが存在します</value></data>
 	<data name="UnhandledExceptionText1"><value>**** エラー ログ: {0} ****</value></data>
 	<data name="UnhandledExceptionText10"><value>エラー発生</value></data>
 	<data name="UnhandledExceptionText11"><value>Administrator権限：</value></data>

OpenTween/Tween.cs
@@ -8699,56 +8699,20 @@ namespace OpenTween
             else
             {
                 var tb = this.statuses.RemovedTab.Pop();
-                if (tb.TabType == MyCommon.TabUsageType.Related)
+                if (this.statuses.ContainsTab(tb.TabName))
                 {
-                    var relatedTab = this.statuses.GetTabByType(MyCommon.TabUsageType.Related);
-                    if (relatedTab != null)
-                    {
-                        // 関連発言なら既存のタブを置き換える
-                        tb.TabName = relatedTab.TabName;
-                        this.ClearTab(tb.TabName, false);
-
-                        this.statuses.ReplaceTab(tb);
-
-                        var tabIndex = this.statuses.Tabs.IndexOf(tb);
-                        this.ListTab.SelectedIndex = tabIndex;
-                    }
-                    else
-                    {
-                        const string TabName = "Related Tweets";
-                        var renamed = TabName;
-                        for (var i = 2; i <= 100; i++)
-                        {
-                            if (!this.statuses.ContainsTab(renamed))
-                                break;
-                            renamed = TabName + i;
-                        }
-                        tb.TabName = renamed;
-
-                        this.statuses.AddTab(tb);
-                        this.AddNewTab(tb, startup: false);
-
-                        var tabIndex = this.statuses.Tabs.Count - 1;
-                        this.ListTab.SelectedIndex = tabIndex;
-                    }
+                    var message = string.Format(Properties.Resources.UndoRemovedTab_DuplicateError, tb.TabName);
+                    MessageBox.Show(this, message, ApplicationSettings.ApplicationName, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    this.statuses.RemovedTab.Push(tb);
+                    return;
                 }
-                else
-                {
-                    var renamed = tb.TabName;
-                    for (var i = 1; i < int.MaxValue; i++)
-                    {
-                        if (!this.statuses.ContainsTab(renamed))
-                            break;
-                        renamed = tb.TabName + "(" + i + ")";
-                    }
-                    tb.TabName = renamed;
 
-                    this.statuses.AddTab(tb);
-                    this.AddNewTab(tb, startup: false);
+                this.statuses.AddTab(tb);
+                this.AddNewTab(tb, startup: false);
 
-                    var tabIndex = this.statuses.Tabs.Count - 1;
-                    this.ListTab.SelectedIndex = tabIndex;
-                }
+                var tabIndex = this.statuses.Tabs.Count - 1;
+                this.ListTab.SelectedIndex = tabIndex;
+
                 this.SaveConfigsTabs();
             }
         }