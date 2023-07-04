70b7659d37d3a8aaf7214bb4e2da5ba0a1befe88
Kimura Youichi <kim.upsilon@bucyou.net>
2023-07-05 06:57:47 +0900

    TweetWithVisibilityResultsに含まれるツイートの読み込みに対応

--- OpenTween/Api/GraphQL/TimelineTweet.cs
+++ OpenTween/Api/GraphQL/TimelineTweet.cs
@@ -49,7 +49,14 @@ namespace OpenTween.Api.GraphQL
 
         public TwitterStatus ToTwitterStatus()
         {
-            var tweetElm = this.Element.Element("tweet_results")?.Element("result") ?? throw CreateParseError();
+            var resultElm = this.Element.Element("tweet_results")?.Element("result") ?? throw CreateParseError();
+            var tweetElm = resultElm.Element("__typename")?.Value switch
+            {
+                "Tweet" => resultElm,
+                "TweetWithVisibilityResults" => resultElm.Element("tweet") ?? throw CreateParseError(),
+                _ => throw CreateParseError(),
+            };
+
             return this.ParseTweet(tweetElm);
         }
 
