opentween/OpenTween

commit 4b517de269909592b2cc34b609c04546883fce64

TweetDetailを使用したツイートの取得に対応

OpenTween/Twitter.cs
@@ -681,8 +681,21 @@ namespace OpenTween
         {
             this.CheckAccountState();
 
-            var status = await this.Api.StatusesShow(id)
-                .ConfigureAwait(false);
+            TwitterStatus status;
+            if (this.Api.AppToken.AuthType == APIAuthType.TwitterComCookie)
+            {
+                var request = new TweetDetailRequest
+                {
+                    FocalTweetId = id,
+                };
+                var tweets = await request.Send(this.Api.Connection).ConfigureAwait(false);
+                status = tweets.Select(x => x.ToTwitterStatus()).Where(x => x.IdStr == id.Id).First();
+            }
+            else
+            {
+                status = await this.Api.StatusesShow(id)
+                    .ConfigureAwait(false);
+            }
 
             var item = this.CreatePostsFromStatusData(status);
 

OpenTween.Tests/OpenTween.Tests.csproj
@@ -76,5 +76,8 @@
     <None Update="Resources\Responses\ListLatestTweetsTimeline_SimpleTweet.json">
       <CopyToOutputDirectory>PreserveNewest</CopyToOutputDirectory>
     </None>
+    <None Update="Resources\Responses\TweetDetail.json">
+      <CopyToOutputDirectory>PreserveNewest</CopyToOutputDirectory>
+    </None>
   </ItemGroup>
 </Project>

OpenTween.Tests/Api/GraphQL/TweetDetailRequestTest.cs
@@ -1,0 +1,64 @@
+﻿// OpenTween - Client of Twitter
+// Copyright (c) 2023 kim_upsilon (@kim_upsilon) <https://upsilo.net/~upsilon/>
+// All rights reserved.
+//
+// This file is part of OpenTween.
+//
+// This program is free software; you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the Free
+// Software Foundation; either version 3 of the License, or (at your option)
+// any later version.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+// for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program. If not, see <http://www.gnu.org/licenses/>, or write to
+// the Free Software Foundation, Inc., 51 Franklin Street - Fifth Floor,
+// Boston, MA 02110-1301, USA.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using OpenTween.Api.TwitterV2;
+using OpenTween.Connection;
+using Xunit;
+
+namespace OpenTween.Api.GraphQL
+{
+    public class TweetDetailRequestTest
+    {
+        [Fact]
+        public async Task Send_Test()
+        {
+            using var responseStream = File.OpenRead("Resources/Responses/TweetDetail.json");
+
+            var mock = new Mock<IApiConnection>();
+            mock.Setup(x =>
+                    x.GetStreamAsync(It.IsAny<Uri>(), It.IsAny<IDictionary<string, string>>())
+                )
+                .Callback<Uri, IDictionary<string, string>>((url, param) =>
+                {
+                    Assert.Equal(new("https://twitter.com/i/api/graphql/-Ls3CrSQNo2fRKH6i6Na1A/TweetDetail"), url);
+                    Assert.Contains(@"""focalTweetId"":""1619433164757413894""", param["variables"]);
+                })
+                .ReturnsAsync(responseStream);
+
+            var request = new TweetDetailRequest
+            {
+                FocalTweetId = new("1619433164757413894"),
+            };
+
+            var tweets = await request.Send(mock.Object).ConfigureAwait(false);
+            Assert.Equal("1619433164757413894", tweets.Single().ToTwitterStatus().IdStr);
+
+            mock.VerifyAll();
+        }
+    }
+}

OpenTween/Api/GraphQL/TweetDetailRequest.cs
@@ -1,0 +1,80 @@
+﻿// OpenTween - Client of Twitter
+// Copyright (c) 2023 kim_upsilon (@kim_upsilon) <https://upsilo.net/~upsilon/>
+// All rights reserved.
+//
+// This file is part of OpenTween.
+//
+// This program is free software; you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the Free
+// Software Foundation; either version 3 of the License, or (at your option)
+// any later version.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+// for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program. If not, see <http://www.gnu.org/licenses/>, or write to
+// the Free Software Foundation, Inc., 51 Franklin Street - Fifth Floor,
+// Boston, MA 02110-1301, USA.
+
+#nullable enable
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.XPath;
+using OpenTween.Connection;
+using OpenTween.Models;
+
+namespace OpenTween.Api.GraphQL
+{
+    public class TweetDetailRequest
+    {
+        private static readonly Uri EndpointUri = new("https://twitter.com/i/api/graphql/-Ls3CrSQNo2fRKH6i6Na1A/TweetDetail");
+
+        required public TwitterStatusId FocalTweetId { get; set; }
+
+        public Dictionary<string, string> CreateParameters()
+        {
+            return new()
+            {
+                ["variables"] = $$"""
+                    {"focalTweetId":"{{this.FocalTweetId.Id}}","with_rux_injections":false,"includePromotedContent":true,"withCommunity":true,"withQuickPromoteEligibilityTweetFields":true,"withBirdwatchNotes":true,"withVoice":true,"withV2Timeline":true}
+                    """,
+                ["features"] = """
+                    {"rweb_lists_timeline_redesign_enabled":true,"responsive_web_graphql_exclude_directive_enabled":true,"verified_phone_label_enabled":false,"creator_subscriptions_tweet_preview_api_enabled":true,"responsive_web_graphql_timeline_navigation_enabled":true,"responsive_web_graphql_skip_user_profile_image_extensions_enabled":false,"tweetypie_unmention_optimization_enabled":true,"responsive_web_edit_tweet_api_enabled":true,"graphql_is_translatable_rweb_tweet_is_translatable_enabled":true,"view_counts_everywhere_api_enabled":true,"longform_notetweets_consumption_enabled":true,"responsive_web_twitter_article_tweet_consumption_enabled":false,"tweet_awards_web_tipping_enabled":false,"freedom_of_speech_not_reach_fetch_enabled":true,"standardized_nudges_misinfo":true,"tweet_with_visibility_results_prefer_gql_limited_actions_policy_enabled":true,"longform_notetweets_rich_text_read_enabled":true,"longform_notetweets_inline_media_enabled":true,"responsive_web_media_download_video_enabled":false,"responsive_web_enhance_cards_enabled":false}
+                    """,
+                ["fieldToggles"] = """
+                    {"withAuxiliaryUserLabels":false,"withArticleRichContentState":false}
+                    """,
+            };
+        }
+
+        public async Task<TimelineTweet[]> Send(IApiConnection apiConnection)
+        {
+            var param = this.CreateParameters();
+            using var stream = await apiConnection.GetStreamAsync(EndpointUri, param);
+            using var jsonReader = JsonReaderWriterFactory.CreateJsonReader(stream, XmlDictionaryReaderQuotas.Max);
+
+            XElement rootElm;
+            try
+            {
+                rootElm = XElement.Load(jsonReader);
+            }
+            catch (IOException ex)
+            {
+                throw new WebApiException("IO Error", ex);
+            }
+
+            return TimelineTweet.ExtractTimelineTweets(rootElm);
+        }
+    }
+}

commit bd9b5fa5fbe9a99a8baf111c9479258138f75e9e

SelfThreadのツイートを取得できない不具合を修正
Fixes: 4b517de2 ("TweetDetailを使用したツイートの取得に対応")

OpenTween/Api/GraphQL/TimelineTweet.cs
@@ -156,7 +156,7 @@ namespace OpenTween.Api.GraphQL
 
         public static TimelineTweet[] ExtractTimelineTweets(XElement element)
         {
-            return element.XPathSelectElements($"//itemContent[itemType[text()='{TypeName}']][tweetDisplayType[text()='Tweet']]")
+            return element.XPathSelectElements($"//itemContent[itemType[text()='{TypeName}']][tweetDisplayType[text()='Tweet' or text()='SelfThread']]")
                 .Select(x => new TimelineTweet(x))
                 .ToArray();
         }

OpenTween/Twitter.cs
@@ -689,7 +689,9 @@ namespace OpenTween
                     FocalTweetId = id,
                 };
                 var tweets = await request.Send(this.Api.Connection).ConfigureAwait(false);
-                status = tweets.Select(x => x.ToTwitterStatus()).Where(x => x.IdStr == id.Id).First();
+                status = tweets.Select(x => x.ToTwitterStatus())
+                    .Where(x => x.IdStr == id.Id)
+                    .FirstOrDefault() ?? throw new WebApiException("Empty result set");
             }
             else
             {

OpenTween.Tests/Api/GraphQL/TimelineTweetTest.cs
@@ -123,5 +123,18 @@ namespace OpenTween.Api.GraphQL
             Assert.Equal("1602775353088524288", post.StatusId.Id);
             Assert.Equal(357750891L, post.UserId);
         }
+
+        [Fact]
+        public void ToStatus_WithTwitterPostFactory_SelfThread_Test()
+        {
+            var rootElm = this.LoadResponseDocument("TimelineTweet_SelfThread.json");
+            var timelineTweet = new TimelineTweet(rootElm);
+            var status = timelineTweet.ToTwitterStatus();
+            var postFactory = new TwitterPostFactory(this.CreateTabInfo());
+            var post = postFactory.CreateFromStatus(status, selfUserId: 1L, new HashSet<long>());
+
+            Assert.Equal("1511751702684499968", post.StatusId.Id);
+            Assert.Equal(40480664L, post.UserId);
+        }
     }
 }

OpenTween.Tests/OpenTween.Tests.csproj
@@ -82,5 +82,8 @@
     <None Update="Resources\Responses\TweetDetail.json">
       <CopyToOutputDirectory>PreserveNewest</CopyToOutputDirectory>
     </None>
+    <None Update="Resources\Responses\TimelineTweet_SelfThread.json">
+      <CopyToOutputDirectory>PreserveNewest</CopyToOutputDirectory>
+    </None>
   </ItemGroup>
 </Project>