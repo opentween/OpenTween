opentween/OpenTween

commit 4122b1280681bf7213c8eb4a46fde409d73d8feb

CreateTweetRequestを使用したツイート投稿に対応

OpenTween/Twitter.cs
@@ -252,18 +252,37 @@ namespace OpenTween
                 return null;
             }
 
-            var response = await this.Api.StatusesUpdate(
-                    param.Text,
-                    param.InReplyToStatusId?.ToTwitterStatusId(),
-                    param.MediaIds,
-                    param.AutoPopulateReplyMetadata,
-                    param.ExcludeReplyUserIds,
-                    param.AttachmentUrl
-                )
-                .ConfigureAwait(false);
+            TwitterStatus status;
 
-            var status = await response.LoadJsonAsync()
-                .ConfigureAwait(false);
+            if (this.Api.AppToken.AuthType == APIAuthType.TwitterComCookie)
+            {
+                var request = new CreateTweetRequest
+                {
+                    TweetText = param.Text,
+                    InReplyToTweetId = param.InReplyToStatusId?.ToTwitterStatusId(),
+                    ExcludeReplyUserIds = param.ExcludeReplyUserIds.Select(x => x.ToString()).ToArray(),
+                    MediaIds = param.MediaIds.Select(x => x.ToString()).ToArray(),
+                    AttachmentUrl = param.AttachmentUrl,
+                };
+
+                status = await request.Send(this.Api.Connection)
+                    .ConfigureAwait(false);
+            }
+            else
+            {
+                var response = await this.Api.StatusesUpdate(
+                        param.Text,
+                        param.InReplyToStatusId?.ToTwitterStatusId(),
+                        param.MediaIds,
+                        param.AutoPopulateReplyMetadata,
+                        param.ExcludeReplyUserIds,
+                        param.AttachmentUrl
+                    )
+                    .ConfigureAwait(false);
+
+                status = await response.LoadJsonAsync()
+                    .ConfigureAwait(false);
+            }
 
             this.UpdateUserStats(status.User);
 

OpenTween/Tween.cs
@@ -1244,7 +1244,7 @@ namespace OpenTween
             var status = new PostStatusParams();
 
             var statusTextCompat = this.FormatStatusText(this.StatusText.Text);
-            if (this.GetRestStatusCount(statusTextCompat) >= 0)
+            if (this.GetRestStatusCount(statusTextCompat) >= 0 && this.tw.Api.AppToken.AuthType == APIAuthType.OAuth1)
             {
                 // auto_populate_reply_metadata や attachment_url を使用しなくても 140 字以内に
                 // 収まる場合はこれらのオプションを使用せずに投稿する

commit 246fdace1302c3a147cf3a7689b6d9d659123bbb

CreateTweetでの2回目以降の投稿がエラーになる不具合を修正
Fixes: 4122b128 ("CreateTweetRequestを使用したツイート投稿に対応")

OpenTween/Twitter.cs
@@ -174,7 +174,7 @@ namespace OpenTween
 
         private readonly TwitterPostFactory postFactory;
 
-        private long previousStatusId = -1L;
+        private string? previousStatusId = null;
 
         public Twitter(TwitterApi api)
         {
@@ -286,10 +286,10 @@ namespace OpenTween
 
             this.UpdateUserStats(status.User);
 
-            if (status.Id == this.previousStatusId)
+            if (status.IdStr == this.previousStatusId)
                 throw new WebApiException("OK:Delaying?");
 
-            this.previousStatusId = status.Id;
+            this.previousStatusId = status.IdStr;
 
             // 投稿したものを返す
             var post = this.CreatePostsFromStatusData(status);