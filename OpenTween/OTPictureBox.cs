opentween/OpenTween

commit 0dee698aedf3825474cc120a7a8729c27a091acd

TweetThumbnailControlクラスからWinFormsに依存しない処理を分離

OpenTween/TweetThumbnailControl.cs
@@ -24,28 +24,18 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
-using System.Diagnostics.CodeAnalysis;
 using System.Drawing;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
 using System.Runtime.InteropServices;
 using System.Text;
-using System.Text.RegularExpressions;
-using System.Threading;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using OpenTween.Models;
 using OpenTween.Thumbnail;
 
 namespace OpenTween
 {
     public partial class TweetThumbnailControl : UserControl
     {
-        protected internal List<OTPictureBox> PictureBox = new();
-        protected MouseWheelMessageFilter filter = new();
-        private ThumbnailGenerator? thumbGenerator;
+        private readonly MouseWheelMessageFilter filter = new();
 
         public event EventHandler<EventArgs>? ThumbnailLoading;
 
@@ -53,14 +43,18 @@ namespace OpenTween
 
         public event EventHandler<ThumbnailImageSearchEventArgs>? ThumbnailImageSearchClick;
 
-        public ThumbnailInfo Thumbnail
-            => (ThumbnailInfo)this.PictureBox[this.scrollBar.Value].Tag;
-
-        private ThumbnailGenerator ThumbGenerator
-            => this.thumbGenerator ?? throw this.NotInitializedException();
+        [Browsable(false)]
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Hidden)]
+        public TweetThumbnail Model { get; } = new();
 
         public TweetThumbnailControl()
-            => this.InitializeComponent();
+        {
+            this.InitializeComponent();
+            this.filter.Register(this.pictureBox);
+
+            this.Model.PropertyChanged +=
+                (s, e) => this.TryInvoke(() => this.Model_PropertyChanged(s, e));
+        }
 
         protected override void Dispose(bool disposing)
         {
@@ -72,160 +66,63 @@ namespace OpenTween
             base.Dispose(disposing);
         }
 
-        public void Initialize(ThumbnailGenerator thumbnailGenerator)
-            => this.thumbGenerator = thumbnailGenerator;
-
-        private Exception NotInitializedException()
-            => new InvalidOperationException("Cannot call before initialization");
-
-        public Task ShowThumbnailAsync(PostClass post)
-            => this.ShowThumbnailAsync(post, CancellationToken.None);
-
-        public async Task ShowThumbnailAsync(PostClass post, CancellationToken cancelToken)
+        private void Model_PropertyChanged(object sender, PropertyChangedEventArgs e)
         {
-            var loadTasks = new List<Task>();
-
-            this.scrollBar.Enabled = false;
-            this.scrollBar.Visible = false;
-
-            if (post.ExpandedUrls.Length == 0 && post.Media.Count == 0 && post.PostGeo == null)
+            switch (e.PropertyName)
             {
-                this.SetThumbnailCount(0);
-                return;
-            }
-
-            var thumbnails = (await this.GetThumbailInfoAsync(post, cancelToken))
-                .ToArray();
-
-            cancelToken.ThrowIfCancellationRequested();
-
-            this.SetThumbnailCount(thumbnails.Length);
-            if (thumbnails.Length == 0)
-                return;
-
-            for (var i = 0; i < thumbnails.Length; i++)
-            {
-                var thumb = thumbnails[i];
-                var picbox = this.PictureBox[i];
-
-                picbox.Tag = thumb;
-                picbox.ContextMenuStrip = this.contextMenuStrip;
-
-                var loadTask = picbox.SetImageFromTask(() => thumb.LoadThumbnailImageAsync(cancelToken));
-                loadTasks.Add(loadTask);
-
-                var tooltipText = thumb.TooltipText;
-                if (!MyCommon.IsNullOrEmpty(tooltipText))
-                {
-                    this.toolTip.SetToolTip(picbox, tooltipText);
-                    picbox.AccessibleDescription = tooltipText;
-                }
-
-                cancelToken.ThrowIfCancellationRequested();
-            }
-
-            if (thumbnails.Length > 1)
-            {
-                this.scrollBar.Enabled = true;
-                this.scrollBar.Visible = true;
-            }
-
-            this.ThumbnailLoading?.Invoke(this, EventArgs.Empty);
-
-            await Task.WhenAll(loadTasks).ConfigureAwait(false);
-        }
-
-        private string GetImageSearchUriGoogle(string image_uri)
-            => @"https://www.google.com/searchbyimage?image_url=" + Uri.EscapeDataString(image_uri);
-
-        private string GetImageSearchUriSauceNao(string imageUri)
-            => @"https://saucenao.com/search.php?url=" + Uri.EscapeDataString(imageUri);
-
-        protected async virtual Task<IEnumerable<ThumbnailInfo>> GetThumbailInfoAsync(PostClass post, CancellationToken token)
-            => await Task.Run(() => this.ThumbGenerator.GetThumbnailsAsync(post, token));
-
-        /// <summary>
-        /// 表示するサムネイルの数を設定する
-        /// </summary>
-        /// <param name="count">表示するサムネイルの数</param>
-        protected void SetThumbnailCount(int count)
-        {
-            if (count == 0 && this.PictureBox.Count == 0)
-                return;
-
-            using (ControlTransaction.Layout(this.panelPictureBox, false))
-            {
-                this.panelPictureBox.Controls.Clear();
-                foreach (var picbox in this.PictureBox)
-                {
-                    var memoryImage = picbox.Image;
-
-                    this.filter.Unregister(picbox);
-
-                    picbox.MouseWheel -= this.PictureBox_MouseWheel;
-                    picbox.DoubleClick -= this.PictureBox_DoubleClick;
-                    picbox.Dispose();
-
-                    memoryImage?.Dispose();
-
-                    // メモリリーク対策 (http://stackoverflow.com/questions/2792427#2793714)
-                    picbox.ContextMenuStrip = null;
-                }
-                this.PictureBox.Clear();
-
-                this.scrollBar.Maximum = (count > 0) ? count - 1 : 0;
-                this.scrollBar.Value = 0;
-
-                for (var i = 0; i < count; i++)
-                {
-                    var picbox = this.CreatePictureBox("pictureBox" + i);
-                    picbox.Visible = i == 0;
-                    picbox.MouseWheel += this.PictureBox_MouseWheel;
-                    picbox.DoubleClick += this.PictureBox_DoubleClick;
-
-                    this.filter.Register(picbox);
-
-                    this.panelPictureBox.Controls.Add(picbox);
-                    this.PictureBox.Add(picbox);
-                }
+                case nameof(TweetThumbnail.ThumbnailAvailable):
+                    this.UpdateThumbnailAvailability();
+                    break;
+                case nameof(TweetThumbnail.SelectedIndex):
+                    this.UpdateSelectedIndex();
+                    break;
+                default:
+                    break;
             }
         }
 
-        [SuppressMessage("Microsoft.Reliability", "CA2000:DisposeObjectsBeforeLosingScope")]
-        protected virtual OTPictureBox CreatePictureBox(string name)
+        private void UpdateThumbnailAvailability()
         {
-            return new OTPictureBox
+            if (this.Model.ThumbnailAvailable)
             {
-                Name = name,
-                SizeMode = PictureBoxSizeMode.Zoom,
-                WaitOnLoad = false,
-                Dock = DockStyle.Fill,
-                AccessibleRole = AccessibleRole.Graphic,
-            };
+                this.UpdateSelectedIndex();
+
+                var hasMultipleThumbnails = this.Model.Thumbnails.Length > 1;
+                this.scrollBar.Enabled = hasMultipleThumbnails;
+                this.scrollBar.Visible = hasMultipleThumbnails;
+
+                if (hasMultipleThumbnails)
+                {
+                    this.scrollBar.Value = 0;
+                    this.scrollBar.Maximum = this.Model.Thumbnails.Length - 1;
+                }
+
+                this.ThumbnailLoading?.Invoke(this, EventArgs.Empty);
+            }
+            else
+            {
+                this.pictureBox.Image = null;
+                this.pictureBox.AccessibleDescription = "";
+                this.toolTip.SetToolTip(this.pictureBox, "");
+                this.scrollBar.Visible = false;
+            }
         }
 
-        public void OpenImage(ThumbnailInfo thumb)
+        private void UpdateSelectedIndex()
+        {
+            if (!this.Model.ThumbnailAvailable)
+                return;
+
+            this.scrollBar.Value = this.Model.SelectedIndex;
+
+            var thumbnail = this.Model.CurrentThumbnail;
+            this.pictureBox.AccessibleDescription = thumbnail.TooltipText;
+            this.toolTip.SetToolTip(this.pictureBox, thumbnail.TooltipText);
+            _ = this.pictureBox.SetImageFromTask(this.Model.LoadSelectedThumbnail);
+        }
+
+        private void OpenImage(ThumbnailInfo thumb)
             => this.ThumbnailDoubleClick?.Invoke(this, new ThumbnailDoubleClickEventArgs(thumb));
-
-        public void ScrollUp()
-        {
-            var newval = this.scrollBar.Value - this.scrollBar.SmallChange;
-
-            if (newval < this.scrollBar.Minimum)
-                newval = this.scrollBar.Minimum;
-
-            this.scrollBar.Value = newval;
-        }
-
-        public void ScrollDown()
-        {
-            var newval = this.scrollBar.Value + this.scrollBar.SmallChange;
-
-            if (newval > this.scrollBar.Maximum)
-                newval = this.scrollBar.Maximum;
-
-            this.scrollBar.Value = newval;
-        }
 
         protected override void ScaleControl(SizeF factor, BoundsSpecified specified)
         {
@@ -234,75 +131,40 @@ namespace OpenTween
         }
 
         private void ScrollBar_ValueChanged(object sender, EventArgs e)
-        {
-            using (ControlTransaction.Layout(this, false))
-            {
-                var value = this.scrollBar.Value;
-                for (var i = 0; i < this.PictureBox.Count; i++)
-                {
-                    this.PictureBox[i].Visible = i == value;
-                }
-            }
-        }
+            => this.Model.SelectedIndex = this.scrollBar.Value;
 
         private void PictureBox_MouseWheel(object sender, MouseEventArgs e)
         {
             if (e.Delta > 0)
-                this.ScrollUp();
+                this.Model.ScrollUp();
             else
-                this.ScrollDown();
+                this.Model.ScrollDown();
         }
 
         private void PictureBox_DoubleClick(object sender, EventArgs e)
-        {
-            if (((PictureBox)sender).Tag is ThumbnailInfo thumb)
-                this.OpenImage(thumb);
-        }
-
-        private void ContextMenuStrip_Opening(object sender, CancelEventArgs e)
-        {
-            var picbox = (OTPictureBox)this.contextMenuStrip.SourceControl;
-            var thumb = (ThumbnailInfo)picbox.Tag;
-
-            var searchTargetUri = thumb.FullSizeImageUrl ?? thumb.ThumbnailImageUrl ?? null;
-            if (searchTargetUri != null)
-            {
-                this.searchImageGoogleMenuItem.Enabled = true;
-                this.searchImageGoogleMenuItem.Tag = searchTargetUri;
-                this.searchImageSauceNaoMenuItem.Enabled = true;
-                this.searchImageSauceNaoMenuItem.Tag = searchTargetUri;
-            }
-            else
-            {
-                this.searchImageGoogleMenuItem.Enabled = false;
-                this.searchImageSauceNaoMenuItem.Enabled = false;
-            }
-        }
+            => this.OpenImage(this.Model.CurrentThumbnail);
 
         private void SearchSimilarImageMenuItem_Click(object sender, EventArgs e)
         {
-            var searchTargetUri = (string)this.searchImageGoogleMenuItem.Tag;
-            var searchUri = this.GetImageSearchUriGoogle(searchTargetUri);
-
+            var searchUri = this.Model.GetImageSearchUriGoogle();
             this.ThumbnailImageSearchClick?.Invoke(this, new ThumbnailImageSearchEventArgs(searchUri));
         }
 
         private void SearchImageSauceNaoMenuItem_Click(object sender, EventArgs e)
         {
-            var searchTargetUri = (string)this.searchImageSauceNaoMenuItem.Tag;
-            var searchUri = this.GetImageSearchUriSauceNao(searchTargetUri);
-
+            var searchUri = this.Model.GetImageSearchUriSauceNao();
             this.ThumbnailImageSearchClick?.Invoke(this, new ThumbnailImageSearchEventArgs(searchUri));
         }
 
         private void OpenMenuItem_Click(object sender, EventArgs e)
-            => this.OpenImage(this.Thumbnail);
+            => this.OpenImage(this.Model.CurrentThumbnail);
 
         private void CopyUrlMenuItem_Click(object sender, EventArgs e)
         {
             try
             {
-                Clipboard.SetText(this.Thumbnail.FullSizeImageUrl ?? this.Thumbnail.MediaPageUrl);
+                var thumb = this.Model.CurrentThumbnail;
+                Clipboard.SetText(thumb.FullSizeImageUrl ?? thumb.MediaPageUrl);
             }
             catch (ExternalException ex)
             {
@@ -314,7 +176,7 @@ namespace OpenTween
         {
             try
             {
-                if (this.PictureBox[this.scrollBar.Value].Image is { } memoryImage)
+                if (this.pictureBox.Image is { } memoryImage)
                     Clipboard.SetImage(memoryImage.Image);
             }
             catch (ExternalException ex)
@@ -334,9 +196,9 @@ namespace OpenTween
 
     public class ThumbnailImageSearchEventArgs : EventArgs
     {
-        public string ImageUrl { get; }
+        public Uri SearchUri { get; }
 
-        public ThumbnailImageSearchEventArgs(string url)
-            => this.ImageUrl = url;
+        public ThumbnailImageSearchEventArgs(Uri uri)
+            => this.SearchUri = uri;
     }
 }

OpenTween.Tests/TweetThumbnailControlTest.cs
@@ -1,332 +1,0 @@
-﻿// OpenTween - Client of Twitter
-// Copyright (c) 2012 kim_upsilon (@kim_upsilon) <https://upsilo.net/~upsilon/>
-// All rights reserved.
-//
-// This file is part of OpenTween.
-//
-// This program is free software; you can redistribute it and/or modify it
-// under the terms of the GNU General Public License as published by the Free
-// Software Foundation; either version 3 of the License, or (at your option)
-// any later version.
-//
-// This program is distributed in the hope that it will be useful, but
-// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
-// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
-// for more details.
-//
-// You should have received a copy of the GNU General Public License along
-// with this program. If not, see <http://www.gnu.org/licenses/>, or write to
-// the Free Software Foundation, Inc., 51 Franklin Street - Fifth Floor,
-// Boston, MA 02110-1301, USA.
-
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Net.Http;
-using System.Reflection;
-using System.Runtime.InteropServices;
-using System.Text.RegularExpressions;
-using System.Threading;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-using Moq;
-using OpenTween.Models;
-using OpenTween.Thumbnail;
-using OpenTween.Thumbnail.Services;
-using Xunit;
-using Xunit.Extensions;
-
-namespace OpenTween
-{
-    public class TweetThumbnailControlTest
-    {
-        private class TestThumbnailService : IThumbnailService
-        {
-            private readonly Regex regex;
-            private readonly string replaceUrl;
-            private readonly string? replaceTooltip;
-
-            public TestThumbnailService(string pattern, string replaceUrl, string? replaceTooltip)
-            {
-                this.regex = new Regex(pattern);
-                this.replaceUrl = replaceUrl;
-                this.replaceTooltip = replaceTooltip;
-            }
-
-            public override async Task<ThumbnailInfo?> GetThumbnailInfoAsync(string url, PostClass post, CancellationToken token)
-            {
-                var match = this.regex.Match(url);
-
-                if (!match.Success) return null;
-
-                if (url.StartsWith("http://slow.example.com/", StringComparison.Ordinal))
-                    await Task.Delay(1000, token).ConfigureAwait(false);
-
-                return new MockThumbnailInfo
-                {
-                    MediaPageUrl = url,
-                    ThumbnailImageUrl = match.Result(this.replaceUrl),
-                    TooltipText = this.replaceTooltip != null ? match.Result(this.replaceTooltip) : null,
-                };
-            }
-
-            private class MockThumbnailInfo : ThumbnailInfo
-            {
-                public override Task<MemoryImage> LoadThumbnailImageAsync(HttpClient http, CancellationToken cancellationToken)
-                    => Task.FromResult(TestUtils.CreateDummyImage());
-            }
-        }
-
-        public TweetThumbnailControlTest()
-            => this.MyCommonSetup();
-
-        private ThumbnailGenerator CreateThumbnailGenerator()
-        {
-            var imgAzyobuziNet = new ImgAzyobuziNet(autoupdate: false);
-            var thumbGenerator = new ThumbnailGenerator(imgAzyobuziNet);
-            thumbGenerator.Services.Clear();
-            thumbGenerator.Services.AddRange(new[]
-            {
-                new TestThumbnailService(@"^https?://foo.example.com/(.+)$", @"http://img.example.com/${1}.png", null),
-                new TestThumbnailService(@"^https?://bar.example.com/(.+)$", @"http://img.example.com/${1}.png", @"${1}"),
-                new TestThumbnailService(@"^https?://slow.example.com/(.+)$", @"http://img.example.com/${1}.png", null),
-            });
-
-            return thumbGenerator;
-        }
-
-        private void MyCommonSetup()
-        {
-            var mockAssembly = new Mock<_Assembly>();
-            mockAssembly.Setup(m => m.GetName()).Returns(new AssemblyName("OpenTween"));
-
-            MyCommon.EntryAssembly = mockAssembly.Object;
-        }
-
-        [WinFormsFact]
-        public void CreatePictureBoxTest()
-        {
-            using var thumbBox = new TweetThumbnailControl();
-
-            var method = typeof(TweetThumbnailControl).GetMethod("CreatePictureBox", BindingFlags.Instance | BindingFlags.NonPublic);
-            var picbox = method.Invoke(thumbBox, new[] { "pictureBox1" }) as PictureBox;
-
-            Assert.NotNull(picbox);
-            Assert.Equal("pictureBox1", picbox!.Name);
-            Assert.Equal(PictureBoxSizeMode.Zoom, picbox.SizeMode);
-            Assert.False(picbox.WaitOnLoad);
-            Assert.Equal(DockStyle.Fill, picbox.Dock);
-
-            picbox.Dispose();
-        }
-
-        [WinFormsFact]
-        public async Task CancelAsyncTest()
-        {
-            var post = new PostClass
-            {
-                TextFromApi = "てすと http://slow.example.com/abcd",
-                Media = new List<MediaInfo>
-                {
-                    new MediaInfo("http://slow.example.com/abcd"),
-                },
-            };
-
-            using var thumbbox = new TweetThumbnailControl();
-            thumbbox.Initialize(this.CreateThumbnailGenerator());
-
-            using var tokenSource = new CancellationTokenSource();
-
-            var task = thumbbox.ShowThumbnailAsync(post, tokenSource.Token);
-
-            tokenSource.Cancel();
-
-            await Assert.ThrowsAnyAsync<OperationCanceledException>(async () => await task);
-            Assert.True(task.IsCanceled);
-        }
-
-        [WinFormsTheory]
-        [InlineData(0)]
-        [InlineData(1)]
-        [InlineData(2)]
-        public void SetThumbnailCountTest(int count)
-        {
-            using var thumbbox = new TweetThumbnailControl();
-            thumbbox.Initialize(this.CreateThumbnailGenerator());
-
-            var method = typeof(TweetThumbnailControl).GetMethod("SetThumbnailCount", BindingFlags.Instance | BindingFlags.NonPublic);
-            method.Invoke(thumbbox, new[] { (object)count });
-
-            Assert.Equal(count, thumbbox.PictureBox.Count);
-
-            var num = 0;
-            foreach (var picbox in thumbbox.PictureBox)
-            {
-                Assert.Equal("pictureBox" + num, picbox.Name);
-                num++;
-            }
-
-            Assert.Equal(thumbbox.PictureBox, thumbbox.panelPictureBox.Controls.Cast<OTPictureBox>());
-
-            Assert.Equal(0, thumbbox.scrollBar.Minimum);
-
-            if (count == 0)
-                Assert.Equal(0, thumbbox.scrollBar.Maximum);
-            else
-                Assert.Equal(count - 1, thumbbox.scrollBar.Maximum);
-        }
-
-        [WinFormsFact]
-        public async Task ShowThumbnailAsyncTest()
-        {
-            var post = new PostClass
-            {
-                TextFromApi = "てすと http://foo.example.com/abcd",
-                Media = new List<MediaInfo>
-                {
-                    new MediaInfo("http://foo.example.com/abcd"),
-                },
-            };
-
-            using var thumbbox = new TweetThumbnailControl();
-            thumbbox.Initialize(this.CreateThumbnailGenerator());
-
-            await thumbbox.ShowThumbnailAsync(post);
-
-            Assert.Equal(0, thumbbox.scrollBar.Maximum);
-            Assert.False(thumbbox.scrollBar.Enabled);
-
-            Assert.Single(thumbbox.PictureBox);
-            Assert.NotNull(thumbbox.PictureBox[0].Image);
-
-            Assert.IsAssignableFrom<ThumbnailInfo>(thumbbox.PictureBox[0].Tag);
-            var thumbinfo = (ThumbnailInfo)thumbbox.PictureBox[0].Tag;
-
-            Assert.Equal("http://foo.example.com/abcd", thumbinfo.MediaPageUrl);
-            Assert.Equal("http://img.example.com/abcd.png", thumbinfo.ThumbnailImageUrl);
-
-            Assert.Equal("", thumbbox.toolTip.GetToolTip(thumbbox.PictureBox[0]));
-        }
-
-        [WinFormsFact]
-        public async Task ShowThumbnailAsyncTest2()
-        {
-            var post = new PostClass
-            {
-                TextFromApi = "てすと http://foo.example.com/abcd http://bar.example.com/efgh",
-                Media = new List<MediaInfo>
-                {
-                    new MediaInfo("http://foo.example.com/abcd"),
-                    new MediaInfo("http://bar.example.com/efgh"),
-                },
-            };
-
-            using var thumbbox = new TweetThumbnailControl();
-            thumbbox.Initialize(this.CreateThumbnailGenerator());
-
-            await thumbbox.ShowThumbnailAsync(post);
-
-            Assert.Equal(1, thumbbox.scrollBar.Maximum);
-            Assert.True(thumbbox.scrollBar.Enabled);
-
-            Assert.Equal(2, thumbbox.PictureBox.Count);
-            Assert.NotNull(thumbbox.PictureBox[0].Image);
-            Assert.NotNull(thumbbox.PictureBox[1].Image);
-
-            Assert.IsAssignableFrom<ThumbnailInfo>(thumbbox.PictureBox[0].Tag);
-            var thumbinfo = (ThumbnailInfo)thumbbox.PictureBox[0].Tag;
-
-            Assert.Equal("http://foo.example.com/abcd", thumbinfo.MediaPageUrl);
-            Assert.Equal("http://img.example.com/abcd.png", thumbinfo.ThumbnailImageUrl);
-
-            Assert.IsAssignableFrom<ThumbnailInfo>(thumbbox.PictureBox[1].Tag);
-            thumbinfo = (ThumbnailInfo)thumbbox.PictureBox[1].Tag;
-
-            Assert.Equal("http://bar.example.com/efgh", thumbinfo.MediaPageUrl);
-            Assert.Equal("http://img.example.com/efgh.png", thumbinfo.ThumbnailImageUrl);
-
-            Assert.Equal("", thumbbox.toolTip.GetToolTip(thumbbox.PictureBox[0]));
-            Assert.Equal("efgh", thumbbox.toolTip.GetToolTip(thumbbox.PictureBox[1]));
-        }
-
-        [WinFormsFact]
-        public async Task ThumbnailLoadingEventTest()
-        {
-            using var thumbbox = new TweetThumbnailControl();
-            thumbbox.Initialize(this.CreateThumbnailGenerator());
-
-            var post = new PostClass
-            {
-                TextFromApi = "てすと",
-                Media = new List<MediaInfo>
-                {
-                },
-            };
-            await TestUtils.NotRaisesAsync<EventArgs>(
-                x => thumbbox.ThumbnailLoading += x,
-                x => thumbbox.ThumbnailLoading -= x,
-                () => thumbbox.ShowThumbnailAsync(post)
-            );
-
-            var post2 = new PostClass
-            {
-                TextFromApi = "てすと http://foo.example.com/abcd",
-                Media = new List<MediaInfo>
-                    {
-                        new MediaInfo("http://foo.example.com/abcd"),
-                    },
-            };
-
-            await Assert.RaisesAsync<EventArgs>(
-                x => thumbbox.ThumbnailLoading += x,
-                x => thumbbox.ThumbnailLoading -= x,
-                () => thumbbox.ShowThumbnailAsync(post2)
-            );
-        }
-
-        [WinFormsFact]
-        public async Task ScrollTest()
-        {
-            var post = new PostClass
-            {
-                TextFromApi = "てすと http://foo.example.com/abcd http://foo.example.com/efgh",
-                Media = new List<MediaInfo>
-                {
-                    new MediaInfo("http://foo.example.com/abcd"),
-                    new MediaInfo("http://foo.example.com/efgh"),
-                },
-            };
-
-            using var thumbbox = new TweetThumbnailControl();
-            thumbbox.Initialize(this.CreateThumbnailGenerator());
-
-            await thumbbox.ShowThumbnailAsync(post);
-
-            Assert.Equal(0, thumbbox.scrollBar.Minimum);
-            Assert.Equal(1, thumbbox.scrollBar.Maximum);
-
-            thumbbox.scrollBar.Value = 0;
-
-            thumbbox.ScrollDown();
-            Assert.Equal(1, thumbbox.scrollBar.Value);
-            Assert.False(thumbbox.PictureBox[0].Visible);
-            Assert.True(thumbbox.PictureBox[1].Visible);
-
-            thumbbox.ScrollDown();
-            Assert.Equal(1, thumbbox.scrollBar.Value);
-            Assert.False(thumbbox.PictureBox[0].Visible);
-            Assert.True(thumbbox.PictureBox[1].Visible);
-
-            thumbbox.ScrollUp();
-            Assert.Equal(0, thumbbox.scrollBar.Value);
-            Assert.True(thumbbox.PictureBox[0].Visible);
-            Assert.False(thumbbox.PictureBox[1].Visible);
-
-            thumbbox.ScrollUp();
-            Assert.Equal(0, thumbbox.scrollBar.Value);
-            Assert.True(thumbbox.PictureBox[0].Visible);
-            Assert.False(thumbbox.PictureBox[1].Visible);
-        }
-    }
-}

commit 214c2931ce478fa82bf95d8bdf2781fc8cad0fc6

動画のサムネイル表示時に再生可能であることを示すアイコンが表示されない不具合を修正
Fixes: 0dee698a ("TweetThumbnailControlクラスからWinFormsに依存しない処理を分離")

OpenTween/OTPictureBox.cs
@@ -68,6 +68,21 @@ namespace OpenTween
 
         private MemoryImage? memoryImage;
 
+        public bool PlayableMark
+        {
+            get => this.playableMark;
+            set
+            {
+                if (value == this.playableMark)
+                    return;
+
+                this.playableMark = value;
+                this.Invalidate();
+            }
+        }
+
+        private bool playableMark = false;
+
         [Localizable(true)]
         [DefaultValue(PictureBoxSizeMode.Normal)]
         public new PictureBoxSizeMode SizeMode
@@ -177,8 +192,14 @@ namespace OpenTween
 
         private void DrawPlayableMark(PaintEventArgs pe)
         {
-            if (!(this.Tag is ThumbnailInfo thumb && thumb.IsPlayable)) return;
-            if (base.Image == base.InitialImage || base.Image == base.ErrorImage) return;
+            if (!this.PlayableMark)
+                return;
+
+            if (this.Image == null)
+                return;
+
+            if (base.Image == base.InitialImage || base.Image == base.ErrorImage)
+                return;
 
             var overlayImage = Properties.Resources.PlayableOverlayImage;
 

OpenTween/TweetThumbnailControl.cs
@@ -109,6 +109,7 @@ namespace OpenTween
             this.scrollBar.Value = this.Model.SelectedIndex;
 
             var thumbnail = this.Model.CurrentThumbnail;
+            this.pictureBox.PlayableMark = thumbnail.IsPlayable;
             this.pictureBox.AccessibleDescription = thumbnail.TooltipText;
             this.toolTip.SetToolTip(this.pictureBox, thumbnail.TooltipText);
             _ = this.pictureBox.SetImageFromTask(this.Model.LoadSelectedThumbnail);