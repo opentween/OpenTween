opentween/OpenTween

commit 96817b42a75b3e0088dec6e573124f2d5a7a1b30

graphqlエンドポイントを使用した検索タイムラインの取得に対応

OpenTween/Twitter.cs
@@ -766,11 +766,11 @@ namespace OpenTween
             return minimumId;
         }
 
-        private long? CreatePostsFromSearchJson(TwitterSearchResult items, PublicSearchTabModel tab, bool read, bool more)
+        private long? CreatePostsFromSearchJson(TwitterStatus[] statuses, PublicSearchTabModel tab, bool read, bool more)
         {
             long? minimumId = null;
 
-            foreach (var status in items.Statuses)
+            foreach (var status in statuses)
             {
                 if (minimumId == null || minimumId.Value > status.Id)
                     minimumId = status.Id;
@@ -1029,24 +1029,47 @@ namespace OpenTween
         {
             var count = GetApiResultCount(MyCommon.WORKERTYPE.PublicSearch, more, false);
 
-            long? maxId = null;
-            long? sinceId = null;
-            if (more)
+            TwitterStatus[] statuses;
+            if (this.Api.AppToken.AuthType == APIAuthType.TwitterComCookie)
             {
-                maxId = tab.OldestId - 1;
+                var request = new SearchTimelineRequest(tab.SearchWords)
+                {
+                    Count = count,
+                    Cursor = more ? tab.CursorBottom : null,
+                };
+                var response = await request.Send(this.Api.Connection)
+                    .ConfigureAwait(false);
+
+                var convertedStatuses = response.Tweets.Select(x => x.ToTwitterStatus());
+                if (!SettingManager.Instance.Common.IsListsIncludeRts)
+                    convertedStatuses = convertedStatuses.Where(x => x.RetweetedStatus == null);
+
+                statuses = convertedStatuses.ToArray();
+                tab.CursorBottom = response.CursorBottom;
             }
             else
             {
-                sinceId = tab.SinceId;
-            }
+                long? maxId = null;
+                long? sinceId = null;
+                if (more)
+                {
+                    maxId = tab.OldestId - 1;
+                }
+                else
+                {
+                    sinceId = tab.SinceId;
+                }
 
-            var searchResult = await this.Api.SearchTweets(tab.SearchWords, tab.SearchLang, count, maxId, sinceId)
-                .ConfigureAwait(false);
+                var searchResult = await this.Api.SearchTweets(tab.SearchWords, tab.SearchLang, count, maxId, sinceId)
+                    .ConfigureAwait(false);
+
+                statuses = searchResult.Statuses;
+            }
 
             if (!TabInformations.GetInstance().ContainsTab(tab))
                 return;
 
-            var minimumId = this.CreatePostsFromSearchJson(searchResult, tab, read, more);
+            var minimumId = this.CreatePostsFromSearchJson(statuses, tab, read, more);
 
             if (minimumId != null)
                 tab.OldestId = minimumId.Value;

OpenTween.Tests/Api/GraphQL/SearchTimelineRequestTest.cs
@@ -1,0 +1,94 @@
+﻿// OpenTween - Client of Twitter
+// Copyright (c) 2023 kim_upsilon (@kim_upsilon) <https://upsilo.net/~upsilon/>
+// All rights reserved.
+//
+// This file is part of OpenTween.
+//
+// This program is free software; you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the Free
+// Software Foundation; either version 3 of the License, or (at your option)
+// any later version.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+// for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program. If not, see <http://www.gnu.org/licenses/>, or write to
+// the Free Software Foundation, Inc., 51 Franklin Street - Fifth Floor,
+// Boston, MA 02110-1301, USA.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using OpenTween.Connection;
+using Xunit;
+
+namespace OpenTween.Api.GraphQL
+{
+    public class SearchTimelineRequestTest
+    {
+        [Fact]
+        public async Task Send_Test()
+        {
+            using var responseStream = File.OpenRead("Resources/Responses/SearchTimeline_SimpleTweet.json");
+
+            var mock = new Mock<IApiConnection>();
+            mock.Setup(x =>
+                    x.GetStreamAsync(It.IsAny<Uri>(), It.IsAny<IDictionary<string, string>>())
+                )
+                .Callback<Uri, IDictionary<string, string>>((url, param) =>
+                {
+                    Assert.Equal(new("https://twitter.com/i/api/graphql/lZ0GCEojmtQfiUQa5oJSEw/SearchTimeline"), url);
+                    Assert.Equal(2, param.Count);
+                    Assert.Equal("""{"rawQuery":"#OpenTween","count":20,"product":"Latest"}""", param["variables"]);
+                    Assert.True(param.ContainsKey("features"));
+                })
+                .ReturnsAsync(responseStream);
+
+            var request = new SearchTimelineRequest(rawQuery: "#OpenTween")
+            {
+                Count = 20,
+            };
+
+            var response = await request.Send(mock.Object).ConfigureAwait(false);
+            Assert.Single(response.Tweets);
+            Assert.Equal("DAADDAABCgABFnlh4hraMAYKAAIOTm0DEhTAAQAIAAIAAAACCAADAAAAAAgABAAAAAAKAAUX8j3ezIAnEAoABhfyPd7Mf9jwAAA", response.CursorBottom);
+
+            mock.VerifyAll();
+        }
+
+        [Fact]
+        public async Task Send_RequestCursor_Test()
+        {
+            using var responseStream = File.OpenRead("Resources/Responses/SearchTimeline_SimpleTweet.json");
+
+            var mock = new Mock<IApiConnection>();
+            mock.Setup(x =>
+                    x.GetStreamAsync(It.IsAny<Uri>(), It.IsAny<IDictionary<string, string>>())
+                )
+                .Callback<Uri, IDictionary<string, string>>((url, param) =>
+                {
+                    Assert.Equal(new("https://twitter.com/i/api/graphql/lZ0GCEojmtQfiUQa5oJSEw/SearchTimeline"), url);
+                    Assert.Equal(2, param.Count);
+                    Assert.Equal("""{"rawQuery":"#OpenTween","count":20,"product":"Latest","cursor":"aaa"}""", param["variables"]);
+                    Assert.True(param.ContainsKey("features"));
+                })
+                .ReturnsAsync(responseStream);
+
+            var request = new SearchTimelineRequest(rawQuery: "#OpenTween")
+            {
+                Count = 20,
+                Cursor = "aaa",
+            };
+
+            await request.Send(mock.Object).ConfigureAwait(false);
+            mock.VerifyAll();
+        }
+    }
+}

OpenTween.Tests/OpenTween.Tests.csproj
@@ -61,6 +61,9 @@
     <None Update="Resources\Responses\Error_NotFound.json">
       <CopyToOutputDirectory>PreserveNewest</CopyToOutputDirectory>
     </None>
+    <None Update="Resources\Responses\SearchTimeline_SimpleTweet.json">
+      <CopyToOutputDirectory>PreserveNewest</CopyToOutputDirectory>
+    </None>
     <None Update="Resources\Responses\TimelineTweet_PromotedTweet.json">
       <CopyToOutputDirectory>PreserveNewest</CopyToOutputDirectory>
     </None>

OpenTween/Api/GraphQL/SearchTimelineRequest.cs
@@ -1,0 +1,116 @@
+﻿// OpenTween - Client of Twitter
+// Copyright (c) 2023 kim_upsilon (@kim_upsilon) <https://upsilo.net/~upsilon/>
+// All rights reserved.
+//
+// This file is part of OpenTween.
+//
+// This program is free software; you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the Free
+// Software Foundation; either version 3 of the License, or (at your option)
+// any later version.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+// for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program. If not, see <http://www.gnu.org/licenses/>, or write to
+// the Free Software Foundation, Inc., 51 Franklin Street - Fifth Floor,
+// Boston, MA 02110-1301, USA.
+
+#nullable enable
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.XPath;
+using OpenTween.Connection;
+
+namespace OpenTween.Api.GraphQL
+{
+    public class SearchTimelineRequest
+    {
+        private static readonly Uri EndpointUri = new("https://twitter.com/i/api/graphql/lZ0GCEojmtQfiUQa5oJSEw/SearchTimeline");
+
+        public string RawQuery { get; set; }
+
+        public int Count { get; set; } = 20;
+
+        public string? Cursor { get; set; }
+
+        public SearchTimelineRequest(string rawQuery)
+            => this.RawQuery = rawQuery;
+
+        public Dictionary<string, string> CreateParameters()
+        {
+            return new()
+            {
+                ["variables"] = "{" +
+                    $@"""rawQuery"":""{JsonUtils.EscapeJsonString(this.RawQuery)}""," +
+                    $@"""count"":{this.Count}," +
+                    $@"""product"":""Latest""" +
+                    (this.Cursor != null ? $@",""cursor"":""{JsonUtils.EscapeJsonString(this.Cursor)}""" : "") +
+                    "}",
+                ["features"] = "{" +
+                    @"""responsive_web_graphql_exclude_directive_enabled"":true," +
+                    @"""verified_phone_label_enabled"":false," +
+                    @"""responsive_web_home_pinned_timelines_enabled"":true," +
+                    @"""creator_subscriptions_tweet_preview_api_enabled"":true," +
+                    @"""responsive_web_graphql_timeline_navigation_enabled"":true," +
+                    @"""responsive_web_graphql_skip_user_profile_image_extensions_enabled"":false," +
+                    @"""c9s_tweet_anatomy_moderator_badge_enabled"":true," +
+                    @"""tweetypie_unmention_optimization_enabled"":true," +
+                    @"""responsive_web_edit_tweet_api_enabled"":true," +
+                    @"""graphql_is_translatable_rweb_tweet_is_translatable_enabled"":true," +
+                    @"""view_counts_everywhere_api_enabled"":true," +
+                    @"""longform_notetweets_consumption_enabled"":true," +
+                    @"""responsive_web_twitter_article_tweet_consumption_enabled"":false," +
+                    @"""tweet_awards_web_tipping_enabled"":false," +
+                    @"""freedom_of_speech_not_reach_fetch_enabled"":true," +
+                    @"""standardized_nudges_misinfo"":true," +
+                    @"""tweet_with_visibility_results_prefer_gql_limited_actions_policy_enabled"":true," +
+                    @"""longform_notetweets_rich_text_read_enabled"":true," +
+                    @"""longform_notetweets_inline_media_enabled"":true," +
+                    @"""responsive_web_media_download_video_enabled"":false," +
+                    @"""responsive_web_enhance_cards_enabled"":false" +
+                    "}",
+            };
+        }
+
+        public async Task<TimelineResponse> Send(IApiConnection apiConnection)
+        {
+            var param = this.CreateParameters();
+
+            XElement rootElm;
+            try
+            {
+                using var stream = await apiConnection.GetStreamAsync(EndpointUri, param);
+                using var jsonReader = JsonReaderWriterFactory.CreateJsonReader(stream, XmlDictionaryReaderQuotas.Max);
+                rootElm = XElement.Load(jsonReader);
+            }
+            catch (IOException ex)
+            {
+                throw new WebApiException("IO Error", ex);
+            }
+            catch (NotSupportedException ex)
+            {
+                // NotSupportedException: Stream does not support reading. のエラーが時々報告される
+                throw new WebApiException("Stream Error", ex);
+            }
+
+            ErrorResponse.ThrowIfError(rootElm);
+
+            var tweets = TimelineTweet.ExtractTimelineTweets(rootElm);
+            var cursorBottom = rootElm.XPathSelectElement("//content[__typename[text()='TimelineTimelineCursor']][cursorType[text()='Bottom']]/value")?.Value;
+
+            return new(tweets, cursorBottom);
+        }
+    }
+}

OpenTween/Models/PublicSearchTabModel.cs
@@ -45,6 +45,8 @@ namespace OpenTween.Models
 
         public long SinceId { get; set; }
 
+        public string? CursorBottom { get; set; }
+
         public string SearchWords
         {
             get => this.searchWords;

commit 083dc2d52777762e8efff1209e5547c54407126e

検索タイムラインで誤って公式RTが除外される不具合を修正
Fixes: 96817b42 ("graphqlエンドポイントを使用した検索タイムラインの取得に対応")

OpenTween/Twitter.cs
@@ -1052,11 +1052,7 @@ namespace OpenTween
                 var response = await request.Send(this.Api.Connection)
                     .ConfigureAwait(false);
 
-                var convertedStatuses = response.Tweets.Select(x => x.ToTwitterStatus());
-                if (!SettingManager.Instance.Common.IsListsIncludeRts)
-                    convertedStatuses = convertedStatuses.Where(x => x.RetweetedStatus == null);
-
-                statuses = convertedStatuses.ToArray();
+                statuses = response.Tweets.Select(x => x.ToTwitterStatus()).ToArray();
                 tab.CursorBottom = response.CursorBottom;
             }
             else