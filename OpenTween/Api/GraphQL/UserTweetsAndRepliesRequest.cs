opentween/OpenTween

commit 3b20d4c862521b70c37759eb4ea7b1a97f245d46

graphqlエンドポイントを使用したユーザータイムラインの取得に対応

OpenTween/Twitter.cs
@@ -660,7 +660,7 @@ namespace OpenTween
                 tab.OldestId = minimumId;
         }
 
-        public async Task GetUserTimelineApi(bool read, string userName, UserTimelineTabModel tab, bool more)
+        public async Task GetUserTimelineApi(bool read, UserTimelineTabModel tab, bool more)
         {
             this.CheckAccountState();
 
@@ -671,24 +671,39 @@ namespace OpenTween
                 count = Math.Min(count, 99);
 
             TwitterStatus[] statuses;
-            if (MyCommon.IsNullOrEmpty(userName))
+            if (this.Api.AppToken.AuthType == APIAuthType.TwitterComCookie)
             {
-                var target = tab.ScreenName;
-                if (MyCommon.IsNullOrEmpty(target)) return;
-                userName = target;
-                statuses = await this.Api.StatusesUserTimeline(userName, count)
+                var userId = tab.UserId;
+                if (MyCommon.IsNullOrEmpty(userId))
+                {
+                    var user = await this.GetUserInfo(tab.ScreenName)
+                        .ConfigureAwait(false);
+
+                    userId = user.IdStr;
+                    tab.UserId = user.IdStr;
+                }
+
+                var request = new UserTweetsRequest(userId)
+                {
+                    Count = count,
+                    Cursor = more ? tab.CursorBottom : null,
+                };
+                var response = await request.Send(this.Api.Connection)
                     .ConfigureAwait(false);
+
+                statuses = response.Tweets.Select(x => x.ToTwitterStatus()).ToArray();
+                tab.CursorBottom = response.CursorBottom;
             }
             else
             {
                 if (more)
                 {
-                    statuses = await this.Api.StatusesUserTimeline(userName, count, maxId: tab.OldestId as TwitterStatusId)
+                    statuses = await this.Api.StatusesUserTimeline(tab.ScreenName, count, maxId: tab.OldestId as TwitterStatusId)
                         .ConfigureAwait(false);
                 }
                 else
                 {
-                    statuses = await this.Api.StatusesUserTimeline(userName, count)
+                    statuses = await this.Api.StatusesUserTimeline(tab.ScreenName, count)
                         .ConfigureAwait(false);
                 }
             }

OpenTween.Tests/OpenTween.Tests.csproj
@@ -100,5 +100,8 @@
     <None Update="Resources\Responses\UserByScreenName.json">
       <CopyToOutputDirectory>PreserveNewest</CopyToOutputDirectory>
     </None>
+    <None Update="Resources\Responses\UserTweets_SimpleTweet.json">
+      <CopyToOutputDirectory>PreserveNewest</CopyToOutputDirectory>
+    </None>
   </ItemGroup>
 </Project>

OpenTween.Tests/Api/GraphQL/UserTweetsRequestTest.cs
@@ -1,0 +1,96 @@
+﻿// OpenTween - Client of Twitter
+// Copyright (c) 2023 kim_upsilon (@kim_upsilon) <https://upsilo.net/~upsilon/>
+// All rights reserved.
+//
+// This file is part of OpenTween.
+//
+// This program is free software; you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the Free
+// Software Foundation; either version 3 of the License, or (at your option)
+// any later version.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+// for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program. If not, see <http://www.gnu.org/licenses/>, or write to
+// the Free Software Foundation, Inc., 51 Franklin Street - Fifth Floor,
+// Boston, MA 02110-1301, USA.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using OpenTween.Connection;
+using Xunit;
+
+namespace OpenTween.Api.GraphQL
+{
+    public class UserTweetsRequestTest
+    {
+        [Fact]
+        public async Task Send_Test()
+        {
+            using var responseStream = File.OpenRead("Resources/Responses/UserTweets_SimpleTweet.json");
+
+            var mock = new Mock<IApiConnection>();
+            mock.Setup(x =>
+                    x.GetStreamAsync(It.IsAny<Uri>(), It.IsAny<IDictionary<string, string>>())
+                )
+                .Callback<Uri, IDictionary<string, string>>((url, param) =>
+                {
+                    Assert.Equal(new("https://twitter.com/i/api/graphql/2GIWTr7XwadIixZDtyXd4A/UserTweets"), url);
+                    Assert.Equal(3, param.Count);
+                    Assert.Equal("""{"userId":"40480664","count":20,"includePromotedContent":true,"withQuickPromoteEligibilityTweetFields":true,"withVoice":true,"withV2Timeline":true}""", param["variables"]);
+                    Assert.True(param.ContainsKey("features"));
+                    Assert.True(param.ContainsKey("fieldToggles"));
+                })
+                .ReturnsAsync(responseStream);
+
+            var request = new UserTweetsRequest(userId: "40480664")
+            {
+                Count = 20,
+            };
+
+            var response = await request.Send(mock.Object).ConfigureAwait(false);
+            Assert.Single(response.Tweets);
+            Assert.Equal("DAABCgABF_tTnZu__-0KAAIWZa6KTRoAAwgAAwAAAAIAAA", response.CursorBottom);
+
+            mock.VerifyAll();
+        }
+
+        [Fact]
+        public async Task Send_RequestCursor_Test()
+        {
+            using var responseStream = File.OpenRead("Resources/Responses/UserTweets_SimpleTweet.json");
+
+            var mock = new Mock<IApiConnection>();
+            mock.Setup(x =>
+                    x.GetStreamAsync(It.IsAny<Uri>(), It.IsAny<IDictionary<string, string>>())
+                )
+                .Callback<Uri, IDictionary<string, string>>((url, param) =>
+                {
+                    Assert.Equal(new("https://twitter.com/i/api/graphql/2GIWTr7XwadIixZDtyXd4A/UserTweets"), url);
+                    Assert.Equal(3, param.Count);
+                    Assert.Equal("""{"userId":"40480664","count":20,"includePromotedContent":true,"withQuickPromoteEligibilityTweetFields":true,"withVoice":true,"withV2Timeline":true,"cursor":"aaa"}""", param["variables"]);
+                    Assert.True(param.ContainsKey("features"));
+                    Assert.True(param.ContainsKey("fieldToggles"));
+                })
+                .ReturnsAsync(responseStream);
+
+            var request = new UserTweetsRequest(userId: "40480664")
+            {
+                Count = 20,
+                Cursor = "aaa",
+            };
+
+            await request.Send(mock.Object).ConfigureAwait(false);
+            mock.VerifyAll();
+        }
+    }
+}

OpenTween/Api/GraphQL/UserTweetsRequest.cs
@@ -1,0 +1,102 @@
+﻿// OpenTween - Client of Twitter
+// Copyright (c) 2023 kim_upsilon (@kim_upsilon) <https://upsilo.net/~upsilon/>
+// All rights reserved.
+//
+// This file is part of OpenTween.
+//
+// This program is free software; you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the Free
+// Software Foundation; either version 3 of the License, or (at your option)
+// any later version.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+// for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program. If not, see <http://www.gnu.org/licenses/>, or write to
+// the Free Software Foundation, Inc., 51 Franklin Street - Fifth Floor,
+// Boston, MA 02110-1301, USA.
+
+#nullable enable
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.XPath;
+using OpenTween.Connection;
+
+namespace OpenTween.Api.GraphQL
+{
+    public class UserTweetsRequest
+    {
+        private static readonly Uri EndpointUri = new("https://twitter.com/i/api/graphql/2GIWTr7XwadIixZDtyXd4A/UserTweets");
+
+        public string UserId { get; set; }
+
+        public int Count { get; set; } = 20;
+
+        public string? Cursor { get; set; }
+
+        public UserTweetsRequest(string userId)
+            => this.UserId = userId;
+
+        public Dictionary<string, string> CreateParameters()
+        {
+            return new()
+            {
+                ["variables"] = "{" +
+                    $@"""userId"":""{JsonUtils.EscapeJsonString(this.UserId)}""," +
+                    $@"""count"":20," +
+                    $@"""includePromotedContent"":true," +
+                    $@"""withQuickPromoteEligibilityTweetFields"":true," +
+                    $@"""withVoice"":true," +
+                    $@"""withV2Timeline"":true" +
+                    (this.Cursor != null ? $@",""cursor"":""{JsonUtils.EscapeJsonString(this.Cursor)}""" : "") +
+                    "}",
+                ["features"] = """
+                    {"rweb_lists_timeline_redesign_enabled":true,"responsive_web_graphql_exclude_directive_enabled":true,"verified_phone_label_enabled":false,"creator_subscriptions_tweet_preview_api_enabled":true,"responsive_web_graphql_timeline_navigation_enabled":true,"responsive_web_graphql_skip_user_profile_image_extensions_enabled":false,"tweetypie_unmention_optimization_enabled":true,"responsive_web_edit_tweet_api_enabled":true,"graphql_is_translatable_rweb_tweet_is_translatable_enabled":true,"view_counts_everywhere_api_enabled":true,"longform_notetweets_consumption_enabled":true,"responsive_web_twitter_article_tweet_consumption_enabled":false,"tweet_awards_web_tipping_enabled":false,"freedom_of_speech_not_reach_fetch_enabled":true,"standardized_nudges_misinfo":true,"tweet_with_visibility_results_prefer_gql_limited_actions_policy_enabled":true,"longform_notetweets_rich_text_read_enabled":true,"longform_notetweets_inline_media_enabled":true,"responsive_web_media_download_video_enabled":false,"responsive_web_enhance_cards_enabled":false}
+                    """,
+                ["fieldToggles"] = """
+                    {"withAuxiliaryUserLabels":false,"withArticleRichContentState":false}
+                    """,
+            };
+        }
+
+        public async Task<TimelineResponse> Send(IApiConnection apiConnection)
+        {
+            var param = this.CreateParameters();
+
+            XElement rootElm;
+            try
+            {
+                using var stream = await apiConnection.GetStreamAsync(EndpointUri, param);
+                using var jsonReader = JsonReaderWriterFactory.CreateJsonReader(stream, XmlDictionaryReaderQuotas.Max);
+                rootElm = XElement.Load(jsonReader);
+            }
+            catch (IOException ex)
+            {
+                throw new WebApiException("IO Error", ex);
+            }
+            catch (NotSupportedException ex)
+            {
+                // NotSupportedException: Stream does not support reading. のエラーが時々報告される
+                throw new WebApiException("Stream Error", ex);
+            }
+
+            ErrorResponse.ThrowIfError(rootElm);
+
+            var tweets = TimelineTweet.ExtractTimelineTweets(rootElm);
+            var cursorBottom = rootElm.XPathSelectElement("//content[__typename[text()='TimelineTimelineCursor']][cursorType[text()='Bottom']]/value")?.Value;
+
+            return new(tweets, cursorBottom);
+        }
+    }
+}

OpenTween/Models/TabInformations.cs
@@ -253,7 +253,10 @@ namespace OpenTween.Models
                 MyCommon.TabUsageType.UserDefined
                     => new FilterTabModel(tabName),
                 MyCommon.TabUsageType.UserTimeline
-                    => new UserTimelineTabModel(tabName, tabSetting.User!),
+                    => new UserTimelineTabModel(tabName, tabSetting.User!)
+                    {
+                        UserId = tabSetting.UserId,
+                    },
                 MyCommon.TabUsageType.PublicSearch
                     => new PublicSearchTabModel(tabName)
                     {

OpenTween/Models/UserTimelineTabModel.cs
@@ -43,7 +43,11 @@ namespace OpenTween.Models
 
         public string ScreenName { get; }
 
+        public string? UserId { get; set; }
+
         public PostId? OldestId { get; set; }
+
+        public string? CursorBottom { get; set; }
 
         public UserTimelineTabModel(string tabName, string screenName)
             : base(tabName)
@@ -64,7 +68,7 @@ namespace OpenTween.Models
 
             progress.Report("UserTimeline refreshing...");
 
-            await tw.GetUserTimelineApi(read, this.ScreenName, this, backward)
+            await tw.GetUserTimelineApi(read, this, backward)
                 .ConfigureAwait(false);
 
             TabInformations.GetInstance().DistributePosts();

OpenTween/Setting/SettingTabs.cs
@@ -84,6 +84,14 @@ namespace OpenTween
             public string? User { get; set; }
 
             /// <summary>
+            /// 表示するユーザーのID (<see cref="MyCommon.TabUsageType.UserTimeline"/> で使用)
+            /// </summary>
+            /// <remarks>
+            /// v3.7.1 まで存在しない項目のため、空の場合は <see cref="User"/> から補う必要がある
+            /// </remarks>
+            public string? UserId { get; set; }
+
+            /// <summary>
             /// 検索文字列 (<see cref="MyCommon.TabUsageType.PublicSearch"/> で使用)
             /// </summary>
             public string SearchWords { get; set; } = "";

OpenTween/Tween.cs
@@ -5807,6 +5807,7 @@ namespace OpenTween
                         break;
                     case UserTimelineTabModel userTab:
                         tabSetting.User = userTab.ScreenName;
+                        tabSetting.UserId = userTab.UserId;
                         break;
                     case PublicSearchTabModel searchTab:
                         tabSetting.SearchWords = searchTab.SearchWords;

commit e0df3263de50c28b223259c8a646fef7dbc195ff

ユーザータイムラインにリプライが含まれない不具合を修正
Fixes: 3b20d4c8 ("graphqlエンドポイントを使用したユーザータイムラインの取得に対応")

OpenTween/Api/GraphQL/UserTweetsAndRepliesRequest.cs
@@ -35,9 +35,9 @@ using OpenTween.Connection;
 
 namespace OpenTween.Api.GraphQL
 {
-    public class UserTweetsRequest
+    public class UserTweetsAndRepliesRequest
     {
-        private static readonly Uri EndpointUri = new("https://twitter.com/i/api/graphql/2GIWTr7XwadIixZDtyXd4A/UserTweets");
+        private static readonly Uri EndpointUri = new("https://twitter.com/i/api/graphql/YlkSUg0mRBx7-EkxCvc-bw/UserTweetsAndReplies");
 
         public string UserId { get; set; }
 
@@ -45,7 +45,7 @@ namespace OpenTween.Api.GraphQL
 
         public string? Cursor { get; set; }
 
-        public UserTweetsRequest(string userId)
+        public UserTweetsAndRepliesRequest(string userId)
             => this.UserId = userId;
 
         public Dictionary<string, string> CreateParameters()
@@ -54,18 +54,15 @@ namespace OpenTween.Api.GraphQL
             {
                 ["variables"] = "{" +
                     $@"""userId"":""{JsonUtils.EscapeJsonString(this.UserId)}""," +
-                    $@"""count"":20," +
+                    $@"""count"":{this.Count}," +
                     $@"""includePromotedContent"":true," +
-                    $@"""withQuickPromoteEligibilityTweetFields"":true," +
+                    $@"""withCommunity"":true," +
                     $@"""withVoice"":true," +
                     $@"""withV2Timeline"":true" +
                     (this.Cursor != null ? $@",""cursor"":""{JsonUtils.EscapeJsonString(this.Cursor)}""" : "") +
                     "}",
                 ["features"] = """
-                    {"rweb_lists_timeline_redesign_enabled":true,"responsive_web_graphql_exclude_directive_enabled":true,"verified_phone_label_enabled":false,"creator_subscriptions_tweet_preview_api_enabled":true,"responsive_web_graphql_timeline_navigation_enabled":true,"responsive_web_graphql_skip_user_profile_image_extensions_enabled":false,"tweetypie_unmention_optimization_enabled":true,"responsive_web_edit_tweet_api_enabled":true,"graphql_is_translatable_rweb_tweet_is_translatable_enabled":true,"view_counts_everywhere_api_enabled":true,"longform_notetweets_consumption_enabled":true,"responsive_web_twitter_article_tweet_consumption_enabled":false,"tweet_awards_web_tipping_enabled":false,"freedom_of_speech_not_reach_fetch_enabled":true,"standardized_nudges_misinfo":true,"tweet_with_visibility_results_prefer_gql_limited_actions_policy_enabled":true,"longform_notetweets_rich_text_read_enabled":true,"longform_notetweets_inline_media_enabled":true,"responsive_web_media_download_video_enabled":false,"responsive_web_enhance_cards_enabled":false}
-                    """,
-                ["fieldToggles"] = """
-                    {"withAuxiliaryUserLabels":false,"withArticleRichContentState":false}
+                    {"responsive_web_graphql_exclude_directive_enabled":true,"verified_phone_label_enabled":false,"responsive_web_home_pinned_timelines_enabled":true,"creator_subscriptions_tweet_preview_api_enabled":true,"responsive_web_graphql_timeline_navigation_enabled":true,"responsive_web_graphql_skip_user_profile_image_extensions_enabled":false,"c9s_tweet_anatomy_moderator_badge_enabled":true,"tweetypie_unmention_optimization_enabled":true,"responsive_web_edit_tweet_api_enabled":true,"graphql_is_translatable_rweb_tweet_is_translatable_enabled":true,"view_counts_everywhere_api_enabled":true,"longform_notetweets_consumption_enabled":true,"responsive_web_twitter_article_tweet_consumption_enabled":false,"tweet_awards_web_tipping_enabled":false,"freedom_of_speech_not_reach_fetch_enabled":true,"standardized_nudges_misinfo":true,"tweet_with_visibility_results_prefer_gql_limited_actions_policy_enabled":true,"longform_notetweets_rich_text_read_enabled":true,"longform_notetweets_inline_media_enabled":true,"responsive_web_media_download_video_enabled":false,"responsive_web_enhance_cards_enabled":false}
                     """,
             };
         }

OpenTween/Twitter.cs
@@ -679,7 +679,7 @@ namespace OpenTween
                     tab.UserId = user.IdStr;
                 }
 
-                var request = new UserTweetsRequest(userId)
+                var request = new UserTweetsAndRepliesRequest(userId)
                 {
                     Count = count,
                     Cursor = more ? tab.CursorBottom : null,
@@ -690,6 +690,7 @@ namespace OpenTween
                 statuses = response.Tweets
                     .Where(x => !x.IsTombstone)
                     .Select(x => x.ToTwitterStatus())
+                    .Where(x => x.User.IdStr == userId) // リプライツリーに含まれる他ユーザーのツイートを除外
                     .ToArray();
 
                 tab.CursorBottom = response.CursorBottom;

OpenTween.Tests/Api/GraphQL/UserTweetsAndRepliesRequestTest.cs
@@ -31,12 +31,12 @@ using Xunit;
 
 namespace OpenTween.Api.GraphQL
 {
-    public class UserTweetsRequestTest
+    public class UserTweetsAndRepliesRequestTest
     {
         [Fact]
         public async Task Send_Test()
         {
-            using var responseStream = File.OpenRead("Resources/Responses/UserTweets_SimpleTweet.json");
+            using var responseStream = File.OpenRead("Resources/Responses/UserTweetsAndReplies_SimpleTweet.json");
 
             var mock = new Mock<IApiConnection>();
             mock.Setup(x =>
@@ -44,15 +44,14 @@ namespace OpenTween.Api.GraphQL
                 )
                 .Callback<Uri, IDictionary<string, string>>((url, param) =>
                 {
-                    Assert.Equal(new("https://twitter.com/i/api/graphql/2GIWTr7XwadIixZDtyXd4A/UserTweets"), url);
-                    Assert.Equal(3, param.Count);
-                    Assert.Equal("""{"userId":"40480664","count":20,"includePromotedContent":true,"withQuickPromoteEligibilityTweetFields":true,"withVoice":true,"withV2Timeline":true}""", param["variables"]);
+                    Assert.Equal(new("https://twitter.com/i/api/graphql/YlkSUg0mRBx7-EkxCvc-bw/UserTweetsAndReplies"), url);
+                    Assert.Equal(2, param.Count);
+                    Assert.Equal("""{"userId":"40480664","count":20,"includePromotedContent":true,"withCommunity":true,"withVoice":true,"withV2Timeline":true}""", param["variables"]);
                     Assert.True(param.ContainsKey("features"));
-                    Assert.True(param.ContainsKey("fieldToggles"));
                 })
                 .ReturnsAsync(responseStream);
 
-            var request = new UserTweetsRequest(userId: "40480664")
+            var request = new UserTweetsAndRepliesRequest(userId: "40480664")
             {
                 Count = 20,
             };
@@ -67,7 +66,7 @@ namespace OpenTween.Api.GraphQL
         [Fact]
         public async Task Send_RequestCursor_Test()
         {
-            using var responseStream = File.OpenRead("Resources/Responses/UserTweets_SimpleTweet.json");
+            using var responseStream = File.OpenRead("Resources/Responses/UserTweetsAndReplies_SimpleTweet.json");
 
             var mock = new Mock<IApiConnection>();
             mock.Setup(x =>
@@ -75,15 +74,14 @@ namespace OpenTween.Api.GraphQL
                 )
                 .Callback<Uri, IDictionary<string, string>>((url, param) =>
                 {
-                    Assert.Equal(new("https://twitter.com/i/api/graphql/2GIWTr7XwadIixZDtyXd4A/UserTweets"), url);
-                    Assert.Equal(3, param.Count);
-                    Assert.Equal("""{"userId":"40480664","count":20,"includePromotedContent":true,"withQuickPromoteEligibilityTweetFields":true,"withVoice":true,"withV2Timeline":true,"cursor":"aaa"}""", param["variables"]);
+                    Assert.Equal(new("https://twitter.com/i/api/graphql/YlkSUg0mRBx7-EkxCvc-bw/UserTweetsAndReplies"), url);
+                    Assert.Equal(2, param.Count);
+                    Assert.Equal("""{"userId":"40480664","count":20,"includePromotedContent":true,"withCommunity":true,"withVoice":true,"withV2Timeline":true,"cursor":"aaa"}""", param["variables"]);
                     Assert.True(param.ContainsKey("features"));
-                    Assert.True(param.ContainsKey("fieldToggles"));
                 })
                 .ReturnsAsync(responseStream);
 
-            var request = new UserTweetsRequest(userId: "40480664")
+            var request = new UserTweetsAndRepliesRequest(userId: "40480664")
             {
                 Count = 20,
                 Cursor = "aaa",

OpenTween.Tests/OpenTween.Tests.csproj
@@ -106,7 +106,7 @@
     <None Update="Resources\Responses\UserByScreenName.json">
       <CopyToOutputDirectory>PreserveNewest</CopyToOutputDirectory>
     </None>
-    <None Update="Resources\Responses\UserTweets_SimpleTweet.json">
+    <None Update="Resources\Responses\UserTweetsAndReplies_SimpleTweet.json">
       <CopyToOutputDirectory>PreserveNewest</CopyToOutputDirectory>
     </None>
   </ItemGroup>