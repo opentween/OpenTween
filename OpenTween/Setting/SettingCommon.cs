opentween/OpenTween

commit b43c97bf6ed9af79b47fdbfe6ea23fdb3ca3b160

ITwitterCredentialとアクセス手段ごとの具象クラスを追加

OpenTween/Setting/SettingCommon.cs
@@ -398,6 +398,20 @@ namespace OpenTween
             };
         }
 
+        public ITwitterCredential GetTwitterCredential()
+        {
+            var appToken = this.GetTwitterAppToken();
+
+            return appToken.AuthType switch
+            {
+                APIAuthType.OAuth1
+                    => new TwitterCredentialOAuth1(appToken, this.TwitterOAuth1ConsumerKey, this.TwitterOAuth1ConsumerSecret),
+                APIAuthType.TwitterComCookie
+                    => new TwitterCredentialCookie(appToken),
+                _ => new TwitterCredentialNone(),
+            };
+        }
+
         private string Encrypt(string password)
         {
             if (MyCommon.IsNullOrEmpty(password)) password = "";

OpenTween.Tests/Api/TwitterApiTest.cs
@@ -59,8 +59,9 @@ namespace OpenTween.Api
             Assert.IsType<TwitterApiConnection>(twitterApi.ApiConnection);
 
             var apiConnection = (TwitterApiConnection)twitterApi.ApiConnection!;
-            Assert.Equal("*** AccessToken ***", apiConnection.AccessToken);
-            Assert.Equal("*** AccessSecret ***", apiConnection.AccessSecret);
+            var credential = Assert.IsType<TwitterCredentialOAuth1>(apiConnection.Credential);
+            Assert.Equal("*** AccessToken ***", credential.Token);
+            Assert.Equal("*** AccessSecret ***", credential.TokenSecret);
 
             Assert.Equal(100L, twitterApi.CurrentUserId);
             Assert.Equal("hogehoge", twitterApi.CurrentScreenName);
@@ -74,8 +75,9 @@ namespace OpenTween.Api
             Assert.IsType<TwitterApiConnection>(twitterApi.ApiConnection);
 
             apiConnection = (TwitterApiConnection)twitterApi.ApiConnection!;
-            Assert.Equal("*** AccessToken2 ***", apiConnection.AccessToken);
-            Assert.Equal("*** AccessSecret2 ***", apiConnection.AccessSecret);
+            credential = Assert.IsType<TwitterCredentialOAuth1>(apiConnection.Credential);
+            Assert.Equal("*** AccessToken2 ***", credential.Token);
+            Assert.Equal("*** AccessSecret2 ***", credential.TokenSecret);
 
             Assert.Equal(200L, twitterApi.CurrentUserId);
             Assert.Equal("foobar", twitterApi.CurrentScreenName);

OpenTween.Tests/Connection/TwitterApiConnectionTest.cs
@@ -57,7 +57,7 @@ namespace OpenTween.Connection
         {
             using var mockHandler = new HttpMessageHandlerMock();
             using var http = new HttpClient(mockHandler);
-            using var apiConnection = new TwitterApiConnection(ApiKey.Create(""), ApiKey.Create(""), "", "");
+            using var apiConnection = new TwitterApiConnection();
             apiConnection.Http = http;
 
             mockHandler.Enqueue(x =>
@@ -95,7 +95,7 @@ namespace OpenTween.Connection
         {
             using var mockHandler = new HttpMessageHandlerMock();
             using var http = new HttpClient(mockHandler);
-            using var apiConnection = new TwitterApiConnection(ApiKey.Create(""), ApiKey.Create(""), "", "");
+            using var apiConnection = new TwitterApiConnection();
             apiConnection.Http = http;
 
             mockHandler.Enqueue(x =>
@@ -132,7 +132,7 @@ namespace OpenTween.Connection
         {
             using var mockHandler = new HttpMessageHandlerMock();
             using var http = new HttpClient(mockHandler);
-            using var apiConnection = new TwitterApiConnection(ApiKey.Create(""), ApiKey.Create(""), "", "");
+            using var apiConnection = new TwitterApiConnection();
             apiConnection.Http = http;
 
             mockHandler.Enqueue(x =>
@@ -175,7 +175,7 @@ namespace OpenTween.Connection
         {
             using var mockHandler = new HttpMessageHandlerMock();
             using var http = new HttpClient(mockHandler);
-            using var apiConnection = new TwitterApiConnection(ApiKey.Create(""), ApiKey.Create(""), "", "");
+            using var apiConnection = new TwitterApiConnection();
             apiConnection.Http = http;
 
             mockHandler.Enqueue(x =>
@@ -219,7 +219,7 @@ namespace OpenTween.Connection
         {
             using var mockHandler = new HttpMessageHandlerMock();
             using var http = new HttpClient(mockHandler);
-            using var apiConnection = new TwitterApiConnection(ApiKey.Create(""), ApiKey.Create(""), "", "");
+            using var apiConnection = new TwitterApiConnection();
             apiConnection.Http = http;
 
             mockHandler.Enqueue(x =>
@@ -251,7 +251,7 @@ namespace OpenTween.Connection
         {
             using var mockHandler = new HttpMessageHandlerMock();
             using var http = new HttpClient(mockHandler);
-            using var apiConnection = new TwitterApiConnection(ApiKey.Create(""), ApiKey.Create(""), "", "");
+            using var apiConnection = new TwitterApiConnection();
             apiConnection.Http = http;
 
             mockHandler.Enqueue(x =>
@@ -285,7 +285,7 @@ namespace OpenTween.Connection
         {
             using var mockHandler = new HttpMessageHandlerMock();
             using var http = new HttpClient(mockHandler);
-            using var apiConnection = new TwitterApiConnection(ApiKey.Create(""), ApiKey.Create(""), "", "");
+            using var apiConnection = new TwitterApiConnection();
             using var image = TestUtils.CreateDummyImage();
             apiConnection.Http = http;
 
@@ -331,7 +331,7 @@ namespace OpenTween.Connection
         {
             using var mockHandler = new HttpMessageHandlerMock();
             using var http = new HttpClient(mockHandler);
-            using var apiConnection = new TwitterApiConnection(ApiKey.Create(""), ApiKey.Create(""), "", "");
+            using var apiConnection = new TwitterApiConnection();
             apiConnection.Http = http;
 
             mockHandler.Enqueue(async x =>
@@ -371,7 +371,7 @@ namespace OpenTween.Connection
         {
             using var mockHandler = new HttpMessageHandlerMock();
             using var http = new HttpClient(mockHandler);
-            using var apiConnection = new TwitterApiConnection(ApiKey.Create(""), ApiKey.Create(""), "", "");
+            using var apiConnection = new TwitterApiConnection();
             apiConnection.HttpUpload = http;
 
             using var image = TestUtils.CreateDummyImage();
@@ -441,7 +441,7 @@ namespace OpenTween.Connection
         {
             using var mockHandler = new HttpMessageHandlerMock();
             using var http = new HttpClient(mockHandler);
-            using var apiConnection = new TwitterApiConnection(ApiKey.Create(""), ApiKey.Create(""), "", "");
+            using var apiConnection = new TwitterApiConnection();
             apiConnection.HttpUpload = http;
 
             mockHandler.Enqueue(async x =>
@@ -486,7 +486,7 @@ namespace OpenTween.Connection
         {
             using var mockHandler = new HttpMessageHandlerMock();
             using var http = new HttpClient(mockHandler);
-            using var apiConnection = new TwitterApiConnection(ApiKey.Create(""), ApiKey.Create(""), "", "");
+            using var apiConnection = new TwitterApiConnection();
             apiConnection.Http = http;
 
             mockHandler.Enqueue(async x =>
@@ -520,7 +520,7 @@ namespace OpenTween.Connection
         {
             using var mockHandler = new HttpMessageHandlerMock();
             using var http = new HttpClient(mockHandler);
-            using var apiConnection = new TwitterApiConnection(ApiKey.Create(""), ApiKey.Create(""), "", "");
+            using var apiConnection = new TwitterApiConnection();
             apiConnection.Http = http;
 
             mockHandler.Enqueue(async x =>
@@ -555,7 +555,7 @@ namespace OpenTween.Connection
         {
             using var mockHandler = new HttpMessageHandlerMock();
             using var http = new HttpClient(mockHandler);
-            using var apiConnection = new TwitterApiConnection(ApiKey.Create(""), ApiKey.Create(""), "", "");
+            using var apiConnection = new TwitterApiConnection();
             apiConnection.Http = http;
 
             mockHandler.Enqueue(x =>

OpenTween.Tests/Connection/TwitterCredentialCookieTest.cs
@@ -1,0 +1,47 @@
+﻿// OpenTween - Client of Twitter
+// Copyright (c) 2023 kim_upsilon (@kim_upsilon) <https://upsilo.net/~upsilon/>
+// All rights reserved.
+//
+// This file is part of OpenTween.
+//
+// This program is free software; you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the Free
+// Software Foundation; either version 3 of the License, or (at your option)
+// any later version.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+// for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program. If not, see <http://www.gnu.org/licenses/>, or write to
+// the Free Software Foundation, Inc., 51 Franklin Street - Fifth Floor,
+// Boston, MA 02110-1301, USA.
+
+using System.Net.Http;
+using Xunit;
+
+namespace OpenTween.Connection
+{
+    public class TwitterCredentialCookieTest
+    {
+        [Fact]
+        public void CreateHttpHandler_Test()
+        {
+            var appToken = new TwitterAppToken
+            {
+                AuthType = APIAuthType.TwitterComCookie,
+                TwitterComCookie = "aaa=bbb",
+            };
+            var credential = new TwitterCredentialCookie(appToken);
+
+            using var innerHandler = new HttpClientHandler();
+            using var handler = credential.CreateHttpHandler(innerHandler);
+
+            var cookieHandler = Assert.IsType<TwitterComCookieHandler>(handler);
+            Assert.Equal("aaa=bbb", cookieHandler.RawCookie);
+            Assert.Same(innerHandler, cookieHandler.InnerHandler);
+        }
+    }
+}

OpenTween.Tests/Connection/TwitterCredentialOAuth1Test.cs
@@ -1,0 +1,51 @@
+﻿// OpenTween - Client of Twitter
+// Copyright (c) 2023 kim_upsilon (@kim_upsilon) <https://upsilo.net/~upsilon/>
+// All rights reserved.
+//
+// This file is part of OpenTween.
+//
+// This program is free software; you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the Free
+// Software Foundation; either version 3 of the License, or (at your option)
+// any later version.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+// for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program. If not, see <http://www.gnu.org/licenses/>, or write to
+// the Free Software Foundation, Inc., 51 Franklin Street - Fifth Floor,
+// Boston, MA 02110-1301, USA.
+
+using System.Net.Http;
+using Xunit;
+
+namespace OpenTween.Connection
+{
+    public class TwitterCredentialOAuth1Test
+    {
+        [Fact]
+        public void CreateHttpHandler_Test()
+        {
+            var appToken = new TwitterAppToken
+            {
+                AuthType = APIAuthType.OAuth1,
+                OAuth1CustomConsumerKey = ApiKey.Create("consumer_key"),
+                OAuth1CustomConsumerSecret = ApiKey.Create("consumer_secret"),
+            };
+            var credential = new TwitterCredentialOAuth1(appToken, "access_token", "access_secret");
+
+            using var innerHandler = new HttpClientHandler();
+            using var handler = credential.CreateHttpHandler(innerHandler);
+
+            var oauthHandler = Assert.IsType<OAuthHandler>(handler);
+            Assert.Equal("consumer_key", oauthHandler.ConsumerKey.Value);
+            Assert.Equal("consumer_secret", oauthHandler.ConsumerSecret.Value);
+            Assert.Equal("access_token", oauthHandler.AccessToken);
+            Assert.Equal("access_secret", oauthHandler.AccessSecret);
+            Assert.Same(innerHandler, oauthHandler.InnerHandler);
+        }
+    }
+}

OpenTween.Tests/TweenMainTest.cs
@@ -48,13 +48,7 @@ namespace OpenTween
             using var imageCache = new ImageCache();
             using var iconAssets = new IconAssetsManager();
             var thumbnailGenerator = new ThumbnailGenerator(new(autoupdate: false));
-            var twitterAppToken = new TwitterAppToken
-            {
-                AuthType = APIAuthType.OAuth1,
-                OAuth1CustomConsumerKey = ApiKey.Create("aaa"),
-                OAuth1CustomConsumerSecret = ApiKey.Create("bbb"),
-            };
-            twitter.Initialize(twitterAppToken, "", "", "", 0L);
+            twitter.Initialize(new TwitterCredentialNone(), "", 0L);
 
             using var tweenMain = new TweenMain(settings, tabinfo, twitter, imageCache, iconAssets, thumbnailGenerator);
         }

OpenTween/Api/TwitterApi.cs
@@ -62,13 +62,13 @@ namespace OpenTween.Api
         }
 
         public void Initialize(string accessToken, string accessSecret, long userId, string screenName)
-            => this.Initialize(this.AppToken, accessToken, accessSecret, userId, screenName);
+            => this.Initialize(new TwitterCredentialOAuth1(this.AppToken, accessToken, accessSecret), userId, screenName);
 
-        public void Initialize(TwitterAppToken appToken, string accessToken, string accessSecret, long userId, string screenName)
+        public void Initialize(ITwitterCredential credential, long userId, string screenName)
         {
-            this.AppToken = appToken;
+            this.AppToken = credential.AppToken;
 
-            var newInstance = new TwitterApiConnection(this.AppToken, accessToken, accessSecret);
+            var newInstance = new TwitterApiConnection(credential);
             var oldInstance = Interlocked.Exchange(ref this.ApiConnection, newInstance);
             oldInstance?.Dispose();
 

OpenTween/AppendSettingDialog.cs
@@ -195,7 +195,7 @@ namespace OpenTween
                         };
 
                         using var twitterApi = new TwitterApi();
-                        twitterApi.Initialize(appToken, "", "", 0L, "");
+                        twitterApi.Initialize(new TwitterCredentialCookie(appToken), 0L, "");
                         var twitterUser = await twitterApi.AccountVerifyCredentials();
                         newAccount.UserId = twitterUser.Id;
                         newAccount.Username = twitterUser.ScreenName;
@@ -294,7 +294,7 @@ namespace OpenTween
             if (MyCommon.IsNullOrEmpty(pin))
                 return null; // キャンセルされた場合
 
-            var accessTokenResponse = await TwitterApiConnection.GetAccessTokenAsync(appToken, requestToken, pin);
+            var accessTokenResponse = await TwitterApiConnection.GetAccessTokenAsync(requestToken, pin);
 
             return new UserAccount
             {

commit 96f6312b1b3468ad417aa7a2661457b29d92bf6a

TwitterCredentialOAuth1に渡す引数の誤りを修正
Fixes: b43c97bf ("ITwitterCredentialとアクセス手段ごとの具象クラスを追加")

OpenTween/Setting/SettingCommon.cs
@@ -354,7 +354,7 @@ namespace OpenTween
         public string Username = "";
         public long UserId = 0;
 
-        public APIAuthType TwitterAuthType { get; set; }
+        public APIAuthType TwitterAuthType { get; set; } = APIAuthType.OAuth1;
 
         public string TwitterOAuth1ConsumerKey { get; set; } = "";
 
@@ -405,7 +405,7 @@ namespace OpenTween
             return appToken.AuthType switch
             {
                 APIAuthType.OAuth1
-                    => new TwitterCredentialOAuth1(appToken, this.TwitterOAuth1ConsumerKey, this.TwitterOAuth1ConsumerSecret),
+                    => new TwitterCredentialOAuth1(appToken, this.Token, this.TokenSecret),
                 APIAuthType.TwitterComCookie
                     => new TwitterCredentialCookie(appToken),
                 _ => new TwitterCredentialNone(),