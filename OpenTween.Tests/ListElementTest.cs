2e5a033965984f40bae17850fe000a4b32027c78
Kimura Youichi <kim.upsilon@bucyou.net>
2024-01-11 04:42:45 +0900

    TweenMain.AddNewTabメソッドに対するテストコードを追加

---
+++ OpenTween.Tests/ListElementTest.cs
@@ -0,0 +1,54 @@
+﻿// OpenTween - Client of Twitter
+// Copyright (c) 2024 kim_upsilon (@kim_upsilon) <https://upsilo.net/~upsilon/>
+// All rights reserved.
+//
+// This file is part of OpenTween.
+//
+// This program is free software; you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the Free
+// Software Foundation; either version 3 of the License, or (at your option)
+// any later version.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+// for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program. If not, see <http://www.gnu.org/licenses/>, or write to
+// the Free Software Foundation, Inc., 51 Franklin Street - Fifth Floor,
+// Boston, MA 02110-1301, USA.
+
+using Xunit;
+
+namespace OpenTween
+{
+    public class ListElementTest
+    {
+        [Fact]
+        public void ToString_PublicTest()
+        {
+            var list = new ListElement
+            {
+                Id = 12345L,
+                Name = "tetete",
+                Username = "opentween",
+                IsPublic = true,
+            };
+            Assert.Equal("@opentween/tetete [Public]", list.ToString());
+        }
+
+        [Fact]
+        public void ToString_ProtectedTest()
+        {
+            var list = new ListElement
+            {
+                Id = 12345L,
+                Name = "tetete",
+                Username = "opentween",
+                IsPublic = false,
+            };
+            Assert.Equal("@opentween/tetete [Protected]", list.ToString());
+        }
+    }
+}
