opentween/OpenTween

commit 0533dad6a6fc44d714c9976cfef2b726161afc8f

cursorを使用した新着投稿の取得に対応

OpenTween/Models/PublicSearchTabModel.cs
@@ -45,6 +45,8 @@ namespace OpenTween.Models
 
         public PostId? SinceId { get; set; }
 
+        public string? CursorTop { get; set; }
+
         public string? CursorBottom { get; set; }
 
         public string SearchWords

OpenTween.Tests/Api/GraphQL/ListLatestTweetsTimelineRequestTest.cs
@@ -59,6 +59,7 @@ namespace OpenTween.Api.GraphQL
 
             var response = await request.Send(mock.Object).ConfigureAwait(false);
             Assert.Single(response.Tweets);
+            Assert.Equal("DAABCgABF0HfRMjAJxEKAAIWes8rE1oQAAgAAwAAAAEAAA", response.CursorTop);
             Assert.Equal("DAABCgABF0HfRMi__7QKAAIVAxUYmFWQAwgAAwAAAAIAAA", response.CursorBottom);
 
             mock.VerifyAll();

OpenTween.Tests/Api/GraphQL/SearchTimelineRequestTest.cs
@@ -58,6 +58,7 @@ namespace OpenTween.Api.GraphQL
 
             var response = await request.Send(mock.Object).ConfigureAwait(false);
             Assert.Single(response.Tweets);
+            Assert.Equal("DAADDAABCgABFnlh4hraMAYKAAIOTm0DEhTAAQAIAAIAAAABCAADAAAAAAgABAAAAAAKAAUX8j3ezIAnEAoABhfyPd7Mf9jwAAA", response.CursorTop);
             Assert.Equal("DAADDAABCgABFnlh4hraMAYKAAIOTm0DEhTAAQAIAAIAAAACCAADAAAAAAgABAAAAAAKAAUX8j3ezIAnEAoABhfyPd7Mf9jwAAA", response.CursorBottom);
 
             mock.VerifyAll();

OpenTween.Tests/Api/GraphQL/UserTweetsAndRepliesRequestTest.cs
@@ -58,6 +58,7 @@ namespace OpenTween.Api.GraphQL
 
             var response = await request.Send(mock.Object).ConfigureAwait(false);
             Assert.Single(response.Tweets);
+            Assert.Equal("DAABCgABF_tTnZvAJxEKAAIWes8rE1oQAAgAAwAAAAEAAA", response.CursorTop);
             Assert.Equal("DAABCgABF_tTnZu__-0KAAIWZa6KTRoAAwgAAwAAAAIAAA", response.CursorBottom);
 
             mock.VerifyAll();

OpenTween/Api/GraphQL/ListLatestTweetsTimelineRequest.cs
@@ -106,9 +106,10 @@ namespace OpenTween.Api.GraphQL
             ErrorResponse.ThrowIfError(rootElm);
 
             var tweets = TimelineTweet.ExtractTimelineTweets(rootElm);
+            var cursorTop = rootElm.XPathSelectElement("//content[__typename[text()='TimelineTimelineCursor']][cursorType[text()='Top']]/value")?.Value;
             var cursorBottom = rootElm.XPathSelectElement("//content[__typename[text()='TimelineTimelineCursor']][cursorType[text()='Bottom']]/value")?.Value;
 
-            return new(tweets, cursorBottom);
+            return new(tweets, cursorTop, cursorBottom);
         }
     }
 }

OpenTween/Api/GraphQL/SearchTimelineRequest.cs
@@ -108,9 +108,10 @@ namespace OpenTween.Api.GraphQL
             ErrorResponse.ThrowIfError(rootElm);
 
             var tweets = TimelineTweet.ExtractTimelineTweets(rootElm);
+            var cursorTop = rootElm.XPathSelectElement("//content[__typename[text()='TimelineTimelineCursor']][cursorType[text()='Top']]/value")?.Value;
             var cursorBottom = rootElm.XPathSelectElement("//content[__typename[text()='TimelineTimelineCursor']][cursorType[text()='Bottom']]/value")?.Value;
 
-            return new(tweets, cursorBottom);
+            return new(tweets, cursorTop, cursorBottom);
         }
     }
 }

OpenTween/Api/GraphQL/TimelineResponse.cs
@@ -31,6 +31,7 @@ namespace OpenTween.Api.GraphQL
 {
     public record TimelineResponse(
         TimelineTweet[] Tweets,
+        string? CursorTop,
         string? CursorBottom
     );
 }

OpenTween/Api/GraphQL/UserTweetsAndRepliesRequest.cs
@@ -91,9 +91,10 @@ namespace OpenTween.Api.GraphQL
             ErrorResponse.ThrowIfError(rootElm);
 
             var tweets = TimelineTweet.ExtractTimelineTweets(rootElm);
+            var cursorTop = rootElm.XPathSelectElement("//content[__typename[text()='TimelineTimelineCursor']][cursorType[text()='Top']]/value")?.Value;
             var cursorBottom = rootElm.XPathSelectElement("//content[__typename[text()='TimelineTimelineCursor']][cursorType[text()='Bottom']]/value")?.Value;
 
-            return new(tweets, cursorBottom);
+            return new(tweets, cursorTop, cursorBottom);
         }
     }
 }

commit edbb5b56479724f49880164e09fb08928d7dc305

検索タブのSearchWordsが変更されてもcursorがリセットされない不具合を修正
Fixes: 0533dad6 ("cursorを使用した新着投稿の取得に対応")

OpenTween/Models/PublicSearchTabModel.cs
@@ -99,12 +99,14 @@ namespace OpenTween.Models
         }
 
         /// <summary>
-        /// タブ更新時に使用する SinceId, OldestId をリセットする
+        /// 差分更新用の cursor をリセットする（検索条件が変更された時に使用する）
         /// </summary>
         public void ResetFetchIds()
         {
             this.SinceId = null;
             this.OldestId = null;
+            this.CursorTop = null;
+            this.CursorBottom = null;
         }
     }
 }