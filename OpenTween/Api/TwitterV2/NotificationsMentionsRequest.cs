opentween/OpenTween

commit da1189a5cb56b8a5b7818b1d481cfab887abd54e

/notifications/mentions.json を使用したReplyタブの更新に対応

OpenTween/Api/TwitterV2/NotificationsMentionsRequest.cs
@@ -1,0 +1,188 @@
+﻿// OpenTween - Client of Twitter
+// Copyright (c) 2024 kim_upsilon (@kim_upsilon) <https://upsilo.net/~upsilon/>
+// All rights reserved.
+//
+// This file is part of OpenTween.
+//
+// This program is free software; you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the Free
+// Software Foundation; either version 3 of the License, or (at your option)
+// any later version.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+// for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program. If not, see <http://www.gnu.org/licenses/>, or write to
+// the Free Software Foundation, Inc., 51 Franklin Street - Fifth Floor,
+// Boston, MA 02110-1301, USA.
+
+#nullable enable
+
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.XPath;
+using OpenTween.Api.DataModel;
+using OpenTween.Api.GraphQL;
+using OpenTween.Connection;
+
+namespace OpenTween.Api.TwitterV2
+{
+    public class NotificationsMentionsRequest
+    {
+        public static readonly string EndpointName = "/2/notifications/mentions";
+
+        private static readonly Uri EndpointUri = new("https://twitter.com/i/api/2/notifications/mentions.json");
+
+        public int Count { get; set; } = 100;
+
+        public string? Cursor { get; set; }
+
+        public Dictionary<string, string> CreateParameters()
+        {
+            var param = new Dictionary<string, string>()
+            {
+                ["include_profile_interstitial_type"] = "1",
+                ["include_blocking"] = "1",
+                ["include_blocked_by"] = "1",
+                ["include_followed_by"] = "1",
+                ["include_want_retweets"] = "1",
+                ["include_mute_edge"] = "1",
+                ["include_can_dm"] = "1",
+                ["include_can_media_tag"] = "1",
+                ["include_ext_has_nft_avatar"] = "1",
+                ["include_ext_is_blue_verified"] = "1",
+                ["include_ext_verified_type"] = "1",
+                ["include_ext_profile_image_shape"] = "1",
+                ["skip_status"] = "1",
+                ["cards_platform"] = "Web-12",
+                ["include_cards"] = "1",
+                ["include_ext_alt_text"] = "true",
+                ["include_ext_limited_action_results"] = "true",
+                ["include_quote_count"] = "true",
+                ["include_reply_count"] = "1",
+                ["tweet_mode"] = "extended",
+                ["include_ext_views"] = "true",
+                ["include_entities"] = "true",
+                ["include_user_entities"] = "true",
+                ["include_ext_media_color"] = "true",
+                ["include_ext_media_availability"] = "true",
+                ["include_ext_sensitive_media_warning"] = "true",
+                ["include_ext_trusted_friends_metadata"] = "true",
+                ["send_error_codes"] = "true",
+                ["simple_quoted_tweet"] = "true",
+                ["requestContext"] = "ptr",
+                ["ext"] = "mediaStats,highlightedLabel,hasNftAvatar,voiceInfo,birdwatchPivot,superFollowMetadata,unmentionInfo,editControl",
+                ["count"] = this.Count.ToString(CultureInfo.InvariantCulture),
+            };
+
+            if (!MyCommon.IsNullOrEmpty(this.Cursor))
+                param["cursor"] = this.Cursor;
+
+            return param;
+        }
+
+        public async Task<NotificationsResponse> Send(IApiConnection apiConnection)
+        {
+            var request = new GetRequest
+            {
+                RequestUri = EndpointUri,
+                Query = this.CreateParameters(),
+                EndpointName = EndpointName,
+            };
+
+            using var response = await apiConnection.SendAsync(request)
+                .ConfigureAwait(false);
+
+            var responseBytes = await response.ReadAsBytes()
+                .ConfigureAwait(false);
+
+            ResponseRoot parsedObjects;
+            XElement rootElm;
+            try
+            {
+                parsedObjects = MyCommon.CreateDataFromJson<ResponseRoot>(responseBytes);
+
+                using var jsonReader = JsonReaderWriterFactory.CreateJsonReader(
+                    responseBytes,
+                    XmlDictionaryReaderQuotas.Max
+                );
+
+                rootElm = XElement.Load(jsonReader);
+            }
+            catch (SerializationException ex)
+            {
+                var responseText = Encoding.UTF8.GetString(responseBytes);
+                throw TwitterApiException.CreateFromException(ex, responseText);
+            }
+            catch (XmlException ex)
+            {
+                var responseText = Encoding.UTF8.GetString(responseBytes);
+                throw new TwitterApiException("Invalid JSON", ex) { ResponseText = responseText };
+            }
+
+            ErrorResponse.ThrowIfError(rootElm);
+
+            var tweetIds = rootElm.XPathSelectElements("//content/item/content/tweet/id")
+                .Select(x => x.Value)
+                .ToArray();
+
+            var statuses = new List<TwitterStatus>(tweetIds.Length);
+            foreach (var tweetId in tweetIds)
+            {
+                if (!parsedObjects.GlobalObjects.Tweets.TryGetValue(tweetId, out var tweet))
+                    continue;
+
+                var userId = tweet.UserId;
+                if (!parsedObjects.GlobalObjects.Users.TryGetValue(userId, out var user))
+                    continue;
+
+                tweet.User = user;
+                statuses.Add(tweet);
+            }
+
+            var tweets = TimelineTweet.ExtractTimelineTweets(rootElm);
+            var cursorTop = rootElm.XPathSelectElement("//content/operation/cursor[cursorType[text()='Top']]/value")?.Value;
+            var cursorBottom = rootElm.XPathSelectElement("//content/operation/cursor[cursorType[text()='Bottom']]/value")?.Value;
+
+            return new(statuses.ToArray(), cursorTop, cursorBottom);
+        }
+
+        [DataContract]
+        private record ResponseRoot(
+            [property: DataMember(Name = "globalObjects")]
+            ResponseGlobalObjects GlobalObjects
+        );
+
+        [DataContract]
+        private record ResponseGlobalObjects(
+            [property: DataMember(Name = "users")]
+            Dictionary<string, TwitterUser> Users,
+            [property: DataMember(Name = "tweets")]
+            Dictionary<string, ResponseTweet> Tweets
+        );
+
+        [DataContract]
+        private class ResponseTweet : TwitterStatus
+        {
+            [DataMember(Name = "user_id")]
+            public string UserId { get; set; } = "";
+        }
+
+        public readonly record struct NotificationsResponse(
+            TwitterStatus[] Statuses,
+            string? CursorTop,
+            string? CursorBottom
+        );
+    }
+}

OpenTween.Tests/Api/TwitterV2/NotificationsMentionsRequestTest.cs
@@ -1,0 +1,98 @@
+﻿// OpenTween - Client of Twitter
+// Copyright (c) 2024 kim_upsilon (@kim_upsilon) <https://upsilo.net/~upsilon/>
+// All rights reserved.
+//
+// This file is part of OpenTween.
+//
+// This program is free software; you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published by the Free
+// Software Foundation; either version 3 of the License, or (at your option)
+// any later version.
+//
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+// or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+// for more details.
+//
+// You should have received a copy of the GNU General Public License along
+// with this program. If not, see <http://www.gnu.org/licenses/>, or write to
+// the Free Software Foundation, Inc., 51 Franklin Street - Fifth Floor,
+// Boston, MA 02110-1301, USA.
+
+using System.Threading.Tasks;
+using Moq;
+using OpenTween.Api.GraphQL;
+using OpenTween.Connection;
+using Xunit;
+
+namespace OpenTween.Api.TwitterV2
+{
+    public class NotificationsMentionsRequestTest
+    {
+        [Fact]
+        public async Task Send_Test()
+        {
+            using var apiResponse = await TestUtils.CreateApiResponse("Resources/Responses/NotificationsMentions.json");
+
+            var mock = new Mock<IApiConnection>();
+            mock.Setup(x =>
+                    x.SendAsync(It.IsAny<IHttpRequest>())
+                )
+                .Callback<IHttpRequest>(x =>
+                {
+                    var request = Assert.IsType<GetRequest>(x);
+                    Assert.Equal(new("https://twitter.com/i/api/2/notifications/mentions.json"), request.RequestUri);
+                    var query = request.Query!;
+                    Assert.Equal("20", query["count"]);
+                    Assert.DoesNotContain("cursor", query);
+                    Assert.Equal("/2/notifications/mentions", request.EndpointName);
+                })
+                .ReturnsAsync(apiResponse);
+
+            var request = new NotificationsMentionsRequest()
+            {
+                Count = 20,
+            };
+
+            var response = await request.Send(mock.Object);
+            var status = Assert.Single(response.Statuses);
+            Assert.Equal("1748671085438988794", status.IdStr);
+            Assert.Equal("40480664", status.User.IdStr);
+
+            Assert.Equal("DAABDAABCgABAAAAAC4B0ZQIAAIAAAACCAADm5udsQgABCaolIMACwACAAAAC0FZMG1xVjB6VEZjAAA", response.CursorTop);
+            Assert.Equal("DAACDAABCgABAAAAAC4B0ZQIAAIAAAACCAADm5udsQgABCaolIMACwACAAAAC0FZMG1xVjB6VEZjAAA", response.CursorBottom);
+
+            mock.VerifyAll();
+        }
+
+        [Fact]
+        public async Task Send_RequestCursorTest()
+        {
+            using var apiResponse = await TestUtils.CreateApiResponse("Resources/Responses/NotificationsMentions.json");
+
+            var mock = new Mock<IApiConnection>();
+            mock.Setup(x =>
+                    x.SendAsync(It.IsAny<IHttpRequest>())
+                )
+                .Callback<IHttpRequest>(x =>
+                {
+                    var request = Assert.IsType<GetRequest>(x);
+                    Assert.Equal(new("https://twitter.com/i/api/2/notifications/mentions.json"), request.RequestUri);
+                    var query = request.Query!;
+                    Assert.Equal("20", query["count"]);
+                    Assert.Equal("aaa", query["cursor"]);
+                    Assert.Equal("/2/notifications/mentions", request.EndpointName);
+                })
+                .ReturnsAsync(apiResponse);
+
+            var request = new NotificationsMentionsRequest()
+            {
+                Count = 20,
+                Cursor = "aaa",
+            };
+
+            await request.Send(mock.Object);
+            mock.VerifyAll();
+        }
+    }
+}

OpenTween/Models/MentionsTabModel.cs
@@ -43,6 +43,10 @@ namespace OpenTween.Models
 
         public PostId? OldestId { get; set; }
 
+        public string? CursorTop { get; set; }
+
+        public string? CursorBottom { get; set; }
+
         public MentionsTabModel()
             : this(MyCommon.DEFAULTTAB.REPLY)
         {

OpenTween/Tween.cs
@@ -7000,7 +7000,8 @@ namespace OpenTween
                         authByCookie ? HomeLatestTimelineRequest.EndpointName : "/statuses/home_timeline",
                     MyCommon.TabUsageType.UserDefined =>
                         authByCookie ? HomeLatestTimelineRequest.EndpointName : "/statuses/home_timeline",
-                    MyCommon.TabUsageType.Mentions => "/statuses/mentions_timeline",
+                    MyCommon.TabUsageType.Mentions =>
+                        authByCookie ? NotificationsMentionsRequest.EndpointName : "/statuses/mentions_timeline",
                     MyCommon.TabUsageType.Favorites => "/favorites/list",
                     MyCommon.TabUsageType.DirectMessage => "/direct_messages/events/list",
                     MyCommon.TabUsageType.UserTimeline =>

OpenTween/Twitter.cs
@@ -651,15 +651,35 @@ namespace OpenTween
             var count = GetApiResultCount(MyCommon.WORKERTYPE.Reply, more, startup);
 
             TwitterStatus[] statuses;
-            if (more)
+            if (this.Api.AuthType == APIAuthType.TwitterComCookie)
             {
-                statuses = await this.Api.StatusesMentionsTimeline(count, maxId: tab.OldestId as TwitterStatusId)
+                var request = new NotificationsMentionsRequest
+                {
+                    Count = Math.Min(count, 50),
+                    Cursor = more ? tab.CursorBottom : tab.CursorTop,
+                };
+                var response = await request.Send(this.Api.Connection)
                     .ConfigureAwait(false);
+
+                statuses = response.Statuses;
+
+                tab.CursorBottom = response.CursorBottom;
+
+                if (!more)
+                    tab.CursorTop = response.CursorTop;
             }
             else
             {
-                statuses = await this.Api.StatusesMentionsTimeline(count)
-                    .ConfigureAwait(false);
+                if (more)
+                {
+                    statuses = await this.Api.StatusesMentionsTimeline(count, maxId: tab.OldestId as TwitterStatusId)
+                        .ConfigureAwait(false);
+                }
+                else
+                {
+                    statuses = await this.Api.StatusesMentionsTimeline(count)
+                        .ConfigureAwait(false);
+                }
             }
 
             var minimumId = this.CreatePostsFromJson(statuses, MyCommon.WORKERTYPE.Reply, tab, read);

commit 358edfe186baabdaeecdefd4bc3113ce15fe12ff

不要なExtractTimelineTweetsメソッドの呼び出しを削除
Fixes: da1189a5 ("/notifications/mentions.json を使用したReplyタブの更新に対応")

OpenTween/Api/TwitterV2/NotificationsMentionsRequest.cs
@@ -151,7 +151,6 @@ namespace OpenTween.Api.TwitterV2
                 statuses.Add(tweet);
             }
 
-            var tweets = TimelineTweet.ExtractTimelineTweets(rootElm);
             var cursorTop = rootElm.XPathSelectElement("//content/operation/cursor[cursorType[text()='Top']]/value")?.Value;
             var cursorBottom = rootElm.XPathSelectElement("//content/operation/cursor[cursorType[text()='Bottom']]/value")?.Value;
 